643d14ab9c5c23cf02c9981b458fce7f6d31cdb0
Jeremy Walker <jez.walker@gmail.com>
2020-06-26 17:01:05 +0100

    Add tests

--- app/javascript/components/iteration_summary.jsx
+++ app/javascript/components/iteration_summary.jsx
@@ -16,29 +16,37 @@ export function IterationsSummaryTable({ solutionId, iterations }) {
 
   useEffect(() => {
     const received = data => {
+      console.log("recieved");
       console.log(data);
       setIterations(data.iterations);
-    }
+    };
+
+    const connected = () => {
+      console.log("connected");
+    };
+
+    const rejected = () => {
+      console.log("rejected");
+    };
 
     const subscription = consumer.subscriptions.create(
       { channel: "IterationsChannel", solution_id: solutionId },
       {
-        received
+        received, connected, rejected
       }
     );
+    console.log(subscription.consumer)
     return () => subscription.unsubscribe();
   }, [solutionId]);
 
-  return (
-    stateIterations.map((iteration, idx) => {
-      return (
-        <IterationSummary 
-          key={iteration.id} 
-          id={iteration.id} 
-          time={iteration.time} 
-          idx={idx}
-        />
-      );
-    })
-  )
+  return stateIterations.map((iteration, idx) => {
+    return (
+      <IterationSummary
+        key={iteration.id}
+        id={iteration.id}
+        time={iteration.time}
+        idx={idx}
+      />
+    );
+  });
 }

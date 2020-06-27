49cf282a2e3d0be057c83f2485b4abc3fa682b7a
Jeremy Walker <jez.walker@gmail.com>
2020-06-27 16:20:28 +0100

    Extra component into Rails helper

---
+++ app/helpers/components_helper.rb
@@ -0,0 +1,17 @@
+module ComponentsHelper
+  def iterations_summary_table(solution)
+    react_component("iterations-summary-table", {
+      solution_id: solution.id, 
+      iterations: solution.serialized_iterations
+    })
+  end
+
+  private
+  def react_component(id, data)
+    tag :div, {
+      "data-react-#{id}": true,
+      "data-react-data": data.to_json
+    }
+  end
+end
+

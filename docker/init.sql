d8292da7c75afa1bd71748635f63b404b582c99a
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-24 16:03:25 +0200

    Normalize quotes

--- docker/init.sql
+++ docker/init.sql
@@ -1,13 +1,13 @@
 CREATE USER 'exercism_v3' IDENTIFIED BY 'exercism_v3';
 
-CREATE DATABASE exercism_v3_development;
-ALTER DATABASE exercism_v3_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `exercism_v3_development`;
+ALTER DATABASE `exercism_v3_development` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON `exercism_v3_development`.* TO 'exercism_v3';
 
-CREATE DATABASE exercism_v3_dj_development;
-ALTER DATABASE exercism_v3_dj_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `exercism_v3_dj_development`;
+ALTER DATABASE `exercism_v3_dj_development` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON `exercism_v3_dj_development`.* TO 'exercism_v3';
 
-CREATE DATABASE exercism_v3_test;
-ALTER DATABASE exercism_v3_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `exercism_v3_test`;
+ALTER DATABASE `exercism_v3_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON `exercism_v3_test`.* TO 'exercism_v3';

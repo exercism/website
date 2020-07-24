d96ec29024a23c1c91c8f74dd847b631b6349b74
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-24 16:24:01 +0200

    Restructure things a bit

---
+++ app/controllers/test/components/maintaining/iterations_summary_table_controller.rb
@@ -0,0 +1,5 @@
+class Test::Components::Maintaining:IterationsSummaryTableController < ApplicationController
+  def index
+    @solution = Iteration.order(created_at: :desc).take(50)
+  end
+end
--- test/system/react_components/example/iterations_summary_table_test.rb
+++ test/system/react_components/example/iterations_summary_table_test.rb
@@ -1,11 +1,11 @@
 require "application_system_test_case"
 
-class Test::IterationsSummaryTableTest < ApplicationSystemTestCase
+class Example::IterationsSummaryTableTest < ApplicationSystemTestCase
   test "visiting the index" do
     solution = create :concept_solution
     iteration = create :iteration, solution: solution
 
-    visit test_components_iterations_summary_table_url(solution.id)
+    visit test_components_example_iterations_summary_table_url(solution.id)
     sleep(0.1) # Set the websocket connection
 
     assert_text "#{iteration.id}: pending"

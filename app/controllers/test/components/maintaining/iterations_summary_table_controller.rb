3ad9d449a3b96c5309431fea9e79e357a0be9fe6
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-28 15:35:23 +0200

    Fix failing system test

--- app/controllers/test/components/maintaining/iterations_summary_table_controller.rb
+++ app/controllers/test/components/maintaining/iterations_summary_table_controller.rb
@@ -1,5 +1,5 @@
 class Test::Components::Maintaining::IterationsSummaryTableController < ApplicationController
   def index
-    @solution = Iteration.order(created_at: :desc).take(50)
+    @iterations = Iteration.order(created_at: :desc).take(50)
   end
 end

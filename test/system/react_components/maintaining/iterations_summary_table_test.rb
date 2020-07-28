828d7c55719e82bb45606bd4f8bb0dbc8b2c34cc
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-28 16:00:23 +0200

    Use modules for react component tests

--- test/system/react_components/maintaining/iterations_summary_table_test.rb
+++ test/system/react_components/maintaining/iterations_summary_table_test.rb
@@ -1,17 +1,19 @@
 require "application_system_test_case"
 
-class Maintaining::IterationsSummaryTableTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    solution = create :concept_solution
-    iteration = create :iteration, solution: solution
+module Maintaining
+  class IterationsSummaryTableTest < ApplicationSystemTestCase
+    test "visiting the index" do
+      visit test_components_maintaining_iterations_summary_table_url
+      sleep(0.1) # Set the websocket connection
 
-    visit test_components_maintaining_iterations_summary_table_url
-    sleep(0.1) # Set the websocket connection
+      solution = create :concept_solution
+      iteration = create :iteration, solution: solution
 
-    iteration.tests_passed!
-    iteration.broadcast!
+      iteration.tests_passed!
+      iteration.broadcast!
 
-    sleep(0.1) # Wait for websocket to broadcast
-    assert_text iteration.id.to_s
+      sleep(0.1) # Wait for websocket to broadcast
+      assert_text iteration.id.to_s
+    end
   end
 end

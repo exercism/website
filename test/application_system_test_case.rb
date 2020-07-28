525ff005fef3ff32f09f992c91a58a59b64b5738
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-28 15:37:05 +0200

    Allow running Chrome in Docker

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,5 +2,8 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  driven_by :selenium, using: :headless_chrome
+  driven_by :selenium, using: :headless_chrome do |driver_option|
+    # Without this argument, Chrome cannot be started in Docker
+    driver_option.add_argument('no-sandbox')
+  end
 end

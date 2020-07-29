2b9ce35d3773935e6fc4030a5795c2c3257778e4
Jeremy Walker <jez.walker@gmail.com>
2020-07-29 18:49:47 +0100

    Get everything playing together

--- docker/init.dev.sh
+++ docker/init.dev.sh
@@ -1,6 +1,5 @@
-bundle exec bin/create_dynamodb_config
+setup_exercism_config --settings-file=/usr/src/settings/settings.yml
 bin/rails db:create
 bin/rails db:migrate
-bin/rails log:clear tmp:clear
 bin/rails runner "scripts/setup_dynamodb_locally.rb"
 hivemind -p 3020 ./Procfile.docker.dev

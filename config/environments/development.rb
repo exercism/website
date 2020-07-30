c3732e154e5d7b20fdb52817393b809ad5b0fe1f
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-30 16:40:47 +0200

    Set Ruby host for CSRF fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "website" if ENV['EXERCISM_DOCKER']
 end

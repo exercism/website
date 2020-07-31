e85f11da317691ecc0a79cd8d6ac1e30cc4c35b5
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-31 11:52:32 +0200

    Fix merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,6 @@
 class PagesController < ApplicationController
   def index
-<<<<<<< HEAD
     # solution = Solution.first
     # IterationsChannel.broadcast!(solution)
-=======
-    solution = Solution.last
-
-    IterationsChannel.broadcast!(solution)
-    IterationChannel.broadcast!(solution.iterations.last)
->>>>>>> Add iterations page
   end
 end

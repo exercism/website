12d36d3b80d266fb73906ddd7ac17659a03fd455
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-31 14:28:09 +0200

    Use C# two-fer as iteration

--- app/controllers/tmp/iterations_controller.rb
+++ app/controllers/tmp/iterations_controller.rb
@@ -1,23 +1,19 @@
 class Tmp::IterationsController < ApplicationController
   def create
     user = User.create!(handle: SecureRandom.uuid)
-    ruby = Track.find_by(slug: :ruby) ||
-           Track.create!(slug: 'ruby', title: 'Ruby', repo_url: "http://github.com/exercism/ruby")
-    exercise = ConceptExercise.find_by(track: ruby, slug: "two-fer") ||
-               ConceptExercise.create!(track: ruby, uuid: SecureRandom.uuid, slug: "two-fer", prerequisites: [], title: "Two Fer")
+    track = Track.find_by(slug: :csharp) ||
+           Track.create!(slug: 'csharp', title: 'C#', repo_url: "http://github.com/exercism/csharp")
+    exercise = ConceptExercise.find_by(track: track, slug: "two-fer") ||
+               ConceptExercise.create!(track: track, uuid: SecureRandom.uuid, slug: "two-fer", prerequisites: [], title: "Two Fer")
     solution = ConceptSolution.create!(uuid: SecureRandom.uuid, user: user, exercise: exercise)
+
+    git_exercise = track.repo.exercise('two-fer', track.repo.head_sha)
+    example = git_exercise.read_file_blob("Example.cs")
 
     files = [
       {
-        filename: "two_fer.rb",
-        content: %{
-  class TwoFer
-    def self.two_fer(name=nil)
-      "One for \#{name}, one for me"
-    end
-  end
-  # #{SecureRandom.hex}
-        }.strip
+        filename: "TwoFer.cs",
+        content: example
       }
     ]
 

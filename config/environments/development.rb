226e701aeb4620548074d6f27088bee077358720
Erik Schierboom <erik_schierboom@hotmail.com>
2020-07-31 12:25:05 +0200

    Fix file changes not showing up in Docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # routes, locales, etc.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker unless ENV['EXERCISM_DOCKER']
 
   config.hosts << "website" if ENV['EXERCISM_DOCKER']
 end

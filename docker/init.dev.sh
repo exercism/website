3a97ca5204079ac21f7ab7be16621657fdd9811b
Josh Goebel <me@joshgoebel.com>
2020-08-04 16:31:14 -0400

    move migration back to sep line

--- docker/init.dev.sh
+++ docker/init.dev.sh
@@ -1,5 +1,6 @@
 echo "Create and migrate DBs"
-bundle exec bin/rails db:create db:migrate
+bundle exec bin/rails db:create
+bundle exec bin/rails db:migrate
 
 echo "Set up local AWS"
 overmind start -p 3020 -s /usr/src/app/tmp/overmind.sock -f ./Procfile.docker.dev

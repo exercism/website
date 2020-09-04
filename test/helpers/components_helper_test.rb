681964614d8c2183b86d278f531caec1987189f1
Karlo Soriano <karlo@thalamus.ai>
2020-09-04 14:56:49 +0800

    Add test to check whether correct props are passed in

---
+++ test/helpers/components_helper_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class ComponentsHelperTest < ActionView::TestCase
+  include ComponentsHelper
+
+  test "#notification_icon passes the correct parameters" do
+    user = create(:user)
+    create(:notification, user: user, read_at: nil)
+
+    assert_component_equal notification_icon(user), { id: "notification-icon", props: { count: 1 } }
+  end
+
+  private
+  def assert_component_equal(component, params)
+    assert_dom_equal(
+      %(<div data-react-#{params[:id]}="true" data-react-data=#{params[:props].to_json} />),
+      component
+    )
+  end
+end

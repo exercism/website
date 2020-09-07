e9ae2dca46c06d93582a38195b4b3be566ecc94d
Karlo Soriano <karlo@thalamus.ai>
2020-09-07 08:31:57 +0800

    Assert correct initial state for <NotificationIcon />

--- test/system/components/notifications/icon_test.rb
+++ test/system/components/notifications/icon_test.rb
@@ -3,6 +3,15 @@ require "application_system_test_case"
 module Components
   module Notifications
     class IconTest < ApplicationSystemTestCase
+      test "shows the correct initial state" do
+        create(:user)
+
+        visit test_components_notifications_icon_path
+
+        assert_text "Count\n0"
+        assert_text "Unread\nfalse"
+      end
+
       test "increments number when a notification is created" do
         create(:user)
 

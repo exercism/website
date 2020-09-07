8fab2cb50f603c3db8527ec6bb3e419c7b2adbbb
Karlo Soriano <karlo@thalamus.ai>
2020-09-07 08:24:53 +0800

    Move system test to components namespace

---
+++ test/system/components/notifications/icon_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+module Components
+  module Notifications
+    class IconTest < ApplicationSystemTestCase
+      test "increments number when a notification is created" do
+        create(:user)
+
+        visit test_components_notifications_icon_path
+        click_on "Create notification"
+
+        assert_text "Count\n1", wait: 1
+      end
+
+      test "shows unread state when a new notification is created" do
+        create(:user)
+
+        visit test_components_notifications_icon_path
+        click_on "Create notification"
+
+        assert_text "Unread\ntrue", wait: 1
+      end
+    end
+  end
+end

122ce27f0384881ceeae30b271d1ba0f26e1e839
Karlo Soriano <karlo@thalamus.ai>
2020-09-07 08:29:29 +0800

    Add WebsocketsHelpers to make tests less brittle

--- test/system/components/notifications/icon_test.rb
+++ test/system/components/notifications/icon_test.rb
@@ -7,18 +7,20 @@ module Components
         create(:user)
 
         visit test_components_notifications_icon_path
+        wait_for_websockets
         click_on "Create notification"
 
-        assert_text "Count\n1", wait: 1
+        assert_text "Count\n1"
       end
 
       test "shows unread state when a new notification is created" do
         create(:user)
 
         visit test_components_notifications_icon_path
+        wait_for_websockets
         click_on "Create notification"
 
-        assert_text "Unread\ntrue", wait: 1
+        assert_text "Unread\ntrue"
       end
     end
   end

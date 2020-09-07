14dbc2699e86d8af3515c859f8ad900f99693a00
Karlo Soriano <karlo@thalamus.ai>
2020-09-07 08:37:05 +0800

    Remove unread state from <NotificationIcon />

--- app/javascript/components/notifications/icon.jsx
+++ app/javascript/components/notifications/icon.jsx
@@ -4,15 +4,16 @@ import consumer from '../../application/action_cable_consumer'
 function reducer(state, action) {
   switch (action.type) {
     case 'notification.created':
-      return { count: state.count + 1, unread: true }
+      return { count: state.count + 1 }
   }
 }
 
-export function NotificationIcon({ count, unread }) {
-  const [state, dispatch] = useReducer(reducer, {
-    count: count,
-    unread: unread,
-  })
+export function NotificationIcon({ count }) {
+  const [state, dispatch] = useReducer(reducer, { count: count })
+
+  function isUnread() {
+    return state.count > 0
+  }
 
   useEffect(() => {
     const received = (data) => {
@@ -31,7 +32,7 @@ export function NotificationIcon({ count, unread }) {
       <dt>Count</dt>
       <dd>{state.count}</dd>
       <dt>Unread</dt>
-      <dd>{state.unread.toString()}</dd>
+      <dd>{isUnread().toString()}</dd>
     </dl>
   )
 }

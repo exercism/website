bee8faabdd19bf605b9405441e851448131d5e0e
Karlo Soriano <karlo@thalamus.ai>
2020-09-10 09:09:26 +0800

    Make table test matcher less brittle

--- test/system/components/mentoring/mentor_conversations_list_test.rb
+++ test/system/components/mentoring/mentor_conversations_list_test.rb
@@ -1,25 +1,30 @@
 require "application_system_test_case"
+require_relative "../../../support/table_matchers"
 
 module Components
   module Mentoring
     class MentorConversationsListTest < ApplicationSystemTestCase
+      include TableMatchers
+
       test "shows correct information" do
         visit test_components_mentoring_mentor_conversations_list_url
 
-        within("tbody > tr:first-child") do
-          within("td:nth-child(1)") do
+        row = {
+          "Track icon" => lambda {
             assert_css "img[src='https://assets.exercism.io/tracks/ruby-hex-white.png'][alt='icon indicating Ruby']"
-          end
-          within("td:nth-child(2)") { assert_css "img[src='https://robohash.org/exercism'][alt='avatar for mentee']" }
-          within("td:nth-child(3)") { assert_text "mentee" }
-          within("td:nth-child(4)") { assert_text "Series" }
-          within("td:nth-child(5)") { assert_text "true" }
-          within("td:nth-child(6)") { assert_text "true" }
-          within("td:nth-child(7)") { assert_text "true" }
-          within("td:nth-child(8)") { assert_text "15" }
-          within("td:nth-child(9)") { assert_text "a year ago" }
-          within("td:nth-child(10)") { assert_text "https://exercism.io/conversations/1" }
-        end
+          },
+          "Mentee avatar" => -> { assert_css "img[src='https://robohash.org/exercism'][alt='avatar for mentee']" },
+          "Mentee handle" => "mentee",
+          "Exercise title" => "Series",
+          "Starred?" => "true",
+          "Mentored previously?" => "true",
+          "New iteration?" => "true",
+          "Posts count" => "15",
+          "Updated at" => "a year ago",
+          "URL" => "https://exercism.io/conversations/1"
+        }
+
+        assert_table_row first("table"), row
       end
     end
   end

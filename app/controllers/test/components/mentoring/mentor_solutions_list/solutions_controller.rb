fdafa0f8f11cb6ff54d6ed8c840e4e3619cbc17c
Karlo Soriano <karlo@thalamus.ai>
2020-09-19 11:58:38 +0800

    Add sorter to <MentorSolutionsList />

--- app/controllers/test/components/mentoring/mentor_solutions_list/solutions_controller.rb
+++ app/controllers/test/components/mentoring/mentor_solutions_list/solutions_controller.rb
@@ -27,6 +27,19 @@ class Test::Components::Mentoring::MentorSolutionsList::SolutionsController < Ap
         status: "First timer",
         updatedAt: 1.week.ago.iso8601,
         url: "https://exercism.io/solutions/2"
+      },
+      {
+        trackId: 3,
+        trackTitle: "C#",
+        trackIconUrl: "https://assets.exercism.io/tracks/csharp-hex-white.png",
+        menteeAvatarUrl: "https://robohash.org/exercism_3",
+        menteeHandle: "Frank",
+        exerciseTitle: "Zipper",
+        isStarred: false,
+        haveMentoredPreviously: true,
+        status: "First timer",
+        updatedAt: 1.week.ago.iso8601,
+        url: "https://exercism.io/solutions/3"
       }
     ]
 
@@ -36,10 +49,22 @@ class Test::Components::Mentoring::MentorSolutionsList::SolutionsController < Ap
     if params[:filter].present?
       results = results.select { |result| result[:menteeHandle].downcase.include?(params[:filter].downcase) }
     end
+    results = sort(results) if params[:sort].present?
 
     render json: {
       results: results[page - 1, per],
       meta: { current: page, total: results.size }
     }
+  end
+
+  def sort(results)
+    case params[:sort]
+    when 'exercise'
+      results.sort_by { |c| c[:exerciseTitle] }
+    when 'recent'
+      results.sort_by { |c| c[:updatedAt] }.reverse
+    else
+      results.sort_by { |c| c[:menteeHandle] }
+    end
   end
 end

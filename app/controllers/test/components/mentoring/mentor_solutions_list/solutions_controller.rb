ab752ef4f05689f840077993cdd3ba8efb74383f
Karlo Soriano <karlo@thalamus.ai>
2020-09-19 11:24:48 +0800

    Show error messages when unable to load solutions

--- app/controllers/test/components/mentoring/mentor_solutions_list/solutions_controller.rb
+++ app/controllers/test/components/mentoring/mentor_solutions_list/solutions_controller.rb
@@ -1,5 +1,7 @@
 class Test::Components::Mentoring::MentorSolutionsList::SolutionsController < ApplicationController
   def index
+    return head :internal_server_error if params[:state] == "Error"
+
     results = [
       {
         trackTitle: "Ruby",

c20516f12f9776cac91fca14fea2b9cdea60984d
Erik Schierboom <erik_schierboom@hotmail.com>
2020-09-23 14:52:08 +0200

    Move to sub-directory structure for view component helpers

--- app/helpers/view_components/notification_icon.rb
+++
@@ -1,9 +0,0 @@
-module ViewComponents
-  class NotificationIcon < ViewComponent
-    initialize_with :user
-
-    def to_s
-      react_component("notification-icon", { count: user.notifications.count })
-    end
-  end
-end
--- test/helpers/view_components/notifications/icon_test.rb
+++ test/helpers/view_components/notifications/icon_test.rb
@@ -1,11 +1,11 @@
-require_relative "./view_component_test_case"
+require_relative "../view_component_test_case"
 
-class NotificationIconTest < ViewComponentTestCase
+class Notifications::IconTest < ViewComponentTestCase
   test "#notification_icon passes the correct parameters" do
     user = create(:user)
     create(:notification, user: user, read_at: nil)
 
-    assert_component_equal ViewComponents::NotificationIcon.new(user).to_s,
+    assert_component_equal ViewComponents::Notifications::Icon.new(user).to_s,
                            { id: "notification-icon", props: { count: 1 } }
   end
 end

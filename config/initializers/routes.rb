a6a307f17bb6b890167024ab3b70a497c58e24f7
Jeremy Walker <jez.walker@gmail.com>
2020-09-25 15:14:45 +0100

    Use Exercism::Routes for routing

--- config/initializers/routes.rb
+++ config/initializers/routes.rb
@@ -1,4 +1,12 @@
 class Exercism::Routes
+  def self.method_missing(name, *args)
+    routes.send(name, *args)
+  end
+
+  def self.respond_to_missing?(name, include_all = false)
+    routes.respond_to?(name, include_all)
+  end
+
   def self.personal_solution_url(solution)
     [
       host,

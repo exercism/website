018bf0c710387aa0db318740c334a819284dc1d2
Jeremy Walker <jez.walker@gmail.com>
2020-09-25 14:25:48 +0100

    Refactor sort options into the relevant view component class

--- app/controllers/test/components/mentoring/inbox_controller.rb
+++ app/controllers/test/components/mentoring/inbox_controller.rb
@@ -1,11 +1,5 @@
 class Test::Components::Mentoring::InboxController < ApplicationController
-  def show
-    @sort_options = [
-      { value: 'recent', label: 'Sort by Most Recent' },
-      { value: 'exercise', label: 'Sort by Exercise' },
-      { value: 'student', label: 'Sort by Student' }
-    ]
-  end
+  def show; end
 
   def tracks
     # By passing a state param, we can mock our controller's response.
--- app/helpers/view_components/mentoring/inbox.rb
+++ app/helpers/view_components/mentoring/inbox.rb
@@ -1,15 +1,24 @@
 module ViewComponents
   module Mentoring
     class Inbox < ViewComponent
-      initialize_with :conversations_request, :tracks_request, :sort_options
+      initialize_with :conversations_request, :tracks_request
 
       def to_s
-        react_component("mentoring-inbox", {
-                          conversations_request: conversations_request,
-                          tracks_request: tracks_request,
-                          sort_options: sort_options
-                        })
+        react_component(
+          "mentoring-inbox",
+          {
+            conversations_request: conversations_request,
+            tracks_request: tracks_request,
+            sort_options: SORT_OPTIONS
+          }
+        )
       end
+
+      SORT_OPTIONS = [
+        { value: 'recent', label: 'Sort by Most Recent' },
+        { value: 'exercise', label: 'Sort by Exercise' },
+        { value: 'student', label: 'Sort by Student' }
+      ].freeze
     end
   end
 end

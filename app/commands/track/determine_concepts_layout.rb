01034d3ad8cb4c699bf14268f5a7413d595fd9ab
Jeremy Walker <jez.walker@gmail.com>
2020-09-27 22:42:32 +0100

    Refactor graph code

--- app/commands/track/determine_concepts_layout.rb
+++ app/commands/track/determine_concepts_layout.rb
@@ -6,96 +6,115 @@ class Track
   class DetermineConceptsLayout
     include Mandate
 
-    attr_reader(
-      :nodes,
-      :edges,
-      :node_lookup_by_concept,
-      :node_lookup_by_index,
-      :adjacency,
-      :has_cycle
-    )
-
     def initialize(track_config)
-      @nodes = make_nodes(track_config['exercises'])
-      @node_lookup_by_concept = make_concept_lookup
-      @node_lookup_by_index = make_index_lookup
-      @edges = make_edges
-      @adjacency = GraphUtils.make_adjacency_list(self)
-      @has_cycle = GraphUtils.contains_cycle?(adjacency)
+      @graph = Graph.new(track_config['exercises'])
     end
 
     # Layout computes the ordering of the exercises in a track by creating
     # a graph then traversing it to discover and memoize the progression
     def call
-      return [] if nodes.empty?
-      return :error if has_cycle
+      return [] if graph.empty?
 
-      node_indegrees = GraphUtils.find_indegree(adjacency)
-      source_nodes = GraphUtils.find_sources(node_indegrees)
-      exercise_depths = GraphUtils.find_depths(source_nodes, adjacency)
+      # TODO: Add a proper exception
+      raise if graph.has_cycle?
 
-      blank_layout = Array.new(exercise_depths.max + 1) { [] }
-      exercise_depths.each_with_index.each_with_object(blank_layout) do |(depth, index), layers|
-        layers[depth] << node_lookup_by_index[index]
+      Array.new(graph.levels.max + 1) { [] }.tap do |layout|
+        graph.levels.each.with_index do |level, idx|
+          layout[level] << graph.node_for_index(idx)
+        end
       end
     end
 
     private
-    def make_nodes(exercises)
-      exercises.each_with_index.reduce([]) do |a, (exercise, i)|
-        a << Node.new(
-          index: i,
-          slug: exercise['slug'],
-          uuid: exercise['uuid'],
-          concepts: exercise['concepts'],
-          prerequisites: exercise['prerequisites']
-        )
-      end.freeze
-    end
+    attr_reader :graph
 
-    def make_concept_lookup
-      nodes.each_with_object({}) do |node, lookup|
-        node.concepts.each { |concept| lookup[concept] = node }
-      end.freeze
-    end
+    class Graph
+      include Mandate
 
-    def make_index_lookup
-      nodes.index_by(&:index).freeze
-    end
+      # Edge representing a directed edge
+      Edge = Struct.new(:from, :to, keyword_init: true)
 
-    def make_edges
-      nodes.each_with_object([]) do |node, aggregate|
-        node.prerequisites.each_with_object(aggregate) do |prereq, a|
-          prereq_node = node_lookup_by_concept[prereq]
-          a << Edge.new(from: prereq_node, to: node) unless prereq_node.nil?
-        end
-      end.freeze
-    end
+      # Node for representing an exercise within a track
+      Node = Struct.new(:index, :slug, :uuid, :concepts, :prerequisites, keyword_init: true)
 
-    #
-    # Supporting Graph Classes
-    #
+      # Add public readers
+      attr_reader :nodes, :edges
 
-    # Edge representing a directed edge
-    class Edge
-      attr_reader :from, :to
-
-      def initialize(from:, to:)
-        @from = from
-        @to = to
+      def initialize(exercises)
+        @exercises = exercises
+        @nodes = determine_nodes
+        @edges = determine_edges
       end
-    end
 
-    # Node for representing an exercise within a track
-    class Node
-      attr_reader :index, :slug, :concepts, :prerequisites
+      memoize
+      def empty?
+        nodes.empty?
+      end
 
-      def initialize(index:, slug:, uuid:, concepts:, prerequisites:)
-        @index = index
-        @slug = slug
-        @uuid = uuid
-        @concepts = concepts.reduce([]) { |a, concept| a << concept.dup }
-        @prerequisites = prerequisites.reduce([]) { |a, prereq| a << prereq.dup }
+      memoize
+      def has_cycle?
+        GraphUtils.contains_cycle?(adjacencies)
+      end
+
+      def node_for_concept(concept)
+        nodes_keyed_by_concept[concept]
+      end
+
+      def node_for_index(index)
+        nodes_keyed_by_index[index]
+      end
+
+      memoize
+      def levels
+        GraphUtils.calculate_levels(adjacencies)
+      end
+
+      private
+      attr_reader :exercises
+
+      # Creates adjacency list for a graph with directed edges
+      memoize
+      def adjacencies
+        Array.new(nodes.length) { [] }.tap do |adjacencies|
+          edges.each do |edge|
+            from = edge.from.index
+            to = edge.to.index
+            adjacencies[from] << to
+          end
+        end
+      end
+
+      def determine_nodes
+        exercises.map.with_index do |exercise, idx|
+          Node.new(
+            index: idx,
+            slug: exercise['slug'],
+            uuid: exercise['uuid'],
+            concepts: exercise['concepts'],
+            prerequisites: exercise['prerequisites']
+          )
+        end.freeze
+      end
+
+      def determine_edges
+        nodes.flat_map do |node|
+          node.prerequisites.map do |prereq|
+            prereq_node = node_for_concept(prereq)
+            Edge.new(from: prereq_node, to: node) unless prereq_node.nil?
+          end
+        end.freeze
+      end
+
+      memoize
+      def nodes_keyed_by_concept
+        nodes.each_with_object({}) do |node, lookup|
+          node.concepts.each { |concept| lookup[concept] = node }
+        end.freeze
+      end
+
+      memoize
+      def nodes_keyed_by_index
+        nodes.index_by(&:index).freeze
       end
     end
   end
--- lib/graph_utils.rb
+++ lib/graph_utils.rb
@@ -1,24 +1,20 @@
 # Collection of graph utilities for manipulating and traversing graphs
 module GraphUtils
-  # Creates adjacency list for a graph with directed edges
-  def self.make_adjacency_list(graph)
-    blank_adjacency = Array.new(graph.nodes.length) { [] }
-    graph.edges.each_with_object(blank_adjacency) do |edge, memo_adjacency|
-      from = edge.from.index
-      to = edge.to.index
-      memo_adjacency[from] << to
-    end
+  def self.calculate_levels(adjacencies)
+    node_indegrees = find_indegree(adjacencies)
+    source_nodes = find_sources(node_indegrees)
+    find_depths(source_nodes, adjacencies)
   end
 
-  # Examines the graph structure (through an adjacency list) looking
+  # Examines the graph structure (through an adjacencies list) looking
   # for a cycle
-  def self.contains_cycle?(adjacency)
-    visited = Array.new(adjacency.length).fill(false)
-    on_stack = Array.new(adjacency.length).fill(false)
+  def self.contains_cycle?(adjacencies)
+    visited = Array.new(adjacencies.length).fill(false)
+    on_stack = Array.new(adjacencies.length).fill(false)
     stack = []
 
     # Check from each node in the case of disjunct graphs
-    0.upto(adjacency.length - 1).each do |source|
+    0.upto(adjacencies.length - 1).each do |source|
       next if visited[source]
 
       stack << source
@@ -35,7 +31,7 @@ module GraphUtils
           stack.pop
         end
 
-        adjacency[n].each do |adjacent_node|
+        adjacencies[n].each do |adjacent_node|
           if !visited[adjacent_node]
             stack << adjacent_node
           elsif on_stack[adjacent_node]
@@ -49,12 +45,13 @@ module GraphUtils
     false
   end
 
-  # From an adjacency list, find the number of incoming edges to a node (the in-degree)
-  def self.find_indegree(adjacency)
-    blank_indegrees = Array.new(adjacency.length) { 0 }
-    adjacency.each_with_object(blank_indegrees) do |adj_nodes, indegrees|
-      adj_nodes.each_with_object(indegrees) do |adj_node, indegrees|
-        indegrees[adj_node] += 1
+  # From an adjacencies list, find the number of incoming edges to a node (the in-degree)
+  def self.find_indegree(adjacencies)
+    Array.new(adjacencies.length) { 0 }.tap do |indegrees|
+      adjacencies.each do |adj_nodes|
+        adj_nodes.each do |adj_node|
+          indegrees[adj_node] += 1
+        end
       end
     end
   end
@@ -62,19 +59,21 @@ module GraphUtils
   # From a list of in-degrees, where the index represents a node index,
   # determine the sources (roots) of the graph where the in-degree is 0
   def self.find_sources(indegrees)
-    indegrees.each_with_index.filter { |indegree, _| indegree.zero? }.map { |_, index| index }
+    indegrees.each_with_index.
+      filter { |indegree, _| indegree.zero? }.
+      map { |_, idx| idx }
   end
 
   # Assign a depth to all nodes in the graph
   # The node is assigned the depth when it is encountered on the max-length route
-  def self.find_depths(source_nodes, adjacency)
-    max_depths = Array.new(adjacency.length).fill(0)
+  def self.find_depths(source_nodes, adjacencies)
+    max_depths = Array.new(adjacencies.length).fill(0)
 
     stack = source_nodes.map { |source_index| [source_index, 0] }
     until stack.empty?
       node_index, depth = stack.pop
       max_depths[node_index] = depth if depth > max_depths[node_index]
-      adjacency[node_index].each { |child| stack << [child, depth + 1] }
+      adjacencies[node_index].each { |child| stack << [child, depth + 1] }
     end
 
     max_depths

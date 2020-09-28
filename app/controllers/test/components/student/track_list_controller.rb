fd4da5ecc43b5f1809b4a3d439cc7e0aad10591c
Karlo Soriano <karlo@thalamus.ai>
2020-09-28 12:04:24 +0800

    Add endpoint for filtering for <Student.TrackList />

--- app/controllers/test/components/student/track_list_controller.rb
+++ app/controllers/test/components/student/track_list_controller.rb
@@ -2,4 +2,8 @@ class Test::Components::Student::TrackListController < ApplicationController
   def show
     @data = API::TracksSerializer.new(Track.all, User.first)
   end
+
+  def tracks
+    render json: API::TracksSerializer.new(Track.all, User.first).to_json
+  end
 end
--- app/helpers/view_components/student/track_list.rb
+++ app/helpers/view_components/student/track_list.rb
@@ -1,10 +1,12 @@
 module ViewComponents
   module Student
     class TrackList < ViewComponent
-      initialize_with :data
+      initialize_with :data, :request
 
       def to_s
-        react_component("student-track-list", data)
+        react_component("student-track-list", {
+                          request: request.deep_merge({ options: { initialData: data } })
+                        })
       end
     end
   end

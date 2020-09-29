46580372589bc3a36c11c96f3ac7e5a53a7017d4
Karlo Soriano <karlo@thalamus.ai>
2020-09-29 13:45:59 +0800

    Add popup to <Mentoring.Queue />

--- app/javascript/components/mentoring/Queue/Solution.jsx
+++ app/javascript/components/mentoring/Queue/Solution.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import React, { useState } from 'react'
 import { fromNow } from '../../../utils/time'
 import { TrackIcon } from '../../common/TrackIcon'
+import { usePopper } from 'react-popper'
 
 export function Solution({
   trackTitle,
@@ -14,25 +16,54 @@ export function Solution({
   updatedAt,
   url,
 }) {
+  const [referenceElement, setReferenceElement] = useState(null)
+  const [popperElement, setPopperElement] = useState(null)
+  const [showPopper, setShowPopper] = useState(false)
+  const { styles, attributes } = usePopper(referenceElement, popperElement)
+
   return (
-    <tr>
-      <td>
-        <TrackIcon track={{ title: trackTitle, iconUrl: trackIconUrl }} />
-      </td>
-      <td>
-        <img
-          style={{ width: 100 }}
-          src={menteeAvatarUrl}
-          alt={`avatar for ${menteeHandle}`}
-        />
-      </td>
-      <td>{menteeHandle}</td>
-      <td>{exerciseTitle}</td>
-      <td>{isStarred.toString()}</td>
-      <td>{haveMentoredPreviously.toString()}</td>
-      <td>{status}</td>
-      <td>{fromNow(updatedAt)}</td>
-      <td>{url}</td>
-    </tr>
+    <>
+      <tr
+        ref={setReferenceElement}
+        onMouseEnter={() => {
+          setShowPopper(true)
+        }}
+        onMouseLeave={() => {
+          setShowPopper(false)
+        }}
+      >
+        <td>
+          <TrackIcon track={{ title: trackTitle, iconUrl: trackIconUrl }} />
+          <img
+            style={{ width: 100 }}
+            src={trackIconUrl}
+            alt={`icon indicating ${trackTitle}`}
+          />
+        </td>
+        <td>
+          <img
+            style={{ width: 100 }}
+            src={menteeAvatarUrl}
+            alt={`avatar for ${menteeHandle}`}
+          />
+        </td>
+        <td>{menteeHandle}</td>
+        <td>{exerciseTitle}</td>
+        <td>{isStarred.toString()}</td>
+        <td>{haveMentoredPreviously.toString()}</td>
+        <td>{status}</td>
+        <td>{fromNow(updatedAt)}</td>
+        <td>{url}</td>
+      </tr>
+      {showPopper && (
+        <div
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          Lorem ipsum
+        </div>
+      )}
+    </>
   )
 }

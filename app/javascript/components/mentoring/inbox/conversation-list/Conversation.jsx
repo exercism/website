125b7b39511e9d1cfd6f6806eb0964805a9638e9
Karlo Soriano <karlo@thalamus.ai>
2020-09-29 13:45:59 +0800

    Add popup to <Mentoring.Inbox />

--- app/javascript/components/mentoring/inbox/conversation-list/Conversation.jsx
+++ app/javascript/components/mentoring/inbox/conversation-list/Conversation.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import dayjs from 'dayjs'
 import RelativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(RelativeTime)
+import { usePopper } from 'react-popper'
 
 export function Conversation({
   trackTitle,
@@ -16,30 +17,54 @@ export function Conversation({
   updatedAt,
   url,
 }) {
+  const [referenceElement, setReferenceElement] = useState(null)
+  const [popperElement, setPopperElement] = useState(null)
+  const [showPopper, setShowPopper] = useState(null)
+  const { styles, attributes } = usePopper(referenceElement, popperElement)
+
   return (
-    <tr>
-      <td>
-        <img
-          style={{ width: 100 }}
-          src={trackIconUrl}
-          alt={`icon for ${trackTitle} track`}
-        />
-      </td>
-      <td>
-        <img
-          style={{ width: 100 }}
-          src={menteeAvatarUrl}
-          alt={`avatar for ${menteeHandle}`}
-        />
-      </td>
-      <td>{menteeHandle}</td>
-      <td>{exerciseTitle}</td>
-      <td>{isStarred.toString()}</td>
-      <td>{haveMentoredPreviously.toString()}</td>
-      <td>{isNewIteration.toString()}</td>
-      <td>{postsCount}</td>
-      <td>{dayjs(updatedAt).fromNow()}</td>
-      <td>{url}</td>
-    </tr>
+    <>
+      <tr
+        ref={setReferenceElement}
+        onMouseEnter={() => {
+          setShowPopper(true)
+        }}
+        onMouseLeave={() => {
+          setShowPopper(null)
+        }}
+      >
+        <td>
+          <img
+            style={{ width: 100 }}
+            src={trackIconUrl}
+            alt={`icon for ${trackTitle} track`}
+          />
+        </td>
+        <td>
+          <img
+            style={{ width: 100 }}
+            src={menteeAvatarUrl}
+            alt={`avatar for ${menteeHandle}`}
+          />
+        </td>
+        <td>{menteeHandle}</td>
+        <td>{exerciseTitle}</td>
+        <td>{isStarred.toString()}</td>
+        <td>{haveMentoredPreviously.toString()}</td>
+        <td>{isNewIteration.toString()}</td>
+        <td>{postsCount}</td>
+        <td>{dayjs(updatedAt).fromNow()}</td>
+        <td>{url}</td>
+      </tr>
+      {showPopper && (
+        <div
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          Lorem ipsum
+        </div>
+      )}
+    </>
   )
 }

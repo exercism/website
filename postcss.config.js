92ddd3d035a867bd6bec2378a89875be5503f78a
Jeremy Walker <jez.walker@gmail.com>
2020-09-29 18:12:41 +0100

    Explicitely use postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    //require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {

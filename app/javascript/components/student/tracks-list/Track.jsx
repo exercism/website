f017fa444771d51337e457aa6a10b6d3718e5388
Jeremy Walker <jez.walker@gmail.com>
2020-10-01 20:01:55 +0100

    Use React component for tracks list

--- app/javascript/components/student/tracks-list/Track.jsx
+++ app/javascript/components/student/tracks-list/Track.jsx
@@ -4,29 +4,34 @@ import { ProgressBar } from './ProgressBar'
 
 export function Track({ track }) {
   return (
-    <tr>
-      <td>
-        <TrackIcon track={track} />
-      </td>
-      <td>{track.title}</td>
-      <td>{track.numConceptExercises}</td>
-      <td>{track.numPracticeExercises}</td>
-      <td>{track.webUrl}</td>
-      <td>{track.isNew.toString()}</td>
-      <td>{track.tags.join(', ')}</td>
-      <td>{track.isJoined.toString()}</td>
-      <td>{track.numCompletedConceptExercises}</td>
-      <td>{track.numCompletedPracticeExercises}</td>
-      <td>
-        {track.isJoined && (
-          <ProgressBar
-            numConceptExercises={track.numConceptExercises}
-            numPracticeExercises={track.numPracticeExercises}
-            numCompletedConceptExercises={track.numCompletedConceptExercises}
-            numCompletedPracticeExercises={track.numCompletedPracticeExercises}
-          />
-        )}
-      </td>
-    </tr>
+    <a className="c-track" href={track.webUrl}>
+      <TrackIcon track={track} />
+      <div className="info">
+        <h3 className="title">{track.title}</h3>
+        <ul className="counts">
+          <li>{track.numConceptExercises} concepts</li>
+          <li>{track.numPracticeExercises} exercises</li>
+        </ul>
+      </div>
+      <div style={{ display: 'none' }}>
+        <div>{track.isNew.toString()}</div>
+        <div>{track.tags.join(', ')}</div>
+        <div>{track.isJoined.toString()}</div>
+        <div>{track.numCompletedConceptExercises}</div>
+        <div>{track.numCompletedPracticeExercises}</div>
+        <div>
+          {track.isJoined && (
+            <ProgressBar
+              numConceptExercises={track.numConceptExercises}
+              numPracticeExercises={track.numPracticeExercises}
+              numCompletedConceptExercises={track.numCompletedConceptExercises}
+              numCompletedPracticeExercises={
+                track.numCompletedPracticeExercises
+              }
+            />
+          )}
+        </div>
+      </div>
+    </a>
   )
 }

3701d4faa29d03438c8fca46808c2e016be509ab
Karlo Soriano <karlo@thalamus.ai>
2020-10-07 11:50:13 +0800

    Update test/javascript/utils/url-params.test.js
    Co-authored-by: Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>

--- test/javascript/utils/url-params.test.js
+++ test/javascript/utils/url-params.test.js
@@ -9,7 +9,7 @@ test('toString() returns empty string when object is undefined', () => {
 })
 
 test('toString() converts arrays correctly', () => {
-  const object = { tags: ['oop', 'static'] }
+  const object = { tags: ['oop', 'static'], foo: 'bar' }
 
   expect(new UrlParams(object).toString()).toEqual('tags[]=oop&tags[]=static')
 })

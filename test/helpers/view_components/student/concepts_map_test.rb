8424449261ba0df956bdaec8e92d5c911a7e5027
Tim Austin <tim@neenjaw.com>
2020-10-08 15:51:36 -0600

    test update

--- test/helpers/view_components/student/concepts_map_test.rb
+++ test/helpers/view_components/student/concepts_map_test.rb
@@ -10,10 +10,7 @@ module Student
       }
       component = ViewComponents::Student::ConceptsMap.new(data).to_s
 
-      # TODO: Question: What is the second argument of this function supposed to be?
-      #   A hash representing the html structure?
-      assert_component_equal component,
-        { id: "concepts-map", props: { data: data } }
+      assert_component_equal component, "concepts-map", { graph: data }
     end
   end
 end

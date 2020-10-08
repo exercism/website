76d88a136159e22c85beeba48424d46caf0004bf
Tim Austin <tim@neenjaw.com>
2020-10-08 17:47:49 -0600

    Update docker/dev/init.dev.sh

--- docker/dev/init.dev.sh
+++ docker/dev/init.dev.sh
@@ -12,7 +12,7 @@ echo "Create and migrate DBs"
 mysql -u root -ppassword -h "$DB_HOST" < docker/dev/init.sql
 bundle exec bin/rails db:migrate
 
-num_users = $(bundle exec rails r "p User.count")
+num_users=$(bundle exec rails r "p User.count")
 if [[ "${num_users}" -eq 0 ]]
 then
     bundle exec bin/rails db:seed

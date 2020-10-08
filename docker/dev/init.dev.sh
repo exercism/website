a6f94029249beb4d6a05b8df2169041ede69cd59
Tim Austin <tim@neenjaw.com>
2020-10-08 16:56:31 -0600

    Add conditional db seeding to `init.dev.sh`

--- docker/dev/init.dev.sh
+++ docker/dev/init.dev.sh
@@ -12,5 +12,11 @@ echo "Create and migrate DBs"
 mysql -u root -ppassword -h "$DB_HOST" < docker/dev/init.sql
 bundle exec bin/rails db:migrate
 
+num_users = $(rails r "p User.count")
+if [[ "${num_users}" -eq 0 ]]
+then
+    bundle exec bin/rails db:seed
+fi
+
 echo "Start up the website stack"
 overmind start -p 3020 -s /usr/src/app/tmp/overmind.sock -f ./Procfile.docker.dev

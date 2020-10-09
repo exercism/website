12a3000b598acbe218cfa3efc8be6f5671900d7e
Tim Austin <tim@neenjaw.com>
2020-10-09 11:37:55 -0600

    Dockerfile Update: Add conditional db seeding to `init.dev.sh` (#182)
    * Add conditional db seeding to `init.dev.sh`
    
    * Update docker/dev/init.dev.sh
    
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>
    
    * Update docker/dev/init.dev.sh
    
    * Updated
    
    * shell script changed for bourne shell
    Script was previously written for bourne again shell (bash)
    where the docker image calls `init.dev.sh` using `sh` rather than `bash`
    So it was failing at the `if` comparison since `[[ ... ]]` only exists
    in `bash`.
    
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- docker/dev/init.dev.sh
+++ docker/dev/init.dev.sh
@@ -12,5 +12,12 @@ echo "Create and migrate DBs"
 mysql -u root -ppassword -h "$DB_HOST" < docker/dev/init.sql
 bundle exec bin/rails db:migrate
 
+num_users=$(bundle exec rails r "p User.count" 2>/dev/null)
+echo "${num_users} user(s) found."
+if [ "${num_users}" -eq 0 ]; then
+    echo "Reseeding..."
+    bundle exec bin/rails db:seed
+fi
+
 echo "Start up the website stack"
 overmind start -p 3020 -s /usr/src/app/tmp/overmind.sock -f ./Procfile.docker.dev

5f8219c9669cafa2a37b97f65a413b063c990eee
Karlo Soriano <karlo@thalamus.ai>
2020-10-09 09:35:25 +0800

    Stop header from jumping around (#177)
    * Stop header from jumping around
    
    * Update app/javascript/components/student/tracks-list/Header.jsx
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * Use data instead of latestData for <Header />
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- app/javascript/components/student/TracksList.jsx
+++ app/javascript/components/student/TracksList.jsx
@@ -1,11 +1,10 @@
 import React, { useReducer } from 'react'
-import { useRequestQuery } from '../../hooks/request-query'
+import { usePaginatedRequestQuery } from '../../hooks/request-query'
 import { Search } from './tracks-list/Search'
 import { StatusFilter } from './tracks-list/StatusFilter'
 import { TagsFilter } from './tracks-list/TagsFilter'
 import { List } from './tracks-list/List'
 import { Header } from './tracks-list/Header'
-import { Loading } from '../common/Loading'
 
 function reducer(state, action) {
   switch (action.type) {
@@ -32,10 +31,12 @@ function reducer(state, action) {
 
 export function TracksList({ statusOptions, tagOptions, ...props }) {
   const [request, dispatch] = useReducer(reducer, props.request)
-  const { data, isSuccess, isError, isLoading } = useRequestQuery(
-    'track-list',
-    request
-  )
+  const {
+    resolvedData,
+    latestData,
+    isError,
+    isFetching,
+  } = usePaginatedRequestQuery('track-list', request)
 
   return (
     <div className="c-tracks-list">
@@ -46,16 +47,15 @@ export function TracksList({ statusOptions, tagOptions, ...props }) {
         </div>
       </section>
       <section className="md-container">
-        {isSuccess && <Header data={data} query={request.query} />}
+        <Header data={latestData} query={request.query} />
         <StatusFilter
           value={request.query.status}
           dispatch={dispatch}
           options={statusOptions}
         />
 
-        {isLoading && <Loading />}
         {isError && <p>Something went wrong</p>}
-        {isSuccess && <List data={data} />}
+        {resolvedData && <List data={resolvedData} />}
       </section>
     </div>
   )

77a9448dfd33f13323fabec481561523b08c863a
Jeremy Walker <jez.walker@gmail.com>
2020-10-12 00:55:21 +0100

    WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Specify AnyCable WebSocket server URL to use by JS client
   config.after_initialize do
-    config.action_cable.url = ActionCable.server.config.url = "ws://staging.exercism.io:3334/cable"
+    config.action_cable.url = ActionCable.server.config.url = "ws://exercism.lol:3334/cable"
     # config.action_cable.allowed_request_origins = %r{http://localhost.*} # May need this
   end
   # Settings specified here will take precedence over those in config/application.rb.
@@ -103,3 +103,7 @@ Rails.application.configure do
 
   config.action_controller.asset_host = "exercism-assets-staging.s3.eu-west-2.amazonaws.com"
 end
+
+Rails.application.routes.default_url_options = {
+  host: "http://exercism.lol"
+}

24f11966353abe4a48b61f0476f8c3e2bc053f6a
Jeremy Walker <jez.walker@gmail.com>
2020-10-12 00:39:43 +0100

    WIP

--- docker/compile_assets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This is run in the context of the Dockerfile
-RAILS_ENV=production RACK_ENV=production NODE_ENV=production bundle exec bin/webpack
-
--- docker/webserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# TODO: Remove the reset and the seed once we're in the beta
-cd .. && 
-cd .. && 

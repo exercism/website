742a658ab965cdb6c8d8a94f4224419dc9fc39ab
Tim Austin <tim@neenjaw.com>
2020-10-13 12:04:04 -0600

    test data updated to new form

--- app/controllers/test/components/student/concepts_map_controller.rb
+++ app/controllers/test/components/student/concepts_map_controller.rb
@@ -3,45 +3,51 @@ class Test::Components::Student::ConceptsMapController < ApplicationController #
     @data = {
       concepts: [
         {
-          index: 0,
           web_url: "basics-url",
           slug: "basics",
+          name: "Basics",
           status: "completed"
         },
         {
           index: 1,
           web_url: "booleans-url",
           slug: "booleans",
+          name: "Booleans",
           status: "unlocked"
         },
         {
           index: 2,
           web_url: "numbers-url",
           slug: "floating-point-numbers",
+          name: "Floating Point Numbers",
           status: "completed"
         },
         {
           index: 3,
           web_url: "numbers-url",
           slug: "integers",
+          name: "Integer Numbers",
           status: "completed"
         },
         {
           index: 4,
           web_url: "anonymous-fns-url",
           slug: "bit-manipulation",
+          name: "Bit Manipulation",
           status: "unlocked"
         },
         {
           index: 5,
           web_url: "anonymous-fns-url",
           slug: "closures",
+          name: "Closures",
           status: "unlocked"
         },
         {
           index: 6,
           web_url: "anonymous-fns-url",
           slug: "anonymous-functions",
+          name: "Anonymous Functions",
           status: "unlocked"
         }
       ],

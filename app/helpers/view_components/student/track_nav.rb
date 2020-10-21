89e02bc2bbca7c2dd3b1cf7fe989373537728590
Jeremy Walker <jez.walker@gmail.com>
2020-10-21 16:25:42 +0100

    Add nav to concepts page

--- app/helpers/view_components/student/track_nav.rb
+++ app/helpers/view_components/student/track_nav.rb
@@ -10,28 +10,39 @@ module ViewComponents
 
         tag.nav(class: 'c-track-nav') do
           tag.div(class: 'lg-container container') do
-            link_to("<-", Exercism::Routes.tracks_path, class: "back", 'aria-label': "Back to all tracks") +
-              image_tag(@track.icon_url, class: 'c-track-icon') +
-              tag.div(@track.title, class: 'title') +
-              tag.div(class: 'tabs') do
-                link_to("#", class: tab_class(:overview)) do
-                  "Overview"
-                end +
-                  link_to(
-                    Exercism::Routes.track_concepts_path(@track),
-                    class: tab_class(:concepts)
-                  ) do
-                    "Concepts"
-                  end +
-                  link_to("#", class: tab_class(:exerises)) do
-                    "Exercises"
-                  end
-              end
+            safe_join([
+                        link_to("<-", Exercism::Routes.tracks_path, class: "back", 'aria-label': "Back to all tracks"),
+                        image_tag(@track.icon_url, class: 'c-track-icon'),
+                        tag.div(@track.title, class: 'title'),
+                        tag.div(safe_join(tabs), class: 'tabs')
+                      ])
           end
         end
       end
 
       private
+      def tabs
+        [
+          link_to(
+            Exercism::Routes.track_path(@track),
+            class: tab_class(:overview)
+          ) do
+            "Overview"
+          end,
+
+          link_to(
+            Exercism::Routes.track_concepts_path(@track),
+            class: tab_class(:concepts)
+          ) do
+            "Concepts"
+          end,
+
+          link_to("#", class: tab_class(:exerises)) do
+            "Exercises"
+          end
+        ]
+      end
+
       def tab_class(tab)
         tab == selected_tab ? "selected" : ""
       end

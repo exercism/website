c57027bb61b47f21acb5b6cc7d402a31dfc82d84
Jeremy Walker <jez.walker@gmail.com>
2020-10-21 17:27:47 +0100

    Add icons

--- app/helpers/view_components/view_component.rb
+++ app/helpers/view_components/view_component.rb
@@ -2,9 +2,11 @@ module ViewComponents
   class ViewComponent
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::UrlHelper
+    include IconsHelper
 
     # TODO: This can probably be removed
     include ActionView::Helpers::AssetTagHelper
+
     include ActionView::Context
 
     extend Mandate::Memoize

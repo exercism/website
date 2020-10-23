75ae58da8b503a96c71cd33505d0874be9b4700e
Karlo Soriano <karlo@thalamus.ai>
2020-10-23 14:37:53 +0800

    Clean up Editor.test.js

--- test/javascript/components/student/Editor.test.js
+++ test/javascript/components/student/Editor.test.js
@@ -5,73 +5,106 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { Editor } from '../../../../app/javascript/components/student/Editor'
 
-const server = setupServer(
-  rest.post('https://exercism.test/submissions', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        submission: {
-          id: 2,
-          tests_status: 'pending',
-          test_runs: [],
-          message: '',
-        },
-      })
-    )
-  }),
-  rest.post('https://exercism.test/submissions/pass', (req, res, ctx) => {
-    return res(
-      ctx.json({
-        submission: { id: 2, tests_status: 'pass', test_runs: [], message: '' },
-      })
-    )
-  })
-)
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
-
 test('clears current submission when resubmitting', async () => {
-  const { getByText, queryByText } = render(
+  const server = setupServer(
+    rest.post('https://exercism.test/submissions', (req, res, ctx) => {
+      return res(
+        ctx.json({
+          submission: {
+            id: 2,
+            tests_status: 'pending',
+            test_runs: [],
+            message: '',
+          },
+        })
+      )
+    })
+  )
+  server.listen()
+
+  const { getByText, getByLabelText, queryByText } = render(
     <Editor endpoint="https://exercism.test/submissions" />
   )
-
+  fireEvent.change(getByLabelText('Code'), { target: { value: 'Code' } })
   fireEvent.click(getByText('Submit'))
   await waitFor(() =>
     expect(queryByText('Status: pending')).toBeInTheDocument()
   )
-
+  fireEvent.change(getByLabelText('Code'), {
+    target: { value: 'Changed code' },
+  })
   fireEvent.click(getByText('Submit'))
+
   await waitFor(() =>
     expect(queryByText('Status: pending')).not.toBeInTheDocument()
   )
   await waitFor(() =>
     expect(queryByText('Status: pending')).toBeInTheDocument()
   )
+
+  server.close()
 })
 
 test('shows message when test times out', async () => {
-  const { getByText, queryByText } = render(
+  const server = setupServer(
+    rest.post('https://exercism.test/submissions', (req, res, ctx) => {
+      return res(
+        ctx.json({
+          submission: {
+            id: 2,
+            tests_status: 'pending',
+            test_runs: [],
+            message: '',
+          },
+        })
+      )
+    })
+  )
+  server.listen()
+
+  const { getByText, getByLabelText, queryByText } = render(
     <Editor endpoint="https://exercism.test/submissions" timeout={0} />
   )
-
+  fireEvent.change(getByLabelText('Code'), { target: { value: 'Code' } })
   fireEvent.click(getByText('Submit'))
   await waitFor(() =>
     expect(queryByText('Status: pending')).toBeInTheDocument()
   )
+
   await waitFor(() =>
     expect(queryByText('Status: timeout')).toBeInTheDocument()
   )
+
+  server.close()
 })
 
 test('does not time out when tests have resolved', async () => {
-  const { getByText, queryByText } = render(
-    <Editor endpoint="https://exercism.test/submissions/pass" timeout={0} />
+  const server = setupServer(
+    rest.post('https://exercism.test/submissions', (req, res, ctx) => {
+      return res(
+        ctx.json({
+          submission: {
+            id: 2,
+            tests_status: 'pass',
+            test_runs: [],
+            message: '',
+          },
+        })
+      )
+    })
   )
+  server.listen()
 
+  const { getByText, getByLabelText, queryByText } = render(
+    <Editor endpoint="https://exercism.test/submissions" timeout={0} />
+  )
+  fireEvent.change(getByLabelText('Code'), { target: { value: 'Code' } })
   fireEvent.click(getByText('Submit'))
   await waitFor(() => expect(queryByText('Status: pass')).toBeInTheDocument())
+
   await waitFor(() =>
     expect(queryByText('Status: timeout')).not.toBeInTheDocument()
   )
+
+  server.close()
 })

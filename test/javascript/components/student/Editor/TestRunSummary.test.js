1124c141262e5d678b7d1d9d387bd30f84ceada5
Karlo Soriano <karlo@thalamus.ai>
2020-10-26 11:09:51 +0800

    Fix test importing

--- test/javascript/components/student/Editor/TestRunSummary.test.js
+++ test/javascript/components/student/Editor/TestRunSummary.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { TestRunSummary } from '../../../../../app/javascript/components/student/Editor/TestRunSummary'
+import { TestRunSummary } from '../../../../../app/javascript/components/student/editor/TestRunSummary'
 
 test('hides cancel button if test run has resolved', async () => {
   const { queryByText } = render(

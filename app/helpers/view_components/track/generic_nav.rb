97a3fb113a881de28391f18d724737ff833d1130
Jeremy Walker <jez.walker@gmail.com>
2020-10-26 17:29:12 +0000

    Remove back button for now

--- app/helpers/view_components/track/generic_nav.rb
+++ app/helpers/view_components/track/generic_nav.rb
@@ -7,12 +7,12 @@ module ViewComponents
         tag.nav(class: klass) do
           tag.div(class: 'lg-container container') do
             safe_join([
-              link_to(
-                graphical_icon("arrow-left"),
-                Exercism::Routes.tracks_path,
-                class: "back",
-                'aria-label': "Back to all tracks"
-              ),
+              # link_to(
+              #   graphical_icon("arrow-left"),
+              #   Exercism::Routes.tracks_path,
+              #   class: "back",
+              #   'aria-label': "Back to all tracks"
+              # ),
               image_tag(track.icon_url, class: 'c-track-icon'),
               tag.div(track.title, class: 'title')
             ] + elements)

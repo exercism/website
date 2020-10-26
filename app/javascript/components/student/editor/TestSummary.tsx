1cc7c261dafae3eadc07d5f2f1f6537b0032d315
Karlo Soriano <karlo@thalamus.ai>
2020-10-26 11:24:13 +0800

    Add isPresent function to explicity check test messages

--- app/javascript/components/student/editor/TestSummary.tsx
+++ app/javascript/components/student/editor/TestSummary.tsx
@@ -7,6 +7,9 @@ export function TestSummary({ test }: { test: Test }) {
     [TestStatus.FAIL]: 'Failed',
     [TestStatus.ERROR]: 'Failed',
   }
+  const isPresent = useCallback((str) => {
+    return str !== undefined && str !== null && str !== ''
+  }, [])
 
   return (
     <details
@@ -15,8 +18,8 @@ export function TestSummary({ test }: { test: Test }) {
       <summary>
         {statusLabels[test.status]}: {test.name}
       </summary>
-      {test.message && <p>Message: {test.message}</p>}
-      {test.output && <p>Output: {test.output}</p>}
+      {isPresent(test.message) ? <p>Message: {test.message}</p> : null}
+      {isPresent(test.output) ? <p>Output: {test.output}</p> : null}
     </details>
   )
 }

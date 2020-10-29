59821ab3f6b83ea8a1724ef644989d27930d3bcd
Erik Schierboom <erik_schierboom@hotmail.com>
2020-10-29 11:09:52 +0100

    Revert change to paging

--- app/controllers/test/components/mentoring/queue_controller.rb
+++ app/controllers/test/components/mentoring/queue_controller.rb
@@ -49,7 +49,7 @@ class Test::Components::Mentoring::QueueController < Test::BaseController
     ]
 
     page = params.fetch(:page, 1).to_i
-    per = params.fetch(:per, 2).to_i
+    per = params.fetch(:per, 1).to_i
 
     if params[:filter].present?
       results = results.select { |result| result[:mentee_handle].downcase.include?(params[:filter].downcase) }

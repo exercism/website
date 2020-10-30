7d2cdeda06bc1d94843d9eabee0f1ac031785ec1
Tim Austin <tim@neenjaw.com>
2020-10-30 00:29:09 -0600

    update command

--- app/commands/user_track/generate_concept_status_mapping.rb
+++ app/commands/user_track/generate_concept_status_mapping.rb
@@ -12,8 +12,8 @@ class UserTrack
       {}.tap do |output|
         set_status = ->(slugs, status) { slugs.each { |slug| output[slug] = status } }
 
-        set_status.(all_concepts, :locked)
-        set_status.(available_concepts, :unlocked)
+        set_status.(all_concepts, :unavailable)
+        set_status.(available_concepts, :available)
         set_status.(learnt_concepts, :complete)
       end
     end

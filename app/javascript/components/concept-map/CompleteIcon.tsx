5432a16cd60b4e2a375e9dbbbdadcd3925f47b39
Jeremy Walker <jez.walker@gmail.com>
2020-10-30 22:45:39 +0000

    Readd description

--- app/javascript/components/concept-map/CompleteIcon.tsx
+++ app/javascript/components/concept-map/CompleteIcon.tsx
@@ -18,6 +18,7 @@ export const CompleteIcon = ({
       xmlns="http://www.w3.org/2000/svg"
       className="complete-icon"
     >
+      <title>You have mastered this concept</title>
       <circle cx="8" cy="8" r="8" fill={fillColor} />
       <path
         d="M11.1545 5.13611L7.31292 10.6239C7.21009 10.7716 7.04251 10.8609 6.86253 10.8639C6.68254 10.8669 6.5121 10.7831 6.40445 10.6389L5.42725 9.33611"

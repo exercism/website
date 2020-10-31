2cc986e2ec1d182175bdb3723c2660425ccbef73
Tim Austin <tim@neenjaw.com>
2020-10-31 15:49:59 -0600

    typo

--- app/commands/user_track/generate_concept_exercise_mapping.rb
+++ app/commands/user_track/generate_concept_exercise_mapping.rb
@@ -35,8 +35,8 @@ class UserTrack
 
       user_track.track.concepts.each_with_object({}) do |concept, hash|
         hash[concept.slug] = {
-          exercises: prereq_counts[concept.id.to_i] + taught_counts[concept.id.to_i],
-          exercises_completed: solved_prereq_counts[concept.id.to_i] + solved_taught_counts[concept.id.to_i]
+          exercises: prereq_counts[concept.id].to_i + taught_counts[concept.id].to_i,
+          exercises_completed: solved_prereq_counts[concept.id].to_i + solved_taught_counts[concept.id].to_i
         }
       end
     end

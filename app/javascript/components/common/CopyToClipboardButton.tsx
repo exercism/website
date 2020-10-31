95c5af4f1aa19dbec6697c8454fe2a3a864e624f
Erik Schierboom <erik_schierboom@hotmail.com>
2020-10-31 20:37:36 +0100

    Add support for reacting to keypresses

--- app/javascript/components/common/CopyToClipboardButton.tsx
+++ app/javascript/components/common/CopyToClipboardButton.tsx
@@ -1,16 +1,61 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
+
+const KEY_NAMES = Object.freeze({
+  SPACE: ' ',
+})
+
+const KEY_NAMES_LEGACY = Object.freeze({
+  SPACE: 'Space',
+})
 
 export function CopyToClipboardButton({ textToCopy }: { textToCopy: string }) {
   if (navigator.clipboard === undefined) {
     return null
   }
 
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
   const [justCopied, setJustCopied] = useState(false)
 
-  const onClick = useCallback(async () => {
+  const copyTextToClipboard = async () => {
     await navigator.clipboard.writeText(textToCopy)
     setJustCopied(true)
-  }, [textToCopy, setJustCopied])
+  }
+
+  const onKeyPress = useCallback(
+    async (e: KeyboardEvent) => {
+      if ((e.code === 'KeyC' || e.key === 'c') && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault()
+        await copyTextToClipboard()
+        return
+      }
+
+      if (e.key === KEY_NAMES_LEGACY.SPACE || e.key === KEY_NAMES.SPACE) {
+        e.preventDefault()
+        buttonRef.current?.click()
+        return
+      }
+    },
+
+    [textToCopy, setJustCopied]
+  )
+
+  const onClick = useCallback(
+    async (e) => {
+      e.preventDefault()
+      await copyTextToClipboard()
+    },
+    [textToCopy, setJustCopied]
+  )
+
+  const onFocus = useCallback(
+    () => buttonRef.current?.addEventListener('keydown', onKeyPress),
+    [textToCopy, setJustCopied]
+  )
+
+  const onBlur = useCallback(
+    () => buttonRef.current?.removeEventListener('keydown', onKeyPress),
+    [textToCopy, setJustCopied]
+  )
 
   useEffect(() => {
     if (!justCopied) {
@@ -24,8 +69,11 @@ export function CopyToClipboardButton({ textToCopy }: { textToCopy: string }) {
 
   return (
     <button
+      ref={buttonRef}
       type="button"
       onClick={onClick}
+      onFocus={onFocus}
+      onBlur={onBlur}
       className={`c-copy-to-clipboard-button ${justCopied ? 'copied' : ''}`}
       aria-label={`Copy ${textToCopy} to the cliboard`}
     >
--- test/javascript/components/common/CopyToClipboardButton.test.js
+++ test/javascript/components/common/CopyToClipboardButton.test.js
@@ -3,16 +3,39 @@ import { render, waitFor, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { CopyToClipboardButton } from '../../../../app/javascript/components/common/CopyToClipboardButton'
 
-test('copies text to clipboard when clicking', async () => {
+const mockClipboardApi = () => {
   // The clipboard API is not supported so we have to mock it
   navigator.clipboard = { writeText: () => {} }
   jest.spyOn(navigator.clipboard, 'writeText')
+}
+
+test('copies text to clipboard when clicking', async () => {
+  mockClipboardApi()
 
   const { getByText } = render(
     <CopyToClipboardButton textToCopy="exercism download --track=ruby --exercise=bob" />
   )
 
   fireEvent.click(getByText('Copy'))
+
+  await waitFor(() =>
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+      'exercism download --track=ruby --exercise=bob'
+    )
+  )
+})
+
+test('copies text to clipboard when clicking', async () => {
+  mockClipboardApi()
+
+  const { queryByRole } = render(
+    <CopyToClipboardButton textToCopy="exercism download --track=ruby --exercise=bob" />
+  )
+
+  const copyButton = queryByRole('button')
+  copyButton.focus()
+
+  fireEvent.keyDown(copyButton, { code: 'KeyC', ctrlKey: true })
 
   await waitFor(() =>
     expect(navigator.clipboard.writeText).toHaveBeenCalledWith(

241a8ce8fbc3f88f8890bf1c1cf77199105d1b08
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-03 09:25:19 +0100

    Update tooltip icon (#243)
    * Don't render clipboard button if navigator is not set
    
    * Use useCallback on click event
    
    * Add button type
    
    * Add aria-label to copy button
    
    * Add support for reacting to keypresses
    
    * Add icon for clipboard

--- app/javascript/components/common/CopyToClipboardButton.tsx
+++ app/javascript/components/common/CopyToClipboardButton.tsx
@@ -1,12 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
+
+const KEY_NAMES = Object.freeze({
+  SPACE: ' ',
+})
+
+const KEY_NAMES_LEGACY = Object.freeze({
+  SPACE: 'Space',
+})
 
 export function CopyToClipboardButton({ textToCopy }: { textToCopy: string }) {
+  if (navigator.clipboard === undefined) {
+    return null
+  }
+
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
   const [justCopied, setJustCopied] = useState(false)
 
-  const onClick = async () => {
+  const copyTextToClipboard = async () => {
     await navigator.clipboard.writeText(textToCopy)
     setJustCopied(true)
   }
+
+  const onKeyPress = useCallback(
+    async (e: KeyboardEvent) => {
+      if ((e.code === 'KeyC' || e.key === 'c') && (e.ctrlKey || e.metaKey)) {
+        e.preventDefault()
+        await copyTextToClipboard()
+        return
+      }
+
+      if (e.key === KEY_NAMES_LEGACY.SPACE || e.key === KEY_NAMES.SPACE) {
+        e.preventDefault()
+        buttonRef.current?.click()
+        return
+      }
+    },
+
+    [textToCopy, setJustCopied]
+  )
+
+  const onClick = useCallback(
+    async (e) => {
+      e.preventDefault()
+      await copyTextToClipboard()
+    },
+    [textToCopy, setJustCopied]
+  )
+
+  const onFocus = useCallback(
+    () => buttonRef.current?.addEventListener('keydown', onKeyPress),
+    [textToCopy, setJustCopied]
+  )
+
+  const onBlur = useCallback(
+    () => buttonRef.current?.removeEventListener('keydown', onKeyPress),
+    [textToCopy, setJustCopied]
+  )
 
   useEffect(() => {
     if (!justCopied) {
@@ -20,8 +69,13 @@ export function CopyToClipboardButton({ textToCopy }: { textToCopy: string }) {
 
   return (
     <button
+      ref={buttonRef}
+      type="button"
       onClick={onClick}
+      onFocus={onFocus}
+      onBlur={onBlur}
       className={`c-copy-to-clipboard-button ${justCopied ? 'copied' : ''}`}
+      aria-label={`Copy ${textToCopy} to the cliboard`}
     >
       {justCopied ? 'Copied' : 'Copy'}
     </button>

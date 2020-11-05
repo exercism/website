dcc68fa247231f26b883dd541d4e312b6ea0fc28
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-05 10:59:28 +0100

    Update app/javascript/components/common/GraphicalIcon.tsx
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- app/javascript/components/common/GraphicalIcon.tsx
+++ app/javascript/components/common/GraphicalIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function GraphicalIcon({ icon }: { icon: string }) {
   return (
-    <svg className="c-graphical-icon" role="img">
+    <svg className="c-graphical-icon" role="presentation">
       <use xlinkHref={`#${icon}`} />
     </svg>
   )

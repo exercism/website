52dfa1bc53328f7169dfaf06073ab5207c1b9702
Jeremy Walker <jez.walker@gmail.com>
2020-11-06 17:43:11 +0000

    Bring UserSummary in line with -- standards

--- app/javascript/components/tooltips/UserSummary.tsx
+++ app/javascript/components/tooltips/UserSummary.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRequestQuery } from '../../hooks/request-query'
+import { Icon } from '../common/Icon'
 import { Loading } from '../common/Loading'
 
 type UserSummaryData = {
@@ -37,36 +38,33 @@ export function UserSummary({
       {isSuccess &&
         (data === undefined ? null : (
           <div>
-            <header>
+            <header className="--header">
               {/* TODO: Use rounded_bg_img helper */}
               <div
                 className="c-rounded-bg-img"
                 style={{ backgroundImage: `url(${data.avatarUrl})` }}
                 aria-label={`avatar for ${data.handle}`}
               />
-              <div className="identifier">
-                <h4>{data.name}</h4>
-                <div className="handle">@{data.handle}</div>
+              <div className="--identifier">
+                <h4 className="--name">{data.name}</h4>
+                <div className="--handle">@{data.handle}</div>
               </div>
               {/* TODO: Use reputation helper */}
               <div
                 className="c-reputation"
                 aria-label={`${data.reputation.total} reputation`}
               >
-                <svg role="img" className="icon ">
+                <svg role="img" className="--icon ">
                   <title>Reputation</title>
                   <use href="#reputation"></use>
                 </svg>
                 {data.reputation.total}
               </div>
             </header>
-            {data.bio && <div className="bio">{data.bio}</div>}
+            {data.bio && <div className="--bio">{data.bio}</div>}
             {data.location && (
-              <div className="location">
-                {/* TODO: Use icon helper */}
-                <svg role="img" className="icon" aria-label="Located in">
-                  <use href="#location"></use>
-                </svg>
+              <div className="--location">
+                <Icon icon="location" alt="Located in" />
                 {data.location}
               </div>
             )}

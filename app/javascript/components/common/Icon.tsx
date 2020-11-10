9b5de964f14f10851b673b914ea3d74b7b627018
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-10 16:08:48 +0100

    Allow passing class name to Icon and GraphicalIcon components (#261)
    * Allow passing class name to Icon and GraphicalIcon components
    
    * Don't render classname if not needed

--- app/javascript/components/common/Icon.tsx
+++ app/javascript/components/common/Icon.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-export function Icon({ icon, alt }: { icon: string; alt: string }) {
+export function Icon({
+  icon,
+  alt,
+  className,
+}: {
+  icon: string
+  alt: string
+  className?: string
+}) {
+  let classNames = ['c-icon']
+  if (className !== undefined) {
+    classNames.push(className)
+  }
+
   return (
-    <svg className="c-icon" role="img">
+    <svg className={classNames.join(' ')} role="img">
       <title>{alt}</title>
       <use xlinkHref={`#${icon}`} />
     </svg>

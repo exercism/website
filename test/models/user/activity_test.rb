3b336714d48bc6585a62e95f3b37e1481fc76885
Jeremy Walker <jez.walker@gmail.com>
2020-11-11 02:32:54 +0000

    Fix sporadically failing test

--- test/models/user/activity_test.rb
+++ test/models/user/activity_test.rb
@@ -52,34 +52,36 @@ class User::ActivityTest < ActiveSupport::TestCase
   end
 
   test "rendering data rebuilds if cache is empty" do
-    user = create :user
-    exercise = create(:concept_exercise)
+    freeze_time do
+      user = create :user
+      exercise = create(:concept_exercise)
 
-    activity = User::Activities::StartedExerciseActivity.create!(
-      user: user,
-      track: exercise.track,
-      params: {
-        exercise: exercise
+      activity = User::Activities::StartedExerciseActivity.create!(
+        user: user,
+        track: exercise.track,
+        params: {
+          exercise: exercise
+        }
+      )
+      activity.update!(rendering_data_cache: {})
+
+      # Reload it to check nothing is memoized
+      activity = User::Activity.last
+      assert_equal({}, activity.rendering_data_cache)
+      assert_equal exercise.title, activity.rendering_data.exercise_title
+      assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
+      assert_equal "/tracks/csharp/exercises/datetime", activity.rendering_data.url
+      assert_equal "You started a new exercise", activity.rendering_data.text
+      assert_equal Time.current, activity.rendering_data.occurred_at
+
+      activity = User::Activity.last
+      cache_data = {
+        'exercise_icon_name' => exercise.icon_name,
+        'exercise_title' => exercise.title,
+        'url' => "/tracks/csharp/exercises/datetime",
+        'text' => "You started a new exercise"
       }
-    )
-    activity.update!(rendering_data_cache: {})
-
-    # Reload it to check nothing is memoized
-    activity = User::Activity.last
-    assert_equal({}, activity.rendering_data_cache)
-    assert_equal exercise.title, activity.rendering_data.exercise_title
-    assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
-    assert_equal "/tracks/csharp/exercises/datetime", activity.rendering_data.url
-    assert_equal "You started a new exercise", activity.rendering_data.text
-    assert_equal Time.current, activity.rendering_data.occurred_at
-
-    activity = User::Activity.last
-    cache_data = {
-      'exercise_icon_name' => exercise.icon_name,
-      'exercise_title' => exercise.title,
-      'url' => "/tracks/csharp/exercises/datetime",
-      'text' => "You started a new exercise"
-    }
-    assert_equal cache_data, activity.rendering_data_cache
+      assert_equal cache_data, activity.rendering_data_cache
+    end
   end
 end

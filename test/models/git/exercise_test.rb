fe116cc032d8c182fa4f440caa752a16dde8f296
Jeremy Walker <jez.walker@gmail.com>
2020-11-12 13:59:52 +0000

    Fix namespacing issue that sometimes breaks tests

--- test/models/git/exercise_test.rb
+++ test/models/git/exercise_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 module Git
   class ExerciseTest < Minitest::Test
     def test_editor_solution_files
-      exercise = Exercise.new(:ruby, :bob, "HEAD", "practice")
+      exercise = Git::Exercise.new(:ruby, :bob, "HEAD", "practice")
 
       expected_files = ["bob.rb"]
       assert_equal expected_files, exercise.editor_solution_files.keys
@@ -11,20 +11,20 @@ module Git
     end
 
     def test_read_file_blob
-      exercise = Exercise.new(:ruby, :bob, "HEAD", "practice")
+      exercise = Git::Exercise.new(:ruby, :bob, "HEAD", "practice")
 
       assert_equal "stub content\n", exercise.read_file_blob('bob.rb')
     end
 
     def test_non_ignored_files
-      exercise = Exercise.new(:csharp, :datetime, "HEAD", "concept")
+      exercise = Git::Exercise.new(:csharp, :datetime, "HEAD", "concept")
 
       assert_equal exercise.non_ignored_filepaths, exercise.non_ignored_files.keys
       assert exercise.non_ignored_files[".docs/hints.md"].start_with?("## General")
     end
 
     def test_non_ignored_filepaths
-      exercise = Exercise.new(:csharp, :datetime, "HEAD", "concept")
+      exercise = Git::Exercise.new(:csharp, :datetime, "HEAD", "concept")
 
       expected_filepaths = [
         ".docs/hints.md",

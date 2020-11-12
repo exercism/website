90c50e35f8523a035d7a7492a981a1b6ecaf51fc
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-12 13:18:07 +0100

    Convert UrlParams class to TS

--- app/javascript/utils/url-params.ts
+++ app/javascript/utils/url-params.ts
@@ -1,12 +1,18 @@
+type Params = {
+  [key: string]: any
+}
+
 export class UrlParams {
-  constructor(object) {
-    this.object = object || {}
+  private readonly params: Params
+
+  constructor(params: Params | undefined | null) {
+    this.params = params || {}
   }
 
   toString() {
-    return Object.keys(this.object)
+    return Object.keys(this.params)
       .map((key) => {
-        const value = this.object[key]
+        const value = this.params[key]
 
         // &foo=value
         if (!Array.isArray(value)) {

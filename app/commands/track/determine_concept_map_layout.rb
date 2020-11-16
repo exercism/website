15b4131ad01e1107c128d0dde6546d0329075d4c
Tim Austin <tim@neenjaw.com>
2020-11-15 19:11:03 -0600

    path to url change was: Exercism::Routes.tooltip_track_concept_path is now: Exercism::Routes.tooltip_track_concept_url

--- app/commands/track/determine_concept_map_layout.rb
+++ app/commands/track/determine_concept_map_layout.rb
@@ -32,7 +32,7 @@ class Track
             slug: concept.slug,
             name: concept.name,
             web_url: Exercism::Routes.track_concept_url(track, concept),
-            tooltip_url: Exercism::Routes.tooltip_track_concept_path(track, concept)
+            tooltip_url: Exercism::Routes.tooltip_track_concept_url(track, concept)
           }
         end
       end

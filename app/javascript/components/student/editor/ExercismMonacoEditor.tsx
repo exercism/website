b74e8dde029c6b93413154b1b74c5b411ed29ba2
Karlo Soriano <karlo@thalamus.ai>
2020-11-20 23:17:24 +0800

    Dont language servers for now

--- app/javascript/components/student/editor/ExercismMonacoEditor.tsx
+++ app/javascript/components/student/editor/ExercismMonacoEditor.tsx
@@ -44,57 +44,57 @@ export function ExercismMonacoEditor({
   value: string | null | undefined
   theme: string
 }) {
-  const languageServerUrl: string = useMemo(() => {
-    const languageServerHost = document.querySelector<HTMLMetaElement>(
-      'meta[name="language-server-url"]'
-    )?.content
+  // const languageServerUrl: string = useMemo(() => {
+  //   const languageServerHost = document.querySelector<HTMLMetaElement>(
+  //     'meta[name="language-server-url"]'
+  //   )?.content
 
-    if (!languageServerHost) {
-      throw 'Language server host not found'
-    }
+  //   if (!languageServerHost) {
+  //     throw 'Language server host not found'
+  //   }
 
-    return normalizeUrl(`${languageServerHost}/${language}/${uuidv4()}`)
-  }, [document, language, uuidv4])
+  //   return normalizeUrl(`${languageServerHost}/${language}/${uuidv4()}`)
+  // }, [document, language, uuidv4])
 
-  useEffect(() => {
-    const webSocket = new ReconnectingWebsocket(languageServerUrl, [], {
-      maxReconnectionDelay: 10000,
-      minReconnectionDelay: 1000,
-      reconnectionDelayGrowFactor: 1.3,
-      connectionTimeout: 10000,
-      maxRetries: Infinity,
-      debug: false,
-    })
+  // useEffect(() => {
+  //   const webSocket = new ReconnectingWebsocket(languageServerUrl, [], {
+  //     maxReconnectionDelay: 10000,
+  //     minReconnectionDelay: 1000,
+  //     reconnectionDelayGrowFactor: 1.3,
+  //     connectionTimeout: 10000,
+  //     maxRetries: Infinity,
+  //     debug: false,
+  //   })
 
-    listen({
-      webSocket,
-      onConnection: (connection) => {
-        const languageClient = new MonacoLanguageClient({
-          name: 'Language Client',
-          clientOptions: {
-            documentSelector: [language],
-            errorHandler: {
-              error: () => ErrorAction.Continue,
-              closed: () => CloseAction.DoNotRestart,
-            },
-          },
-          connectionProvider: {
-            get: (errorHandler, closeHandler) => {
-              return Promise.resolve(
-                createConnection(connection, errorHandler, closeHandler)
-              )
-            },
-          },
-        })
-        const disposable = languageClient.start()
-        connection.onClose(() => disposable.dispose())
-      },
-    })
+  //   listen({
+  //     webSocket,
+  //     onConnection: (connection) => {
+  //       const languageClient = new MonacoLanguageClient({
+  //         name: 'Language Client',
+  //         clientOptions: {
+  //           documentSelector: [language],
+  //           errorHandler: {
+  //             error: () => ErrorAction.Continue,
+  //             closed: () => CloseAction.DoNotRestart,
+  //           },
+  //         },
+  //         connectionProvider: {
+  //           get: (errorHandler, closeHandler) => {
+  //             return Promise.resolve(
+  //               createConnection(connection, errorHandler, closeHandler)
+  //             )
+  //           },
+  //         },
+  //       })
+  //       const disposable = languageClient.start()
+  //       connection.onClose(() => disposable.dispose())
+  //     },
+  //   })
 
-    return () => {
-      webSocket.close()
-    }
-  }, [languageServerUrl])
+  //   return () => {
+  //     webSocket.close()
+  //   }
+  // }, [languageServerUrl])
 
   const handleEditorDidMount = (
     editor: monacoEditor.editor.IStandaloneCodeEditor

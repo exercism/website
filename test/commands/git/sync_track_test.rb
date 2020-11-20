7ef1653ca0c76dc12c777228c27be719685eeab8
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-20 12:01:01 +0100

    Simplify track sync tests

--- test/commands/git/sync_track_test.rb
+++ test/commands/git/sync_track_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class Git::SyncTrackTest < ActiveSupport::TestCase
   test "no change when git SHA matches HEAD SHA" do
-    track = create :track, slug: 'fsharp', active: true, git_sha: "HEAD"
+    track = create :track, slug: 'fsharp', git_sha: "HEAD"
 
     Git::SyncTrack.(track)
 
@@ -10,7 +10,7 @@ class Git::SyncTrackTest < ActiveSupport::TestCase
   end
 
   test "no change when there are no track changes in the diff" do
-    track = create :track, slug: 'fsharp', active: true, git_sha: "765f921ce85917cfe22b2a608d370f67da57820b"
+    track = create :track, slug: 'fsharp', git_sha: "765f921ce85917cfe22b2a608d370f67da57820b"
 
     Git::SyncTrack.(track)
 
@@ -18,11 +18,7 @@ class Git::SyncTrackTest < ActiveSupport::TestCase
   end
 
   test "git SHA changes to HEAD SHA when there are changes" do
-    track = create :track, slug: "fsharp",
-                           title: "F#!",
-                           active: false,
-                           blurb: "F# is a strongly-typed, functional language that is part of Microsoft's .NET language stack. F# can elegantly handle almost every problem you throw at it.", # rubocop:disable Layout/LineLength
-                           git_sha: "f290a29144b93b21e2399cd532b22562d83b6a52"
+    track = create :track, slug: 'fsharp', active: false, git_sha: "f290a29144b93b21e2399cd532b22562d83b6a52"
 
     Git::SyncTrack.(track)
 

49766b9186a2aa0470e746d8b02b1e7571120797
Karlo Soriano <karlo@thalamus.ai>
2020-11-20 18:18:38 +0800

    Revert to simpler localstorage hook

--- app/javascript/utils/use-storage.ts
+++ app/javascript/utils/use-storage.ts
@@ -1,8 +1,34 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { StoredMemoryValue, useMutableMemoryValue } from 'use-memory-value'
 
 export function useStorage<T>(key: string, initialValue: T) {
   const memoryValue = new StoredMemoryValue<T>(key, true, initialValue)
 
   return useMutableMemoryValue(memoryValue)
+}
+
+export function useLocalStorage(key: string, initialValue: string) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key)
+
+      return item || initialValue
+    } catch (error) {
+      console.log(error)
+
+      return initialValue
+    }
+  })
+
+  const setValue = (value: string) => {
+    try {
+      setStoredValue(value)
+
+      localStorage.setItem(key, value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return [storedValue, setValue]
 }

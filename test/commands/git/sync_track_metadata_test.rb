db9dd2280b3e5bdab03bf93f188174bb5ef6d203
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-24 14:00:18 +0100

    Rename track syncing

--- test/commands/git/sync_track_metadata_test.rb
+++ test/commands/git/sync_track_metadata_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
-class Git::SyncTrackTest < ActiveSupport::TestCase
+class Git::SyncTrackMetadataMetadataTest < ActiveSupport::TestCase
   test "no change when git sync SHA matches HEAD SHA" do
     track = create :track, slug: 'fsharp', synced_to_git_sha: "HEAD"
 
-    Git::SyncTrack.(track)
+    Git::SyncTrackMetadata.(track)
 
     refute track.changed?
   end
@@ -12,7 +12,7 @@ class Git::SyncTrackTest < ActiveSupport::TestCase
   test "git sync SHA changes to HEAD SHA when there are no changes" do
     track = create :track, slug: 'fsharp', synced_to_git_sha: "765f921ce85917cfe22b2a608d370f67da57820b"
 
-    Git::SyncTrack.(track)
+    Git::SyncTrackMetadata.(track)
 
     assert_equal track.git.head_sha, track.synced_to_git_sha
   end
@@ -20,7 +20,7 @@ class Git::SyncTrackTest < ActiveSupport::TestCase
   test "git sync SHA changes to HEAD SHA when there are changes" do
     track = create :track, slug: 'fsharp', active: false, synced_to_git_sha: "f290a29144b93b21e2399cd532b22562d83b6a52"
 
-    Git::SyncTrack.(track)
+    Git::SyncTrackMetadata.(track)
 
     assert_equal track.git.head_sha, track.synced_to_git_sha
   end
@@ -32,7 +32,7 @@ class Git::SyncTrackTest < ActiveSupport::TestCase
                            blurb: "F# is a strongly-typed, functional language that is part of Microsoft's .NET language stack. F# can elegantly handle almost every problem you throw at it.", # rubocop:disable Layout/LineLength
                            synced_to_git_sha: "f290a29144b93b21e2399cd532b22562d83b6a52"
 
-    Git::SyncTrack.(track)
+    Git::SyncTrackMetadata.(track)
 
     assert_equal "F#", track.title
     assert track.active

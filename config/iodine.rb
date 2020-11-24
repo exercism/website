22002de5265964bac5f6a577d2af8efa238b4406
Jeremy Walker <jez.walker@gmail.com>
2020-11-24 11:55:40 +0000

    Revert iodine and rack-attack (#302)

--- config/iodine.rb
+++
@@ -1,5 +0,0 @@
-if defined?(Iodine)
-  Iodine.threads = ENV.fetch("RAILS_MAX_THREADS", 5).to_i if Iodine.threads.zero?
-  Iodine.workers = ENV.fetch("WEB_CONCURRENCY", 2).to_i if Iodine.workers.zero?
-  Iodine::DEFAULT_SETTINGS[:port] ||= ENV.fetch("PORT") if ENV.fetch("PORT")
-end

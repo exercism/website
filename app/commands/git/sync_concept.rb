8976ba0ecad3495e0e36e1052f33955c4ea6c2ea
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-24 13:39:24 +0100

    Work on adding tests for git synced SHA for concept

--- app/commands/git/sync_concept.rb
+++ app/commands/git/sync_concept.rb
@@ -4,28 +4,48 @@ module Git
     initialize_with :concept
 
     def call
-      lookup_head_commit
-      update_concept! if concept_modified?
+      lookup_head_and_current_commit
+      update_concept! unless concept_synced_to_head?
     end
 
     private
-    attr_reader :head_commit
+    attr_reader :current_commit, :head_commit
 
-    def lookup_head_commit
+    def lookup_head_and_current_commit
       concept.git.update!
+
+      @current_commit = concept.git.lookup_commit(concept.synced_to_git_sha)
       @head_commit = concept.git.head_commit
     end
 
+    def concept_synced_to_head?
+      current_commit.oid == head_commit.oid
+    end
+
     def update_concept!
+      return concept.update!(synced_to_git_sha: head_commit.oid) unless concept_modified?
+
       concept.update!(
         slug: config_concept[:slug],
-        name: config_concept[:name]
+        name: config_concept[:name],
+        synced_to_git_sha: head_commit.oid
       )
     end
 
     def concept_modified?
+      return false unless track_config_modified?
+
       config_concept[:slug] != concept.slug ||
         config_concept[:name] != concept.name
+    end
+
+    def track_config_modified?
+      return false if current_commit.oid == head_commit.oid
+
+      diff.each_delta.any? do |delta|
+        delta.old_file[:path] == concept.track.git.config_filepath ||
+          delta.new_file[:path] == concept.track.git.config_filepath
+      end
     end
 
     memoize
@@ -33,6 +53,11 @@ module Git
       # TODO: determine what to do when the concept could not be found
       head_config = concept.track.git.config(commit: head_commit)
       head_config[:concepts].find { |e| e[:uuid] == concept.uuid }
+    end
+
+    memoize
+    def diff
+      head_commit.diff(current_commit)
     end
   end
 end

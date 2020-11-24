2129ba83fec97adecda6c2d0d45b8c79dfe22215
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-24 12:54:23 +0100

    Add synced_to_git_sha field to concept

--- test/factories/track/concepts.rb
+++ test/factories/track/concepts.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     slug { "concept_#{SecureRandom.hex(4)}" }
     name { slug.titleize }
     blurb { "A cracking concept" }
+    synced_to_git_sha { "HEAD" }
 
     trait :with_git_data do
       track { create(:track, slug: "csharp") }

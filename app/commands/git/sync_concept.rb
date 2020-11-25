08ec2d107fa3859c304f78d4e2fc40df00ffee02
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-25 11:17:33 +0100

    Add concept config.json tests

--- app/commands/git/sync_concept.rb
+++ app/commands/git/sync_concept.rb
@@ -46,8 +46,8 @@ module Git
 
       diff = head_git_concept.commit.diff(synced_git_concept.commit)
       diff.each_delta.any? do |delta|
-        delta.old_file[:path] == git_track.config_filepath ||
-          delta.new_file[:path] == git_track.config_filepath
+        delta.old_file[:path] == head_git_track.config_filepath ||
+          delta.new_file[:path] == head_git_track.config_filepath
       end
     end
 

4c8f5584755ca2b34019d7d3f54c943c39eb7ed1
Karlo Soriano <karlo@thalamus.ai>
2020-11-26 13:30:45 +0800

    Handle cases where OAuth login matches an existing user's email

--- test/commands/user/authenticate_from_omniauth_test.rb
+++ test/commands/user/authenticate_from_omniauth_test.rb
@@ -10,4 +10,38 @@ class User::AuthenticateFromOmniauthTest < ActiveSupport::TestCase
     user.reload
     assert_equal "user@exercism.io", user.email
   end
+
+  test "sets provider and uid for email matches" do
+    user = create :user, email: "user@exercism.io"
+    auth = stub(provider: "github", uid: "111", info: stub(email: "user@exercism.io"))
+
+    User::AuthenticateFromOmniauth.(auth)
+
+    user.reload
+    assert_equal "111", user.uid
+    assert_equal "github", user.provider
+  end
+
+  test "confirms user and changes password if email matches" do
+    SecureRandom.stubs(:uuid).returns("12345678")
+    user = create :user, email: "user@exercism.io", confirmed_at: nil
+    auth = stub(provider: "github", uid: "111", info: stub(email: "user@exercism.io"))
+
+    User::AuthenticateFromOmniauth.(auth)
+
+    user.reload
+    assert user.confirmed?
+    assert user.valid_password?("12345678")
+  end
+
+  test "does not change password if user confirmed" do
+    SecureRandom.stubs(:uuid).returns("12345678")
+    user = create :user, email: "user@exercism.io", confirmed_at: Date.new(2016, 12, 25)
+    auth = stub(provider: "github", uid: "111", info: stub(email: "user@exercism.io"))
+
+    User::AuthenticateFromOmniauth.(auth)
+
+    user.reload
+    refute user.valid_password?("12345678")
+  end
 end

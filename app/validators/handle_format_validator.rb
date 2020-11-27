02f62ba12930f2f25b5e5e7df1a2a7b0b7289f8f
Karlo Soriano <karlo@thalamus.ai>
2020-11-27 10:07:12 +0800

    Remove deprecation warnings

--- app/validators/handle_format_validator.rb
+++ app/validators/handle_format_validator.rb
@@ -2,6 +2,6 @@ class HandleFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.nil?
 
-    record.errors[attribute] << "must have only letters, numbers, or hyphens." unless /^[a-zA-Z0-9-]+$/.match?(value)
+    record.errors.add(attribute, "must have only letters, numbers, or hyphens.") unless /^[a-zA-Z0-9-]+$/.match?(value)
   end
 end

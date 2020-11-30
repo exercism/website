a8510d95c7dcc23ef4be45ed2b48dcf12352993c
Karlo Soriano <karlo@thalamus.ai>
2020-11-30 09:57:41 +0800

    Move migration into the original create user migration

--- db/migrate/20201127032403_add_accept_fields_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddAcceptFieldsToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :accepted_privacy_policy_at, :datetime
-    add_column :users, :accepted_terms_at, :datetime
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_032403) do
+ActiveRecord::Schema.define(version: 2020_11_09_170425) do
 
   create_table "badges", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -310,11 +310,11 @@ ActiveRecord::Schema.define(version: 2020_11_27_032403) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.datetime "accepted_privacy_policy_at"
+    t.datetime "accepted_terms_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "featured_badge_id"
-    t.datetime "accepted_privacy_policy_at"
-    t.datetime "accepted_terms_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["featured_badge_id"], name: "index_users_on_featured_badge_id"

9bdbbf4329979cd809991e8babd105eb8c55cf5e
Jeremy Walker <jez.walker@gmail.com>
2020-11-30 23:13:58 +0000

    Add mentor dashboard page

--- app/javascript/components/mentoring/inbox/TrackFilter.jsx
+++ app/javascript/components/mentoring/inbox/TrackFilter.jsx
@@ -18,7 +18,6 @@ export function TrackFilter({ request, setTrack }) {
       {isError && <p>Something went wrong</p>}
       {isSuccess && (
         <>
-          <label htmlFor="track-filter-track">Track</label>
           <select id="track-filter-track" onChange={handleChange}>
             <option value={''}>All</option>
             {data.map((track) => {

b1023389fad747cf5ec35885e51bbdfee874d60a
Jeremy Walker <jez.walker@gmail.com>
2020-12-01 12:13:11 +0000

    Fix flakey test

--- test/models/user/activities/submitted_iteration_activity_test.rb
+++ test/models/user/activities/submitted_iteration_activity_test.rb
@@ -23,24 +23,26 @@ class User::Activities::SubmittedIterationActivityTest < ActiveSupport::TestCase
   end
 
   test "rendering_data is valid" do
-    user = create :user
-    exercise = create :concept_exercise
-    solution = create :concept_solution, user: user, exercise: exercise
-    iteration = create :iteration, solution: solution
+    freeze_time do
+      user = create :user
+      exercise = create :concept_exercise
+      solution = create :concept_solution, user: user, exercise: exercise
+      iteration = create :iteration, solution: solution
 
-    activity = User::Activities::SubmittedIterationActivity.create!(
-      user: user,
-      track: exercise.track,
-      params: {
-        iteration: iteration,
-        exercise: exercise
-      }
-    )
+      activity = User::Activities::SubmittedIterationActivity.create!(
+        user: user,
+        track: exercise.track,
+        params: {
+          iteration: iteration,
+          exercise: exercise
+        }
+      )
 
-    assert_equal exercise.title, activity.rendering_data.exercise_title
-    assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
-    assert_equal "/tracks/csharp/exercises/datetime/iterations?idx=0", activity.rendering_data.url
-    assert_equal "You submitted an iteration to", activity.rendering_data.text
-    assert_equal Time.current, activity.rendering_data.occurred_at
+      assert_equal exercise.title, activity.rendering_data.exercise_title
+      assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
+      assert_equal "/tracks/csharp/exercises/datetime/iterations?idx=0", activity.rendering_data.url
+      assert_equal "You submitted an iteration to", activity.rendering_data.text
+      assert_equal Time.current, activity.rendering_data.occurred_at
+    end
   end
 end

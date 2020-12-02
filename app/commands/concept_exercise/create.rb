65eb0747081446edf7c9253e24c5aa223eee3712
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-02 13:37:38 +0100

    Remove unused namespacing

--- app/commands/concept_exercise/create.rb
+++ app/commands/concept_exercise/create.rb
@@ -5,7 +5,7 @@ class ConceptExercise
     initialize_with :uuid, :track, :attributes
 
     def call
-      ::ConceptExercise.create_or_find_by!(uuid: uuid, track: track) do |ce|
+      ConceptExercise.create_or_find_by!(uuid: uuid, track: track) do |ce|
         ce.attributes = attributes
       end
     end

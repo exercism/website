01b8e854021fc9eb186c52be0abac2a6e5dda2b5
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-02 12:32:10 +0100

    Introduce attributes parameter for create command

--- app/commands/concept_exercise/create.rb
+++ app/commands/concept_exercise/create.rb
@@ -2,17 +2,11 @@ class ConceptExercise
   class Create
     include Mandate
 
-    initialize_with :uuid, :slug, :title, :taught_concepts, :prerequisites, :deprecated, :git_sha, :synced_to_git_sha, :track
+    initialize_with :uuid, :track, :attributes
 
     def call
-      ::ConceptExercise.create_or_find_by!(uuid: uuid, track: track) do |e|
-        e.slug = slug
-        e.title = title
-        e.taught_concepts = taught_concepts
-        e.prerequisites = prerequisites
-        e.deprecated = deprecated
-        e.git_sha = git_sha
-        e.synced_to_git_sha = synced_to_git_sha
+      ::ConceptExercise.create_or_find_by!(uuid: uuid, track: track) do |ce|
+        ce.attributes = attributes
       end
     end
   end
--- app/commands/practice_exercise/create.rb
+++ app/commands/practice_exercise/create.rb
@@ -2,16 +2,13 @@ class PracticeExercise
   class Create
     include Mandate
 
-    initialize_with :uuid, :slug, :title, :prerequisites, :deprecated, :git_sha, :synced_to_git_sha, :track
+    # :slug, :title, :prerequisites, :deprecated, :git_sha, :synced_to_git_sha,
+
+    initialize_with :uuid, :track, :attributes
 
     def call
-      ::PracticeExercise.create_or_find_by!(uuid: uuid, track: track) do |e|
-        e.slug = slug
-        e.title = title
-        e.prerequisites = prerequisites
-        e.deprecated = deprecated
-        e.git_sha = git_sha
-        e.synced_to_git_sha = synced_to_git_sha
+      PracticeExercise.create_or_find_by!(uuid: uuid, track: track) do |pe|
+        pe.attributes = attributes
       end
     end
   end

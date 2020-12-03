388933ba7738aa8aededda7a781223572c16b1ed
Jeremy Walker <jez.walker@gmail.com>
2020-12-03 17:47:16 +0000

    Set secrets before initializing app

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
-
 # Set secrets from Exercism secrets
 Rails.application.secrets.secret_key_base =
   Exercism.secrets.website_secret_key_base
+
+# Initialize the Rails application.
+Rails.application.initialize!

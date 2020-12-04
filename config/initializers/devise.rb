01021e76219d5376885b587a3d29ebff2ce0f36d
Jeremy Walker <jez.walker@gmail.com>
2020-12-04 12:32:50 +0000

    Configure Devise correctly

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,10 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, "APP_ID", "APP_SECRET", scope: "user:email"
+  config.omniauth :github,
+    Exercism.secrets.github_omniauth_app_id,
+    Exercism.secrets.github_omniauth_app_secret,
+    scope: "user:email"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or

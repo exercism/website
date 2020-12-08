8a5bbcac773b2dfd06ddd7c6af234b87f5f4b823
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-08 14:56:31 +0100

    Inline uuid

--- test/controllers/webhooks/base_test_case.rb
+++ test/controllers/webhooks/base_test_case.rb
@@ -3,14 +3,15 @@ require 'test_helper'
 module Webhooks
   class BaseTestCase < ActionDispatch::IntegrationTest
     def headers(payload)
-      delivery = SecureRandom.uuid
-      signature = 'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
-        Exercism.secrets.github_webhooks_secret, payload.to_json)
-
       @headers = {
-        'HTTP_X_GITHUB_DELIVERY' => delivery,
-        'HTTP_X_HUB_SIGNATURE_256' => signature
+        'HTTP_X_GITHUB_DELIVERY' => SecureRandom.uuid,
+        'HTTP_X_HUB_SIGNATURE_256' => signature(payload)
       }
+    end
+
+    def signature(payload)
+      'sha256=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
+        Exercism.secrets.github_webhooks_secret, payload.to_json)
     end
   end
 end

6f0dc22d9718b2cf1adb74565a848ee1ebdd0317
Jeremy Walker <jez.walker@gmail.com>
2020-12-09 00:31:09 +0000

    Fix flakey test

--- test/models/user/activities/completed_exercise_activity_test.rb
+++ test/models/user/activities/completed_exercise_activity_test.rb
@@ -18,18 +18,20 @@ class User::Activities::CompletedExerciseActivityTest < ActiveSupport::TestCase
   end
 
   test "rendering_data is valid" do
-    user = create :user
-    exercise = create(:concept_exercise)
+    freeze_time do
+      user = create :user
+      exercise = create(:concept_exercise)
 
-    activity = User::Activities::CompletedExerciseActivity.create!(
-      user: user,
-      track: exercise.track,
-      params: { exercise: exercise }
-    )
-    assert_equal exercise.title, activity.rendering_data.exercise_title
-    assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
-    assert_equal "/tracks/csharp/exercises/datetime", activity.rendering_data.url
-    assert_equal "You completed an exercise", activity.rendering_data.text
-    assert_equal Time.current, activity.rendering_data.occurred_at
+      activity = User::Activities::CompletedExerciseActivity.create!(
+        user: user,
+        track: exercise.track,
+        params: { exercise: exercise }
+      )
+      assert_equal exercise.title, activity.rendering_data.exercise_title
+      assert_equal exercise.icon_name, activity.rendering_data.exercise_icon_name
+      assert_equal "/tracks/csharp/exercises/datetime", activity.rendering_data.url
+      assert_equal "You completed an exercise", activity.rendering_data.text
+      assert_equal Time.current, activity.rendering_data.occurred_at
+    end
   end
 end

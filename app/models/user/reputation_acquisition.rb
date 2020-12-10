6ddc53c1e3198841802fcd1d310d5bf60e316800
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-10 08:52:13 +0100

    Move validates above before_create

--- app/models/user/reputation_acquisition.rb
+++ app/models/user/reputation_acquisition.rb
@@ -2,15 +2,15 @@ class User::ReputationAcquisition < ApplicationRecord
   belongs_to :user
   belongs_to :reason_object, polymorphic: true, optional: true
 
-  before_create do
-    self.amount = REASON_AMOUNTS[self.reason]
-  end
-
   validates :reason, inclusion: {
     in: %i[exercise_authorship exercise_contributorship],
     message: "%<value>s is not a valid reason",
     strict: ReputationAcquisitionReasonInvalid
   }
+
+  before_create do
+    self.amount = REASON_AMOUNTS[self.reason]
+  end
 
   def reason
     super.to_sym

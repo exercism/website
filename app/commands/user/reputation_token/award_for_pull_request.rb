4ed6048d86cab0c5658898ae5244ee3a3bb14494
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-11 16:33:42 +0100

    Add test for awarding code contribution token based on PR

--- app/commands/user/reputation_token/award_for_pull_request.rb
+++ app/commands/user/reputation_token/award_for_pull_request.rb
@@ -6,16 +6,14 @@ class User
       initialize_with :action, :author, :url, :html_url
 
       def call
-        # TODO
-        # user = User.find_by(github_username: commit.author[:email])
+        user = User.find_by(github_username: author)
 
         # # TODO: decide what to do with user that cannot be found
         # next unless user
 
         # https://api.github.com/repos/exercism/v3/pulls/2731/reviews
 
-        # commit_link = "#{track.repo_url}/commit/#{commit.oid}"
-        # User::ReputationToken::CodeContribution::Create.(user, commit_link)
+        User::ReputationToken::CodeContribution::Create.(user, html_url)
       end
     end
   end
--- test/commands/user/reputation_token/award_for_pull_request_test.rb
+++ test/commands/user/reputation_token/award_for_pull_request_test.rb
@@ -2,19 +2,14 @@ require "test_helper"
 
 class User::ReputationToken::AwardForPullRequestTest < ActiveSupport::TestCase
   test "adds code contributions for commits made after synced git SHA" do
-    skip
+    action = 'closed'
+    login = 'user22'
+    url = 'https://api.github.com/repos/exercism/v3/pulls/1347'
+    html_url = 'https://github.com/exercism/v3/pull/1347'
+    user = create :user, handle: "User22", github_username: "user22"
 
-    track = create :track, slug: 'fsharp', synced_to_git_sha: '55494f90e0d0f4b8eddeab2d8883da38483c4984'
-    first_user = create :user, handle: "ErikSchierboom", github_username: "erik@schierboom.org"
-    second_user = create :user, handle: "SleeplessByte", github_username: "sleepless@byte.org"
+    User::ReputationToken::AwardForPullRequest.(action, login, url, html_url)
 
-    User::ReputationToken::AwardForPullRequest.(track)
-
-    assert first_user.reputation_tokens.find_by(reason: :committed_code,
-                                                external_link: "#{track.repo_url}/commit/d34f921e3249f6494026835c1191fe2608f687a9").present? # rubocop:disable Layout/LineLength
-    assert first_user.reputation_tokens.find_by(reason: :committed_code,
-                                                external_link: "#{track.repo_url}/commit/4afa682a5e45dc4e93d9fe4a7ac3b1b8cdbe1ba8").present? # rubocop:disable Layout/LineLength
-    assert second_user.reputation_tokens.find_by(reason: :committed_code,
-                                                 external_link: "#{track.repo_url}/commit/300f657115eb82e7a6c8c73e351c19b08de3d077").present? # rubocop:disable Layout/LineLength
+    assert user.reputation_tokens.where(reason: :committed_code, external_link: 'https://github.com/exercism/v3/pull/1347').exists?
   end
 end

1617cf8b33a09d3cc4b6daf03bf996e264fdae8d
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-15 15:25:45 +0100

    Add tests for reputation token create commands

--- app/commands/user/reputation_token/exercise_authorship/create.rb
+++ app/commands/user/reputation_token/exercise_authorship/create.rb
@@ -9,11 +9,12 @@ class User
         def call
           User::ReputationToken.create_or_find_by!(
             user: authorship.author,
-            context: authorship,
-            context_key: "authored_exercise/#{authorship.exercise.uuid}",
-            reason: 'authored_exercise',
-            category: :authoring
-          )
+            context_key: "authored_exercise/#{authorship.exercise.uuid}"
+          ) do |rt|
+            rt.context = authorship
+            rt.reason = 'authored_exercise'
+            rt.category = :authoring
+          end
         end
       end
     end
--- app/commands/user/reputation_token/exercise_contributorship/create.rb
+++ app/commands/user/reputation_token/exercise_contributorship/create.rb
@@ -9,11 +9,12 @@ class User
         def call
           User::ReputationToken.create_or_find_by!(
             user: contributorship.contributor,
-            context: contributorship,
-            context_key: "contributed_to_exercise/#{contributorship.exercise.uuid}",
-            reason: 'contributed_to_exercise',
-            category: :authoring
-          )
+            context_key: "contributed_to_exercise/#{contributorship.exercise.uuid}"
+          ) do |rt|
+            rt.context = contributorship
+            rt.reason = 'contributed_to_exercise'
+            rt.category = :authoring
+          end
         end
       end
     end

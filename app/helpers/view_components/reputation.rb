80603726a7964d403e4b2b56cf69217e81892af1
Jeremy Walker <jez.walker@gmail.com>
2020-12-15 18:44:12 +0000

    Split out two reputation types

--- app/helpers/view_components/reputation.rb
+++ app/helpers/view_components/reputation.rb
@@ -1,29 +1,12 @@
 module ViewComponents
   class Reputation < ViewComponent
-    def initialize(user, primary: false, has_notification: false)
-      @user = user
-      @primary = primary
-      @has_notification = has_notification
-    end
+    initialize_with :user
 
     def to_s
-      classes = ["c-reputation"]
-      classes << "--primary" if primary
-
-      link_to("#", class: classes.join(" "), 'aria-label': "#{@user.reputation} reputation") do
-        tags = [
-          tag.div(class: "--inner") do
-            icon(:reputation, "Reputation") +
-              tag.span(user.reputation)
-          end
-        ]
-        tags << tag.div('', class: "--notification") if has_notification
-
-        safe_join(tags)
+      tag.div(class: "c-reputation", 'aria-label': "#{@user.reputation} reputation") do
+        icon(:reputation, "Reputation") +
+          tag.span(user.reputation)
       end
     end
-
-    private
-    attr_reader :user, :primary, :has_notification
   end
 end

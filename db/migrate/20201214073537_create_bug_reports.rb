f925f212d537c3f59b26333054c48cedc200bb12
Karlo Soriano <karlo@thalamus.ai>
2020-12-16 06:13:29 +0800

    Update db/migrate/20201214073537_create_bug_reports.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- db/migrate/20201214073537_create_bug_reports.rb
+++ db/migrate/20201214073537_create_bug_reports.rb
@@ -1,7 +1,7 @@
 class CreateBugReports < ActiveRecord::Migration[6.1]
   def change
     create_table :bug_reports do |t|
-      t.belongs_to :user, null: false
+      t.belongs_to :user, null: false, foreign_key: true
       t.text :body, null: false
 
       t.timestamps

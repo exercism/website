d4a3b9337533c047e062a8296da41c13eb050033
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-15 13:50:42 +0100

    Add context_key to user_reputation_token factory

--- test/factories/user/reputation_tokens.rb
+++ test/factories/user/reputation_tokens.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user_reputation_token, class: 'User::ReputationToken' do
     user
     category { :authoring }
+    context_key { "authored_exercise/#{SecureRandom.hex(4)}" }
     reason { 'authored_exercise' }
   end
 end

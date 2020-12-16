9152b23f2c5c93e2cccfb8ac20097d1a44ea2415
Jeremy Walker <jez.walker@gmail.com>
2020-12-16 23:32:59 +0000

    Ensure user is logged in for system tests

--- test/system/components/maintaining/submissions_summary_table_test.rb
+++ test/system/components/maintaining/submissions_summary_table_test.rb
@@ -3,6 +3,8 @@ require "application_system_test_case"
 module Maintaining
   class SubmissionsSummaryTableTest < ApplicationSystemTestCase
     test "visiting the index" do
+      sign_in!
+
       visit test_components_maintaining_submissions_summary_table_url
       wait_for_websockets
 

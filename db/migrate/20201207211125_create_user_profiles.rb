5916779d8535b1b3f3755d62b514fdcdbb977246
Jeremy Walker <jez.walker@gmail.com>
2020-12-17 15:31:30 +0000

    Fix weird issue with migrations and seeds breaking together

--- db/migrate/20201207211125_create_user_profiles.rb
+++ db/migrate/20201207211125_create_user_profiles.rb
@@ -13,16 +13,5 @@ class CreateUserProfiles < ActiveRecord::Migration[6.1]
     end
     
     add_column :users, :bio, :text, null: true
-
-    User.update_all(bio: "I am a developer with a passion for learning new languages. I love programming. I've done all the languages. I like the good languages the best.")
-    User.first&.update(name: "Erik ShireBOOM")
-    User.first&.create_profile(
-      location: "Bree, Middle Earth",
-      github: "iHiD",
-      twitter: "iHiD",
-      linkedin: "iHiD",
-      medium: "iHiD",
-      website: "https://ihid.info"
-    )
   end
 end

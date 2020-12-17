4b7e1782dcfd40e971fca51a869909dc108ded5c
Jeremy Walker <jez.walker@gmail.com>
2020-12-17 00:24:11 +0000

    Set secret key base as an ENV var

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Set secrets from Exercism secrets
-Rails.application.secrets.secret_key_base =
-  Exercism.secrets.website_secret_key_base
+# Rails is very pinickity over this being set and really
+# really wants it to be an ENV var. So let's just give it
+# its way, even though this is pretty ugly.
+ENV["SECRET_KEY_BASE"] = Exercism.secrets.website_secret_key_base
 
 # Initialize the Rails application.
 Rails.application.initialize!

b4ae915730b0f406857eddb0cf09d24c4952a626
Karlo Soriano <karlo@thalamus.ai>
2020-12-23 07:36:04 +0800

    Rewrite test to assert more than one post is loaded

--- test/javascript/components/mentoring/DiscussionPostList.test.js
+++ test/javascript/components/mentoring/DiscussionPostList.test.js
@@ -1,43 +1,68 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import '@testing-library/jest-dom/extend-expect'
 import { DiscussionPostList } from '../../../../app/javascript/components/mentoring/DiscussionPostList'
 
 test('displays all posts', async () => {
+  const twoDaysAgo = new Date(new Date() - 1000 * 60 * 60 * 24 * 2)
+  const posts = [
+    {
+      id: 1,
+      author_handle: 'author',
+      author_avatar_url: 'http://exercism.test/image',
+      by_student: true,
+      content_html: '<p>Hello</p>',
+      updated_at: new Date().toISOString(),
+    },
+    {
+      id: 2,
+      author_handle: 'other-author',
+      author_avatar_url: 'http://exercism.test/other-image',
+      by_student: false,
+      content_html: '<p>Goodbye</p>',
+      updated_at: twoDaysAgo.toISOString(),
+    },
+  ]
   const server = setupServer(
     rest.get('https://exercism.test/posts', (req, res, ctx) => {
-      return res(
-        ctx.json([
-          {
-            id: 1,
-            author_handle: 'author',
-            author_avatar_url: 'http://exercism.test/image',
-            by_student: true,
-            content_html: '<p>Hello</p>',
-            updated_at: new Date().toISOString(),
-          },
-        ])
-      )
+      return res(ctx.json(posts))
     })
   )
   server.listen()
 
-  const { queryByText, getByTestId } = render(
-    <DiscussionPostList endpoint="https://exercism.test/posts" />
+  render(<DiscussionPostList endpoint="https://exercism.test/posts" />)
+
+  expect(
+    await screen.findByRole('status', {
+      name: 'Discussion post list loading indicator',
+    })
+  ).toBeInTheDocument()
+  waitForElementToBeRemoved(
+    screen.queryByRole('status', {
+      name: 'Discussion post list loading indicator',
+    })
   )
 
-  await waitFor(() => {
-    expect(getByTestId('author-avatar')).toHaveAttribute(
-      'src',
-      'http://exercism.test/image'
-    )
-    expect(queryByText('author')).toBeInTheDocument()
-    expect(queryByText('Student')).toBeInTheDocument()
-    expect(queryByText('Hello')).toBeInTheDocument()
-    expect(queryByText('a few seconds ago')).toBeInTheDocument()
-  })
+  expect(
+    await screen.findByRole('img', { name: 'Avatar of author' })
+  ).toHaveAttribute('src', 'http://exercism.test/image')
+  expect(await screen.findByText('author')).toBeInTheDocument()
+  expect(await screen.findByText('Student')).toBeInTheDocument()
+  expect(await screen.findByText('Hello')).toBeInTheDocument()
+  expect(await screen.findByText('a few seconds ago')).toBeInTheDocument()
+  expect(
+    await screen.findByRole('img', { name: 'Avatar of other-author' })
+  ).toHaveAttribute('src', 'http://exercism.test/other-image')
+  expect(await screen.findByText('other-author')).toBeInTheDocument()
+  expect(await screen.findByText('Student')).toBeInTheDocument()
+  expect(await screen.findByText('Goodbye')).toBeInTheDocument()
+  expect(await screen.findByText('2 days ago')).toBeInTheDocument()
 
   server.close()
 })

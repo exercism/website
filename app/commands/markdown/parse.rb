197eb14a447a01f11c256950a32d49f1e9e4b39c
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-22 16:20:25 +0100

    Remove unneeded rendered

--- app/commands/markdown/parse.rb
+++ app/commands/markdown/parse.rb
@@ -36,34 +36,4 @@ class Markdown::Parse
     doc = Markdown::RenderDoc.(preprocessed_text)
     Markdown::Render.(doc, nofollow_links)
   end
-
-  class Renderer < CommonMarker::HtmlRenderer
-    def initialize(options:, nofollow_links: false)
-      super(options: options)
-      @nofollow_links = nofollow_links
-    end
-
-    private
-    attr_reader :nofollow_links
-
-    def link(node)
-      out('<a href="', node.url.nil? ? '' : escape_href(node.url), '" target="_blank"')
-      out(' title="', escape_html(node.title), '"') if node.title.present?
-      out(' rel="nofollow"') if nofollow_links
-      out('>', :children, '</a>')
-    end
-
-    def code_block(node)
-      block do
-        out("<pre#{sourcepos(node)}><code")
-        if node.fence_info.present?
-          out(' class="language-', node.fence_info.split(/\s+/)[0], '">')
-        else
-          out(' class="language-plain">')
-        end
-        out(escape_html(node.string_content))
-        out('</code></pre>')
-      end
-    end
-  end
 end

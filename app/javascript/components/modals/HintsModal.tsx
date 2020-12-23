bb6ff60056d38aa360119211941fd8fa83166b7b
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-23 14:43:19 +0100

    Open general hints by default

--- app/javascript/components/modals/HintsModal.tsx
+++ app/javascript/components/modals/HintsModal.tsx
@@ -5,16 +5,20 @@ import { Modal } from './Modal'
 const Hints = ({
   heading,
   hints,
+  open,
 }: {
   heading: string
   hints: string[] | undefined
+  open?: boolean
 }) => {
   if (hints === undefined || hints.length === 0) {
     return null
   }
 
+  const props = open ? { open: true } : {}
+
   return (
-    <details className="c-details">
+    <details className="c-details" {...props}>
       <summary className="--summary">{heading}</summary>
       <ul>
         {hints.map((hint, idx) => (
@@ -37,7 +41,7 @@ export const HintsModal = ({
 }): JSX.Element => {
   return (
     <Modal open={open} onClose={onClose} {...props}>
-      <Hints hints={instructions.generalHints} heading="General" />
+      <Hints hints={instructions.generalHints} heading="General" open={true} />
       {instructions.tasks.map((task, idx) => (
         <Hints key={idx} hints={task.hints} heading={task.title} />
       ))}

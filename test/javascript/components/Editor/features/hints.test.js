c22c23f172300dedd44311379efade934ccab325
Erik Schierboom <erik_schierboom@hotmail.com>
2020-12-23 15:26:38 +0100

    Add hints test

---
+++ test/javascript/components/Editor/features/hints.test.js
@@ -0,0 +1,26 @@
+jest.mock('../../../../../app/javascript/components/editor/FileEditor')
+
+import React from 'react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { Editor } from '../../../../../app/javascript/components/Editor'
+
+test('show hints', async () => {
+  const { queryByText } = render(
+    <Editor
+      files={[{ filename: 'lasagna.rb', content: 'class Lasagna' }]}
+      instructions={{
+        overview: '',
+        generalHints: ['Please use the docs'],
+        tasks: [{ title: 'Do complex task', hints: ['Really you should!'] }],
+      }}
+    />
+  )
+
+  fireEvent.click(queryByText('Hints'))
+
+  await waitFor(() => expect(queryByText('General')).toBeInTheDocument())
+  await waitFor(() =>
+    expect(queryByText('Really you should!')).toBeInTheDocument()
+  )
+})

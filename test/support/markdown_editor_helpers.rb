0606857ced0bcde842aeb2975d1f2f509c8f1334
Karlo Soriano <karlo@thalamus.ai>
2021-01-04 08:02:10 +0800

    Add sleep to make sure markdown editor is loaded

--- test/support/markdown_editor_helpers.rb
+++ test/support/markdown_editor_helpers.rb
@@ -1,9 +1,11 @@
 module MarkdownEditorHelpers
   def fill_in_editor(text)
+    sleep(0.5)
     execute_script("document.querySelector('.CodeMirror').CodeMirror.setValue('#{text}')")
   end
 
   def assert_editor_text(expected)
+    sleep(0.5)
     actual = evaluate_script("document.querySelector('.CodeMirror').CodeMirror.getValue()")
 
     assert_equal expected, actual

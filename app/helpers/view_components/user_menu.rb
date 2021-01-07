3ff59fa3488430f3e2f78517b1f36feaf6b6338a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-07 11:47:22 +0100

    Add pre-rendered profile menu dropdown

--- app/helpers/view_components/user_menu.rb
+++ app/helpers/view_components/user_menu.rb
@@ -1,12 +1,24 @@
 module ViewComponents
   class UserMenu < ViewComponent
     def to_s
-      tag.div(class: "user-menu") do
+      tag.div(class: "user-menu", data: { dropdown_prerendered: profile_menu_dropdown }) do
         rounded_bg_img(
           "https://avatars3.githubusercontent.com/u/135246?s=460",
           "#{current_user.name}'s uploaded avatar"
         ) +
           icon("more-vertical", "Profile Menu")
+      end
+    end
+
+    private
+    def profile_menu_dropdown
+      tag.nav do
+        tag.ul do
+          tag.li("My Journey") +
+            tag.li("Profile") +
+            tag.li("Settings") +
+            tag.li("Sign out")
+        end
       end
     end
   end

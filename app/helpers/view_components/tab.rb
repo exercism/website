9eb6e3d420c7469889d36d63548ce79e28de6790
Jeremy Walker <jez.walker@gmail.com>
2021-01-07 14:05:34 +0000

    Add tabs to journey page

---
+++ app/helpers/view_components/tab.rb
@@ -0,0 +1,14 @@
+module ViewComponents
+  class Tab < ViewComponent
+    extend Mandate::Memoize
+
+    initialize_with :text, :icon, :href, :selected
+
+    def to_s
+      link_to href, class: "c-tab #{'selected' if selected}" do
+        graphical_icon(icon) +
+          tag.span(text, data: { text: text })
+      end
+    end
+  end
+end

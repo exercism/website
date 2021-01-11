637f915a9d69ddec9fd8f3b9d04c453fe616c9a4
Karlo Soriano <karlo@thalamus.ai>
2021-01-11 10:26:03 +0800

    Show API errors when unable to mark discussion as nothing to do

--- app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
+++ app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useMutation } from 'react-query'
 import { Loading } from '../../common'
+import {
+  ErrorBoundary,
+  useErrorHandler,
+  FallbackProps,
+} from 'react-error-boundary'
+import { APIError } from '../../types'
 
-export const MarkAsNothingToDoButton = ({
-  endpoint,
-}: {
+type ComponentProps = {
   endpoint: string
-}): JSX.Element | null => {
-  const [mutation, { status }] = useMutation(() => {
-    return fetch(endpoint, { method: 'PATCH' })
+}
+
+const ERROR_MESSAGE_TIMEOUT = 500
+
+export const MarkAsNothingToDoButton = (props: ComponentProps): JSX.Element => {
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Component {...props} />
+    </ErrorBoundary>
+  )
+}
+
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  useEffect(() => {
+    setTimeout(resetErrorBoundary, ERROR_MESSAGE_TIMEOUT)
+  }, [resetErrorBoundary])
+
+  return (
+    <div>
+      <p>{error.message}</p>
+    </div>
+  )
+}
+
+const Component = ({ endpoint }: ComponentProps): JSX.Element | null => {
+  const handleError = useErrorHandler()
+  const [mutation, { status, error }] = useMutation(() => {
+    return fetch(endpoint, { method: 'PATCH' }).then((response) => {
+      if (!response.ok) {
+        throw response
+      }
+    })
   })
+
+  useEffect(() => {
+    if (!error) {
+      return
+    }
+
+    if (error instanceof Error) {
+      handleError(new Error('Unable to mark discussion as nothing to do'))
+    } else if (error instanceof Response) {
+      error.json().then((res: { error: APIError }) => {
+        handleError(new Error(res.error.message))
+      })
+    }
+  }, [error, handleError])
 
   switch (status) {
     case 'idle':

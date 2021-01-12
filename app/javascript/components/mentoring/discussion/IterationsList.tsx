cd6aae7f6689eb67afb80692281901ef416bfc9a
Karlo Soriano <karlo@thalamus.ai>
2021-01-13 07:42:41 +0800

    Add previous / next iteration buttons

--- app/javascript/components/mentoring/discussion/IterationsList.tsx
+++ app/javascript/components/mentoring/discussion/IterationsList.tsx
@@ -11,6 +11,8 @@ export const IterationsList = ({
   current: Iteration
   onClick: (iteration: Iteration) => void
 }): JSX.Element => {
+  const currentIndex = iterations.indexOf(current)
+
   return (
     <nav className="iterations">
       {iterations.map((iteration) => (
@@ -21,6 +23,23 @@ export const IterationsList = ({
           selected={current === iteration}
         />
       ))}
+
+      <button
+        type="button"
+        aria-label="Go to previous iteration"
+        onClick={() => onClick(iterations[currentIndex - 1])}
+        disabled={iterations[0] === current}
+      >
+        Previous
+      </button>
+      <button
+        type="button"
+        aria-label="Go to next iteration"
+        onClick={() => onClick(iterations[currentIndex + 1])}
+        disabled={iterations[iterations.length - 1] === current}
+      >
+        Next
+      </button>
     </nav>
   )
 }

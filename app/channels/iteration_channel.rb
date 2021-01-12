ef2bb434e9ff5a3ff9e28975dd72a98c62543730
Karlo Soriano <karlo@thalamus.ai>
2021-01-13 06:40:23 +0800

    Raise error for unauthorized connection to the IterationChannel

--- app/channels/iteration_channel.rb
+++ app/channels/iteration_channel.rb
@@ -1,9 +1,12 @@
 class IterationChannel < ApplicationCable::Channel
+  class UnauthorizedConnectionError < RuntimeError
+  end
+
   def subscribed
     # Assert that the user owns this iteration
     iteration = Iteration.find_by!(uuid: params[:uuid])
 
-    return unless iteration.viewable_by?(current_user)
+    raise UnauthorizedConnectionError unless iteration.viewable_by?(current_user)
 
     # Don't use persisted objects for stream_for
     stream_for iteration.id

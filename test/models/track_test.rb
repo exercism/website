22d5246955f0eb587b4ab750a41ac10a7afa2e0d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-22 15:23:39 +0100

    Add test for track with and without regex

--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -29,13 +29,23 @@ class TrackTest < ActiveSupport::TestCase
     assert_equal track.slug, track.to_param
   end
 
-  test "Retrieves test_regexp" do
+  test "Retrieves test_regexp for track with test regex" do
     track = create :track
     assert_equal(/test/, track.test_regexp)
   end
 
-  test "Retrieves ignore_regexp" do
+  test "Retrieves test_regexp for track without test regex" do
+    track = create :track, repo_url: TestHelpers.git_repo_url("track-naked")
+    assert_equal(/[tT]est/, track.test_regexp)
+  end
+
+  test "Retrieves ignore_regexp for track with ignore regex" do
     track = create :track
     assert_equal(/[iI]gno/, track.ignore_regexp)
+  end
+
+  test "Retrieves ignore_regexp for track without ignore regex" do
+    track = create :track, repo_url: TestHelpers.git_repo_url("track-naked")
+    assert_equal(/[iI]gnore/, track.ignore_regexp)
   end
 end

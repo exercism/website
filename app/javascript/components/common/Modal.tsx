44fb2397971e08047ea29f7243c5f9b3223e6eab
Karlo Soriano <karlo@thalamus.ai>
2021-01-28 14:40:48 +0800

    Add [data-modal-close] to close modals

--- app/javascript/components/common/Modal.tsx
+++ app/javascript/components/common/Modal.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Modal as BaseModal } from '../modals/Modal'
 
 export const Modal = ({ html }: { html: string }): JSX.Element => {
+  const contentRef = useRef<HTMLDivElement | null>(null)
+  const [isMounted, setIsMounted] = useState(false)
+  const [open, setOpen] = useState(true)
+
+  const onMount = (instance: HTMLDivElement) => {
+    contentRef.current = instance
+
+    setIsMounted(true)
+  }
+
+  useEffect(() => {
+    if (!isMounted || !contentRef.current) {
+      return
+    }
+
+    const handleClick = () => {
+      setOpen(false)
+    }
+
+    const elements = contentRef.current.querySelectorAll('[data-modal-close]')
+
+    elements.forEach((element) => {
+      element.addEventListener('click', handleClick)
+    })
+
+    return () => {
+      elements.forEach((element) => {
+        element.removeEventListener('click', handleClick)
+      })
+    }
+  }, [isMounted])
+
   return (
-    <BaseModal open={true} onClose={() => {}} className="">
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+    <BaseModal open={open} onClose={() => {}} className="">
+      <div ref={onMount} dangerouslySetInnerHTML={{ __html: html }} />
     </BaseModal>
   )
 }
--- test/system/components/common/modal_test.rb
+++ test/system/components/common/modal_test.rb
@@ -8,6 +8,14 @@ module Components
 
         assert_selector "h1", text: "Hello"
       end
+
+      test "user closes modal" do
+        visit test_components_common_modal_path
+
+        click_on "Close"
+
+        assert_no_selector "h1", text: "Hello"
+      end
     end
   end
 end

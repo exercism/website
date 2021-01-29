8d72b6638088bb54cbe7f308a2db768954bdf3c3
Jeremy Walker <jez.walker@gmail.com>
2021-01-29 14:04:14 +0000

    Use img tags, not svgs (#463)

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -228,6 +228,6 @@ document.addEventListener('turbolinks:load', () => {
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-//const images = require.context('../images', true)
-//const imagePath = (name: any) => images(name)
+
+const images = require.context('../images', true)
+const imagePath = (name: any) => images(name)

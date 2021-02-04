b50df6ba93bd6c56642c81fc04b8f4152c21a801
Karlo Soriano <karlo@thalamus.ai>
2021-02-04 17:09:00 +0800

    Hide modal when clicking cancel button

--- app/javascript/components/mentoring/discussion/EndSessionButton.tsx
+++ app/javascript/components/mentoring/discussion/EndSessionButton.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { EndSessionModal } from '../../modals/EndSessionModal'
+import { ModalProps } from '../../modals/Modal'
 
 export const EndSessionButton = ({
   endpoint,
+  modalProps,
 }: {
   endpoint: string
+  modalProps?: ModalProps
 }): JSX.Element => {
   const [open, setOpen] = useState(false)
 
@@ -18,7 +21,14 @@ export const EndSessionButton = ({
       >
         End session
       </button>
-      <EndSessionModal endpoint={endpoint} open={open} />
+      <EndSessionModal
+        endpoint={endpoint}
+        open={open}
+        onCancel={() => {
+          setOpen(false)
+        }}
+        {...modalProps}
+      />
     </React.Fragment>
   )
 }

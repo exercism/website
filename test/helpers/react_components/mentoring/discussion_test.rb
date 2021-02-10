33f01ff5a5b45bc77ddb7b1eae593b3ffeb64e7b
Karlo Soriano <karlo@thalamus.ai>
2021-02-10 14:00:21 +0800

    Fix failing tests

--- test/helpers/react_components/mentoring/discussion_test.rb
+++ test/helpers/react_components/mentoring/discussion_test.rb
@@ -109,7 +109,7 @@ module Mentoring
         component.links[:mark_as_nothing_to_do]
     end
 
-    test "#links adds link to finish discussion when discussion is finished" do
+    test "links adds link to finish discussion when discussion is finished" do
       discussion = create :solution_mentor_discussion, finished_at: nil
       comp = ReactComponents::Mentoring::Discussion.new(discussion)
       assert_equal Exercism::Routes.finish_api_mentor_discussion_path(discussion), comp.links[:finish]

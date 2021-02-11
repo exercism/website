36b9a41b446af192b504eb39374bfc6a6d6f01ca
Erik Schierboom <erik_schierboom@hotmail.com>
2021-02-11 09:46:19 +0100

    Minor refactoring

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -16,7 +16,7 @@ module API
 
       unless content
         begin
-          content = Git::Exercise.for_solution(solution).read_file_blob(params[:filepath])
+          content = solution.git_exercise.read_file_blob(params[:filepath])
         rescue StandardError
           return render_file_not_found
         end

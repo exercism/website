703a6c582c6bd31e569c905e80d2d698ecad7346
Karlo Soriano <karlo@thalamus.ai>
2021-02-14 11:37:50 +0800

    Add test for refetching notifications when a websocket message is received

--- test/system/flows/user_loads_notifications_test.rb
+++ test/system/flows/user_loads_notifications_test.rb
@@ -1,9 +1,11 @@
 require "application_system_test_case"
 require_relative "../../support/capybara_helpers"
+require_relative "../../support/websockets_helpers"
 
 module Flows
   class UserLoadsNotificationsTest < ApplicationSystemTestCase
     include CapybaraHelpers
+    include WebsocketsHelpers
 
     test "user views notifications" do
       user = create :user
@@ -13,6 +15,23 @@ module Flows
       use_capybara_host do
         sign_in!(user)
         visit dashboard_path
+        within(".c-notification") { assert_text "1" }
+        find(".c-notification").click
+
+        assert_text "You have been awarded the Rookie badge."
+      end
+    end
+
+    test "refetches on websocket notification" do
+      user = create :user
+      badge = create :rookie_badge
+
+      use_capybara_host do
+        sign_in!(user)
+        visit dashboard_path
+        wait_for_websockets
+        create :acquired_badge_notification, user: user, params: { badge: badge }
+        NotificationsChannel.broadcast_changed(user)
         within(".c-notification") { assert_text "1" }
         find(".c-notification").click
 

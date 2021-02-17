406eca02eb91d8626aae5d87a7c822c2a4f95b5c
Erik Schierboom <erik_schierboom@hotmail.com>
2021-02-17 12:34:38 +0100

    Show introduction for practice exercises

--- app/models/practice_exercise.rb
+++ app/models/practice_exercise.rb
@@ -4,20 +4,5 @@ class PracticeExercise < Exercise
       where('exercise_prerequisites.track_concept_id': concept)
   end
 
-  # TODO: Split tracks READMEs out into instructions
-  # and then use that via Git"
-  def instructions
-    %(
-Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-
-- Bob answers 'Sure.' if you ask him a question.
-- He answers 'Whoa, chill out!' if you yell at him.
-- He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
-- He says 'Fine. Be that way!' if you address him without actually saying anything.
-- He answers 'Whatever.' to anything else.
-
-Bob's conversational partner is a purist when it comes to written communication and
-always follows normal rules regarding sentence punctuation in English.
-    ).strip
-  end
+  delegate :introduction, :instructions, to: :git
 end
---
+++ test/integration/exercises/show_external_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class Tracks::ShowExternalTest < ActionDispatch::IntegrationTest
+  test "the instructions are shown for a concept exercise" do
+    create :concept_exercise
+    get "/tracks/ruby/exercises/strings/"
+    assert_select ".instructions", text: /You have three tasks/
+  end
+
+  test "the instructions are shown for a practice exercise" do
+    create :practice_exercise
+    get "/tracks/ruby/exercises/bob/"
+    assert_select ".instructions", text: /Instructions for bob/
+  end
+
+  test "the introduction is not shown for a concept exercise" do
+    create :concept_exercise
+    get "/tracks/ruby/exercises/strings/"
+    assert_select ".introduction", false
+  end
+
+  test "the introduction is shown for a practice exercise that has an introduction" do
+    create :practice_exercise
+    get "/tracks/ruby/exercises/bob/"
+    assert_select ".introduction", text: /Introduction for bob/
+  end
+
+  test "the introduction is not shown for a practice exercise without an introduction" do
+    create :practice_exercise, slug: 'allergies'
+    get "/tracks/ruby/exercises/allergies/"
+    assert_select ".introduction", false
+  end
+end

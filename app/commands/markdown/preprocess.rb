8374c99a86ffb7dc3291086d9780cb85f31c455d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-02-18 10:30:47 +0100

    Refactor mutation

--- app/commands/markdown/preprocess.rb
+++ app/commands/markdown/preprocess.rb
@@ -5,40 +5,40 @@ class Markdown::Preprocess
     @doc = doc
     @text = text
     @strip_h1 = strip_h1
-    @deletions = []
-    @updates = []
+    @mutations = []
   end
 
   def call
     strip_h1_headings! if strip_h1
-    apply_mutations_to_text!
+    apply_mutations! if mutations.present?
 
     { doc: doc, text: text }
   end
 
   private
-  attr_reader :doc, :text, :strip_h1, :lines, :deletions, :updates
+  attr_reader :doc, :text, :strip_h1, :lines, :mutations
 
   def strip_h1_headings!
     doc.each do |node|
-      next unless node.type == :header && node.header_level == 1
-
-      node.delete
-      deletions << node
+      mutations << { type: :delete, node: node } if node.type == :header && node.header_level == 1
     end
   end
 
-  def apply_mutations_to_text!
-    return if deletions.empty? && updates.empty?
-
+  def apply_mutations!
     @lines = text.lines(chomp: true)
 
-    apply_deletions_to_text!
+    mutations_in_reverse_line_order.each do |mutation|
+      case mutation[:type]
+      when :delete
+        lines.delete_at(mutation[:node].sourcepos[:start_line] - 1)
+        mutation[:node].delete
+      end
+    end
 
     @text = lines.join("\n")
   end
 
-  def apply_deletions_to_text!
-    deletions.reverse_each { |deletion| lines.delete_at(deletion.sourcepos[:start_line] - 1) }
+  def mutations_in_reverse_line_order
+    mutations.sort_by { |mutation| -mutation[:node].sourcepos[:start_line] }
   end
 end

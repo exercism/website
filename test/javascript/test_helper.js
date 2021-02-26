8be619b1d73a98177f5fc21592c222faa7206d98
Karlo Soriano <karlo@thalamus.ai>
2021-02-26 12:11:05 +0800

    Clear query cache after each test

--- test/javascript/test_helper.js
+++ test/javascript/test_helper.js
@@ -1,3 +1,9 @@
 import fetch from 'isomorphic-fetch'
+import '@testing-library/jest-dom/extend-expect'
+import { queryCache } from 'react-query'
 
 global.fetch = fetch
+
+afterEach(async () => {
+  queryCache.clear()
+})

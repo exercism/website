42af1a660bf1fd5dca8b99d198e59e5db6339635
Jeremy Walker <jez.walker@gmail.com>
2021-02-27 22:52:55 +0000

    Add some more post-test clean up for safety

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,13 +1,16 @@
 import { waitFor } from '@testing-library/react'
 import { queryCache } from 'react-query'
+import flushPromises from 'flush-promises'
+import { act } from '@testing-library/react'
 
 afterEach(async () => {
   queryCache.clear()
-})
 
-afterEach(async () => {
   // waitFor is important here. If there are queries that are being fetched at
   // the end of the test and we continue on to the next test before waiting for
   // them to finalize, the tests can impact each other in strange ways.
   await waitFor(() => expect(queryCache.isFetching).toBe(0))
+
+  await flushPromises()
+  return act(async () => await null)
 })

e3ccd41eded26dde5f35d1e59d26b44646854531
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-04 13:35:42 +0100

    Basic import working

--- app/commands/git/sync_pull_requests.rb
+++ app/commands/git/sync_pull_requests.rb
@@ -5,49 +5,45 @@ module Git
     initialize_with :track
 
     def call
-      api_pull_requests.each do |api_pr|
-        ::GithubPullRequest.create_or_find_by!(github_id: api_pr[:pull_request][:id]) do |pr|
-          pr.github_username = api_pr[:pull_request][:user][:login]
-          pr.github_event = api_pr
+      fetch!
+      import!
+    end
+
+    private
+    attr_reader :pull_requests
+
+    def import!
+      pull_requests.each do |pr|
+        ::GithubPullRequest.create_or_find_by!(github_id: pr[:pull_request][:id]) do |p|
+          p.github_username = pr[:pull_request][:user][:login]
+          p.github_event = pr
         end
       end
     end
 
-    private
-    def api_pull_requests
-      api_data[:repository][:pullRequests][:nodes].map do |pr|
-        {
-          action: 'closed',
-          pull_request: {
-            id: pr[:databaseId],
-            user: {
-              login: pr[:author][:login]
-            },
-            url: "https://api.github.com/repos/#{repository_full_name}/pulls/#{pr[:number]}",
-            html_url: pr[:url],
-            labels: pr[:labels][:nodes].map { |node| node[:name] },
-            state: 'closed',
-            number: pr[:number],
-            merged: pr[:merged],
-            reviews: pr[:reviews][:nodes].map { |node| { user: { login: node[:author][:login] } } }
-          },
-          repository: {
-            full_name: repository_full_name
-          }
-        }
+    def fetch!
+      # TODO: support rate limiting
+      after_cursor = nil
+      @pull_requests = []
+
+      loop do
+        response = fetch_for_cursor(after_cursor)
+        @pull_requests += pull_requests_from_response(response)
+
+        page_info = response[:data][:repository][:pullRequests][:pageInfo]
+        break unless page_info[:hasNextPage]
+
+        after_cursor = page_info[:endCursor]
       end
     end
 
-    def repository_full_name
-      api_data[:repository][:nameWithOwner]
-    end
+    def fetch_for_cursor(after_cursor)
+      after_cursor_argument = after_cursor.nil? ? '' : ", after: \"#{after_cursor}\""
 
-    memoize
-    def api_data
       query = "{
         repository(owner: \"exercism\", name: \"#{track.slug}\") {
           nameWithOwner
-          pullRequests(first: 100, states:[CLOSED, MERGED]) {
+          pullRequests(first: 100, states:[CLOSED, MERGED]#{after_cursor_argument}) {
             nodes {
               url
               databaseId
@@ -71,6 +67,7 @@ module Git
             }
             pageInfo {
               hasNextPage
+              endCursor
             }
           }
         }
@@ -80,9 +77,33 @@ module Git
         }
       }"
 
-      # TODO: support paging
-      results = octokit_client.post("https://api.github.com/graphql", { query: query }.to_json)
-      results[:data]
+      octokit_client.post("https://api.github.com/graphql", { query: query }.to_json)
+    end
+
+    def pull_requests_from_response(response)
+      # We're transforming the GraphQL response to the format used by the REST API.
+      # This allows us to work with pull requests using a single code path.
+      response[:data][:repository][:pullRequests][:nodes].map do |pr|
+        {
+          action: 'closed',
+          pull_request: {
+            id: pr[:databaseId],
+            user: {
+              login: pr[:author][:login]
+            },
+            url: "https://api.github.com/repos/#{response[:data][:repository][:nameWithOwner]}/pulls/#{pr[:number]}",
+            html_url: pr[:url],
+            labels: pr[:labels][:nodes].map { |node| node[:name] },
+            state: 'closed',
+            number: pr[:number],
+            merged: pr[:merged],
+            reviews: pr[:reviews][:nodes].map { |node| { user: { login: node[:author][:login] } } }
+          },
+          repository: {
+            full_name: response[:data][:repository][:nameWithOwner]
+          }
+        }
+      end
     end
 
     memoize

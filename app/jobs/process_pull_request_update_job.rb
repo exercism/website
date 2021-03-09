5f350a8ae3eb262c6aa1695fdcb00df75cc73965
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-09 17:35:22 +0100

    Fetch the reviews in the process pr update job

--- app/jobs/process_pull_request_update_job.rb
+++ app/jobs/process_pull_request_update_job.rb
@@ -2,6 +2,19 @@ class ProcessPullRequestUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(action, github_username, params)
+    # Fetch the reviews, as GitHub pull request events don't contain them
+    params[:reviews] = reviews
+
     User::ReputationToken::AwardForPullRequest.(action, github_username, params)
+  end
+
+  private
+  def reviews
+    octokit_client.pull_request_reviews(repo, pr_number)
+  end
+
+  memoize
+  def octokit_client
+    Octokit::Client.new(access_token: Exercism.secrets.github_access_token)
   end
 end

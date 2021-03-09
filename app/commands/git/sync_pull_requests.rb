931ab627be96ff07caf1fec7f2386bd16181d915
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-09 09:54:35 +0100

    Simplify paging for pr syncing

--- app/commands/git/sync_pull_requests.rb
+++ app/commands/git/sync_pull_requests.rb
@@ -12,22 +12,18 @@ module Git
 
     private
     def repos
-      fetched = []
-
-      loop.with_index do |_, page|
-        # The GraphQL API could also have been used. That would have led to more
-        # efficient retrieval (less data returned), but we decided against it as
-        # the code would be far more verbose
-        response = octokit_client.search_repositories("org:exercism is:public", page: page + 1, per_page: 100)
-        fetched += response[:items].map { |item| item[:full_name] }
-
-        break fetched if fetched.size >= response[:total_count]
-      end
+      # The GraphQL API could also have been used. That would have led to more
+      # efficient retrieval (less data returned), but we decided against it as
+      # the code would be far more verbose
+      response = octokit_client.search_repositories("org:exercism is:public")
+      response[:items].map { |item| item[:full_name] }
     end
 
     memoize
     def octokit_client
-      Octokit::Client.new(access_token: Exercism.secrets.github_access_token)
+      Octokit::Client.new(access_token: Exercism.secrets.github_access_token).tap do |c|
+        c.auto_paginate = true
+      end
     end
   end
 end

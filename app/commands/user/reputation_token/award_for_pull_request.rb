91d4459e88c66f1fa51a238c3a4c551f7a04ca3a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-09 17:40:12 +0100

    Add todo for PR updating

--- app/commands/user/reputation_token/award_for_pull_request.rb
+++ app/commands/user/reputation_token/award_for_pull_request.rb
@@ -6,6 +6,8 @@ class User
       initialize_with :action, :github_username, :params
 
       def call
+        # TODO: update pull request
+
         User::ReputationToken::AwardForPullRequestAuthor.(action, github_username, params)
         User::ReputationToken::AwardForPullRequestReviewers.(action, github_username, params)
       end

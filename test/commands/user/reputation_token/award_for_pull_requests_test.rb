4c80d48b2a5dbba6b5cd7be234698809208f2bed
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-11 12:52:43 +0100

    Fix test class name

--- test/commands/user/reputation_token/award_for_pull_requests_test.rb
+++ test/commands/user/reputation_token/award_for_pull_requests_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
-class User::ReputationToken::AwardForPullRequestsForUserTest < ActiveSupport::TestCase
-  test "awards reputation for authored pull requests" do
+class User::ReputationToken::AwardForPullRequestsTest < ActiveSupport::TestCase
+  test "awards reputation for all authored pull requests" do
     user_1 = create :user, handle: "User1", github_username: "user-1"
     user_2 = create :user, handle: "User2", github_username: "user-2"
     create :git_pull_request, :random, author_github_username: "user-1"
@@ -14,7 +14,7 @@ class User::ReputationToken::AwardForPullRequestsForUserTest < ActiveSupport::Te
     assert_equal 1, User::ReputationTokens::CodeContributionToken.where(user: user_2).size
   end
 
-  test "award reputation for reviewed pull requests" do
+  test "award reputation for all reviewed pull requests" do
     user_1 = create :user, handle: "User1", github_username: "user-1"
     user_2 = create :user, handle: "User2", github_username: "user-2"
     create :git_pull_request_review, :random, reviewer_github_username: "user-2"
@@ -28,7 +28,7 @@ class User::ReputationToken::AwardForPullRequestsForUserTest < ActiveSupport::Te
     assert_equal 3, User::ReputationTokens::CodeReviewToken.where(user: user_2).size
   end
 
-  test "award reputation for merged pull requests" do
+  test "award reputation for all merged pull requests" do
     user_1 = create :user, handle: "User1", github_username: "user-1"
     user_2 = create :user, handle: "User2", github_username: "user-2"
     create :git_pull_request, :random, merged_by_github_username: "user-2"
@@ -40,7 +40,7 @@ class User::ReputationToken::AwardForPullRequestsForUserTest < ActiveSupport::Te
     assert_equal 1, User::ReputationTokens::CodeMergeToken.where(user: user_2).size
   end
 
-  test "award reputation for authoring, merging and reviewing pull requests" do
+  test "award reputation for authoring, merging and reviewing pull requests at the same time" do
     create :user, handle: "User1", github_username: "user-1"
     create :user, handle: "User2", github_username: "user-2"
     create :git_pull_request, :random, author_github_username: "user-1"

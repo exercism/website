25a8a5992d6a5ebb8d00ad9579a8a647452f71f0
Jeremy Walker <jez.walker@gmail.com>
2021-03-23 12:40:28 +0000

    Improve time helper

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -2,8 +2,11 @@ module TimeHelper
   def time_ago_in_words(time, short: false)
     t = super(time)
     return t unless short
+    return "now" if t == "less than a minute"
+
+    t.gsub!(/^about /, '')
 
     parts = t.split(' ')
-    "#{parts[0]}#{parts[1][0]} #{parts[2]}"
+    "#{parts[0]}#{parts[1][0]}"
   end
 end

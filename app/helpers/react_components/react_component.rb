202212656fb70224c0dad4d00459db5fa56c86ba
Jeremy Walker <jez.walker@gmail.com>
2021-03-26 16:07:59 +0000

    Add loading state to exercise tooltip

--- app/helpers/react_components/react_component.rb
+++ app/helpers/react_components/react_component.rb
@@ -2,7 +2,7 @@ module ReactComponents
   class ReactComponent < ViewComponents::ViewComponent
     include ActionView::Helpers::TagHelper
 
-    def to_s(id, data, fitted: false, wrapper_class_modifier: "")
+    def to_s(id, data, fitted: false, wrapper_class_modifier: nil, style: nil)
       css_classes = ["c-react-component"]
       css_classes << "c-react-wrapper-#{id}"
       css_classes << "c-react-wrapper-#{id}-#{wrapper_class_modifier}" if wrapper_class_modifier.present?
@@ -11,6 +11,7 @@ module ReactComponents
         "",
         {
           class: css_classes.join(" "),
+          style: style,
           "data-react-#{id}": true,
           "data-react-data": data.to_json
         }

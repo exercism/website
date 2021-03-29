e6f9f6f846cfe1c13d717ec53e486d125c273414
Karlo Soriano <karlo@thalamus.ai>
2021-03-29 15:16:39 +0800

    Update test run summary header to show number of failed tests

--- test/javascript/components/Editor/TestRunSummaryHeaderMessage.test.tsx
+++ test/javascript/components/Editor/TestRunSummaryHeaderMessage.test.tsx
@@ -4,13 +4,13 @@ import '@testing-library/jest-dom/extend-expect'
 import { TestRunSummaryHeaderMessage } from '../../../../app/javascript/components/editor/TestRunSummaryHeaderMessage'
 
 test('returns message for version 1 test runner', async () => {
-  render(<TestRunSummaryHeaderMessage version={1} />)
+  render(<TestRunSummaryHeaderMessage version={1} numFailedTests={2} />)
 
   expect(screen.getByText('Tests failed')).toBeInTheDocument()
 })
 
 test('returns message for version 2 test runner', async () => {
-  render(<TestRunSummaryHeaderMessage version={2} />)
+  render(<TestRunSummaryHeaderMessage version={2} numFailedTests={2} />)
 
-  expect(screen.getByText('1 test failure')).toBeInTheDocument()
+  expect(screen.getByText('2 test failures')).toBeInTheDocument()
 })

d32665d8ff429783104216c2fdd47a41b871a108
Jeremy Walker <jez.walker@gmail.com>
2021-03-29 22:20:46 +0100

    Fix test

--- test/javascript/components/common/CommunitySolution.test.js
+++ test/javascript/components/common/CommunitySolution.test.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import { CommunitySolution } from '../../../../../app/javascript/components/common/MentorSolution'
+import { CommunitySolution } from '../../../../app/javascript/components/common/CommunitySolution'
 
 test('shows CTA to contribute notes when notes isnt present', async () => {
   const solution = {
-    mentor: {
+    author: {
       handle: 'handle',
       avatarUrl: 'url',
+    },
+    links: {
+      publicUrl: 'https://google.com',
     },
   }
   const exercise = { title: 'Exercise' }

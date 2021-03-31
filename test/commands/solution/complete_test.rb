9dbe3c9570cf00d7979c46d20eeecb85d0ede273
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-31 14:18:30 +0200

    Only mark concepts as learnt when completing concept exercises

--- test/commands/solution/complete_test.rb
+++ test/commands/solution/complete_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class Solution::CompleteTest < ActiveSupport::TestCase
-  test "sets solution as completed" do
+  test "sets concept exercise solution as completed" do
     exercise = create :concept_exercise
 
     user = create :user
@@ -13,7 +13,7 @@ class Solution::CompleteTest < ActiveSupport::TestCase
     assert solution.reload.completed?
   end
 
-  test "sets concepts as learnt" do
+  test "sets concept exercise concepts as learnt" do
     track = create :track
     concept = create :track_concept, track: track
     exercise = create :concept_exercise, track: track
@@ -26,5 +26,17 @@ class Solution::CompleteTest < ActiveSupport::TestCase
     Solution::Complete.(solution, user_track)
 
     assert user_track.concept_learnt?(concept)
+  end
+
+  test "sets practice exercise solution as completed" do
+    exercise = create :practice_exercise
+
+    user = create :user
+    user_track = create :user_track, user: user, track: exercise.track
+    solution = create :practice_solution, user: user, exercise: exercise
+
+    Solution::Complete.(solution, user_track)
+
+    assert solution.reload.completed?
   end
 end

677c21e59f81b8cf1d5fe5f003c8010e15592299
Jeremy Walker <jez.walker@gmail.com>
2021-04-01 20:34:36 +0100

    Disable Rails settings. TO CHECK: DOES THIS STILL WORK??

--- config/initializers/json.rb
+++ config/initializers/json.rb
@@ -2,5 +2,7 @@
 # https://github.com/ohler55/oj/blob/develop/pages/Rails.md
 
 require 'oj'
-Oj.optimize_rails
+# Oj.optimize_rails
 Oj.add_to_json
+# Oj.default_options = {:mode => :compat }
+# Oj.default_options[:time_format] = :unix

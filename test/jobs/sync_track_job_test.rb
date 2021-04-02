6bca18d29d92ce49f998af9410d5abc58734180b
Jeremy Walker <jez.walker@gmail.com>
2021-04-02 16:44:01 +0100

    Add force_sync option to track syncing job (#696)

--- test/jobs/sync_track_job_test.rb
+++ test/jobs/sync_track_job_test.rb
@@ -4,7 +4,7 @@ class SyncTrackJobTest < ActiveJob::TestCase
   test "track is synced from git" do
     track = create :track, slug: 'csharp'
 
-    Git::SyncTrack.expects(:call).with(track)
+    Git::SyncTrack.expects(:call).with(track, force_sync: false)
     SyncTrackJob.perform_now(track)
   end
 end

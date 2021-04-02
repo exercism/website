ecb3ac03b09e6e49257e0cc31a651ab189b5e0a9
Jeremy Walker <jez.walker@gmail.com>
2021-04-02 16:27:35 +0100

    Add force_sync option to track syncing job

--- app/jobs/sync_track_job.rb
+++ app/jobs/sync_track_job.rb
@@ -1,7 +1,7 @@
 class SyncTrackJob < ApplicationJob
   queue_as :default
 
-  def perform(track)
-    Git::SyncTrack.(track)
+  def perform(track, force_sync: false)
+    Git::SyncTrack.(track, force_sync: force_sync)
   end
 end

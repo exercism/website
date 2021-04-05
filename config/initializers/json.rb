f4e23d966b58b5cf96ea08aac88ebdf67bf0e93c
Jeremy Walker <jez.walker@gmail.com>
2021-04-05 23:50:25 +0100

    Fix things

--- config/initializers/json.rb
+++ config/initializers/json.rb
@@ -2,7 +2,4 @@
 # https://github.com/ohler55/oj/blob/develop/pages/Rails.md
 
 require 'oj'
-# Oj.optimize_rails
-Oj.add_to_json
-# Oj.default_options = {:mode => :compat }
-# Oj.default_options[:time_format] = :unix
+Oj.optimize_rails

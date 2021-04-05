8053b1b998e0b3f0daf002f1754998bf7e228954
Jeremy Walker <jez.walker@gmail.com>
2021-04-05 16:15:30 +0100

    Use headless Chrome for CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -59,11 +59,11 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     Capybara.use_default_driver
   end
 
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  # driven_by :selenium, using: :headless_chrome do |driver_option|
-  #   # Without this argument, Chrome cannot be started in Docker
-  #   driver_option.add_argument('no-sandbox')
-  # end
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome do |driver_option|
+    # Without this argument, Chrome cannot be started in Docker
+    driver_option.add_argument('no-sandbox')
+  end
 
   def sign_in!(user = nil)
     @current_user = user || create(:user)

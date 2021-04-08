98a0e8fdf3c36c2d39230428455704626a3572cd
Karlo Soriano <karlo@thalamus.ai>
2021-04-08 14:39:04 +0800

    Add delay before rendering spinner for results zone

--- app/javascript/components/ResultsZone.tsx
+++ app/javascript/components/ResultsZone.tsx
@@ -1,14 +1,38 @@
+import React, { useEffect, useState, useRef } from 'react'
 import { Icon } from './common'
-import React from 'react'
 
-export const ResultsZone = ({
-  isFetching,
-  children,
-}: React.PropsWithChildren<{ isFetching: boolean }>): JSX.Element => {
+const DELAY_MS = 250
+
+export const ResultsZone = (
+  props: React.PropsWithChildren<{ isFetching: boolean }>
+): JSX.Element => {
+  const [isFetching, setIsFetching] = useState(false)
+  const timerRef = useRef<NodeJS.Timeout | null>(null)
   const classNames = `c-results-zone ${isFetching ? '--fetching' : ''}`
+
+  useEffect(() => {
+    if (props.isFetching) {
+      timerRef.current = setTimeout(() => setIsFetching(true), DELAY_MS)
+    } else {
+      setIsFetching(false)
+
+      if (timerRef.current) {
+        clearTimeout(timerRef.current)
+      }
+    }
+
+    return () => {
+      if (!timerRef.current) {
+        return
+      }
+
+      clearTimeout(timerRef.current)
+    }
+  }, [props.isFetching])
+
   return (
     <div className={classNames}>
-      {children}
+      {props.children}
       <div className="--fetching-overlay">
         <Icon icon="spinner" alt="Loading data" />
       </div>

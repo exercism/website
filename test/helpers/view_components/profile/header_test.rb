35959016bfcf7cb2198973e9759c6832e7400e78
Jeremy Walker <jez.walker@gmail.com>
2021-04-09 17:29:22 +0100

    Fix the test

--- test/helpers/view_components/profile/header_test.rb
+++ test/helpers/view_components/profile/header_test.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class ViewComponents::Profile::HeaderTest < ActionView::TestCase
   test "roles" do
-    user = create :user
+    user = create :user, roles: [:must_be_present]
     create :user_profile, user: user
 
-    user.stubs(founder?: true, staff?: true, admin?: true, maintainer?: true)
+    user.stubs(founder?: true, staff?: true, maintainer?: true)
     html = render(ViewComponents::Profile::Header.new(user, user.profile, nil))
     assert_includes html, "Exercism Founder"
     refute_includes html, "Exercism Staff"

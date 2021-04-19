6197eda5ab4a38eacd315580bf31ffb1ccd534cf
Jeremy Walker <jez.walker@gmail.com>
2021-04-19 22:38:31 +0100

    Only show active tracks in list header

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -13,7 +13,7 @@ class TracksController < ApplicationController
     @num_tracks = Track.count
 
     # TODO: Change this to only select the fields needed for an icon
-    @track_icon_urls = Track.order('rand()').limit(8).map(&:icon_url)
+    @track_icon_urls = Track.active.order('rand()').limit(8).map(&:icon_url)
   end
 
   def show

b2df0ef919ec95a45b43b6ab8c6a4394fa202467
Karlo Soriano <karlo@thalamus.ai>
2021-04-22 15:51:50 +0800

    Fix failing tests

--- test/javascript/components/Editor/features/instructions.test.js
+++ test/javascript/components/Editor/features/instructions.test.js
@@ -1,4 +1,4 @@
-jest.mock('../../../../../app/javascript/components/editor/FileEditor')
+jest.mock('../../../../../app/javascript/components/editor/FileEditorAce')
 
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'

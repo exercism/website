1a9ec2d555d18af7d71ac1748dc3989fa10039d9
Jeremy Walker <jez.walker@gmail.com>
2021-04-22 13:22:14 +0100

    Add test

--- app/helpers/react_components/student/exercise_status_dot.rb
+++ app/helpers/react_components/student/exercise_status_dot.rb
@@ -1,14 +1,19 @@
 module ReactComponents
   module Student
     class ExerciseStatusDot < ReactComponent
-      initialize_with :exercise
+      def initialize(exercise, user_track)
+        @exercise = exercise
+        @user_track = user_track || UserTrack::External.new(exercise.track)
+
+        super()
+      end
 
       def to_s
         super(
           "student-exercise-status-dot",
           {
             slug: exercise.slug,
-            exercise_status: exercise_status,
+            exercise_status: user_track.exercise_status(exercise),
             type: exercise.git_type,
             links: {
               exercise: Exercism::Routes.track_exercise_url(exercise.track, exercise.slug),
@@ -19,11 +24,7 @@ module ReactComponents
       end
 
       private
-      def exercise_status
-        user_track = UserTrack.for(current_user, exercise.track)
-
-        user_track.exercise_status(exercise)
-      end
+      attr_reader :exercise, :user_track
     end
   end
 end

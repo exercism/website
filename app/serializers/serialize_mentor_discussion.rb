5b57433f13a5cf505f2367341c8f87662b1a945c
Karlo Soriano <karlo@thalamus.ai>
2021-04-26 18:15:06 +0800

    Clean up SerializeMentorDiscussion

--- app/serializers/serialize_mentor_discussion.rb
+++ app/serializers/serialize_mentor_discussion.rb
@@ -24,32 +24,47 @@ class SerializeMentorDiscussion
         title: discussion.exercise.title,
         icon_url: discussion.exercise.icon_url
       },
-      is_finished: context == :student ? discussion.finished_for_student? : discussion.finished_for_mentor?,
-      is_unread: if context == :student
-                   discussion.posts.where(seen_by_student: false).exists?
-                 else
-                   discussion.posts.where(seen_by_mentor: false).exists?
-                 end,
+      is_finished: finished?,
+      is_unread: unread?,
       # TODO
       is_starred: true,
       posts_count: discussion.posts.count,
       iterations_count: discussion.iterations.count,
       created_at: discussion.created_at.iso8601,
       status: discussion.status,
-      links: {
-        self: if context == :student
-                Exercism::Routes.track_exercise_mentor_discussion_path(discussion.track, discussion.exercise, discussion)
-              else
-                Exercism::Routes.mentoring_discussion_path(discussion)
-              end,
-        posts: if context == :student
-                 Exercism::Routes.api_solution_discussion_posts_url(discussion.solution.uuid, discussion)
-               else
-                 Exercism::Routes.api_mentoring_discussion_posts_url(discussion)
-               end,
+      links: links
+    }
+  end
+
+  def finished?
+    if context == :student
+      discussion.finished_for_student?
+    else
+      discussion.finished_for_mentor?
+    end
+  end
+
+  def unread?
+    if context == :student
+      discussion.posts.where(seen_by_student: false).exists?
+    else
+      discussion.posts.where(seen_by_mentor: false).exists?
+    end
+  end
+
+  def links
+    if context == :student
+      {
+        self: Exercism::Routes.track_exercise_mentor_discussion_path(discussion.track, discussion.exercise, discussion),
+        posts: Exercism::Routes.api_solution_discussion_posts_url(discussion.solution.uuid, discussion)
+      }
+    else
+      {
+        self: Exercism::Routes.mentoring_discussion_path(discussion),
+        posts: Exercism::Routes.api_mentoring_discussion_posts_url(discussion),
         finish: Exercism::Routes.finish_api_mentoring_discussion_path(discussion),
         mark_as_nothing_to_do: Exercism::Routes.mark_as_nothing_to_do_api_mentoring_discussion_path(discussion)
       }
-    }
+    end
   end
 end

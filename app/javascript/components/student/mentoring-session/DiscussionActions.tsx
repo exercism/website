276ac0483d0f57bfbda962978b45996dfe3c38f4
Karlo Soriano <karlo@thalamus.ai>
2021-04-26 16:46:16 +0800

    Hide end discussion button if discussion is ended

--- app/javascript/components/student/mentoring-session/DiscussionActions.tsx
+++ app/javascript/components/student/mentoring-session/DiscussionActions.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FinishButton } from './FinishButton'
 import { MentorDiscussion } from '../../types'
+import { GraphicalIcon } from '../../common'
 
 type Links = {
   exercise: string
@@ -13,5 +14,12 @@ export const DiscussionActions = ({
   discussion: MentorDiscussion
   links: Links
 }): JSX.Element => {
-  return <FinishButton discussion={discussion} links={links} />
+  return discussion.isFinished ? (
+    <div className="finished">
+      <GraphicalIcon icon="completed-check-circle" />
+      Ended
+    </div>
+  ) : (
+    <FinishButton discussion={discussion} links={links} />
+  )
 }

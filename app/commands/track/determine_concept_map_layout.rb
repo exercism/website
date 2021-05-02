5978e2f8a2a406507f4024072b1e93967856e489
Tim Austin <tim@neenjaw.com>
2021-05-01 19:54:30 -0600

    update concept map connections A while ago the concept map connected concept exercises to concept exercises. Then a shift was made to display individual concepts rather than exercises. This is effectively created a subgraph (supergraph?), but when exploding the exercise into individual taugh concepts, edges were still being created with the exercise as the frame of reference, potentially creating extra edges when a concept exercise taught multiple concepts. This commit simplifies this logic to create nodes/edges using only concepts. Tests pass.

--- app/commands/track/determine_concept_map_layout.rb
+++ app/commands/track/determine_concept_map_layout.rb
@@ -26,15 +26,13 @@ class Track
 
     memoize
     def concepts
-      concepts = graph.nodes.flat_map do |exercise|
-        exercise.taught_concepts.map do |concept|
-          {
-            slug: concept.slug,
-            name: concept.name,
-            web_url: Exercism::Routes.track_concept_url(track, concept),
-            tooltip_url: Exercism::Routes.tooltip_track_concept_url(track, concept)
-          }
-        end
+      concepts = graph.nodes.flat_map do |concept|
+        {
+          slug: concept.slug,
+          name: concept.name,
+          web_url: Exercism::Routes.track_concept_url(track.slug, concept.slug),
+          tooltip_url: Exercism::Routes.tooltip_track_concept_url(track.slug, concept.slug)
+        }
       end
 
       concepts.sort_by { |concept| concept[:slug] }
@@ -42,47 +40,34 @@ class Track
 
     memoize
     def concept_levels
-      exercise_levels.map { |level| level.flat_map(&:taught_concepts).map(&:slug) }
+      node_levels.map { |level| level.map(&:slug) }
     end
 
     memoize
     def concept_connections
-      exercise_connections.flat_map do |connection|
-        from_concepts = connection[:from].taught_concepts
-        to_concepts = connection[:to].taught_concepts
-
-        from_concepts.product(to_concepts).map do |(from, to)|
-          { from: from.slug, to: to.slug }
+      node_levels.drop(1).flat_map.with_index do |level, level_idx|
+        level.flat_map do |node|
+          node.prerequisites.
+            map { |prerequisite| graph.node_for_concept(prerequisite.slug) }.
+            compact.
+            select { |prerequisite_node| prerequisite_node.level == level_idx }.
+            map { |prerequisite_node| { from: prerequisite_node.slug, to: node.slug } }
         end
       end
     end
 
     memoize
-    def exercise_levels
+    def node_levels
       return [] if graph.empty?
 
       raise TrackHasCyclicPrerequisiteError if graph.has_cycle?
 
-      Array.new(graph.exercise_levels.max + 1) { [] }.tap do |level|
-        graph.exercise_levels.each.with_index do |level_idx, node_idx|
+      Array.new(graph.concept_levels.max + 1) { [] }.tap do |level|
+        graph.concept_levels.each.with_index do |level_idx, node_idx|
           node = graph.node_for_index(node_idx)
           node.level = level_idx
 
           level[level_idx] << node
-        end
-      end
-    end
-
-    memoize
-    def exercise_connections
-      exercise_levels.drop(1).each.with_index.flat_map do |level, level_idx|
-        level.flat_map do |node|
-          node.prerequisites.
-            map { |prerequisite| graph.node_for_concept(prerequisite) }.
-            compact.
-            uniq.
-            select { |prerequisite_node| prerequisite_node.level == level_idx }.
-            map { |prerequisite_node| { from: prerequisite_node, to: node } }
         end
       end
     end
@@ -96,7 +81,7 @@ class Track
       Edge = Struct.new(:from, :to, keyword_init: true)
 
       # Node for representing an exercise within a track
-      Node = Struct.new(:index, :slug, :taught_concepts, :prerequisites, :level, keyword_init: true)
+      Node = Struct.new(:index, :slug, :name, :prerequisites, :level, keyword_init: true)
 
       def initialize(track)
         @track = track
@@ -123,7 +108,7 @@ class Track
       end
 
       memoize
-      def exercise_levels
+      def concept_levels
         GraphUtils.calculate_levels(adjacencies)
       end
 
@@ -143,20 +128,27 @@ class Track
       end
 
       def determine_nodes
-        track.concept_exercises.includes(:taught_concepts, :prerequisites).map.with_index do |exercise, idx|
-          Node.new(
-            index: idx,
-            slug: exercise.slug,
-            taught_concepts: exercise.taught_concepts.to_a,
-            prerequisites: exercise.prerequisites.to_a
-          )
-        end.freeze
+        nodes = track.concept_exercises.includes(:taught_concepts, :prerequisites).flat_map do |exercise|
+          exercise.taught_concepts.map do |concept|
+            Node.new(
+              index: nil,
+              slug: concept.slug,
+              name: concept.name,
+              prerequisites: exercise.prerequisites.to_a
+            )
+          end
+        end
+
+        nodes.map.with_index do |node, index|
+          node.index = index
+          node
+        end
       end
 
       def determine_edges
         nodes.flat_map do |node|
           node.prerequisites.map do |prereq|
-            prereq_node = node_for_concept(prereq)
+            prereq_node = node_for_concept(prereq.slug)
             prereq_node ? Edge.new(from: prereq_node, to: node) : nil
           end
         end.compact.freeze
@@ -164,9 +156,7 @@ class Track
 
       memoize
       def nodes_keyed_by_concept
-        nodes.flat_map do |node|
-          node.taught_concepts.map { |concept| [concept, node] }
-        end.to_h.freeze
+        nodes.index_by(&:slug).freeze
       end
 
       memoize

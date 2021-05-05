5d76bb658e81b3bd23c2aafcb0cd40ebc8a04d39
Jeremy Walker <jez.walker@gmail.com>
2021-05-06 00:33:11 +0100

    Cope with ETL on server

--- db/migrate/20210505220831_add_cache_columns_to_solutions.rb
+++ db/migrate/20210505220831_add_cache_columns_to_solutions.rb
@@ -1,8 +1,8 @@
 class AddCacheColumnsToSolutions < ActiveRecord::Migration[6.1]
   def change
-    add_column :solutions, :num_views, :integer, limit: 3, null: false, default: 0
+    add_column :solutions, :num_views, :integer, limit: 3, null: false, default: 0, if_not_exists: true
+    add_column :solutions, :num_stars, :integer, limit: 3, null: false, default: 0, if_not_exists: true
+    add_column :solutions, :num_comments, :integer, limit: 3, null: false, default: 0, if_not_exists: true
     add_column :solutions, :num_loc, :integer, limit: 3, null: false, default: 0
-    add_column :solutions, :num_stars, :integer, limit: 3, null: false, default: 0
-    add_column :solutions, :num_comments, :integer, limit: 3, null: false, default: 0
   end
 end

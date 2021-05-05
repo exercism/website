4c7d3c97cd2f5f8046a052b62c643bcc090b61e6
Jeremy Walker <jez.walker@gmail.com>
2021-05-06 00:34:04 +0100

    Cope with badly run migration

--- db/migrate/20210505220831_add_cache_columns_to_solutions.rb
+++ db/migrate/20210505220831_add_cache_columns_to_solutions.rb
@@ -3,6 +3,6 @@ class AddCacheColumnsToSolutions < ActiveRecord::Migration[6.1]
     add_column :solutions, :num_views, :integer, limit: 3, null: false, default: 0, if_not_exists: true
     add_column :solutions, :num_stars, :integer, limit: 3, null: false, default: 0, if_not_exists: true
     add_column :solutions, :num_comments, :integer, limit: 3, null: false, default: 0, if_not_exists: true
-    add_column :solutions, :num_loc, :integer, limit: 3, null: false, default: 0
+    add_column :solutions, :num_loc, :integer, limit: 3, null: false, default: 0, if_not_exists: true
   end
 end

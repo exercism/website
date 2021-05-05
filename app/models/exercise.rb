2d599a173cbf89986f51c5a911aee149eedc3358
Jeremy Walker <jez.walker@gmail.com>
2021-05-05 23:05:33 +0100

    Remove old code

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,10 +2,6 @@ class Exercise < ApplicationRecord
   extend FriendlyId
   extend Mandate::Memoize
 
-  # TODO: Remove this once we use external icons
-  include Webpacker::Helper
-  include ActionView::Helpers::AssetUrlHelper
-
   friendly_id :slug, use: [:history]
 
   enum status: {

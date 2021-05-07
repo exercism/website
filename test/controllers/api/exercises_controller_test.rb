6fac9dc0f920b4fa74391e2f66f6c52cdeb83411
Jeremy Walker <jez.walker@gmail.com>
2021-05-07 16:45:18 +0100

    Add more coverage

--- test/controllers/api/exercises_controller_test.rb
+++ test/controllers/api/exercises_controller_test.rb
@@ -22,6 +22,25 @@ module API
       assert_response :success
     end
 
+    test "index should proxy params for user" do
+      setup_user
+      track = create :track
+      user_track = create :user_track, user: @current_user, track: track
+
+      Exercise::Search.expects(:call).with(
+        track,
+        user_track: user_track,
+        criteria: "ru"
+      ).returns(Exercise.page(1))
+
+      get api_track_exercises_path(
+        track,
+        criteria: "ru"
+      ), headers: @headers, as: :json
+
+      assert_response :success
+    end
+
     test "index should search and return exercises" do
       track = create :track
       exercise = create :concept_exercise, track: track, title: "Bob"

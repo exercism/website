288beec5f2baaebe04803c2cfff5550403c7eefb
Jeremy Walker <jez.walker@gmail.com>
2021-05-10 14:57:16 +0100

    Fix test

--- test/system/components/journey/badges_test.rb
+++ test/system/components/journey/badges_test.rb
@@ -22,14 +22,16 @@ module Components
       test "paginates badges" do
         User::AcquiredBadge::Search.stubs(:default_per).returns(1)
         user = create :user
-        rookie_badge = create :rookie_badge
-        create :user_acquired_badge, revealed: true, badge: rookie_badge, user: user, created_at: 1.day.ago
         member_badge = create :member_badge
-        create :user_acquired_badge, revealed: true, badge: member_badge, user: user, created_at: 2.days.ago
+        create :user_acquired_badge, revealed: true, badge: member_badge, user: user
+        rookie_badge = create :rookie_badge
+        create :user_acquired_badge, revealed: true, badge: rookie_badge, user: user
 
         use_capybara_host do
           sign_in!(user)
           visit badges_journey_path
+          assert_no_text "Member"
+          assert_text "Rookie"
 
           click_on "2"
           assert_no_text "Rookie"
@@ -41,23 +43,24 @@ module Components
         User::AcquiredBadge::Search.stubs(:default_per).returns(1)
         user = create :user
         rookie_badge = create :rookie_badge
-        create :user_acquired_badge, revealed: true, badge: rookie_badge, user: user, created_at: 1.day.ago
+        create :user_acquired_badge, revealed: true, badge: rookie_badge, user: user
         member_badge = create :member_badge
-        create :user_acquired_badge, revealed: true, badge: member_badge, user: user, created_at: 2.days.ago
+        create :user_acquired_badge, revealed: true, badge: member_badge, user: user
 
         use_capybara_host do
           sign_in!(user)
           visit badges_journey_path
           select "Sort by Oldest First"
-
-          click_on "2"
           assert_no_text "Member"
           assert_text "Rookie"
+
+          click_on "2"
+          assert_no_text "Rookie"
+          assert_text "Member"
         end
       end
 
       test "searches badges" do
-        User::AcquiredBadge::Search.stubs(:default_per).returns(1)
         user = create :user
         rookie_badge = create :rookie_badge
         create :user_acquired_badge, revealed: true, badge: rookie_badge, user: user, created_at: 1.day.ago
@@ -69,7 +72,6 @@ module Components
           visit badges_journey_path
           fill_in "Search for a badge", with: "Rook"
 
-          click_on "2"
           assert_no_text "Member"
           assert_text "Rookie"
         end

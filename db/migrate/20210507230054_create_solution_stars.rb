7ce9f307f164eb363c823510c3e11e38bd2fa05f
Jeremy Walker <jez.walker@gmail.com>
2021-05-11 17:30:33 +0100

    Fix migration

--- db/migrate/20210507230054_create_solution_stars.rb
+++ db/migrate/20210507230054_create_solution_stars.rb
@@ -1,6 +1,6 @@
 class CreateSolutionStars < ActiveRecord::Migration[6.1]
   def change
-    create_table :solution_stars do |t|
+    create_table :solution_stars, if_not_exists: true do |t|
       t.belongs_to :solution, foreign_key: true, null: false
       t.belongs_to :user, foreign_key: true, null: false
 

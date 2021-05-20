13b33b1a00b673004ea748ad25ea5f219962b7e1
Karlo Soriano <karlo@thalamus.ai>
2021-05-20 11:51:20 +0800

    Remove unused code

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-let ASSET_HOST
-
-if (process.env.NODE_ENV === 'production') {
-  ASSET_HOST =
-    'https://exercism-assets-staging.s3.eu-west-2.amazonaws.com/packs/'
-} else {
-  ASSET_HOST = ''
-}
-
 module.exports = environment

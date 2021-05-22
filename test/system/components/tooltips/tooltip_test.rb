96e7e269d7a54a62d4ed02a37830f2c798ec7456
Jeremy Walker <jez.walker@gmail.com>
2021-05-23 00:17:34 +0100

    Fix tests

--- test/system/components/tooltips/tooltip_test.rb
+++ test/system/components/tooltips/tooltip_test.rb
@@ -4,12 +4,17 @@ module Components
   module Tooltips
     class TooltipTest < ApplicationSystemTestCase
       test "mentored student tooltip renders correctly" do
+        # Create these first so they have the first two ids
+        create :user, handle: 'mentee'
+        create :user, handle: 'someone-else'
+
+        sign_in!
         visit test_components_tooltips_tooltip_path
 
         mentored_students = all('ol.mentored-students > li')
 
         mentored_students[0].hover
-        within(".c-mentored-student-tooltip") { assert_text "mentee" }
+        within(".c-student-tooltip") { assert_text "mentee" }
       end
 
       test "user tooltip renders correctly" do

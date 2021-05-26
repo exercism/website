fddab7774fa5f9d705ecc6577d97f4eb8e177e69
Jeremy Walker <jez.walker@gmail.com>
2021-05-26 16:49:52 +0100

    Add mentoring tab

--- app/helpers/view_components/track/exercise_header.rb
+++ app/helpers/view_components/track/exercise_header.rb
@@ -30,13 +30,9 @@ module ViewComponents
                 ),
 
                 iterations_tab,
+                community_solutions_tab,
+                mentoring_tab
 
-                link_to(
-                  graphical_icon('community-solutions') +
-                  tag.span("Community Solutions", "data-text": "Community Solutions"),
-                  Exercism::Routes.track_exercise_solutions_path(track, exercise),
-                  class: tab_class(:community_solutions)
-                )
               ]
             )
           end + (render ::ReactComponents::Student::OpenEditorButton.new(exercise))
@@ -53,6 +49,24 @@ module ViewComponents
           safe_join(parts),
           Exercism::Routes.track_exercise_iterations_path(track, exercise),
           class: tab_class(:iterations)
+        )
+      end
+
+      def community_solutions_tab
+        link_to(
+          graphical_icon('community-solutions') +
+          tag.span("Community Solutions", "data-text": "Community Solutions"),
+          Exercism::Routes.track_exercise_solutions_path(track, exercise),
+          class: tab_class(:community_solutions)
+        )
+      end
+
+      def mentoring_tab
+        link_to(
+          graphical_icon('mentoring') +
+          tag.span("Mentoring", "data-text": "Mentoring"),
+          Exercism::Routes.track_exercise_mentor_discussions_path(track, exercise),
+          class: tab_class(:mentoring)
         )
       end
 

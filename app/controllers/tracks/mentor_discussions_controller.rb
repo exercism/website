2ac47909212ca5e18df3550d1f301a4811e9f914
Jeremy Walker <jez.walker@gmail.com>
2021-05-28 17:28:24 +0100

    Lock mentoring until an appropriate time (#1043)

--- app/controllers/tracks/mentor_discussions_controller.rb
+++ app/controllers/tracks/mentor_discussions_controller.rb
@@ -3,7 +3,9 @@ class Tracks::MentorDiscussionsController < ApplicationController
   before_action :use_discussion, only: :show
   before_action :disable_site_header!, only: :show
 
-  def index; end
+  def index
+    redirect_to track_exercise_path(@track, @exercise) unless @solution&.iterated?
+  end
 
   def show; end
 
@@ -12,7 +14,7 @@ class Tracks::MentorDiscussionsController < ApplicationController
     @track = Track.find(params[:track_id])
     @user_track = UserTrack.for(current_user, @track, external_if_missing: true)
     @exercise = @track.exercises.find(params[:exercise_id])
-    @solution = Solution.for!(current_user, @exercise)
+    @solution = Solution.for(current_user, @exercise)
   end
 
   def use_discussion

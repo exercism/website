cd9350ce51add18f9f29c706b599a4dad8afa611
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-01 14:57:27 +0200

    Make job into a command

--- app/commands/webhooks/process_push_update.rb
+++ app/commands/webhooks/process_push_update.rb
@@ -15,7 +15,7 @@ module Webhooks
       else
         track = Track.find_by(slug: repo_name)
         SyncTrackJob.perform_later(track) if track
-        NotifyTrackSyncerAboutTrackChangesJob.perform_later(track) if org_wide_file_changed?(track)
+        Github::NotifyTrackSyncerAboutTrackChanges.(track) if org_wide_file_changed?(track)
       end
     end
 
--- test/commands/webhooks/process_push_update_test.rb
+++ test/commands/webhooks/process_push_update_test.rb
@@ -29,7 +29,7 @@ class Webhooks::ProcessPushUpdateTest < ActiveSupport::TestCase
     end
   end
 
-  test "should not enqueue sync push job when pushing to non-valid track" do
+  test "should not enqueue sync push job when pushing to non-track repo" do
     create :track, slug: :ruby
 
     assert_enqueued_jobs 0, only: SyncTrackJob do
@@ -37,51 +37,49 @@ class Webhooks::ProcessPushUpdateTest < ActiveSupport::TestCase
     end
   end
 
-  test "should enqueue notify track org wide file change job when file in .appends directory has been added" do
-    create :track, slug: 'ruby'
+  test "should notify track syncer when commit contains added file in .appends directory" do
+    track = create :track, slug: 'ruby'
 
-    assert_enqueued_jobs 1, only: NotifyTrackSyncerAboutTrackChangesJob do
-      Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
-                                     { added: ['.appends/labels.yml'], removed: [], modified: [] }
-                                   ])
-    end
+    Github::NotifyTrackSyncerAboutTrackChanges.expects(:call).with(track)
+
+    Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
+                                   { added: ['.appends/labels.yml'], removed: [], modified: [] }
+                                 ])
   end
 
-  test "should enqueue notify track org wide file change job when file in .appends directory has been removed" do
-    create :track, slug: 'ruby'
+  test "should notify track syncer when commit contains removed file from .appends directory" do
+    track = create :track, slug: 'ruby'
 
-    assert_enqueued_jobs 1, only: NotifyTrackSyncerAboutTrackChangesJob do
-      Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
-                                     { added: [], removed: ['.appends/issues.json'], modified: [] }
-                                   ])
-    end
+    Github::NotifyTrackSyncerAboutTrackChanges.expects(:call).with(track)
+
+    Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
+                                   { added: [], removed: ['.appends/issues.json'], modified: [] }
+                                 ])
   end
 
-  test "should enqueue notify track org wide file change job when file in .appends directory has been modified" do
-    create :track, slug: 'ruby'
+  test "should notify track syncer when commit contains modified file in .appends directory" do
+    track = create :track, slug: 'ruby'
 
-    assert_enqueued_jobs 1, only: NotifyTrackSyncerAboutTrackChangesJob do
-      Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
-                                     { added: [], removed: [], modified: ['.appends/LICENSE.md'] }
-                                   ])
-    end
+    Github::NotifyTrackSyncerAboutTrackChanges.expects(:call).with(track)
+
+    Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
+                                   { added: [], removed: [], modified: ['.appends/LICENSE.md'] }
+                                 ])
   end
 
-  test "should not enqueue notify track org wide file change job when no file in .appends directory was added/updated/removed" do # rubocop:disable Layout/LineLength
+  test "should not notify track syncer when commit does not contain modified file in .appends directory" do
     create :track, slug: :ruby
 
-    assert_enqueued_jobs 0, only: NotifyTrackSyncerAboutTrackChangesJob do
-      Webhooks::ProcessPushUpdate.('refs/heads/main', 'problem-specs', [
-                                     { added: ['README.md'], removed: ['GENERATORS.md'], modified: ['CONTRIBUTING.md'] }
-                                   ])
-    end
+    Github::NotifyTrackSyncerAboutTrackChanges.expects(:call).never
+
+    Webhooks::ProcessPushUpdate.('refs/heads/main', 'ruby', [
+                                   { added: ['README.md'], removed: ['GENERATORS.md'], modified: ['CONTRIBUTING.md'] }
+                                 ])
   end
 
-  test "should not enqueue notify track org wide file change job when pushing to non-valid track" do
-    create :track, slug: :ruby
+  test "should not notify track syncer when pushing to non-track repo" do
+    Github::NotifyTrackSyncerAboutTrackChanges.expects(:call).never
 
-    assert_enqueued_jobs 0, only: NotifyTrackSyncerAboutTrackChangesJob do
-      Webhooks::ProcessPushUpdate.('refs/heads/main', 'problem-specs', [])
-    end
+    Webhooks::ProcessPushUpdate.('refs/heads/main', 'problem-specs', [])
   end
 end

fbbd5818765a5be9a160f61f12f722f9bafff5cd
Karlo Soriano <karlo@thalamus.ai>
2021-06-02 19:22:41 +0800

    Fix introducer hiding (#1058)

--- app/controllers/api/settings/introducers_controller.rb
+++ app/controllers/api/settings/introducers_controller.rb
@@ -3,7 +3,7 @@ module API
     class IntroducersController < BaseController
       # TODO: This is just a temporary implementation
       def hide
-        session[:hidden_introducers] = [params[:id]]
+        session[:hidden_introducers] = (session[:hidden_introducers] || []).concat([params[:id]])
 
         render json: {}
       end

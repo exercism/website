3ae901bf53872fe3b9bc609169e939a5e96476a1
Jeremy Walker <jez.walker@gmail.com>
2021-06-02 21:20:37 +0100

    Switch to sidekiq.yml

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,6 @@ require_relative 'application'
 # really wants it to be an ENV var. So let's just give it
 # its way, even though this is pretty ugly.
 ENV["SECRET_KEY_BASE"] = Exercism.secrets.website_secret_key_base
-ENV["SKYLIGHT_AUTHENTICATION"] = Exercism.secrets.skylight_key
 
 # Initialize the Rails application.
 Rails.application.initialize!

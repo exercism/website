bc37afe9b684588bc644298f764884ec635f3cae
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-02 09:50:53 +0200

    Add methods to get namespaced labels

--- app/models/github/issue_label.rb
+++ app/models/github/issue_label.rb
@@ -3,4 +3,19 @@ class Github::IssueLabel < ApplicationRecord
     inverse_of: :labels,
     foreign_key: "github_issue_id",
     class_name: "Github::Issue"
+
+  %w[action knowledge module size type].each do |label|
+    define_singleton_method "for_#{label}" do |val|
+      return unless const_get(label.pluralize.upcase.to_sym).include?(val)
+
+      "x:#{label}/#{val}"
+    end
+  end
+
+  ACTIONS = %i[create fix improve proofread sync].freeze
+  KNOWLEDGES = %i[none elementary intermediate advanced].freeze
+  MODULES = %i[analyzer concept_exercise concept generator practice_exercise representer test_runner].freeze
+  SIZES = %i[xs s m l xl].freeze
+  TYPES = %i[ci coding content docker docs].freeze
+  private_constant :ACTIONS, :KNOWLEDGES, :MODULES, :SIZES, :TYPES
 end
---
+++ test/models/github/issue_label_test.rb
@@ -0,0 +1,53 @@
+require "test_helper"
+
+class Github::IssueLabelTest < ActiveSupport::TestCase
+  %i[create fix improve proofread sync].each do |action|
+    test "for_action #{action}" do
+      assert_equal "x:action/#{action}", Github::IssueLabel.for_action(action)
+    end
+  end
+
+  test "for_action unknown" do
+    assert_nil Github::IssueLabel.for_action(:unknown)
+  end
+
+  %i[none elementary intermediate advanced].each do |knowledge|
+    test "for_knowledge #{knowledge}" do
+      assert_equal "x:knowledge/#{knowledge}", Github::IssueLabel.for_knowledge(knowledge)
+    end
+  end
+
+  test "for_knowledge unknown" do
+    assert_nil Github::IssueLabel.for_knowledge(:unknown)
+  end
+
+  %i[analyzer concept_exercise concept generator practice_exercise representer test_runner].each do |mod|
+    test "for_module #{mod}" do
+      assert_equal "x:module/#{mod}", Github::IssueLabel.for_module(mod)
+    end
+  end
+
+  test "for_module unknown" do
+    assert_nil Github::IssueLabel.for_module(:unknown)
+  end
+
+  %i[xs s m l xl].each do |size|
+    test "for_size #{size}" do
+      assert_equal "x:size/#{size}", Github::IssueLabel.for_size(size)
+    end
+  end
+
+  test "for_size unknown" do
+    assert_nil Github::IssueLabel.for_size(:unknown)
+  end
+
+  %i[ci coding content docker docs].each do |size|
+    test "for_type #{size}" do
+      assert_equal "x:type/#{size}", Github::IssueLabel.for_type(size)
+    end
+  end
+
+  test "for_type unknown" do
+    assert_nil Github::IssueLabel.for_type(:unknown)
+  end
+end

288a9ca67d7d41269b74561f01e5223b4f2fa201
Jeremy Walker <jez.walker@gmail.com>
2021-06-03 23:44:23 +0100

    Strip newline from git sha for Skylight

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ENV["SECRET_KEY_BASE"] = Exercism.secrets.website_secret_key_base
 begin
   ref = File.read(Rails.root.join(".git", "HEAD")) # This gives `ref: refs/heads/main`
   path = ref.split(':').last.strip # This extracts `refs/heads/main`
-  ENV["SKYLIGHT_DEPLOY_GIT_SHA"] = File.read(Rails.root.join(".git", path))
+  ENV["SKYLIGHT_DEPLOY_GIT_SHA"] = File.read(Rails.root.join(".git", path)).strip
 rescue StandardError => e
   Bugsnag.notify(e)
 end

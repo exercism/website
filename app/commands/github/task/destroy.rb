9e7558b6402999f46bde673e91943f79d86fe05f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-03 15:40:39 +0200

    Improve

--- app/commands/github/task/destroy.rb
+++ app/commands/github/task/destroy.rb
@@ -6,8 +6,7 @@ module Github
       initialize_with :issue_url
 
       def call
-        task = ::Github::Task.find_by(issue_url: issue_url)
-        task&.destroy
+        Github::Task.where(issue_url: issue_url).destroy_all
       end
     end
   end

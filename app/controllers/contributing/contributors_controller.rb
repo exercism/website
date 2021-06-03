2fda30637111e59f8bba2998414f7b1b723ce20a
Jeremy Walker <jez.walker@gmail.com>
2021-06-03 11:37:35 +0100

    Remove unncessary code (#1071)

--- app/controllers/contributing/contributors_controller.rb
+++ app/controllers/contributing/contributors_controller.rb
@@ -8,22 +8,6 @@ module Contributing
       # TODO: Set these correctly
       @featured_contributor = User.first
       @latest_contributor = User.second
-
-      track_id = Track.find(params[:track]).id if params[:track].present?
-      users = User::ReputationPeriod::Search.(
-        period: params[:period],
-        category: params[:category],
-        track_id: track_id,
-        user_handle: params[:user_handle],
-        page: params[:page]
-      )
-      contextual_data = User::ReputationToken::CalculateContextualData.(
-        users.map(&:id),
-        period: params[:period],
-        category: params[:category],
-        track_id: track_id
-      )
-      @contributors = SerializeContributors.(users, starting_rank: 1, contextual_data: contextual_data)
     end
   end
 end

1a238a59dd3273560817035c871941e40dc14c90
Jeremy Walker <jez.walker@gmail.com>
2021-06-03 14:56:09 +0100

    Add skylight git sha via environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,13 @@ require_relative 'application'
 # its way, even though this is pretty ugly.
 ENV["SECRET_KEY_BASE"] = Exercism.secrets.website_secret_key_base
 
+begin
+  ref = File.read(Rails.root.join(".git", "HEAD")) # This gives `ref: refs/heads/main`
+  path = ref.split(':').last.strip # This extracts `refs/heads/main`
+  ENV["SKYLIGHT_DEPLOY_GIT_SHA"] = File.read(Rails.root.join(".git", path))
+rescue StandardError => e
+  Bugsnag.notify(e)
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!

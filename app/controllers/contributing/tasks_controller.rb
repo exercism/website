43a02c1e8102234db3483e0d4d841199fe3a08ef
Jeremy Walker <jez.walker@gmail.com>
2021-06-04 15:22:26 +0100

    Add more styling

--- app/controllers/contributing/tasks_controller.rb
+++ app/controllers/contributing/tasks_controller.rb
@@ -3,19 +3,28 @@ module Contributing
     skip_before_action :authenticate_user!, only: %i[index]
 
     def index
-      @tasks = [
-        {
-          title: "Write Boolean concept exercise for Python track",
-          isNew: true,
-          track: {
-            title: Track.first.title,
-            icon_url: Track.first.icon_url
-          },
-          links: {
-            github_url: "#"
+      @data = {
+        results: [
+          {
+            title: "Write Boolean concept exercise for Python track",
+            isNew: true,
+            track: {
+              title: Track.first.title,
+              icon_url: Track.first.icon_url
+            },
+            tags: {
+              action: "create",
+              type: "docs",
+              size: "small",
+              knowledge: 'm',
+              module: 'analyzer'
+            },
+            links: {
+              github_url: "#"
+            }
           }
-        }
-      ]
+        ]
+      }
     end
   end
 end

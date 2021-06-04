1bf3beda123db177f9c77cd4aa5c74d54af47558
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-04 14:30:37 +0200

    Stored closed at for pull requests

--- test/controllers/webhooks/pull_request_updates_controller_test.rb
+++ test/controllers/webhooks/pull_request_updates_controller_test.rb
@@ -69,6 +69,7 @@ class Webhooks::PullRequestUpdatesControllerTest < Webhooks::BaseTestCase
         title: "The cat sat on the mat",
         node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
         created_at: "2019-05-15T15:20:33Z",
+        closed_at: nil,
         merged_at: "2019-05-28T08:03:01Z",
         merged: true,
         merged_by: {
@@ -91,6 +92,7 @@ class Webhooks::PullRequestUpdatesControllerTest < Webhooks::BaseTestCase
       number: 4,
       title: "The cat sat on the mat",
       created_at: Time.parse("2019-05-15T15:20:33Z").utc,
+      closed_at: nil,
       merged_at: Time.parse("2019-05-28T08:03:01Z").utc,
       merged: true,
       merged_by_username: 'merger68'
@@ -114,6 +116,7 @@ class Webhooks::PullRequestUpdatesControllerTest < Webhooks::BaseTestCase
         title: "The cat sat on the mat",
         node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
         created_at: "2019-05-15T15:20:33Z",
+        closed_at: nil,
         merged_at: nil,
         merged: false
       },
@@ -133,6 +136,51 @@ class Webhooks::PullRequestUpdatesControllerTest < Webhooks::BaseTestCase
       number: 4,
       title: "The cat sat on the mat",
       created_at: Time.parse("2019-05-15T15:20:33Z").utc,
+      closed_at: nil,
+      merged_at: nil,
+      merged: false,
+      merged_by_username: nil
+    )
+
+    post webhooks_pull_request_updates_path, headers: headers(payload), as: :json, params: payload
+  end
+
+  test "create for closed pr should process pr update when signature is valid" do
+    payload = {
+      action: 'closed',
+      pull_request: {
+        user: {
+          login: 'user22'
+        },
+        url: 'https://api.github.com/repos/exercism/fsharp/pulls/1347',
+        html_url: 'https://github.com/exercism/fsharp/pull/1347',
+        labels: [{ name: "bug" }, { name: "duplicate" }],
+        state: 'closed',
+        number: 4,
+        title: "The cat sat on the mat",
+        node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
+        created_at: "2019-05-15T15:20:33Z",
+        closed_at: "2019-05-20T08:44:11Z",
+        merged_at: nil,
+        merged: false
+      },
+      repository: {
+        full_name: 'exercism/fsharp'
+      }
+    }
+    Webhooks::ProcessPullRequestUpdate.expects(:call).with(
+      action: 'closed',
+      author_username: 'user22',
+      url: 'https://api.github.com/repos/exercism/fsharp/pulls/1347',
+      html_url: 'https://github.com/exercism/fsharp/pull/1347',
+      labels: %w[bug duplicate],
+      state: 'closed',
+      repo: 'exercism/fsharp',
+      node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
+      number: 4,
+      title: "The cat sat on the mat",
+      created_at: Time.parse("2019-05-15T15:20:33Z").utc,
+      closed_at: Time.parse("2019-05-20T08:44:11Z").utc,
       merged_at: nil,
       merged: false,
       merged_by_username: nil

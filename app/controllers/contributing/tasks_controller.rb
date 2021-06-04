1df73c34445cceb6604088c57e30d299b6194f8f
Jeremy Walker <jez.walker@gmail.com>
2021-06-04 17:17:27 +0100

    Add more icons for tasks UI

--- app/controllers/contributing/tasks_controller.rb
+++ app/controllers/contributing/tasks_controller.rb
@@ -3,28 +3,151 @@ module Contributing
     skip_before_action :authenticate_user!, only: %i[index]
 
     def index
-      @data = {
-        results: [
-          {
-            title: "Write Boolean concept exercise for Python track",
-            isNew: true,
-            track: {
-              title: Track.first.title,
-              icon_url: Track.first.icon_url
-            },
-            tags: {
-              action: "create",
-              type: "docs",
-              size: "small",
-              knowledge: 'm',
-              module: 'analyzer'
-            },
-            links: {
-              github_url: "#"
-            }
-          }
-        ]
-      }
+      @data = AssembleTasks.(params)
+
+      # @data = {
+      #   results: [
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "create",
+      #         type: "docs",
+      #         size: "small",
+      #         knowledge: 'elementary',
+      #         module: 'analyzer'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "improve",
+      #         type: "coding",
+      #         size: "tiny",
+      #         knowledge: 'advanced',
+      #         module: 'practice-exercise'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "fix",
+      #         type: "docker",
+      #         size: "medium",
+      #         knowledge: 'intermediate',
+      #         module: 'concept-exercise'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "sync",
+      #         type: "ci",
+      #         size: "large",
+      #         knowledge: 'advanced',
+      #         module: 'test-runner'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "proofread",
+      #         type: "content",
+      #         size: "massive",
+      #         knowledge: 'none',
+      #         module: 'generator'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "fix",
+      #         type: "docker",
+      #         size: "tiny",
+      #         knowledge: 'none',
+      #         module: 'representer'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {
+      #         action: "fix",
+      #         type: "docker",
+      #         size: "tiny",
+      #         knowledge: 'none',
+      #         module: 'concept'
+      #       },
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     },
+
+      #     {
+      #       title: "Write Boolean concept exercise for Python track",
+      #       isNew: true,
+      #       track: {
+      #         title: Track.first.title,
+      #         icon_url: Track.first.icon_url
+      #       },
+      #       tags: {},
+      #       links: {
+      #         github_url: "#"
+      #       }
+      #     }
+      #   ]
+      # }
     end
   end
 end

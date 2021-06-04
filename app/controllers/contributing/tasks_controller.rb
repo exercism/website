4eaf8b718985c062c4cc6791a9cad638b830782d
Jeremy Walker <jez.walker@gmail.com>
2021-06-04 11:55:49 +0100

    Use serialized data

--- app/controllers/contributing/tasks_controller.rb
+++ app/controllers/contributing/tasks_controller.rb
@@ -2,6 +2,20 @@ module Contributing
   class TasksController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index]
 
-    def index; end
+    def index
+      @tasks = [
+        {
+          title: "Write Boolean concept exercise for Python track",
+          isNew: true,
+          track: {
+            title: Track.first.title,
+            icon_url: Track.first.icon_url
+          },
+          links: {
+            github_url: "#"
+          }
+        }
+      ]
+    end
   end
 end

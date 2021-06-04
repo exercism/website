2298fcdd5766ef69a13edc285097ff00d52f3025
Karlo Soriano <karlo@thalamus.ai>
2021-06-04 17:14:28 +0800

    Make tests pass

--- test/system/flows/student_updates_solution_test.rb
+++ test/system/flows/student_updates_solution_test.rb
@@ -10,7 +10,10 @@ module Flows
       track = create :track
       create :user_track, user: user, track: track
       exercise = create :concept_exercise, track: track, title: "Lasagna"
-      create :concept_solution, git_sha: "0913c69f21b3f81477337b259a21fb7278393bc1", exercise: exercise, user: user
+      create :concept_solution,
+        git_sha: "ef19c86ee73dfbd3df8f3d49251008783a51de91",
+        exercise: exercise,
+        user: user
 
       use_capybara_host do
         sign_in!(user)

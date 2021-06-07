d55758973f8376b68deced7cb89d4421bcf8571f
Karlo Soriano <karlo@thalamus.ai>
2021-06-07 13:38:25 +0800

    Scroll to top when switching steps

--- app/javascript/components/common/Walkthrough.tsx
+++ app/javascript/components/common/Walkthrough.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from 'react'
+import $ from 'jquery'
 import initWalkthrough from '@exercism/twine2-story-format/src/index'
 
 export const Walkthrough = ({ html }: { html: string }): JSX.Element => {
   useEffect(() => {
     initWalkthrough
+
+    const scrollToTop = () => {
+      window.scrollTo({ top: 0 })
+    }
+
+    $(window).on('shown.sm.passage', scrollToTop)
+
+    return () => {
+      $(window).off('shown.sm.passage', scrollToTop)
+    }
   }, [])
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />

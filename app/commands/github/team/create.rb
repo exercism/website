98d2564a18fa7eb28cb22fdcd794190ddd8fdf17
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-17 16:11:46 +0200

    Fix failing tests

--- app/commands/github/team/create.rb
+++ app/commands/github/team/create.rb
@@ -1,11 +1,13 @@
-module Github::Team
-  class Create
-    include Mandate
+module Github
+  class Team
+    class Create
+      include Mandate
 
-    initialize_with :name, :repo
+      initialize_with :name, :repo
 
-    def call
-      Exercism.octokit_client.create_team('exercism', name: name, repo_names: [repo])
+      def call
+        Exercism.octokit_client.create_team('exercism', name: name, repo_names: [repo])
+      end
     end
   end
 end

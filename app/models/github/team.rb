ec7e74a6c13776d24ecd82a77aa21c131222ffb1
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-17 16:22:03 +0200

    Add guard to team

--- app/models/github/team.rb
+++ app/models/github/team.rb
@@ -4,23 +4,33 @@ class Github::Team
   initialize_with :name
 
   def add_member(github_username)
+    return unless active?
+
     Exercism.octokit_client.add_team_membership(team_id, github_username)
   end
 
   def remove_member(github_username)
+    return unless active?
+
     Exercism.octokit_client.remove_team_membership(team_id, github_username)
   end
 
   def add_to_repository(repo)
+    return unless active?
+
     Exercism.octokit_client.add_team_repository(team_id, repo)
   end
 
   def remove_from_repository(repo)
+    return unless active?
+
     Exercism.octokit_client.remove_team_repository(team_id, repo)
   end
 
   memoize
   def members
+    return unless active?
+
     Exercism.octokit_client.team_members(team_id)
   end
 
@@ -29,5 +39,10 @@ class Github::Team
     # The octokit client does not surface the API method to retrieve a team
     # by its github team name, so we just call it directly
     Exercism.octokit_client.get("https://api.github.com/orgs/exercism/teams/#{name}")[:id]
+  end
+
+  def active?
+    # TODO: remove once we're live
+    !Rails.env.production?
   end
 end

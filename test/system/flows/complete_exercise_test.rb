6d9a8d3556f5058ec414af17b32d5f3de419f1bb
Jeremy Walker <jez.walker@gmail.com>
2021-06-17 23:26:38 +0100

    Fix over-enthusiastic unlocked modal

--- test/system/flows/complete_exercise_test.rb
+++ test/system/flows/complete_exercise_test.rb
@@ -7,61 +7,77 @@ module Flows
 
     test "completes succesfully" do
       track = create :track
-      basics_concept = create :concept, track: track, name: "Basics"
-      conditionals_concept = create :concept, track: track, name: "Conditionals"
+      basics = create :concept, track: track, name: "Basics"
+      conditionals = create :concept, track: track, name: "Conditionals"
+      strings = create :concept, track: track, name: "Strings"
 
-      strings = create :concept_exercise, track: track
-      strings.taught_concepts << basics_concept
-      hello = create :practice_exercise, track: track, slug: "bob", title: "Bob"
-      hello.prerequisites << basics_concept
+      boutique = create :concept_exercise, track: track, slug: 'boutique', title: "Boutique"
+      boutique.taught_concepts << basics
+
+      bob = create :practice_exercise, track: track, slug: "bob", title: "Bob"
+      bob.prerequisites << basics
 
       cars = create :concept_exercise, track: track, slug: "cars-assemble", title: "Cars Assemble"
-      cars.prerequisites << basics_concept
-      cars.taught_concepts << conditionals_concept
+      cars.prerequisites << basics
+      cars.taught_concepts << conditionals
+
+      leap = create :practice_exercise, track: track, slug: "leap", title: "Leap"
+      leap.prerequisites << basics
+      leap.prerequisites << conditionals
+
+      misc = create(:concept_exercise, track: track)
+      misc.taught_concepts << strings
+      misc.prerequisites << conditionals
 
       user = create :user
       create :user_track, user: user, track: track
-      solution = create :concept_solution, user: user, exercise: strings
+      solution = create :concept_solution, user: user, exercise: boutique
       submission = create :submission, solution: solution
       create :iteration, submission: submission
 
       sign_in!(user)
 
       use_capybara_host do
-        visit track_exercise_url(track, strings)
+        visit track_exercise_url(track, boutique)
 
         click_on "Mark as complete"
         click_on "Confirm"
 
-        assert_text "You've completed\nStrings!"
+        assert_text "You've completed\nBoutique!"
         assert_text "You've unlocked\n2 exercises"
         assert_text "Bob"
         assert_text "Cars Assemble"
+        refute_text "Leap"
         assert_text "Basics"
         assert_text "Conditionals"
+        refute_text "Strings"
 
         click_on "Continue"
-        assert_text "You've completed Strings."
+        assert_text "You've completed Boutique."
+
+        within '.unlocked-exercises' do
+          assert_text "Bob"
+          assert_text "Cars Assemble"
+          refute_text "Leap"
+        end
+
+        within '.unlocked-concepts' do
+          assert_text "Conditionals"
+          refute_text "Strings"
+        end
       end
     end
 
     test "completes succesfully via nudge section" do
       track = create :track
-      basics_concept = create :concept, track: track, name: "Basics"
-      conditionals_concept = create :concept, track: track, name: "Conditionals"
+      basics = create :concept, track: track, name: "Basics"
 
-      strings = create :concept_exercise, track: track
-      strings.taught_concepts << basics_concept
-      hello = create :practice_exercise, track: track, slug: "bob", title: "Bob"
-      hello.prerequisites << basics_concept
-
-      cars = create :concept_exercise, track: track, slug: "cars-assemble", title: "Cars Assemble"
-      cars.prerequisites << basics_concept
-      cars.taught_concepts << conditionals_concept
+      boutique = create :concept_exercise, track: track, slug: 'boutique', title: "Boutique"
+      boutique.taught_concepts << basics
 
       user = create :user
       create :user_track, user: user, track: track
-      solution = create :concept_solution, user: user, exercise: strings
+      solution = create :concept_solution, user: user, exercise: boutique
       submission = create :submission, solution: solution,
                                        tests_status: :passed,
                                        representation_status: :generated,
@@ -71,21 +87,16 @@ module Flows
       sign_in!(user)
 
       use_capybara_host do
-        visit track_exercise_url(track, strings)
+        visit track_exercise_url(track, boutique)
 
         within("section.completion-nudge") { click_on "Mark as complete" }
         click_on "Confirm"
 
-        assert_text "You've completed\nStrings!"
-        assert_text "You've unlocked\n2 exercises"
-        assert_text "Bob"
-        assert_text "Cars Assemble"
-        assert_text "Basics"
-        assert_text "Conditionals"
+        assert_text "You've completed\nBoutique!"
 
         click_on "Continue"
 
-        assert_text "You've completed Strings."
+        assert_text "You've completed Boutique."
         assert_no_css("section.completion-nudge")
       end
     end

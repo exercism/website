0ed32c6e337071d701079008f17c25f9207d6ae9
Jeremy Walker <jez.walker@gmail.com>
2021-06-20 19:00:20 +0100

    Conditionally add things

--- app/helpers/react_components/maintaining/submissions_summary_table.rb
+++ app/helpers/react_components/maintaining/submissions_summary_table.rb
@@ -43,8 +43,7 @@ module ReactComponents
           data = submission.tests_status
           if submission.tests_errored? || submission.tests_exceptioned?
             job = Exercism::ToolingJob.find(submission.test_run.tooling_job_id)
-            data += "\n\nSTDOUT:\n------\n#{job.stdout}"
-            data += "\n\nSTDERR:\n------\n#{job.stderr}"
+            data = append_exception_data(data, job)
           end
           data
         end
@@ -53,8 +52,7 @@ module ReactComponents
           data = submission.representation_status
           if submission.representation_exceptioned?
             job = Exercism::ToolingJob.find(submission.submission_representation.tooling_job_id)
-            data += "\n\nSTDOUT:\n------\n#{job.stdout}"
-            data += "\n\nSTDERR:\n------\n#{job.stderr}"
+            data = append_exception_data(data, job)
           end
           data
         end
@@ -63,11 +61,17 @@ module ReactComponents
           data = submission.analysis_status
           if submission.analysis_exceptioned?
             job = Exercism::ToolingJob.find(submission.analysis.tooling_job_id)
-            data += "\n\nSTDOUT:\n------\n#{job.stdout}"
-            data += "\n\nSTDERR:\n------\n#{job.stderr}"
+            data = append_exception_data(data, job)
           elsif submission.analysis
             data += "\n\n#{submission.analysis.send(:data)}"
           end
+          data
+        end
+
+        def append_exception_data(data, job)
+          data += "\n\nSTDOUT:\n------\n#{job.stdout}" if job.stdout.present?
+          data += "\n\nSTDERR:\n------\n#{job.stderr}" if job.stderr.present?
+          data += "\n\nException Details:\n------\n#{job.execution_exception}" if job.execution_exception.present?
           data
         end
       end

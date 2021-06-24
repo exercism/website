c00649db0548ca22f3fb3917c212e839d3d3d3d5
Jeremy Walker <jez.walker@gmail.com>
2021-06-24 19:32:13 +0100

    Bring the tests in line with changes

--- test/system/flows/student/finish_mentor_discussion/unhappy_test.rb
+++ test/system/flows/student/finish_mentor_discussion/unhappy_test.rb
@@ -24,14 +24,14 @@ module Flows
             sign_in!(user)
             visit track_exercise_mentor_discussion_path(solution.track, solution.exercise, discussion)
             click_on "End discussion"
-            within(".m-confirm-finish-student-mentor-discussion") { click_on "End discussion" }
+            within(".m-confirm-finish-student-mentor-discussion") { click_on "Review and end discussion" }
             click_on "Problematic"
             find('label', text: "Report this discussion to an admin").click
             fill_in "What went wrong?", with: "Bad mentor"
             click_on "Code of Conduct violation"
             find("label", text: "Wrong or misleading information").click
             click_on "Finish"
-            click_on "View your solution"
+            click_on "Go back to your solution"
 
             assert_equal :finished, discussion.reload.status
             assert_text "View mentoring request"

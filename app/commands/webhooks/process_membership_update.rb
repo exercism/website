1a35ed6287b9e2ce83cf6ab46eeec5cc762481db
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-24 10:06:53 +0200

    Ensure members are synced

--- app/commands/webhooks/process_membership_update.rb
+++ app/commands/webhooks/process_membership_update.rb
@@ -7,30 +7,24 @@ module Webhooks
     def call
       # TODO: use organization as defined in Exercism.config.github_organization
       return unless organization_name == 'exercism'
+      return unless action == 'added' || action == 'removed'
+      return unless team
+      return unless user
 
-      case action
-      when 'added'
-        add_membership!
-      when 'removed'
-        remove_membership!
-      end
-    end
-
-    private
-    def add_membership!
+      # If the action was 'removed', that means the membership was removed on GitHub.
+      # However, as the data in our database is leading, we'll re-add the membership
       ContributorTeam::Membership::CreateOrUpdate.(user, team, status: :active)
     end
 
-    def remove_membership!
-      ContributorTeam::Membership::Destroy.(user, team)
-    end
-
+    private
+    memoize
     def user
-      User.find_by!(github_username: user_name)
+      User.find_by(github_username: user_name)
     end
 
+    memoize
     def team
-      ContributorTeam.find_by!(github_name: team_name)
+      ContributorTeam.find_by(github_name: team_name)
     end
   end
 end

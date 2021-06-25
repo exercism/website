3a66ba10e8583e3577c4d048ff28f4f4d5d5d198
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-25 16:48:08 +0200

    Fix more tests

--- test/commands/github/organization_member/remove_when_no_team_memberships_test.rb
+++ test/commands/github/organization_member/remove_when_no_team_memberships_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class Github::OrganizationMember::RemoveWhenNoTeamMembershipsTest < ActiveSupport::TestCase
   test "removes user from organization if user is not a member of any team" do
-    Github::Organization.any_instance.stubs(:team_membership_count_for_user).with('user22').returns(0)
     Github::Organization.any_instance.stubs(:name).returns('exercism')
+    Github::Organization.any_instance.stubs(:team_membership_count_for_user).with('user22').returns(0)
 
     stub_request(:delete, "https://api.github.com/orgs/exercism/members/user22").
       to_return(status: 200, body: "", headers: {})
--- test/commands/webhooks/process_organization_member_update_test.rb
+++ test/commands/webhooks/process_organization_member_update_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class Webhooks::ProcessOrganizationMemberUpdateTest < ActiveSupport::TestCase
   test "adds member if action is 'member_added' and organization is exercism" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     Webhooks::ProcessOrganizationMemberUpdate.('member_added', 'user22', 'exercism')
 
     member = Github::OrganizationMember.find_by(username: 'user22')
@@ -9,18 +11,24 @@ class Webhooks::ProcessOrganizationMemberUpdateTest < ActiveSupport::TestCase
   end
 
   test "does not add member if action is 'member_added' and organization is not exercism" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     Webhooks::ProcessOrganizationMemberUpdate.('member_added', 'user22', 'other-org')
 
     refute Github::OrganizationMember.where(username: 'user22').exists?
   end
 
   test "does not add member if action is not 'member_added'" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     Webhooks::ProcessOrganizationMemberUpdate.('other-action', 'user22', 'exercism')
 
     refute Github::OrganizationMember.where(username: 'user22').exists?
   end
 
   test "makes member alumnus if action is 'member_removed' and organization is exercism" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     create :github_organization_member, username: 'user22'
 
     Webhooks::ProcessOrganizationMemberUpdate.('member_removed', 'user22', 'exercism')
@@ -30,6 +38,8 @@ class Webhooks::ProcessOrganizationMemberUpdateTest < ActiveSupport::TestCase
   end
 
   test "does not makes member alumnus if action is 'member_removed' and organization is not exercism" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     create :github_organization_member, username: 'user22'
 
     Webhooks::ProcessOrganizationMemberUpdate.('member_added', 'user22', 'other-org')
@@ -39,6 +49,8 @@ class Webhooks::ProcessOrganizationMemberUpdateTest < ActiveSupport::TestCase
   end
 
   test "does not remove member if action is not 'member_removed'" do
+    Github::Organization.any_instance.stubs(:name).returns('exercism')
+
     create :github_organization_member, username: 'user22'
 
     Webhooks::ProcessOrganizationMemberUpdate.('other-action', 'user22', 'exercism')

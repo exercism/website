c9db6531efb79b28848dbd964d5cb548b8ae8ec3
Jeremy Walker <jez.walker@gmail.com>
2021-06-25 17:09:45 +0100

    Fix bug with incorrect guessing of mentoring session (#1160)
    * Fix bug with incorrect guessing of mentoring session
    
    * Fix tests
    
    * Fix tests

--- test/factories/mentor/discussions.rb
+++ test/factories/mentor/discussions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :mentor_discussion, class: 'Mentor::Discussion' do
     mentor { create :user }
-    request { create :mentor_request }
     solution { create :practice_solution, track: track }
+    request { create :mentor_request, solution: solution, status: :fulfilled }
 
     transient do
       track do
--- test/helpers/react_components/mentoring/session_test.rb
+++ test/helpers/react_components/mentoring/session_test.rb
@@ -2,7 +2,7 @@ require_relative "../react_component_test_case"
 
 module Mentoring
   class SessionTest < ReactComponentTestCase
-    test "mentoring solution renders correctly" do
+    test "mentoring request renders correctly" do
       TestHelpers.use_website_copy_test_repo!
 
       mentor = create :user
@@ -10,11 +10,57 @@ module Mentoring
       track = create :track, slug: "ruby"
       exercise = create :concept_exercise, track: track, slug: "clock"
       solution = create :concept_solution, user: student, exercise: exercise
-      discussion = create :mentor_discussion, solution: solution, mentor: mentor
       mentor_request = create :mentor_request,
         solution: solution,
         comment_markdown: "Hello",
         updated_at: Time.utc(2016, 12, 25)
+
+      iteration_1 = create :iteration, solution: solution
+      iteration_2 = create :iteration, solution: solution
+      iteration_3 = create :iteration, solution: solution
+
+      component = ReactComponents::Mentoring::Session.new(request: mentor_request)
+      component.stubs(current_user: mentor)
+      scratchpad = ScratchpadPage.new(about: exercise)
+
+      assert_component component,
+        "mentoring-session",
+        {
+          user_id: mentor.id,
+          request: SerializeMentorSessionRequest.(mentor_request),
+          discussion: nil,
+          track: SerializeMentorSessionTrack.(track),
+          exercise: SerializeMentorSessionExercise.(exercise),
+          iterations: [
+            SerializeIteration.(iteration_1).merge(num_comments: 0, unread: false),
+            SerializeIteration.(iteration_2).merge(num_comments: 0, unread: false),
+            SerializeIteration.(iteration_3).merge(num_comments: 0, unread: false)
+          ],
+          student: SerializeStudent.(student, relationship: nil, anonymous_mode: false),
+          mentor_solution: nil,
+          notes: %(<p>Clock introduces students to the concept of value objects and modular arithmetic.</p>\n<p>Note: This exercise changes a lot depending on which version the person has solved.</p>\n), # rubocop:disable Layout/LineLength
+          out_of_date: false,
+          links: {
+            mentor_dashboard: Exercism::Routes.mentoring_inbox_path,
+            exercise: Exercism::Routes.track_exercise_path(track, exercise),
+            scratchpad: Exercism::Routes.api_scratchpad_page_path(scratchpad.category, scratchpad.title)
+          }
+        }
+    end
+
+    test "mentoring discussion renders correctly" do
+      TestHelpers.use_website_copy_test_repo!
+
+      mentor = create :user
+      student = create :user
+      track = create :track, slug: "ruby"
+      exercise = create :concept_exercise, track: track, slug: "clock"
+      solution = create :concept_solution, user: student, exercise: exercise
+      mentor_request = create :mentor_request,
+        solution: solution,
+        comment_markdown: "Hello",
+        updated_at: Time.utc(2016, 12, 25)
+      discussion = create :mentor_discussion, solution: solution, mentor: mentor, request: mentor_request
 
       iteration_1 = create :iteration, solution: solution
       iteration_2 = create :iteration, solution: solution
@@ -24,7 +70,7 @@ module Mentoring
       create :mentor_discussion_post, discussion: discussion, iteration: iteration_3, seen_by_mentor: true
       create :mentor_discussion_post, discussion: discussion, iteration: iteration_3, seen_by_mentor: false
 
-      component = ReactComponents::Mentoring::Session.new(solution)
+      component = ReactComponents::Mentoring::Session.new(discussion: discussion)
       component.stubs(current_user: mentor)
       scratchpad = ScratchpadPage.new(about: exercise)
 

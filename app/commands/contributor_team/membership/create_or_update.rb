328ac4fae6380c0de60596e66fed2f830ba2aa56
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-25 19:01:08 +0200

    Update app/commands/contributor_team/membership/create_or_update.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- app/commands/contributor_team/membership/create_or_update.rb
+++ app/commands/contributor_team/membership/create_or_update.rb
@@ -5,7 +5,7 @@ class ContributorTeam::Membership
     initialize_with :user, :team, :attributes
 
     def call
-      ContributorTeam::Membership.create!(user: user, team: team, **attributes).tap do |_membership|
+      ContributorTeam::Membership.create!(user: user, team: team, **attributes).tap do
         user.update(roles: user.roles + [team.role_for_members]) unless user.roles.include?(team.role_for_members)
 
         team.github_team.add_membership(user.github_username)

ec36bf290d170554294d63c837ad41021b80988f
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-01 16:17:09 +0200

    Reload data when updating concept/exercise authors/contributors (#1185)

--- app/commands/git/sync_exercise_authors.rb
+++ app/commands/git/sync_exercise_authors.rb
@@ -13,7 +13,7 @@ module Git
         authors.find_each { |author| ::Exercise::Authorship::Create.(exercise, author) }
 
         # This is required to remove authors that were already added
-        exercise.update!(authors: authors)
+        exercise.reload.update!(authors: authors)
 
         # TODO: consider what to do with missing authors
         missing_authors = authors_config - authors.pluck(:github_username)

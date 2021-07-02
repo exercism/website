8c302e15fd019f8e687b9ac9591e8a207dbd3606
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-02 13:58:35 +0200

    Use organization to remove membe

--- app/commands/github/organization_member/remove_when_no_team_memberships.rb
+++ app/commands/github/organization_member/remove_when_no_team_memberships.rb
@@ -8,7 +8,7 @@ module Github
       def call
         return if organization.team_membership_count_for_user(github_username) >= 1
 
-        Exercism.octokit_client.remove_organization_member(organization.name, github_username)
+        organization.remove_member(github_username)
       end
 
       private

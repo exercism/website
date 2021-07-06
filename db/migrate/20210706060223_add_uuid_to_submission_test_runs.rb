11f639f8699afe21c619af8c262202760a52da97
Jeremy Walker <jez.walker@gmail.com>
2021-07-06 12:25:35 +0100

    Allow migrations to run in production

--- db/migrate/20210706060223_add_uuid_to_submission_test_runs.rb
+++ db/migrate/20210706060223_add_uuid_to_submission_test_runs.rb
@@ -1,7 +1,8 @@
 class AddUuidToSubmissionTestRuns < ActiveRecord::Migration[6.1]
   def change
-    add_column :submission_test_runs, :uuid, :string, null: false
-
+    add_column :submission_test_runs, :uuid, :string, null: true
+    Submission::TestRun.update_all("`uuid` = UUID()")
+    change_column_null :submission_test_runs, :uuid, false
     add_index :submission_test_runs, :uuid, unique: true
   end
 end
--- db/migrate/20210706061801_add_uuid_to_github_tasks.rb
+++ db/migrate/20210706061801_add_uuid_to_github_tasks.rb
@@ -1,7 +1,8 @@
 class AddUuidToGithubTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :github_tasks, :uuid, :string, null: false
-
+    add_column :github_tasks, :uuid, :string, null: true
+    Github::Task.update_all("`uuid` = UUID()")
+    change_column_null :github_tasks, :uuid, false
     add_index :github_tasks, :uuid, unique: true
   end
 end

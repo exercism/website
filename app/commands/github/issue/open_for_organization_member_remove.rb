0b6fa810d0f808f4ae6d31013499d599d9fc36bb
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-15 15:09:00 +0200

    Simplify issue creation

--- app/commands/github/issue/open_for_organization_member_remove.rb
+++ app/commands/github/issue/open_for_organization_member_remove.rb
@@ -8,47 +8,24 @@ module Github
       initialize_with :organization, :github_username
 
       def call
-        if missing?
-          create_issue
-        elsif closed?
-          reopen_issue
-        end
+        Github::Issue::Open.(repo, title, body)
       end
 
       private
-      def create_issue
-        title = "🤖 Attempt to remove member `#{github_username}` from org `#{organization}`"
-        body = <<~BODY.strip
-          The system tried to automatically remove the user `#{github_username}` from the `#{organization}` organization.
-
-          CC @exercism/maintainers-admin
-        BODY
-
-        Exercism.octokit_client.create_issue(repo, title, body)
-      end
-
-      def reopen_issue
-        Exercism.octokit_client.reopen_issue(repo, issue.number)
-      end
-
-      def missing?
-        issue.nil?
-      end
-
-      def closed?
-        issue.state == "closed"
-      end
-
-      memoize
       def repo
         "exercism/erikschierboom"
       end
 
-      memoize
-      def issue
-        # TODO: Elevate this into exercism-config gem
-        author = "exercism-bot"
-        Exercism.octokit_client.search_issues("\"#{github_username}\" \"#{organization}\" is:issue in:title repo:#{repo} author:#{author}")[:items]&.first # rubocop:disable Layout/LineLength
+      def title
+        "🤖 Attempt to remove member `#{github_username}` from org `#{organization}`"
+      end
+
+      def body
+        <<~BODY.strip
+          The system tried to automatically remove the user `#{github_username}` from the `#{organization}` organization.
+
+          CC @exercism/maintainers-admin
+        BODY
       end
     end
   end

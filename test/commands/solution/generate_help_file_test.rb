343c31bc70527b86609e9fd357bc1ee1c4b14b19
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-16 12:45:16 +0200

    Add test for multiple file paths

--- test/commands/solution/generate_help_file_test.rb
+++ test/commands/solution/generate_help_file_test.rb
@@ -86,4 +86,48 @@ class Solution::GenerateHelpFileTest < ActiveSupport::TestCase
     EXPECTED
     assert_equal expected, contents
   end
+
+  test "generate for exercise with multiple solution files" do
+    exercise = create :practice_exercise
+    solution = create :practice_solution, exercise: exercise
+    Git::Exercise.any_instance.stubs(:solution_filepaths).returns(['bob.rb', 'lib.rb'])
+
+    contents = Solution::GenerateHelpFile.(solution)
+    expected = <<~EXPECTED.strip
+      # Help
+
+      ## Running the tests
+
+      Run the tests using `ruby test`.
+
+      ## Submitting your solution
+
+      You can submit your solution using the `exercism submit bob.rb lib.rb` command.
+      This command will upload your solution to the Exercism website and print the solution page's URL.
+
+      It's possible to submit an incomplete solution which allows you to:
+
+      - See how others have completed the exercise
+      - Request help from a mentor
+
+      ## Need to get help?
+
+      If you'd like help solving the exercise, check the following pages:
+
+      - [Installing Ruby locally](https://exercism.io/docs/tracks/ruby/installation/)
+      - [How to learn Ruby](https://exercism.io/docs/tracks/ruby/learning/)
+      - [Useful Ruby resources](https://exercism.io/docs/tracks/ruby/resources/)
+      - [Testing on the Ruby track](https://exercism.io/docs/tracks/ruby/tests/)
+
+      For more general, non-track specific help, check:
+
+      - [Exercism's support channel on gitter](https://gitter.im/exercism/support)
+      - The [Frequently Asked Questions](https://exercism.io/docs/faq) TODO: (Required) use correct link
+
+      Should those resources not suffice, you could submit your (incomplete) solution to request mentoring.
+
+      Stuck? Try the Ruby gitter channel.
+    EXPECTED
+    assert_equal expected, contents
+  end
 end

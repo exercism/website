adf8408bc2b45d380e29f8cae8caf2711b634ad4
Jeremy Walker <jez.walker@gmail.com>
2021-07-18 12:42:07 +0100

    Add empty state for tracks

--- app/javascript/components/student/tracks-list/List.tsx
+++ app/javascript/components/student/tracks-list/List.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Track } from './Track'
+import { GraphicalIcon } from '../../common'
 import { StudentTrack } from '../../types'
 
 export const List = ({ data }: { data: StudentTrack[] }): JSX.Element => {
   if (data.length === 0) {
-    return <p>No results found</p>
+    return (
+      <div className="empty-state">
+        <GraphicalIcon icon="sad-exercism" className="tw-mb-20" />
+        <h3 className="text-h3 tw-mb-24">No results found</h3>
+        <button className="btn-enhanced btn-s">
+          <GraphicalIcon icon="reset" />
+          <span>Reset search and filters</span>
+        </button>
+      </div>
+    )
   }
 
   const joinedTracks = data.filter((track) => track.isJoined)

f7f2740a41ede02b87f77def80c2c959f06096b5
Karlo Soriano <karlo@thalamus.ai>
2021-07-26 06:17:57 +0800

    Extract AssembleExerciseWidget

--- app/helpers/react_components/common/exercise_widget.rb
+++ app/helpers/react_components/common/exercise_widget.rb
@@ -23,26 +23,24 @@ module ReactComponents
       end
 
       def to_s
-        super("common-exercise-widget", {
-          exercise: SerializeExercise.(exercise, user_track: user_track, recommended: recommended),
-          track: render_track ? SerializeTrack.(exercise.track, user_track) : nil,
-          solution: solution ? SerializeSolution.(solution, user_track: user_track) : nil,
-          links: links,
+        data = AssembleExerciseWidget.(
+          exercise,
+          user_track,
+          solution: solution,
+          with_tooltip: with_tooltip,
           render_as_link: render_as_link,
           render_blurb: render_blurb,
+          render_track: render_track,
+          recommended: recommended,
           skinny: skinny
-        })
+        )
+
+        super("common-exercise-widget", data)
       end
 
       private
       attr_reader :exercise, :user_track, :solution, :with_tooltip, :render_as_link, :render_blurb, :render_track,
         :recommended, :skinny
-
-      def links
-        {
-          tooltip: with_tooltip ? Exercism::Routes.tooltip_track_exercise_path(exercise.track, exercise) : nil
-        }.compact
-      end
     end
   end
 end

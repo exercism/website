23a925fdb6dfb639849b50238afce6eef1de89d7
Karlo Soriano <karlo@thalamus.ai>
2021-07-26 07:36:15 +0800

    Add test to ensure that minimagick is installed

---
+++ test/misc/image_analysis_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class ImageAnalysisTest < ActiveSupport::TestCase
+  test "image analysis works" do
+    user = create :user
+    user.avatar.attach(io: File.open(Rails.root.join("test", "fixtures", "test.jpg")),
+                       filename: "test.jpg",
+                       content_type: "image/jpeg")
+
+    user.avatar.analyze
+
+    blob = ActiveStorage::Blob.last
+    refute_nil blob.metadata["width"]
+    refute_nil blob.metadata["height"]
+  end
+end

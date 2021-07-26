8bd0f5b1bc0ebb60f83501473a6c260709ce7baf
Jeremy Walker <jez.walker@gmail.com>
2021-07-26 19:28:35 +0100

    Don't log deadlocked jobs in Bugsnag (#1319)
    * Don't log deadlocked jobs in Bugsnag
    
    * Guard aagainst deserializatione errors

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,7 +1,11 @@
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  # For some errors we want to put the job back on the queue by raising and
+  # letting Sidekiq handle things, but don't want to raise the exceptions
+  # to bugsnag. This block handles that.
+  skip_bugnag_and_raise = lambda do |exception|
+    exception.define_singleton_method(:skip_bugsnag) { true }
+    raise exception
+  end
+  rescue_from ActiveRecord::Deadlocked, &skip_bugnag_and_raise
+  rescue_from ActiveJob::DeserializationError, &skip_bugnag_and_raise
 end

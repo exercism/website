8cc4b886415f717ae66b226f10e3461bc420da62
Jeremy Walker <jez.walker@gmail.com>
2021-07-26 14:22:07 +0100

    Guard missing iteration in snippet generator

--- app/jobs/generate_iteration_snippet_job.rb
+++ app/jobs/generate_iteration_snippet_job.rb
@@ -2,7 +2,14 @@ class GenerateIterationSnippetJob < ApplicationJob
   queue_as :snippets
 
   def perform(iteration)
-    # TODO: Set this through Exercism config
+    # Sometimes the iteration might be deleted
+    # before we get to the job. There *shouldn't* be
+    # a DB race condition because of the way this is called
+    # in Iteration::Create but if we see missing snippets,
+    # that's probably worth investigating
+    return unless iteration
+
+    # TODO: (Required) Set this through Exercism config
     url = "https://g7ngvhuv5l.execute-api.eu-west-2.amazonaws.com/production/extract_snippet"
     snippet = RestClient.post(
       url,

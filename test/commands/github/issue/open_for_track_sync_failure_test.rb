7cda7523994a0a565653e4dd04566294b6d5c9db
Jeremy Walker <jez.walker@gmail.com>
2021-07-26 22:07:52 +0100

    Guard against no main branch in Syncing

--- test/commands/github/issue/open_for_track_sync_failure_test.rb
+++ test/commands/github/issue/open_for_track_sync_failure_test.rb
@@ -4,7 +4,7 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
   test "opens issue when issue was not yet created" do
     Exercism.config.stubs(:github_bot_username).returns('exercism-bot')
 
-    head_git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
+    git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
 
     # Raise the error and catch it to ensure a proper backtrace
     begin
@@ -12,9 +12,6 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
     rescue StandardError => e
       exception = e
     end
-
-    track = create :track, slug: 'ruby'
-    track.stubs(:git_head_sha).returns(head_git_sha)
 
     stub_request(:get, "https://api.github.com/search/issues?per_page=100&q=%22%F0%9F%A4%96%20Sync%20error%20for%20commit%202e25f7%22%20is:issue%20in:title%20repo:exercism/ruby%20author:exercism-bot"). # rubocop:disable Layout/LineLength
       to_return(
@@ -39,13 +36,13 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
       to_return(status: 200, body: "", headers: {}).
       times(1)
 
-    Github::Issue::OpenForTrackSyncFailure.(track, exception, track.git_head_sha)
+    Github::Issue::OpenForTrackSyncFailure.(create(:track, slug: 'ruby'), exception, git_sha)
   end
 
   test "re-opens issue when issue was closed" do
     Exercism.config.stubs(:github_bot_username).returns('exercism-bot')
 
-    head_git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
+    git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
 
     # Raise the error and catch it to ensure a proper backtrace
     begin
@@ -53,9 +50,6 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
     rescue StandardError => e
       exception = e
     end
-
-    track = create :track, slug: 'ruby'
-    track.stubs(:git_head_sha).returns(head_git_sha)
 
     stub_request(:get, "https://api.github.com/search/issues?per_page=100&q=%22%F0%9F%A4%96%20Sync%20error%20for%20commit%202e25f7%22%20is:issue%20in:title%20repo:exercism/ruby%20author:exercism-bot"). # rubocop:disable Layout/LineLength
       to_return(
@@ -76,13 +70,13 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
       to_return(status: 200, body: "", headers: {}).
       times(1)
 
-    Github::Issue::OpenForTrackSyncFailure.(track, exception, track.git_head_sha)
+    Github::Issue::OpenForTrackSyncFailure.(create(:track, slug: 'ruby'), exception, git_sha)
   end
 
   test "does nothing when issue already open" do
     Exercism.config.stubs(:github_bot_username).returns('exercism-bot')
 
-    head_git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
+    git_sha = "2e25f799c1830b93a8ad65a2bbbb1c50f381e639"
 
     # Raise the error and catch it to ensure a proper backtrace
     begin
@@ -90,9 +84,6 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
     rescue StandardError => e
       exception = e
     end
-
-    track = create :track, slug: 'ruby'
-    track.stubs(:git_head_sha).returns(head_git_sha)
 
     stub_request(:get, "https://api.github.com/search/issues?per_page=100&q=%22%F0%9F%A4%96%20Sync%20error%20for%20commit%202e25f7%22%20is:issue%20in:title%20repo:exercism/ruby%20author:exercism-bot"). # rubocop:disable Layout/LineLength
       to_return(
@@ -108,18 +99,52 @@ class Github::Issue::OpenForTrackSyncFailureTest < ActiveSupport::TestCase
         headers: { 'Content-Type': 'application/json' }
       )
 
-    Github::Issue::OpenForTrackSyncFailure.(track, exception, track.git_head_sha)
+    Github::Issue::OpenForTrackSyncFailure.(create(:track, slug: 'ruby'), exception, git_sha)
 
     # If the GitHub API would have been called, we would not have gotten to this point
   end
 
   test "does nothing when exception is deadlock" do
-    track = create :track
-
     exception = ActiveRecord::Deadlocked.new
 
-    Github::Issue::OpenForTrackSyncFailure.(track, exception, track.git_head_sha)
+    Github::Issue::OpenForTrackSyncFailure.(create(:track), exception, nil)
 
     # If the GitHub API would have been called, we would not have gotten to this point
+  end
+
+  test "different title without sha" do
+    Exercism.config.stubs(:github_bot_username).returns('exercism-bot')
+
+    # Raise the error and catch it to ensure a proper backtrace
+    begin
+      raise StandardError, "Could not find Concept X"
+    rescue StandardError => e
+      exception = e
+    end
+
+    stub_request(:get, "https://api.github.com/search/issues?per_page=100&q=%22%F0%9F%A4%96%20Sync%20error%3A%20Could%20not%20find%20main%20branch%22%20is:issue%20in:title%20repo:exercism/ruby%20author:exercism-bot"). # rubocop:disable Layout/LineLength
+      to_return(
+        status: 200,
+        body: {
+          total_count: 0,
+          incomplete_results: false,
+          items: []
+        }.to_json,
+        headers: { 'Content-Type': 'application/json' }
+      )
+
+    stub_request(:post, "https://api.github.com/repos/exercism/ruby/issues").
+      with do |request|
+        json = JSON.parse(request.body)
+        json["labels"].empty? &&
+          json["title"] == "🤖 Sync error: Could not find main branch" &&
+          json["body"].include?("We hit an error trying to sync the latest commit (unknown) to the website.") && # rubocop:disable Layout/LineLength
+          json["body"].include?("Please tag @exercism/maintainers-admin if you require more information.") &&
+          json["body"].match?(/open_for_track_sync_failure_test\.rb:\d+:in `block in <class:OpenForTrackSyncFailureTest>/)
+      end.
+      to_return(status: 200, body: "", headers: {}).
+      times(1)
+
+    Github::Issue::OpenForTrackSyncFailure.(create(:track, slug: 'ruby'), exception, nil)
   end
 end

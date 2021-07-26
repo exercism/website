5a0b3419d6fcc23559abfe3b69c8ff709f6ccbda
Jeremy Walker <jez.walker@gmail.com>
2021-07-26 18:33:05 +0100

    Don't log deadlocked jobs in Bugsnag

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,13 @@
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
+  rescue_from ActiveRecord::Deadlocked do |exception|
+    exception.instance_eval do
+      def skip_bugsnag
+        true
+      end
+    end
+    raise exception
+  end
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError

dd911e384580f8d6088c1f4dee67964a76bef873
Jeremy Walker <jez.walker@gmail.com>
2021-07-26 13:47:26 +0100

    Only show contributions tab if there are valid contributions (#1312)

--- app/models/user/profile.rb
+++ app/models/user/profile.rb
@@ -17,6 +17,6 @@ class User::Profile < ApplicationRecord
 
   memoize
   def contributions_tab?
-    user.reputation_tokens.count.positive?
+    user.reputation_tokens.where(category: %i[building authoring maintaining]).exists?
   end
 end
--- test/models/user/profile_test.rb
+++ test/models/user/profile_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class User::ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "contributions_tab?" do
+    user = create :user
+    profile = create :user_profile, user: user
+    refute profile.contributions_tab?
+
+    create :user_published_solution_reputation_token, user: user
+    refute profile.reload.contributions_tab?
+
+    User::ReputationToken.destroy_all
+    create :user_code_contribution_reputation_token, user: user
+    assert profile.reload.contributions_tab?
+
+    User::ReputationToken.destroy_all
+    create :user_exercise_author_reputation_token, user: user
+    assert profile.reload.contributions_tab?
+
+    User::ReputationToken.destroy_all
+    create :user_code_merge_reputation_token, user: user
+    assert profile.reload.contributions_tab?
+  end
 end

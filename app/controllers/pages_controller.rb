4ff55fc3876b99c49e2df1ce7c5d9b4bdb992122
Jeremy Walker <jez.walker@gmail.com>
2021-07-29 19:18:23 +0100

    Change icons on the front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,8 @@ class PagesController < ApplicationController
 
     @showcase_exercises = [
       {
-        exercise: Exercise.new(icon_name: "bob", title: "Bob",
-                               blurb: "Bob is a lackadaisical teenager. In conversation, his responses are very limited."),
+        exercise: Exercise.new(icon_name: "allergies", title: "Allergies",
+                               blurb: "Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies."), # rubocop:disable Layout/LineLength
         num_tracks: 40
       },
       {
@@ -18,8 +18,8 @@ class PagesController < ApplicationController
         num_tracks: 60
       },
       {
-        exercise: Exercise.new(icon_name: "hamming", title: "Hamming",
-                               blurb: "Calculate the Hamming difference between two DNA strands."),
+        exercise: Exercise.new(icon_name: "zebra-puzzle", title: "Zebra Puzzle",
+                               blurb: "Which of the residents drinks water? Who owns the zebra? Can you solve the Zebra Puzzle with code?"), # rubocop:disable Layout/LineLength
         num_tracks: 70
       }
     ]

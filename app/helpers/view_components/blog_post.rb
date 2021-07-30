b3639fa7384b5e4eb424725d048baf77f7f25e05
Jeremy Walker <jez.walker@gmail.com>
2021-07-30 14:04:44 +0100

    Add card component

---
+++ app/helpers/view_components/blog_post.rb
@@ -0,0 +1,11 @@
+module ViewComponents
+  class BlogPost < ViewComponent
+    extend Mandate::Memoize
+
+    initialize_with :post
+
+    def to_s
+      render template: 'components/blog_post', locals: { post: post }
+    end
+  end
+end
--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -12,6 +12,7 @@ import '../../css/ui-kit/tracks'
 import '../../css/ui-kit/animations'
 import '../../css/ui-kit/effects'
 
+import '../../css/components/blog-post'
 import '../../css/components/toast'
 import '../../css/components/contributions-summary'
 import '../../css/components/progress'

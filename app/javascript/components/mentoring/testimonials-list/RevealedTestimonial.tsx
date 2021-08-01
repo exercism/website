fafa75a0984a03cf4e5e78bc5ac23ba14fa53da2
Jeremy Walker <jez.walker@gmail.com>
2021-08-01 16:59:14 +0100

    Add button for deleting testimonials

--- app/javascript/components/mentoring/testimonials-list/RevealedTestimonial.tsx
+++ app/javascript/components/mentoring/testimonials-list/RevealedTestimonial.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { fromNow } from '../../../utils/time'
-import { Avatar, GraphicalIcon, TrackIcon } from '../../common'
+import { Avatar, Icon, GraphicalIcon, TrackIcon } from '../../common'
 import { TestimonialModal } from '../../modals/TestimonialModal'
 import { Testimonial } from '../../types'
 
@@ -18,32 +18,36 @@ export const RevealedTestimonial = ({
   }
   return (
     <React.Fragment>
-      <a
-        href="#"
-        onClick={(e) => {
-          setOpen(true)
-          e.preventDefault()
-        }}
-        className="testimonial"
-      >
-        <TrackIcon {...testimonial.track} />
-        <Avatar
-          src={testimonial.student.avatarUrl}
-          handle={testimonial.student.handle}
-        />
-        <div className="info">
-          <div className="student">{testimonial.student.handle}</div>
-          <div className="exercise">
-            on {testimonial.exercise.title} in {testimonial.track.title}
+      <div className="testimonial">
+        <a
+          href="#"
+          onClick={(e) => {
+            setOpen(true)
+            e.preventDefault()
+          }}
+        >
+          <TrackIcon {...testimonial.track} />
+          <Avatar
+            src={testimonial.student.avatarUrl}
+            handle={testimonial.student.handle}
+          />
+          <div className="info">
+            <div className="student">{testimonial.student.handle}</div>
+            <div className="exercise">
+              on {testimonial.exercise.title} in {testimonial.track.title}
+            </div>
           </div>
-        </div>
-        {isRevealed ? <div className="new">New</div> : null}
-        <div className="content">{testimonial.content}</div>
-        <time dateTime={testimonial.createdAt}>
-          {fromNow(testimonial.createdAt)}
-        </time>
-        <GraphicalIcon icon="modal" className="action-icon" />
-      </a>
+          {isRevealed ? <div className="new">New</div> : null}
+          <div className="content">{testimonial.content}</div>
+          <time dateTime={testimonial.createdAt}>
+            {fromNow(testimonial.createdAt)}
+          </time>
+          <GraphicalIcon icon="modal" className="action-icon" />
+        </a>
+        <button className="options-button">
+          <Icon icon="more-horizontal" alt="Options" />
+        </button>
+      </div>
       <TestimonialModal
         open={open}
         testimonial={testimonial}

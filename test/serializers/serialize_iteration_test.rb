6f9bf471a98720a62f5a9052d4a2327d55b7c1a3
Jeremy Walker <jez.walker@gmail.com>
2021-08-02 11:58:10 +0100

    Fix tests

--- test/serializers/serialize_iteration_test.rb
+++ test/serializers/serialize_iteration_test.rb
@@ -38,6 +38,7 @@ class SerializeIterationTest < ActiveSupport::TestCase
         }],
         links: {
           self: Exercism::Routes.track_exercise_iterations_url(track, exercise, idx: iteration.idx),
+          delete: Exercism::Routes.api_solution_iteration_url(iteration.solution.uuid, iteration.uuid),
           solution: Exercism::Routes.track_exercise_url(track, exercise),
           test_run: Exercism::Routes.api_solution_submission_test_run_url(iteration.solution.uuid,
             iteration.submission.uuid),

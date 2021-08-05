d898a4607bbc73b20d90054d9209a4deb25e8428
Jeremy Walker <jez.walker@gmail.com>
2021-08-05 22:01:25 +0100

    Fix the build

--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -17,30 +17,6 @@ class TracksControllerTest < ActionDispatch::IntegrationTest
     assert_template "tracks/index"
   end
 
-  test "index: params get passed correctly" do
-    ruby_1 = create :track, slug: :ruby_1, title: "Ruby #{SecureRandom.hex}"
-    js = create :track, slug: :js, title: "JS #{SecureRandom.hex}"
-    ruby_2 = create :track, slug: :ruby_2, title: "Ruby #{SecureRandom.hex}"
-
-    user = create :user
-    create :user_track, user: user, track: ruby_1
-    create :user_track, user: user, track: js
-
-    sign_in!(user)
-
-    get tracks_url(
-      criteria: "ruby",
-      status: "joined"
-    )
-    assert_response :success
-
-    # Assert only the track that matches the criteria
-    # is retrieved and served.
-    assert_includes @response.body, ruby_1.title
-    refute_includes @response.body, js.title
-    refute_includes @response.body, ruby_2.title
-  end
-
   test "show: 404s silently for missing track" do
     get track_url('foobar')
 
---
+++ test/system/pages/tracks/index_test.rb
@@ -0,0 +1,37 @@
+require "application_system_test_case"
+require_relative "../../../support/capybara_helpers"
+
+module Pages
+  module Tracks
+    class IndexTest < ApplicationSystemTestCase
+      include CapybaraHelpers
+
+      test "index: params get passed correctly" do
+        ruby_1 = create :track, slug: :ruby_1, title: "Ruby #{SecureRandom.hex}"
+        js = create :track, slug: :js, title: "JS #{SecureRandom.hex}"
+        ruby_2 = create :track, slug: :ruby_2, title: "Ruby #{SecureRandom.hex}"
+
+        user = create :user
+        create :user_track, user: user, track: ruby_1
+        create :user_track, user: user, track: js
+
+        use_capybara_host do
+          sign_in!(user)
+
+          visit tracks_url(
+            criteria: "ruby",
+            status: "joined"
+          )
+
+          # Assert only the track that matches the criteria
+          # is retrieved and served.
+          within ".c-tracks-list" do
+            assert_text ruby_1.title
+            refute_text js.title
+            refute_text ruby_2.title
+          end
+        end
+      end
+    end
+  end
+end

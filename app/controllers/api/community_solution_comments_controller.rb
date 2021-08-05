cca645fb1c6d8739445195e6cb303d635c7f1e06
Jeremy Walker <jez.walker@gmail.com>
2021-08-05 15:34:07 +0100

    Implement API endpoints

---
+++ app/controllers/api/community_solution_comments_controller.rb
@@ -0,0 +1,67 @@
+module API
+  class CommunitySolutionCommentsController < BaseController
+    before_action :use_solution
+    before_action :guard_accessible!, only: %i[index create]
+
+    def index
+      comments = SerializeSolutionComments.(@solution.comments, current_user)
+      render json: { comments: comments }
+    end
+
+    def create
+      comment = Solution::Comment::Create.(
+        current_user,
+        @solution,
+        params[:content_markdown]
+      )
+
+      render json: { comment: SerializeSolutionComment.(comment, current_user) }
+    end
+
+    def update
+      comment = Solution::Comment.find_by(uuid: params[:uuid])
+
+      return render_404(:solution_comment_not_found) if comment.blank?
+      return render_403(:solution_comment_not_accessible) unless comment.author == current_user
+
+      if comment.update(content_markdown: params[:content_markdown])
+        # TODO: Readd this
+        # CommentListChannel.notify!(comment.solution)
+        render json: { comment: SerializeSolutionComment.(comment, current_user) }
+      else
+        render_400(:failed_validations, errors: comment.errors)
+      end
+    end
+
+    def destroy
+      comment = Solution::Comment.find_by(uuid: params[:uuid])
+
+      return render_404(:solution_comment_not_found) if comment.blank?
+      return render_403(:solution_comment_not_accessible) unless comment.author == current_user
+
+      if comment.destroy
+        # TODO: Readd this
+        # CommentListChannel.notify!(comment.solution)
+        render json: { comment: SerializeSolutionComment.(comment, current_user) }
+      else
+        render_400(:solution_comment_not_deleted)
+      end
+    end
+
+    private
+    def use_solution
+      @track = Track.find(params[:track_slug])
+      @exercise = @track.exercises.find(params[:exercise_slug])
+      user = User.find_by!(handle: params[:community_solution_handle])
+      @solution = @exercise.solutions.find_by!(user_id: user.id)
+    rescue ActiveRecord::RecordNotFound
+      render_solution_not_found
+    end
+
+    def guard_accessible!
+      return if @solution.published? && @solution.allow_comments?
+
+      render_403(:solution_comments_not_allowed)
+    end
+  end
+end

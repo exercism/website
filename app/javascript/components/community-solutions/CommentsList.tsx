a65aa7e5de0d99748c62d95c9167e48e8ae1fabd
Karlo Soriano <karlo@thalamus.ai>
2021-08-09 16:24:44 +0800

    Add comments zero state to React

--- app/javascript/components/community-solutions/CommentsList.tsx
+++ app/javascript/components/community-solutions/CommentsList.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { fromNow } from '../../utils/time'
-import { Avatar, Reputation } from '../common'
+import { EmptyList } from './comments-list/EmptyList'
+import { List } from './comments-list/List'
 import { SolutionComment } from '../types'
 
 export const CommentsList = ({
@@ -8,33 +8,9 @@ export const CommentsList = ({
 }: {
   comments: readonly SolutionComment[]
 }): JSX.Element => {
-  return (
-    <React.Fragment>
-      {comments.map((comment) => {
-        return (
-          <div className="comment" key={comment.uuid}>
-            <header className="flex items-center mb-16">
-              <Avatar
-                src={comment.author.avatarUrl}
-                handle={comment.author.handle}
-              />
-              <div className="flex flex-col">
-                <div className="flex items-center">
-                  <div className="text-h6 mr-8">{comment.author.handle}</div>
-                  <Reputation value={comment.author.reputation} size="small" />
-                </div>
-                <div className="text-tetColor6 leading-160">
-                  {fromNow(comment.updatedAt)}
-                </div>
-              </div>
-            </header>
-            <div
-              className="c-textual-content --small"
-              dangerouslySetInnerHTML={{ __html: comment.contentHtml }}
-            />
-          </div>
-        )
-      })}
-    </React.Fragment>
-  )
+  if (comments.length === 0) {
+    return <EmptyList />
+  }
+
+  return <List comments={comments} />
 }

2b59bc12107245cba343e19f0b15a6f65924501a
Jeremy Walker <jez.walker@gmail.com>
2021-08-16 13:31:13 +0100

    Fix tests

--- test/system/flows/community_solutions/user_changes_published_iteration_test.rb
+++ test/system/flows/community_solutions/user_changes_published_iteration_test.rb
@@ -32,52 +32,6 @@ module Flows
           assert_button "2", disabled: true
         end
       end
-
-      test "user changes a published iteration via the comments options menu" do
-        track = create :track
-        exercise = create :concept_exercise, track: track
-        author = create :user, handle: "author"
-        create :user_track, user: author, track: track
-        solution = create :concept_solution, :completed, :published, user: author, exercise: exercise
-        submission = create :submission, solution: solution
-        create :iteration, idx: 1, submission: submission
-        submission = create :submission, solution: solution
-        iteration_2 = create :iteration, idx: 2, submission: submission
-        solution.update!(published_iteration: iteration_2)
-
-        use_capybara_host do
-          sign_in!(author)
-          visit track_exercise_solution_url(track, exercise, "author")
-
-          within(".comments") { click_on "Options" }
-          click_on "Change published iterations…"
-          find("label", text: "All iterations").click
-          click_on "Update published solution"
-
-          assert_button "1"
-          assert_button "2", disabled: true
-        end
-      end
-
-      test "other user can not change the published iteration" do
-        track = create :track
-        exercise = create :concept_exercise, track: track
-        author = create :user, handle: "author"
-        create :user_track, user: author, track: track
-        solution = create :concept_solution, :completed, :published, user: author, exercise: exercise
-        submission = create :submission, solution: solution
-        create :iteration, idx: 1, submission: submission
-        submission = create :submission, solution: solution
-        iteration_2 = create :iteration, idx: 2, submission: submission
-        solution.update!(published_iteration: iteration_2)
-
-        use_capybara_host do
-          sign_in!
-          visit track_exercise_solution_url(track, exercise, "author")
-
-          assert_no_button "Publish settings"
-        end
-      end
     end
   end
 end
--- test/system/flows/community_solutions/user_unpublishes_solution_test.rb
+++ test/system/flows/community_solutions/user_unpublishes_solution_test.rb
@@ -30,52 +30,6 @@ module Flows
           assert_text "Publish your solution"
         end
       end
-
-      test "user changes a published iteration via the comments options menu" do
-        track = create :track
-        exercise = create :concept_exercise, track: track
-        author = create :user, handle: "author"
-        create :user_track, user: author, track: track
-        solution = create :concept_solution, :completed, :published, user: author, exercise: exercise
-        submission = create :submission, solution: solution
-        create :iteration, idx: 1, submission: submission
-        submission = create :submission, solution: solution
-        iteration_2 = create :iteration, idx: 2, submission: submission
-        solution.update!(published_iteration: iteration_2)
-
-        use_capybara_host do
-          sign_in!(author)
-          visit track_exercise_solution_url(track, exercise, "author")
-          refute_button "1"
-
-          within(".comments") { click_on "Options" }
-          click_on "Unpublish solution…"
-          within(".m-unpublish-solution") { click_on "Unpublish solution" }
-
-          assert_text "Publish your solution"
-        end
-      end
-
-      test "other user can not unpublish the solution" do
-        track = create :track
-        exercise = create :concept_exercise, track: track
-        author = create :user, handle: "author"
-        create :user_track, user: author, track: track
-        solution = create :concept_solution, :completed, :published, user: author, exercise: exercise
-        submission = create :submission, solution: solution
-        create :iteration, idx: 1, submission: submission
-        submission = create :submission, solution: solution
-        iteration_2 = create :iteration, idx: 2, submission: submission
-        solution.update!(published_iteration: iteration_2)
-
-        use_capybara_host do
-          sign_in!
-          visit track_exercise_solution_url(track, exercise, "author")
-
-          assert_no_button "Publish settings"
-          within(".comments") { assert_no_button "Options" }
-        end
-      end
     end
   end
 end

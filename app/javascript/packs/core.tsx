5de34f86ccb349cda73b82831c9ddda02fee7d87
Jeremy Walker <jez.walker@gmail.com>
2021-08-17 14:18:25 +0100

    Fix URLs not updating

--- app/javascript/packs/core.tsx
+++ app/javascript/packs/core.tsx
@@ -4,7 +4,20 @@ require('@rails/ujs').start()
 require('@rails/activestorage').start()
 
 import '@hotwired/turbo-rails'
+import { navigator } from '@hotwired/turbo'
 
 document.addEventListener('DOMContentLoaded', function () {
   ;(window as any).DOMLoaded = true
+})
+
+document.addEventListener('turbo:before-fetch-request', (e) => {
+  const frame = (e as any).detail.fetchOptions.headers['Turbo-Frame']
+  if (!frame) {
+    return
+  }
+
+  const url = (e as any).detail.url
+  if (url != null) {
+    navigator.history.push(new URL(url))
+  }
 })

12afae38fffec40b546d9418fd43aa308ce9b238
Karlo Soriano <karlo@thalamus.ai>
2021-08-17 14:57:58 +0800

    Improve mark as seen button

--- test/system/flows/journey/reputation_test.rb
+++ test/system/flows/journey/reputation_test.rb
@@ -12,16 +12,16 @@ module Flow
         track = create :track, title: "Ruby"
         create :concept_exercise, track: track
         create :user_code_contribution_reputation_token, user: user, seen: false
+        create :user_code_contribution_reputation_token, user: user, seen: false
 
         use_capybara_host do
           sign_in!(user)
           visit reputation_journey_path
-          click_on "Mark all as seen"
+          click_on "Mark all 2 as seen"
           within(".m-generic-confirmation") { click_on "Continue" }
 
-          # On this page, we don't have a way to know whether all notifications are marked as unseen.
-          # I just checked the reputation button on the top bar instead.
           assert_no_css ".--notification.unseen"
+          assert_button "Mark all as seen", disabled: true
         end
       end
     end

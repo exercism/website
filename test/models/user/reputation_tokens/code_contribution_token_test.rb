c6768999db8473bb86e60de59685f0dcf4264d25
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-17 12:56:43 +0200

    Fallback to current time for code reputation tokens (#1388)

--- test/models/user/reputation_tokens/code_contribution_token_test.rb
+++ test/models/user/reputation_tokens/code_contribution_token_test.rb
@@ -109,6 +109,43 @@ class User::ReputationTokens::CodeContributionTokenTest < ActiveSupport::TestCas
     assert_equal merged_at.to_date, rt.earned_on
   end
 
+  test "uses current time for earned date when closed_at and merged_at are nil" do
+    freeze_time do
+      external_url = 'https://api.github.com/repos/exercism/v3/pulls/1347'
+      repo = 'exercism/ruby'
+      pr_node_id = 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ'
+      pr_number = 1347
+      pr_title = "The cat sat on the mat"
+      level = :large
+      user = create :user, handle: "User22", github_username: "user22"
+
+      User::ReputationToken::Create.(
+        user,
+        :code_contribution,
+        repo: repo,
+        level: level,
+        pr_node_id: pr_node_id,
+        pr_number: pr_number,
+        pr_title: pr_title,
+        merged_at: nil,
+        external_url: external_url
+      )
+
+      assert_equal 1, user.reputation_tokens.size
+      rt = user.reputation_tokens.first
+
+      assert_equal User::ReputationTokens::CodeContributionToken, rt.class
+      assert_equal "You created <strong>PR##{pr_number}</strong> on <strong>ruby</strong>: The cat sat on the mat", rt.text
+      assert_equal 'https://api.github.com/repos/exercism/v3/pulls/1347', rt.external_url
+      assert_equal "#{user.id}|code_contribution|PR#MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ", rt.uniqueness_key
+      assert_equal :building, rt.category
+      assert_equal :contributed_code, rt.reason
+      assert_equal level, rt.level
+      assert_equal 30, rt.value
+      assert_equal Time.current.to_date, rt.earned_on
+    end
+  end
+
   repos = [
     ['exercism/ruby', 'track'],
     ['exercism/ruby-test-runner', 'test runner'],

398c3e2f2c6149b1871085956ffab4c5e7ed3017
Jeremy Walker <jez.walker@gmail.com>
2021-08-20 12:04:11 +0100

    Add further functionality

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -1,33 +1,36 @@
 class UnsubscribeController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :guard_token!
+  before_action :guard_key!
 
-  def show
-    @email_key = params[:key]
-    redirect_to communication_preferences_settings_path unless @email_key
-  end
+  def show; end
 
   def update
     if params[:communication_preferences].present?
-      @preferences.update(
+      @communication_preferences.update(
         params.require(:communication_preferences).reject do |k, _v|
           %w[user_id created_at updated_at token].include?(k)
         end.permit!
       )
     end
 
-    redirect_to({ action: :show, token: @token }, notice: "Your communication Preferences have been updated successfully")
+    redirect_to({ action: :show, token: @token, key: @email_key }, notice: "You have been unsubscribed successfully")
   end
 
   private
   def guard_token!
     @token = params[:token]
-    return redirect_to communication_preferences_settings_path if @token.nil?
+    return redirect_to communication_preferences_settings_path unless @token
 
     begin
-      @preferences = User::CommunicationPreferences.find_by!(token: @token)
+      @communication_preferences = User::CommunicationPreferences.find_by!(token: @token)
     rescue StandardError
       redirect_to communication_preferences_settings_path
     end
+  end
+
+  def guard_key!
+    @email_key = params[:key]
+    redirect_to communication_preferences_settings_path unless @email_key
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -5,6 +5,7 @@
   user_activities
   user_reputation_tokens
   site_updates
+  communication_preferences
 ].each do |category|
   I18n.load_path += Dir[Rails.root.join('config', 'locales', category, '*.{rb,yml}')]
 end

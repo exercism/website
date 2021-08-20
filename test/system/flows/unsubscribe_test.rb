e5f5a2581619412573a022089d6900ba92ef6ff8
Jeremy Walker <jez.walker@gmail.com>
2021-08-20 13:50:25 +0100

    Fix the tests

--- test/system/flows/unsubscribe_test.rb
+++ test/system/flows/unsubscribe_test.rb
@@ -1,6 +1,7 @@
 require "application_system_test_case"
 
 class UnsubscribeTest < ApplicationSystemTestCase
+
   test "redirects with missing token" do
     visit unsubscribe_path(token: nil)
     assert_selector "#page-auth"
@@ -16,21 +17,24 @@ class UnsubscribeTest < ApplicationSystemTestCase
     assert preferences.email_on_mentor_started_discussion_notification
 
     visit unsubscribe_path(token: preferences.token, key: 'email_on_mentor_started_discussion_notification')
-    click_on "Unsubscribe from 'Email me with weekly mentor summaries'"
-    assert_selector "#notice", text: "Your communication Preferences have been updated successfully"
+    assert_text "Email when a mentor starts mentoring my solution"
+    click_on "Unsubscribe from email"
+    assert_text "You have been unsubscribed successfully"
 
     refute preferences.reload.email_on_mentor_started_discussion_notification
   end
 
   test "user unsubscribes using form" do
-    preferences = create :user_communication_preferences, token: SecureRandom.uuid
+    user = create :user
+    preferences = create :user_communication_preferences, user: user
     assert preferences.email_on_mentor_started_discussion_notification
 
-    visit unsubscribe_path(token: preferences.token, key: 'email_on_mentor_started_discussion_notification')
-    click_on "Email me with weekly mentor summaries"
-    click_on "Update preferences"
-    assert_selector "#notice", text: "Your communication Preferences have been updated successfully"
-
-    refute preferences.reload.email_on_mentor_started_discussion_notification
+    use_capybara_host do
+      sign_in!(user)
+      visit unsubscribe_path(token: preferences.token, key: 'email_on_mentor_started_discussion_notification')
+      find('label', text: "Email me when a mentor starts a discussion").click
+      click_on "Change preferences"
+      assert_text "Your preferences have been updated"
+    end
   end
 end

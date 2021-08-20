11c083c15404522f332c926e4e68db50ec882860
Jeremy Walker <jez.walker@gmail.com>
2021-08-20 17:05:41 +0100

    Add tasks to contributing dashboard (#1414)

--- app/controllers/contributing/dashboard_controller.rb
+++ app/controllers/contributing/dashboard_controller.rb
@@ -2,6 +2,19 @@ module Contributing
   class DashboardController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[show]
 
-    def show; end
+    def show
+      @tracks = Track.all.index_by(&:id)
+      @proofreading = build_tasks_list(action: :proofread)
+      @improve_content = build_tasks_list(action: :improve, area: ["concept-exercise", "practice-exercise", :concept])
+      @create_exercises = build_tasks_list(action: :create, area: %w[concept-exercise practice-exercise])
+      @analyzer = build_tasks_list(area: :analyzer)
+      @representer = build_tasks_list(area: :representer)
+      @test_runner = build_tasks_list(area: "test-runner")
+    end
+
+    private
+    def build_tasks_list(filters)
+      Github::Task.where(filters).group(:track_id).count.sort_by { |_, v| -v.size }.to_h
+    end
   end
 end

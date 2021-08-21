5917123201c12b0b7e89ff07272f6608355fe2a2
Jeremy Walker <jez.walker@gmail.com>
2021-08-21 19:38:52 +0100

    Fix legacy redirects

--- app/controllers/legacy_controller.rb
+++ app/controllers/legacy_controller.rb
@@ -1,14 +1,25 @@
 class LegacyController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:solution]
 
   def solution
     solution = Solution.find_by!(uuid: params[:uuid])
-    if user_signed_in? && solution.user_id == current_user.id
-      redirect_to Exercism::Routes.private_solution_url(solution)
-    elsif solution.published?
-      redirect_to Exercism::Routes.published_solution_url(solution)
-    else
-      raise ActiveRecord::RecordNotFound # Simulate a 404
-    end
+    return render_404 unless solution.published?
+
+    redirect_to Exercism::Routes.published_solution_url(solution)
+  end
+
+  def my_solution
+    solution = Solution.find_by!(uuid: params[:uuid])
+    return render_404 unless solution.user_id == current_user.id
+
+    redirect_to Exercism::Routes.private_solution_url(solution)
+  end
+
+  def mentor_solution
+    solution = Solution.find_by!(uuid: params[:uuid])
+    discussion = solution.mentor_discussions.where(mentor: current_user).first
+    return render_404 unless discussion
+
+    redirect_to mentoring_discussion_url(discussion)
   end
 end

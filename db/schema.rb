b1cc6be4ab3f2f108bc5f9ad6ad4e45166fec6b6
Jeremy Walker <jez.walker@gmail.com>
2021-08-21 21:00:27 +0100

    Make request id not null (#1420)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_111047) do
+ActiveRecord::Schema.define(version: 2021_08_21_182454) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false

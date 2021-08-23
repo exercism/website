2792c1ab8269b8a1019b0da1196a19c55cafe059
Karlo Soriano <karlo@thalamus.ai>
2021-08-23 11:01:03 +0800

    Remove digest in editor files serializer

---
+++ app/serializers/serialize_editor_files.rb
@@ -0,0 +1,15 @@
+class SerializeEditorFiles
+  include Mandate
+
+  initialize_with :files
+
+  def call
+    files.map do |filename, data|
+      {
+        filename: filename,
+        type: data[:type],
+        content: data[:content]
+      }
+    end
+  end
+end

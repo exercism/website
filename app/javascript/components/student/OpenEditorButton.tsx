17a5ba611d1cbe92f9f4d28c736ca3c492cbeea7
Karlo Soriano <karlo@thalamus.ai>
2021-08-26 07:06:19 +0800

    Fix link to solving exercises locally docs

--- app/javascript/components/student/OpenEditorButton.tsx
+++ app/javascript/components/student/OpenEditorButton.tsx
@@ -2,21 +2,24 @@ import React from 'react'
 import { CopyToClipboardButton } from '../common'
 import { ComboButton } from '../common/ComboButton'
 import { StartExerciseButton } from './open-editor-button/StartExerciseButton'
-import { GenericTooltip } from '../misc/ExercismTippy'
 
 export const OpenEditorButton = (
   props:
-    | { status: 'locked'; command: string; links: null }
-    | { status: 'available'; command: string; links: { start: string } }
+    | { status: 'locked'; command: string; links: { local: string } }
+    | {
+        status: 'available'
+        command: string
+        links: { start: string; local: string }
+      }
     | {
         status: 'completed' | 'published'
         command: string
-        links: { exercise: string }
+        links: { exercise: string; local: string }
       }
     | {
         status: 'iterated' | 'started'
         command: string
-        links: { exercise: string }
+        links: { exercise: string; local: string }
       }
 ): JSX.Element | null => {
   let primarySegment
@@ -45,17 +48,19 @@ export const OpenEditorButton = (
         <div className="c-open-editor-button-dropdown">
           <h3>Download and work locally</h3>
           <p>
-            Prefer to use the tools you're familiar with, than our online
+            Prefer to use the tools you&apos;re familiar with, than our online
             editor? No problem! You can download this exericse and work on it
-            locally, then submit it when you're happy.
+            locally, then submit it when you&apos;re happy.
           </p>
           <CopyToClipboardButton textToCopy={props.command} />
 
           <p>
             First time using our local setup? Read our{' '}
-            <a href="#">guide to solving exercises locally</a> to understand the
-            flow and instal Exercism locally. Then come back here and use the
-            command above to start the exercise.
+            <a href={props.links.local} target="_blank" rel="noreferrer">
+              guide to solving exercises locally
+            </a>{' '}
+            to understand the flow and instal Exercism locally. Then come back
+            here and use the command above to start the exercise.
           </p>
         </div>
       </ComboButton.DropdownSegment>

cf8c1f39f4cb32fc5b8c97464477d09cd6859edd
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-25 13:33:55 +0200

    Update app/models/exercise/taught_concept.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- app/models/exercise/taught_concept.rb
+++ app/models/exercise/taught_concept.rb
@@ -6,10 +6,12 @@ class Exercise::TaughtConcept < ApplicationRecord
     # We're updating in a single query instead of two queries to avoid race-conditions
     # and using read_committed to avoid deadlocks
     ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
-      concepts_count_sql = Arel.sql(Exercise::TaughtConcept.joins(:exercise).
-        where(exercise: { track: exercise.track_id, status: %i[beta active] }).
-        select("COUNT(DISTINCT(track_concept_id))").
-        to_sql)
+      concepts_count_sql = Arel.sql(
+        Exercise::TaughtConcept.joins(:exercise).
+          where(exercise: { track: exercise.track_id, status: %i[beta active] }).
+          select("COUNT(DISTINCT(track_concept_id))").
+          to_sql
+        )
       Track.where(id: exercise.track_id).update_all("num_concepts = (#{concepts_count_sql})")
     end
   end

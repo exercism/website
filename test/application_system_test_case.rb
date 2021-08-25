39461eecfc79c77a8e2c416393f0e083d1614be0
Jeremy Walker <jez.walker@gmail.com>
2021-08-25 12:58:24 +0100

    Remove global animation disabling

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -10,7 +10,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.default_max_wait_time = 7
   Capybara.enable_aria_label = true
   Capybara.reuse_server = false
-  Capybara.disable_animation = true
+  # Capybara.disable_animation = true
 
   class << self
     attr_accessor :override_should_flunk

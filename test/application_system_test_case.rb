757c942ac5b7c6b08b6a688da908614079877c77
Karlo Soriano <karlo@thalamus.ai>
2021-08-25 11:05:01 +0800

    Disable animations in capybara

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -10,7 +10,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.default_max_wait_time = 7
   Capybara.enable_aria_label = true
   Capybara.reuse_server = false
-  # Capybara.disable_animation = true
+  Capybara.disable_animation = true
 
   class << self
     attr_accessor :override_should_flunk
@@ -59,11 +59,11 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     flunk("JS Errors") if should_flunk
   end
 
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  # driven_by :selenium, using: :headless_chrome do |driver_option|
-  #   # Without this argument, Chrome cannot be started in Docker
-  #   driver_option.add_argument('no-sandbox')
-  # end
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome do |driver_option|
+    # Without this argument, Chrome cannot be started in Docker
+    driver_option.add_argument('no-sandbox')
+  end
 
   def sign_in!(user = nil)
     @current_user = user || create(:user)

4bcf3994c891a5e7d4356bf8e5a25a0f5bbc2028
Karlo Soriano <karlo@thalamus.ai>
2021-08-26 10:27:26 +0800

    Add tootltip to open editor button when exercise is locked

--- app/javascript/components/student/OpenEditorButton.tsx
+++ app/javascript/components/student/OpenEditorButton.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { CopyToClipboardButton } from '../common'
-import { ComboButton } from '../common/ComboButton'
+import {
+  ComboButton,
+  PrimarySegment,
+  DropdownSegment,
+} from '../common/ComboButton'
 import { StartExerciseButton } from './open-editor-button/StartExerciseButton'
 import { GenericTooltip } from '../misc/ExercismTippy'
 
@@ -38,27 +42,32 @@ export const OpenEditorButton = (
   }
 
   return (
-    <ComboButton enabled={props.status !== 'locked'}>
-      <ComboButton.PrimarySegment>{primarySegment}</ComboButton.PrimarySegment>
-      <ComboButton.DropdownSegment>
-        {/* TODO: (required) Firm up this copy and inject the link */}
-        <div className="c-open-editor-button-dropdown">
-          <h3>Download and work locally</h3>
-          <p>
-            Prefer to use the tools you're familiar with, than our online
-            editor? No problem! You can download this exericse and work on it
-            locally, then submit it when you're happy.
-          </p>
-          <CopyToClipboardButton textToCopy={props.command} />
+    <GenericTooltip
+      disabled={props.status !== 'locked'}
+      content="This exercise is currently locked"
+    >
+      <ComboButton enabled={props.status !== 'locked'}>
+        <PrimarySegment>{primarySegment}</PrimarySegment>
+        <DropdownSegment>
+          {/* TODO: (required) Firm up this copy and inject the link */}
+          <div className="c-open-editor-button-dropdown">
+            <h3>Download and work locally</h3>
+            <p>
+              Prefer to use the tools you're familiar with, than our online
+              editor? No problem! You can download this exericse and work on it
+              locally, then submit it when you're happy.
+            </p>
+            <CopyToClipboardButton textToCopy={props.command} />
 
-          <p>
-            First time using our local setup? Read our{' '}
-            <a href="#">guide to solving exercises locally</a> to understand the
-            flow and instal Exercism locally. Then come back here and use the
-            command above to start the exercise.
-          </p>
-        </div>
-      </ComboButton.DropdownSegment>
-    </ComboButton>
+            <p>
+              First time using our local setup? Read our{' '}
+              <a href="#">guide to solving exercises locally</a> to understand
+              the flow and instal Exercism locally. Then come back here and use
+              the command above to start the exercise.
+            </p>
+          </div>
+        </DropdownSegment>
+      </ComboButton>
+    </GenericTooltip>
   )
 }

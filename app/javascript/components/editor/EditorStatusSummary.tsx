2b49b071ec7050994d028821d4261999466d0465
Jeremy Walker <jez.walker@gmail.com>
2021-08-26 17:15:39 +0100

    Remove old statuses

--- app/javascript/components/editor/EditorStatusSummary.tsx
+++ app/javascript/components/editor/EditorStatusSummary.tsx
@@ -9,14 +9,10 @@ export const EditorStatusSummary = ({
   error?: string
 }): JSX.Element | null => {
   switch (status) {
-    case EditorStatus.CREATING_ITERATION:
-      return <p>Submitting...</p>
     case EditorStatus.CREATE_SUBMISSION_FAILED:
       return <p>{error}</p>
     case EditorStatus.REVERT_FAILED:
       return <p>{error}</p>
-    case EditorStatus.REVERTING:
-      return <p>Reverting files...</p>
     default:
       return null
   }

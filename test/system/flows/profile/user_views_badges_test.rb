436aa0df20d4522ed71836320f16886aa41263d8
Karlo Soriano <karlo@thalamus.ai>
2021-08-26 10:58:01 +0800

    Count only revealed badges in profile page

---
+++ test/system/flows/profile/user_views_badges_test.rb
@@ -0,0 +1,23 @@
+require "application_system_test_case"
+require_relative "../../../support/capybara_helpers"
+
+module Flows
+  class UserViewsBadgesTest < ApplicationSystemTestCase
+    include CapybaraHelpers
+
+    test "shows badges" do
+      author = create :user
+      create :user_profile, user: author
+      create :user_acquired_badge, user: author, badge: create(:member_badge), revealed: true
+      create :user_acquired_badge, user: author, badge: create(:rookie_badge)
+
+      use_capybara_host do
+        visit badges_profile_path(author.handle)
+
+        assert_text "Member"
+        refute_text "Rookie"
+        assert_text "1\nCommon"
+      end
+    end
+  end
+end

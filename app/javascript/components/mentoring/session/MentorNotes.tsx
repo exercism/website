8bc208617ae4cf1ead954e70aa116090e8678350
Jeremy Walker <jez.walker@gmail.com>
2021-08-29 11:31:36 +0100

    Refactor slightly

--- app/javascript/components/mentoring/session/MentorNotes.tsx
+++ app/javascript/components/mentoring/session/MentorNotes.tsx
@@ -8,39 +8,36 @@ export const MentorNotes = ({
   notes?: string
   improveUrl: string
 }): JSX.Element => {
-  const ref = useHighlighting<HTMLDivElement>()
+  const prLink = (
+    <a href={improveUrl} target="_blank" rel="noreferrer">
+      Pull Request on GitHub
+    </a>
+  )
 
-  if (notes) {
-    return (
-      <React.Fragment>
-        <div
-          className="c-textual-content --small"
-          ref={ref}
-          dangerouslySetInnerHTML={{ __html: notes }}
-        />
-        <hr className="c-divider --small my-16" />
-        <h3 className="text-h5 mb-4">Improve these notes</h3>
-        <p className="text-p-base">
-          These notes are written by our community. Please help improve them by
-          sending a{' '}
-          <a href={improveUrl} target="_blank" rel="noreferrer">
-            Pull Request on GitHub
-          </a>
-          .
-        </p>
-      </React.Fragment>
-    )
-  } else {
+  if (!notes) {
     return (
       <p className="text-p-base">
         This exercise doesn&apos;t have any mentoring notes yet. Mentoring notes
         are written by our community. Please help get them started for this
-        exercise by sending a{' '}
-        <a href={improveUrl} target="_blank" rel="noreferrer">
-          Pull Request on GitHub
-        </a>
-        .
+        exercise by sending a {prLink}.
       </p>
     )
   }
+
+  const ref = useHighlighting<HTMLDivElement>()
+  return (
+    <React.Fragment>
+      <div
+        className="c-textual-content --small"
+        ref={ref}
+        dangerouslySetInnerHTML={{ __html: notes }}
+      />
+      <hr className="c-divider --small my-16" />
+      <h3 className="text-h5 mb-4">Improve these notes</h3>
+      <p className="text-p-base">
+        These notes are written by our community. Please help improve them by
+        sending a {prLink}.
+      </p>
+    </React.Fragment>
+  )
 }

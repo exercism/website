352d4591c824f17bb49528e08dd0e01cc6740152
Karlo Soriano <karlo@thalamus.ai>
2021-08-29 14:46:07 +0800

    Add syntax highlighting to mentor notes

--- app/javascript/components/mentoring/session/MentorNotes.tsx
+++ app/javascript/components/mentoring/session/MentorNotes.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useHighlighting } from '../../../utils/highlight'
 
 export const MentorNotes = ({ notes }: { notes?: string }): JSX.Element => {
+  const ref = useHighlighting<HTMLDivElement>()
+
   if (notes) {
     return (
       <React.Fragment>
         <div
           className="c-textual-content --small"
+          ref={ref}
           dangerouslySetInnerHTML={{ __html: notes }}
         />
         <p>CTA to improve notes here</p>

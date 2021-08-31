a08249147f57b4916a102c097c99ed75db3cb186
Jeremy Walker <jez.walker@gmail.com>
2021-08-31 16:12:59 +0100

    Use manual param checking for settings

--- test/commands/user/update_test.rb
+++ test/commands/user/update_test.rb
@@ -14,4 +14,21 @@ class User::UpdateTest < ActiveSupport::TestCase
     assert_equal 'iHiD1', user.name
     assert_equal 'iHiD2', profile.twitter
   end
+
+  test "updates user" do
+    user = create :user, name: "foobar"
+
+    User::Update.(user, user: { name: "iHiD1" })
+
+    assert_equal 'iHiD1', user.name
+  end
+
+  test "updates profile" do
+    user = create :user, name: "foobar"
+    profile = create :user_profile, user: user, twitter: "foobar123"
+
+    User::Update.(user, profile: { twitter: "iHiD2" })
+
+    assert_equal 'iHiD2', profile.twitter
+  end
 end

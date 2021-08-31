81b36753ba8d9575117abb0b8ed35cfa2210313b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-08-31 14:44:58 +0200

    Sync rubocop version (#1413)
    * Sync rubocop version
    
    The tests CI workflow now extracts the correct gem version for the rubocop gems from the Gemfile.lock file
    
    * Update Rubocop gem
    
    * Format using rubocop
    
    * Bump rubocop to 1.20.0
    
    * Exclude file from Layout/RescueEnsureAlignment rule

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,24 +7,18 @@ class PagesController < ApplicationController
 
     @showcase_exercises = [
       {
-        exercise: Exercise.new(
-          icon_name: "allergies", title: "Allergies",
-          blurb: "Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies." # rubocop:disable Layout/LineLength
-        ),
+        exercise: Exercise.new(icon_name: "allergies", title: "Allergies",
+          blurb: "Given a person's allergy score, determine whether or not they're allergic to a given item, and their full list of allergies."), # rubocop:disable Layout/LineLength
         num_tracks: 40
       },
       {
-        exercise: Exercise.new(
-          icon_name: "queen-attack", title: "Queen Attack",
-          blurb: "Given the position of two queens on a chess board, indicate whether or not they are positioned so that they can attack each other" # rubocop:disable Layout/LineLength
-        ),
+        exercise: Exercise.new(icon_name: "queen-attack", title: "Queen Attack",
+          blurb: "Given the position of two queens on a chess board, indicate whether or not they are positioned so that they can attack each other"), # rubocop:disable Layout/LineLength
         num_tracks: 60
       },
       {
-        exercise: Exercise.new(
-          icon_name: "zebra-puzzle", title: "Zebra Puzzle",
-          blurb: "Which of the residents drinks water? Who owns the zebra? Can you solve the Zebra Puzzle with code?"
-        ),
+        exercise: Exercise.new(icon_name: "zebra-puzzle", title: "Zebra Puzzle",
+          blurb: "Which of the residents drinks water? Who owns the zebra? Can you solve the Zebra Puzzle with code?"), # rubocop:disable Layout/LineLength
         num_tracks: 70
       }
     ]

864195faed3ca8b145a50f7f40cb59e4792e4831
Jeremy Walker <jez.walker@gmail.com>
2021-09-01 10:13:16 +0100

    Add people

--- app/helpers/react_components/modals/first_time_modal.rb
+++ app/helpers/react_components/modals/first_time_modal.rb
@@ -8,7 +8,7 @@ module ReactComponents
           "modals-first-time-modal",
           {
             endpoint: Exercism::Routes.hide_api_settings_introducer_path(slug),
-            contributors: contributors.map do |c|
+            contributors: contributors.sort_by { rand }.map do |c|
               {
                 handle: c.handle,
                 avatar_url: c.avatar_url
@@ -19,9 +19,8 @@ module ReactComponents
       end
 
       private
-      # TODO: change this
       def contributors
-        User.all
+        User.where(id: [3441, 23_560, 210_814, 38_366, 91_576, 76_721, 88_486, 56_500, 3256, 132_998, 126_320, 266_545, 204_014, 1530, 165_928, 312_980, 2435, 163_024, 416_860, 199_082, 138_856, 352_795]) # rubocop:disable Layout/LineLength
       end
 
       def slug

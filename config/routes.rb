6276dbff4bb04fa5f78be82380c79cab43117ddf
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-01 11:24:28 +0200

    Remove supporters link

--- config/routes.rb
+++ config/routes.rb
@@ -387,7 +387,6 @@ Rails.application.routes.draw do
   #########
   get "cli-walkthrough" => "pages#cli_walkthrough", as: :cli_walkthrough
   get "about" => "pages#about", as: :about_page
-  get "supporters" => "pages#supporters", as: :supporters_page
   get "team" => "pages#team", as: :team_page
 
   get "site.webmanifest" => "meta#site_webmanifest"

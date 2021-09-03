ef44be148a0ec11bfbd4c80e03380fe8498920e7
Jeremy Walker <jez.walker@gmail.com>
2021-09-04 00:37:56 +0100

    Fix missing var

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -3,6 +3,7 @@ module API
     skip_before_action :authenticate_user!
     before_action :authenticate_user
     before_action :use_track
+    before_action :use_exercise, only: [:start]
 
     def index
       render json: AssembleExerciseList.(current_user, @track, params)
@@ -21,6 +22,10 @@ module API
     private
     def use_track
       @track = Track.find(params[:track_slug])
+    end
+
+    def use_exercise
+      @exercise = @track.exercises.find(params[:slug])
     end
   end
 end

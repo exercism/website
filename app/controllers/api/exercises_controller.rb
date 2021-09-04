3735e554a4e2bbce822d4e428a53707d14db8b51
Jeremy Walker <jez.walker@gmail.com>
2021-09-04 01:46:36 +0100

    Guard against editor being used if tracks do not have test runners (#1771)
    * Guard against editor being used if tracks do not have test runners
    
    * Fix missing var

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -3,14 +3,29 @@ module API
     skip_before_action :authenticate_user!
     before_action :authenticate_user
     before_action :use_track
+    before_action :use_exercise, only: [:start]
 
     def index
       render json: AssembleExerciseList.(current_user, @track, params)
     end
 
+    def start
+      Solution::Create.(current_user, @exercise)
+
+      render json: {
+        links: {
+          exercise: Exercism::Routes.edit_track_exercise_url(@track, @exercise)
+        }
+      }
+    end
+
     private
     def use_track
       @track = Track.find(params[:track_slug])
+    end
+
+    def use_exercise
+      @exercise = @track.exercises.find(params[:slug])
     end
   end
 end

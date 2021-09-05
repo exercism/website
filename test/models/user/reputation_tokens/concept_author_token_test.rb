b39b4fd906c7eab3a616eea3636621a54eebb3e6
Jeremy Walker <jez.walker@gmail.com>
2021-09-05 21:48:36 +0100

    Fix ordering of contributors (#1812)
    * Fix the build
    
    * Fix ordering of contributors
    
    * Remove n+1s

--- test/models/user/reputation_tokens/concept_author_token_test.rb
+++ test/models/user/reputation_tokens/concept_author_token_test.rb
@@ -25,6 +25,6 @@ class User::ReputationTokens::ConceptAuthorTokenTest < ActiveSupport::TestCase
     assert_equal :authoring, rt.category
     assert_equal 10, rt.value
     assert_equal concept.created_at.to_date, rt.earned_on
-    assert_equal Exercism::Routes.track_concept_path(track, concept), rt.internal_url
+    assert_equal Exercism::Routes.track_concept_path(track, concept), rt.rendering_data[:internal_url]
   end
 end

b198064fb24b8b0bf20e5b82d2c7feb88573adf4
Jeremy Walker <jez.walker@gmail.com>
2021-09-05 01:28:58 +0100

    Update communication preferences

--- app/serializers/serialize_communication_preferences.rb
+++ app/serializers/serialize_communication_preferences.rb
@@ -5,7 +5,14 @@ class SerializeCommunicationPreferences
 
   def call
     {
-      email_on_mentor_started_discussion_notification: preferences.email_on_mentor_started_discussion_notification
+      email_on_mentor_started_discussion_notification: preferences.email_on_mentor_started_discussion_notification,
+      email_on_mentor_replied_to_discussion_notification: preferences.email_on_mentor_replied_to_discussion_notification,
+      email_on_student_replied_to_discussion_notification: preferences.email_on_student_replied_to_discussion_notification,
+      email_on_student_added_iteration_notification: preferences.email_on_student_added_iteration_notification,
+      email_on_acquired_badge_notification: preferences.email_on_acquired_badge_notification,
+      email_on_general_update_notification: preferences.email_on_general_update_notification,
+      receive_product_updates: preferences.receive_product_updates
+
     }
   end
 end

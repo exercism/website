859b502171baf2507c34819a1a822e5385870799
Karlo Soriano <karlo@thalamus.ai>
2021-09-09 06:10:23 +0800

    Fix shortFromNow for future dates

--- app/javascript/utils/time.ts
+++ app/javascript/utils/time.ts
@@ -10,25 +10,28 @@ export function fromNow(dateTime: ConfigType) {
 
 export function shortFromNow(dateTime: ConfigType) {
   const relative = fromNow(dateTime)
-  if (relative == 'a few seconds ago') {
+
+  if (relative == 'a few seconds ago' || relative === 'in a few seconds') {
     return 'now'
   }
 
   const parts = relative.split(' ')
-  const val = parts[0] == 'a' || parts[0] == 'an' ? 1 : parts[0]
+  const inFuture = parts[0] === 'in'
+  const time = inFuture ? [parts[1], parts[2]] : [parts[0], parts[1]]
+  const val = time[0] == 'a' || time[0] == 'an' ? 1 : time[0]
   let unit
 
-  switch (parts[1]) {
+  switch (time[1]) {
     case 'month':
     case 'months':
       unit = 'mo'
 
       break
     default:
-      unit = parts[1][0]
+      unit = time[1][0]
   }
 
-  return `${val}${unit} ago`
+  return inFuture ? `in ${val}${unit}` : `${val}${unit} ago`
 }
 
 export function timeFormat(dateTime: ConfigType, template: string) {

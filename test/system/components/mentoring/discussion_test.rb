09c323b93dfa043c370b495d86fe6535393e316d
Jeremy Walker <jez.walker@gmail.com>
2021-09-14 22:40:40 +0100

    Fix test

--- test/system/components/mentoring/discussion_test.rb
+++ test/system/components/mentoring/discussion_test.rb
@@ -472,7 +472,7 @@ module Components
           click_on "Guidance"
           click_on "Mentor notes"
           assert_text "These are notes for lasagna"
-          assert_link "Pull Request on GitHub", href: exercise.edit_mentoring_notes_url
+          assert_link "Pull Request on GitHub", href: exercise.mentoring_notes_edit_url
         end
       end
 

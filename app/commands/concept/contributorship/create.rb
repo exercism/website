252ab27dd1a7d185ce97be2619dbffbaabfb8da6
Erik Schierboom <erik_schierboom@hotmail.com>
2021-09-14 22:59:04 +0200

    Award reputation tokens in background job (#1920)

--- app/commands/concept/contributorship/create.rb
+++ app/commands/concept/contributorship/create.rb
@@ -12,7 +12,7 @@ class Concept
           return nil
         end
 
-        User::ReputationToken::Create.(
+        AwardReputationTokenJob.perform_later(
           contributor,
           :concept_contribution,
           contributorship: contributorship

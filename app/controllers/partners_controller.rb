0f7b0d03e503d10e28fa0db17cb648c2f071aac7
Jeremy Walker <jez.walker@gmail.com>
2021-10-02 22:36:50 +0100

    Add the desktop version

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -3,5 +3,6 @@ class PartnersController < ApplicationController
     @track = Track.find('go')
     @num_concepts = @track.concepts.count
     @num_exercises = @track.exercises.count
+    @num_tasks = @track.tasks.count
   end
 end

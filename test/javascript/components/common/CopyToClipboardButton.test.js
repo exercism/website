32ac0b8ac533d55ef7c5ceaf2d77c90f39613960
Karlo Soriano <karlo@thalamus.ai>
2021-10-05 15:13:27 +0800

    Fix tests

--- test/javascript/components/common/CopyToClipboardButton.test.js
+++ test/javascript/components/common/CopyToClipboardButton.test.js
@@ -2,11 +2,10 @@ import React from 'react'
 import { render, waitFor, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { CopyToClipboardButton } from '../../../../app/javascript/components/common/CopyToClipboardButton'
+import * as copyToClipboard from '../../../../app/javascript/utils/copyToClipboard'
 
 const mockClipboardApi = () => {
-  // The clipboard API is not supported so we have to mock it
-  navigator.clipboard = { writeText: () => {} }
-  jest.spyOn(navigator.clipboard, 'writeText')
+  jest.spyOn(copyToClipboard, 'copyToClipboard').mockImplementation(() => null)
 }
 
 test('copies text to clipboard when clicking', async () => {
@@ -19,7 +18,7 @@ test('copies text to clipboard when clicking', async () => {
   fireEvent.click(getByRole('img'))
 
   await waitFor(() =>
-    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+    expect(copyToClipboard.copyToClipboard).toHaveBeenCalledWith(
       'exercism download --track=ruby --exercise=bob'
     )
   )
@@ -38,7 +37,7 @@ test('copies text to clipboard when pressing Ctrl+C', async () => {
   fireEvent.keyDown(copyButton, { code: 'KeyC', ctrlKey: true })
 
   await waitFor(() =>
-    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+    expect(copyToClipboard.copyToClipboard).toHaveBeenCalledWith(
       'exercism download --track=javascript --exercise=bob'
     )
   )
@@ -57,13 +56,3 @@ test('copies text to clipboard when pressing Ctrl+C', async () => {
 //     timeout: 2500,
 //   })
 // })
-
-test('hides component if clipboard API is unavailable', async () => {
-  navigator.clipboard = undefined
-
-  const { queryByText } = render(
-    <CopyToClipboardButton textToCopy="exercism something" />
-  )
-
-  expect(queryByText('Copy')).not.toBeInTheDocument()
-})

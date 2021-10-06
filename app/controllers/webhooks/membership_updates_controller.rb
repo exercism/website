d154f236af72b69561b013b6487c456243979723
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-06 13:33:03 +0200

    Fix failing test

--- app/controllers/webhooks/membership_updates_controller.rb
+++ app/controllers/webhooks/membership_updates_controller.rb
@@ -2,7 +2,7 @@
 # for which the "Memberships" event type must be enabled in GitHub.
 # See https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#membership
 module Webhooks
-  class MembershipUpdatesController < BaseController
+  class MembershipUpdatesController < GithubBaseController
     def create
       ::Webhooks::ProcessMembershipUpdate.(
         # params[:action] does not work as it is populated by Rails with the action method name

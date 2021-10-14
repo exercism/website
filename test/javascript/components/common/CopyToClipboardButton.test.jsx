652daf1e63b678775aa81b304505faebadd6672d
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-14 10:28:45 +0200

    Fix failing clipboard test
    https://github.com/aelbore/esbuild-jest/issues/26

--- test/javascript/components/common/CopyToClipboardButton.test.jsx
+++ test/javascript/components/common/CopyToClipboardButton.test.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { render, waitFor, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { CopyToClipboardButton } from '../../../../app/javascript/components/common/CopyToClipboardButton'
-import * as copyToClipboard from '../../../../app/javascript/utils/copyToClipboard'
 
-const mockClipboardApi = () => {
-  jest.spyOn(copyToClipboard, 'copyToClipboard').mockImplementation(() => null)
-}
+// See https://github.com/aelbore/esbuild-jest/issues/26
+jest.mock('../../../../app/javascript/utils/copyToClipboard', () => ({
+  copyToClipboard: jest.fn(),
+}))
+
+import { copyToClipboard } from '../../../../app/javascript/utils/copyToClipboard'
 
 test('copies text to clipboard when clicking', async () => {
-  mockClipboardApi()
-
   const { getByRole } = render(
     <CopyToClipboardButton textToCopy="exercism download --track=ruby --exercise=bob" />
   )
@@ -18,15 +18,13 @@ test('copies text to clipboard when clicking', async () => {
   fireEvent.click(getByRole('img'))
 
   await waitFor(() =>
-    expect(copyToClipboard.copyToClipboard).toHaveBeenCalledWith(
+    expect(copyToClipboard).toHaveBeenCalledWith(
       'exercism download --track=ruby --exercise=bob'
     )
   )
 })
 
 test('copies text to clipboard when pressing Ctrl+C', async () => {
-  mockClipboardApi()
-
   const { queryByRole } = render(
     <CopyToClipboardButton textToCopy="exercism download --track=javascript --exercise=bob" />
   )
@@ -37,7 +35,7 @@ test('copies text to clipboard when pressing Ctrl+C', async () => {
   fireEvent.keyDown(copyButton, { code: 'KeyC', ctrlKey: true })
 
   await waitFor(() =>
-    expect(copyToClipboard.copyToClipboard).toHaveBeenCalledWith(
+    expect(copyToClipboard).toHaveBeenCalledWith(
       'exercism download --track=javascript --exercise=bob'
     )
   )

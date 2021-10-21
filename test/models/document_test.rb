af924bce3c1273e71773c81702f7469ec2851140
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-21 20:45:48 +0200

    Update document_test.rb

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class DocumentTest < ActiveSupport::TestCase
   test "content_html renders correctly" do
     doc = create :document
-    assert doc.content_html.starts_with?("<h2 id=\"running-tests\">Running Tests</h2>\n<p>Execute the tests with:</p>")
+    assert doc.content_html.starts_with?("<h2 id=\"h-running-tests\">Running Tests</h2>\n<p>Execute the tests with:</p>")
   end
 end

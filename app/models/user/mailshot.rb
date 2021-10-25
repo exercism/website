79ed1b22c0befb7e061dd022a978b68542a4b054
Jeremy Walker <jez.walker@gmail.com>
2021-10-25 20:42:15 +0100

    Add db table for mailshots

---
+++ app/models/user/mailshot.rb
@@ -0,0 +1,9 @@
+class User::Mailshot < ApplicationRecord
+  include Emailable
+
+  # We currently always want this email to be sent, so there
+  # is no communication preference key
+  # TODO: Add a flag for whether the email is a marketing
+  # email and use it to power this.
+  def email_communication_preferences_key; end
+end
---
+++ test/factories/user/mailshots.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :user_mailshot, class: 'User::Mailshot' do
+  end
+end

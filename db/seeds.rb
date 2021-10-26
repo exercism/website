9feb0949de609f00ed8daf47b00bcf296a1efe35
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-26 11:30:54 +0200

    Revert change

--- db/seeds.rb
+++ db/seeds.rb
@@ -97,8 +97,7 @@ track_slugs.each do |track_slug|
       blurb: git_track.blurb,
       tags: git_track.tags,
       repo_url: repo_url,
-      synced_to_git_sha: first_commit.oid,
-      active: false
+      synced_to_git_sha: first_commit.oid
     )
     Git::SyncTrack.(track)
   rescue StandardError => e

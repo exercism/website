7984c5153d34dc0e8d02f1c73ad3f215ab8f419b
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-27 14:28:30 +0200

    Handle iteration_idx is nil

--- app/commands/solution/publish_iteration.rb
+++ app/commands/solution/publish_iteration.rb
@@ -5,13 +5,19 @@ class Solution
     initialize_with :solution, :iteration_idx
 
     def call
-      solution.update!(published_iteration: iteration, num_loc: iteration.num_loc)
+      solution.update!(published_iteration: iteration, num_loc: num_loc)
     end
 
     private
     memoize
     def iteration
       solution.iterations.find_by(idx: iteration_idx)
+    end
+
+    def num_loc
+      return solution.iterations.last.num_loc if iteration_idx.nil?
+
+      iteration.num_loc
     end
   end
 end
--- test/commands/solution/publish_iteration_test.rb
+++ test/commands/solution/publish_iteration_test.rb
@@ -25,4 +25,15 @@ class Solution::PublishIterationTest < ActiveSupport::TestCase
 
     assert_equal iteration.num_loc, solution.num_loc
   end
+
+  test "set solution loc to last iteration loc if publishing all iterations" do
+    solution = create :practice_solution, num_loc: 2
+    iteration = create :iteration, solution: solution, idx: 1, num_loc: 5
+    other_iteration = create :iteration, solution: solution, idx: 2, num_loc: 14
+    solution.update(published_iteration: iteration, published_at: Time.current)
+
+    Solution::PublishIteration.(solution, nil)
+
+    assert_equal other_iteration.num_loc, solution.num_loc
+  end
 end

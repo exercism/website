10055c996892cb1a8f2ae7aeef2a3ebc06e4635a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-10-28 13:26:11 +0200

    Don't bulk update

--- db/migrate/20211028110123_change_solutions_num_loc_to_null.rb
+++ db/migrate/20211028110123_change_solutions_num_loc_to_null.rb
@@ -1,7 +1,5 @@
 class ChangeSolutionsNumLocToNull < ActiveRecord::Migration[6.1]
   def change
     add_column :solutions, :num_loc, :integer, null: true
-
-    Solution.where(num_loc: 0).update(num_loc: nil)
   end
 end

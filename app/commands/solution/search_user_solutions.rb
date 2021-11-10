b63deac67d8ed374721c74b538ac966ea69110e7
Erik Schierboom <erik_schierboom@hotmail.com>
2021-11-10 10:53:18 +0100

    Add fallback for searching user solutions

--- app/commands/solution/search_user_solutions.rb
+++ app/commands/solution/search_user_solutions.rb
@@ -33,6 +33,9 @@ class Solution
       total_count = results["hits"]["total"]["value"].to_i
       Kaminari.paginate_array(solutions, total_count: total_count).
         page(page).per(per)
+    rescue StandardError
+      Fallback.(user, page: page, per: per, track_slug: track_slug, status: status, mentoring_status: mentoring_status,
+criteria: criteria, order: order)
     end
 
     private
@@ -81,6 +84,78 @@ class Solution
         password: ENV['OPENSEARCH_PASSWORD'],
         transport_options: { ssl: { verify: ENV['OPENSEARCH_VERIFY_SSL'] != 'false' } }
       )
+    end
+
+    class Fallback
+      include Mandate
+
+      def initialize(user, criteria: nil, track_slug: nil, status: nil, mentoring_status: nil, page: nil, per: nil, order: nil)
+        @user = user
+        @criteria = criteria
+        @track_slug = track_slug
+        @status = status
+        @mentoring_status = mentoring_status
+        @page = page
+        @per = per
+        @order = order
+      end
+
+      def call
+        @solutions = user.solutions
+        filter_criteria!
+        filter_track!
+        filter_status!
+        filter_mentoring_status!
+        sort!
+
+        @solutions.page(page).per(per)
+      end
+
+      private
+      attr_reader :user, :criteria, :track_slug, :status, :mentoring_status,
+        :per, :page, :order,
+        :solutions
+
+      def filter_criteria!
+        return if criteria.blank?
+
+        @solutions = @solutions.joins(exercise: :track)
+        criteria.strip.split(" ").each do |crit|
+          @solutions = @solutions.where(
+            "exercises.title LIKE ? OR tracks.title LIKE ?",
+            "%#{crit}%",
+            "%#{crit}%"
+          )
+        end
+      end
+
+      def filter_track!
+        return if track_slug.blank?
+
+        @solutions = @solutions.joins(exercise: :track).
+          where('tracks.slug': track_slug)
+      end
+
+      def filter_status!
+        return if status.blank?
+
+        @solutions = @solutions.where(status: status)
+      end
+
+      def filter_mentoring_status!
+        return if mentoring_status.blank?
+
+        @solutions = @solutions.where(mentoring_status: mentoring_status)
+      end
+
+      def sort!
+        case order&.to_sym
+        when :oldest_first
+          @solutions = @solutions.order(id: :asc)
+        else # :newest_first
+          @solutions = @solutions.order(id: :desc)
+        end
+      end
     end
   end
 end
--- test/commands/solution/search_user_solutions_test.rb
+++ test/commands/solution/search_user_solutions_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class Solution::SearchUserSolutionsTest < ActiveSupport::TestCase
-  test "no options returns everything" do
+  test "fallback is called" do
+    user = create :user
+    Solution::SearchUserSolutions::Fallback.expects(:call).with(user, page: 2, per: 15, track_slug: "csharp", status: "published",
+mentoring_status: "none", criteria: "foobar", order: "oldest_first")
+    Elasticsearch::Client.expects(:new).raises
+
+    Solution::SearchUserSolutions.(user, page: 2, per: 15, track_slug: "csharp", status: "published", mentoring_status: "none",
+criteria: "foobar", order: "oldest_first")
+  end
+
+  test "fallback: no options returns everything" do
     user = create :user
     solution_1 = create :concept_solution, user: user
     solution_2 = create :practice_solution, user: user
@@ -9,10 +19,10 @@ class Solution::SearchUserSolutionsTest < ActiveSupport::TestCase
     # Someone else's solution
     create :concept_solution
 
-    assert_equal [solution_2, solution_1], Solution::SearchUserSolutions.(user)
+    assert_equal [solution_2, solution_1], Solution::SearchUserSolutions::Fallback.(user)
   end
 
-  test "criteria" do
+  test "fallback: criteria" do
     user = create :user
     javascript = create :track, title: "JavaScript", slug: "javascript"
     ruby = create :track, title: "Ruby"
@@ -24,15 +34,15 @@ class Solution::SearchUserSolutionsTest < ActiveSupport::TestCase
     ruby_food_solution = create :concept_solution, user: user, exercise: ruby_food
     ruby_bob_solution = create :concept_solution, user: user, exercise: ruby_bob
 
-    assert_equal [ruby_bob_solution, ruby_food_solution, js_bob_solution], Solution::SearchUserSolutions.(user)
-    assert_equal [ruby_bob_solution, ruby_food_solution, js_bob_solution], Solution::SearchUserSolutions.(user, criteria: " ") # rubocop:disable Layout:LineLength
-    assert_equal [ruby_bob_solution, ruby_food_solution], Solution::SearchUserSolutions.(user, criteria: "ru")
-    assert_equal [ruby_bob_solution, js_bob_solution], Solution::SearchUserSolutions.(user, criteria: "bo")
-    assert_equal [ruby_bob_solution].map(&:track), Solution::SearchUserSolutions.(user, criteria: "ru bo").map(&:track)
-    assert_equal [ruby_food_solution], Solution::SearchUserSolutions.(user, criteria: "r ch fo")
+    assert_equal [ruby_bob_solution, ruby_food_solution, js_bob_solution], Solution::SearchUserSolutions::Fallback.(user)
+    assert_equal [ruby_bob_solution, ruby_food_solution, js_bob_solution], Solution::SearchUserSolutions::Fallback.(user, criteria: " ") # rubocop:disable Layout:LineLength
+    assert_equal [ruby_bob_solution, ruby_food_solution], Solution::SearchUserSolutions::Fallback.(user, criteria: "ru")
+    assert_equal [ruby_bob_solution, js_bob_solution], Solution::SearchUserSolutions::Fallback.(user, criteria: "bo")
+    assert_equal [ruby_bob_solution].map(&:track), Solution::SearchUserSolutions::Fallback.(user, criteria: "ru bo").map(&:track)
+    assert_equal [ruby_food_solution], Solution::SearchUserSolutions::Fallback.(user, criteria: "r ch fo")
   end
 
-  test "track_slug" do
+  test "fallback: track_slug" do
     user = create :user
     javascript = create :track, title: "JavaScript", slug: "javascript"
     ruby = create :track, title: "Ruby", slug: "ruby"
@@ -45,78 +55,84 @@ class Solution::SearchUserSolutionsTest < ActiveSupport::TestCase
     js_solution = create :practice_solution, user: user, exercise: js_exercise
     elixir_solution = create :practice_solution, user: user, exercise: elixir_exercise
 
-    assert_equal [elixir_solution, js_solution, ruby_solution], Solution::SearchUserSolutions.(user)
-    assert_equal [js_solution, ruby_solution], Solution::SearchUserSolutions.(user, track_slug: %i[ruby javascript])
-    assert_equal [ruby_solution], Solution::SearchUserSolutions.(user, track_slug: "ruby")
+    assert_equal [elixir_solution, js_solution, ruby_solution], Solution::SearchUserSolutions::Fallback.(user)
+    assert_equal [js_solution, ruby_solution], Solution::SearchUserSolutions::Fallback.(user, track_slug: %i[ruby javascript])
+    assert_equal [ruby_solution], Solution::SearchUserSolutions::Fallback.(user, track_slug: "ruby")
   end
 
-  test "status" do
+  test "fallback: status" do
     user = create :user
     published = create :practice_solution, user: user, status: :published
     completed = create :practice_solution, user: user, status: :completed
     iterated = create :concept_solution, user: user, status: :iterated
 
-    assert_equal [iterated, completed, published], Solution::SearchUserSolutions.(user, status: nil)
-    assert_equal [iterated], Solution::SearchUserSolutions.(user, status: :iterated)
-    assert_equal [iterated], Solution::SearchUserSolutions.(user, status: 'iterated')
-    assert_equal [completed, published], Solution::SearchUserSolutions.(user, status: %i[completed published])
-    assert_equal [completed, published], Solution::SearchUserSolutions.(user, status: %w[completed published])
-    assert_equal [published], Solution::SearchUserSolutions.(user, status: :published)
-    assert_equal [published], Solution::SearchUserSolutions.(user, status: 'published')
+    assert_equal [iterated, completed, published], Solution::SearchUserSolutions::Fallback.(user, status: nil)
+    assert_equal [iterated], Solution::SearchUserSolutions::Fallback.(user, status: :iterated)
+    assert_equal [iterated], Solution::SearchUserSolutions::Fallback.(user, status: 'iterated')
+    assert_equal [completed, published], Solution::SearchUserSolutions::Fallback.(user, status: %i[completed published])
+    assert_equal [completed, published], Solution::SearchUserSolutions::Fallback.(user, status: %w[completed published])
+    assert_equal [published], Solution::SearchUserSolutions::Fallback.(user, status: :published)
+    assert_equal [published], Solution::SearchUserSolutions::Fallback.(user, status: 'published')
   end
 
-  test "mentoring_status" do
+  test "fallback: mentoring_status" do
     user = create :user
     finished = create :concept_solution, user: user, mentoring_status: :finished
     in_progress = create :concept_solution, user: user, mentoring_status: :in_progress
     requested = create :concept_solution, user: user, mentoring_status: :requested
     none = create :concept_solution, user: user, mentoring_status: :none
 
-    assert_equal [none, requested, in_progress, finished], Solution::SearchUserSolutions.(user, mentoring_status: nil)
+    assert_equal [none, requested, in_progress, finished], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: nil)
 
-    assert_equal [none], Solution::SearchUserSolutions.(user, mentoring_status: :none)
-    assert_equal [none], Solution::SearchUserSolutions.(user, mentoring_status: 'none')
+    assert_equal [none], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: :none)
+    assert_equal [none], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: 'none')
 
-    assert_equal [requested], Solution::SearchUserSolutions.(user, mentoring_status: :requested)
-    assert_equal [requested], Solution::SearchUserSolutions.(user, mentoring_status: 'requested')
+    assert_equal [requested], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: :requested)
+    assert_equal [requested], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: 'requested')
 
-    assert_equal [in_progress], Solution::SearchUserSolutions.(user, mentoring_status: :in_progress)
-    assert_equal [in_progress], Solution::SearchUserSolutions.(user, mentoring_status: 'in_progress')
+    assert_equal [in_progress], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: :in_progress)
+    assert_equal [in_progress], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: 'in_progress')
 
-    assert_equal [finished], Solution::SearchUserSolutions.(user, mentoring_status: :finished)
-    assert_equal [finished], Solution::SearchUserSolutions.(user, mentoring_status: 'finished')
+    assert_equal [finished], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: :finished)
+    assert_equal [finished], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: 'finished')
 
-    assert_equal [none, finished], Solution::SearchUserSolutions.(user, mentoring_status: [:none, 'finished'])
+    assert_equal [none, finished], Solution::SearchUserSolutions::Fallback.(user, mentoring_status: [:none, 'finished'])
   end
 
-  test "pagination" do
+  test "fallback: pagination" do
     user = create :user
     solution_1 = create :concept_solution, user: user
     solution_2 = create :concept_solution, user: user
 
-    assert_equal [solution_2], Solution::SearchUserSolutions.(user, page: 1, per: 1)
-    assert_equal [solution_1], Solution::SearchUserSolutions.(user, page: 2, per: 1)
-    assert_equal [solution_2, solution_1], Solution::SearchUserSolutions.(user, page: 1, per: 2)
-    assert_empty Solution::SearchUserSolutions.(user, page: 2, per: 2)
+    assert_equal [solution_2], Solution::SearchUserSolutions::Fallback.(user, page: 1, per: 1)
+    assert_equal [solution_1], Solution::SearchUserSolutions::Fallback.(user, page: 2, per: 1)
+    assert_equal [solution_2, solution_1], Solution::SearchUserSolutions::Fallback.(user, page: 1, per: 2)
+    assert_empty Solution::SearchUserSolutions::Fallback.(user, page: 2, per: 2)
+  end
 
-    # Check it uses defaults for invalid values
+  test "fallback: pagination with invalid values" do
+    user = create :user
+    solution_1 = create :concept_solution, user: user
+    solution_2 = create :concept_solution, user: user
+    Elasticsearch::Client.expects(:new).twice.raises
+
     assert_equal [solution_2, solution_1], Solution::SearchUserSolutions.(user, page: 0, per: 0)
     assert_equal [solution_2, solution_1], Solution::SearchUserSolutions.(user, page: 'foo', per: 'bar')
   end
 
-  test "sort oldest first" do
+  test "fallback: sort oldest first" do
     user = create :user
     old_solution = create :concept_solution, user: user
     new_solution = create :concept_solution, user: user
 
-    assert_equal [old_solution, new_solution], Solution::SearchUserSolutions.(user, order: "oldest_first")
+    assert_equal [old_solution, new_solution], Solution::SearchUserSolutions::Fallback.(user, order: "oldest_first")
   end
 
-  test "sort newest first by default" do
+  test "fallback: sort newest first by default" do
     user = create :user
     old_solution = create :concept_solution, user: user
     new_solution = create :concept_solution, user: user
 
-    assert_equal [new_solution, old_solution], Solution::SearchUserSolutions.(user)
+    assert_equal [new_solution, old_solution], Solution::SearchUserSolutions::Fallback.(user)
   end
 end

2666eeabb60378a63c9eb8dc3550bbd822116e3c
Jeremy Walker <jez.walker@gmail.com>
2021-11-19 11:12:40 +0000

    Remove colors from production logs (#2177)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
+  config.active_record.colorize_logging = false
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]

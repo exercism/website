d8b141f59ecaef05650f0b8bb083486dafeb1c69
Erik Schierboom <erik_schierboom@hotmail.com>
2021-11-22 13:26:21 +0100

    Sync main docs

--- app/models/git/docs.rb
+++ app/models/git/docs.rb
@@ -2,10 +2,12 @@ module Git
   class Docs
     extend Mandate::Memoize
 
+    delegate :head_sha, :fetch!, to: :repo
+
     DEFAULT_REPO_URL = "https://github.com/exercism/docs".freeze
 
-    def initialize(repo_url: DEFAULT_REPO_URL)
-      @repo = Repository.new(repo_url: repo_url)
+    def initialize(repo_url: DEFAULT_REPO_URL, branch_ref: nil)
+      @repo = Repository.new(repo_url: repo_url, branch_ref: branch_ref)
     end
 
     def section_config(section)

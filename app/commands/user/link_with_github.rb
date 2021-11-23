16794270e23a8f27c60fc85845e529d443e23fea
Jeremy Walker <jez.walker@gmail.com>
2021-11-23 17:00:33 +0100

    Add option to link GH and Exercism accounts

---
+++ app/commands/user/link_with_github.rb
@@ -0,0 +1,16 @@
+class User
+  class LinkWithGithub
+    include Mandate
+
+    initialize_with :user, :auth
+
+    def call
+      user.update!(
+        provider: auth.provider,
+        uid: auth.uid
+      )
+
+      User::SetGithubUsername.(user, auth.info.nickname)
+    end
+  end
+end

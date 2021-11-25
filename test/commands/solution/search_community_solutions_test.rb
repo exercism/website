1a20084cf08a90e5325972957fc39d84f03559d5
Erik Schierboom <erik_schierboom@hotmail.com>
2021-11-25 15:00:46 +0100

    Add order argument

--- test/commands/solution/search_community_solutions_test.rb
+++ test/commands/solution/search_community_solutions_test.rb
@@ -19,7 +19,7 @@ class Solution::SearchCommunitySolutionsTest < ActiveSupport::TestCase
     wait_for_opensearch_to_be_synced
 
     assert_equal [solution_2, solution_1],
-      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 10, criteria: "", status: nil, mentoring_status: nil,
+      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 10, order: nil, criteria: "", status: nil, mentoring_status: nil,
 up_to_date: nil)
   end
 
@@ -36,7 +36,7 @@ up_to_date: nil)
     wait_for_opensearch_to_be_synced
 
     assert_equal [solution_2, solution_3, solution_1],
-      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 10, criteria: "", status: nil, mentoring_status: nil,
+      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 10, order: nil, criteria: "", status: nil, mentoring_status: nil,
 up_to_date: nil)
   end
 
@@ -52,13 +52,15 @@ up_to_date: nil)
     wait_for_opensearch_to_be_synced
 
     assert_equal [solution_2],
-      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 1, criteria: "", status: nil, mentoring_status: nil, up_to_date: nil)
-    assert_equal [solution_1],
-      Solution::SearchCommunitySolutions.(exercise, page: 2, per: 1, criteria: "", status: nil, mentoring_status: nil, up_to_date: nil)
-    assert_equal [solution_2, solution_1],
-      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 2, criteria: "", status: nil, mentoring_status: nil, up_to_date: nil)
-    assert_empty Solution::SearchCommunitySolutions.(exercise, page: 2, per: 2, criteria: "", status: nil, mentoring_status: nil,
+      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 1, order: nil, criteria: "", status: nil, mentoring_status: nil,
 up_to_date: nil)
+    assert_equal [solution_1],
+      Solution::SearchCommunitySolutions.(exercise, page: 2, per: 1, order: nil, criteria: "", status: nil, mentoring_status: nil,
+up_to_date: nil)
+    assert_equal [solution_2, solution_1],
+      Solution::SearchCommunitySolutions.(exercise, page: 1, per: 2, order: nil, criteria: "", status: nil, mentoring_status: nil,
+up_to_date: nil)
+    assert_empty Solution::SearchCommunitySolutions.(exercise, page: 2, per: 2, order: nil, criteria: "", status: nil, mentoring_status: nil, up_to_date: nil) # rubocop:disable Layout:LineLength
   end
 
   test "does not try and access values above 10_000" do
@@ -85,11 +87,10 @@ up_to_date: nil)
 
   test "fallback is called" do
     exercise = create :concept_exercise
-    Solution::SearchCommunitySolutions::Fallback.expects(:call).with(exercise, 2, 15, "foobar", :active, :requested, true)
+    Solution::SearchCommunitySolutions::Fallback.expects(:call).with(exercise, 2, 15, "newest", "foobar", :active, :requested, true)
     Elasticsearch::Client.expects(:new).raises
 
-    Solution::SearchCommunitySolutions.(exercise, page: 2, per: 15, criteria: "foobar", status: :active, mentoring_status: :requested,
-up_to_date: true)
+    Solution::SearchCommunitySolutions.(exercise, page: 2, per: 15, order: "newest", criteria: "foobar", status: :active, mentoring_status: :requested, up_to_date: true) # rubocop:disable Layout:LineLength
   end
 
   test "fallback is called when elasticsearch times out" do
@@ -127,7 +128,7 @@ up_to_date: true)
     # A different exercise
     create :concept_solution
 
-    assert_equal [solution_2, solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 10, "", nil, nil, nil)
+    assert_equal [solution_2, solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 10, nil, "", nil, nil, nil)
   end
 
   test "fallback: orders by stars then id" do
@@ -138,7 +139,7 @@ up_to_date: true)
     solution_3 = create :concept_solution, exercise: exercise, published_at: Time.current, status: :published, num_stars: 1
 
     assert_equal [solution_2, solution_3, solution_1],
-      Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 10, "", nil, nil, nil)
+      Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 10, nil, "", nil, nil, nil)
   end
 
   test "fallback: pagination" do
@@ -147,9 +148,9 @@ up_to_date: true)
     solution_1 = create :concept_solution, exercise: exercise, published_at: Time.current, status: :published
     solution_2 = create :concept_solution, exercise: exercise, published_at: Time.current, status: :published
 
-    assert_equal [solution_2], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 1, "", nil, nil, nil)
-    assert_equal [solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 2, 1, "", nil, nil, nil)
-    assert_equal [solution_2, solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 2, "", nil, nil, nil)
-    assert_empty Solution::SearchCommunitySolutions::Fallback.(exercise, 2, 2, "", nil, nil, nil)
+    assert_equal [solution_2], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 1, nil, "", nil, nil, nil)
+    assert_equal [solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 2, 1, nil, "", nil, nil, nil)
+    assert_equal [solution_2, solution_1], Solution::SearchCommunitySolutions::Fallback.(exercise, 1, 2, nil, "", nil, nil, nil)
+    assert_empty Solution::SearchCommunitySolutions::Fallback.(exercise, 2, 2, nil, "", nil, nil, nil)
   end
 end

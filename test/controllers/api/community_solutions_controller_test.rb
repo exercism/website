26f56ce14472ada70037e6b6988584083c1b2939
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-02 15:53:08 +0100

    Fix test

--- test/controllers/api/community_solutions_controller_test.rb
+++ test/controllers/api/community_solutions_controller_test.rb
@@ -62,9 +62,11 @@ module API
     test "index should search and return solutions" do
       track = create :track
       exercise = create :concept_exercise, :random_slug, track: track
-      solution_1 = create :concept_solution, exercise: exercise, published_at: Time.current, num_stars: 11
+      solution_1 = create :concept_solution, exercise: exercise, published_at: Time.current, num_stars: 11,
+published_iteration_head_tests_status: :queued
       create :iteration, solution: solution_1
-      solution_2 = create :concept_solution, exercise: exercise, published_at: Time.current, num_stars: 22
+      solution_2 = create :concept_solution, exercise: exercise, published_at: Time.current, num_stars: 22,
+published_iteration_head_tests_status: :passed
       create :iteration, solution: solution_2
       create :concept_solution, published_at: Time.current, num_stars: 33
 

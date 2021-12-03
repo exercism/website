40d3bba72e70b233e2062c46b75d6494fa319732
Erik Schierboom <erik_schierboom@hotmail.com>
2021-12-03 14:19:38 +0100

    Remove migration

--- db/migrate/20211201132830_add_published_iteration_head_tests_status_to_solutions.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedIterationHeadTestsStatusToSolutions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :solutions, :published_iteration_head_tests_status, :integer, null: false, default: 0
-  end
-end

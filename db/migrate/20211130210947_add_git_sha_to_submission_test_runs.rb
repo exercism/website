8ef0bb6bb0ac6eddc38b9b0b2d2a0f7d329edc4f
Jeremy Walker <jez.walker@gmail.com>
2021-12-03 12:22:05 +0100

    Add migration code to make it easier to migrate

--- db/migrate/20211130210947_add_git_sha_to_submission_test_runs.rb
+++ db/migrate/20211130210947_add_git_sha_to_submission_test_runs.rb
@@ -5,42 +5,44 @@ class AddGitShaToSubmissionTestRuns < ActiveRecord::Migration[6.1]
     execute "ALTER TABLE `submission_test_runs` ADD `git_important_files_hash` varchar(50) NULL, ALGORITHM=INPLACE, LOCK=NONE"
     execute "ALTER TABLE `submission_test_runs` ADD `git_sha` varchar(50) NULL, ALGORITHM=INPLACE, LOCK=NONE"
 
-    return
+    # # Run the following to set the git_important_files_hash on all submissions
+    # ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
+    #   submissions = Submission.joins(:solution).includes(solution: :exercise).
+    #     where(git_important_files_hash: nil).
+    #     select(:id, :git_sha, :solution_id, :exercise_id).
+    #     group_by(&:git_sha)
 
-    # Run the following to set the git_important_files_hash on all submissions
-    ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
-      submissions = Submission.joins(:solution).includes(solution: :exercise).
-        select(:id, :git_sha, :solution_id, :exercise_id).
-        group_by(&:git_sha)
+    #   submissions.each do |git_sha, group|
+    #     exercise = group.first.exercise
+    #     new_hash = Git::GenerateHashForImportantExerciseFiles.(exercise, git_sha: git_sha)
+    #     group.each do |submission|
+    #       submission.update_column(:git_important_files_hash, new_hash)
+    #     end
+    #   end
+    # end
 
-      submissions.each do |git_sha, group|
-        exercise = group.first.exercise
-        new_hash = Git::GenerateHashForImportantExerciseFiles.(exercise, git_sha: git_sha)
-        group.each do |submission|
-          submission.update_column(:git_important_files_hash, new_hash)
-        end
-      end
-    end
+    # # Run the following to set the git_shas on all submission_test_runs
+    # ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
+    #   Submission::TestRun.
+    #     where(git_sha: nil).
+    #     joins(submission: :solution).update_all("submission_test_runs.git_sha = submissions.git_sha")
+    # end
 
-    # Run the following to set the git_shas on all submission_test_runs
-    ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
-      Submission::TestRun.joins(submission: :solution).update_all("submission_test_runs.git_sha = submissions.git_sha")
-    end
+    # # Run the following to set the git_important_files_hash on all submission_test_runs
+    # ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
+    #   test_runs = Submission::TestRun.joins(submission: :solution).includes(submission: {solution: :exercise}).
+    #     where(git_important_files_hash: nil).
+    #     select(:id, :git_sha, :submission_id, :solution_id, :exercise_id).
+    #     group_by(&:git_sha)
 
-    # Run the following to set the git_important_files_hash on all submission_test_runs
-    ActiveRecord::Base.transaction(isolation: Exercism::READ_UNCOMMITTED) do
-      test_runs = Submission::TestRun.joins(submission: :solution).includes(submission: {solution: :exercise}).
-        select(:id, :git_sha, :submission_id, :solution_id, :exercise_id).
-        group_by(&:git_sha)
-
-      test_runs.each do |git_sha, group|
-        exercise = group.first.submission.exercise
-        new_hash = Git::GenerateHashForImportantExerciseFiles.(exercise, git_sha: git_sha)
-        group.each do |test_run|
-          test_run.update_column(:git_important_files_hash, new_hash)
-        end
-      end
-    end
+    #   test_runs.each do |git_sha, group|
+    #     exercise = group.first.submission.exercise
+    #     new_hash = Git::GenerateHashForImportantExerciseFiles.(exercise, git_sha: git_sha)
+    #     group.each do |test_run|
+    #       test_run.update_column(:git_important_files_hash, new_hash)
+    #     end
+    #   end
+    # end
   end
 
   def down

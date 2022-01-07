8e0868bb4ab73fa6f663cf59b5e47d018f5324e2
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-07 08:57:13 +0100

    Remove unused code

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,23 +3,3 @@ Rails.application.configure do
   config.assets.paths << "app/images"
   config.assets.paths << ".built-assets"
 end
-
-# class AssetUrlProcessor
-#   def self.call(input)
-#     # don't know why, copy from other processor
-#     context = input[:environment].context_class.new(input)
-#     data = input[:data].gsub(/url\(["']?(.+?)["']?\)/i) do |match|
-#       asset = $1
-#       if asset && asset !~ /(data:|http)/i
-#         path = context.asset_path(asset)
-#         "url(#{path})"
-#       else
-#         match
-#       end
-#     end
-
-#     { data: data }
-#   end
-# end
-
-# Sprockets.register_postprocessor 'text/css', AssetUrlProcessor

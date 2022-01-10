bfa69062426de25bc3c80dc95028f645c4c97902
Erik Schierboom <erik_schierboom@hotmail.com>
2022-01-10 10:06:41 +0100

    Apply review comments

--- postcss.config.js
+++ postcss.config.js
@@ -8,24 +8,3 @@ module.exports = {
     process.env.NODE_ENV === 'production' ? require('postcss-minify') : null,
   ],
 }
-
-//module.exports = {
-//  plugins: [
-//    // require('postcss-nesting'),
-//    // require('autoprefixer'),
-
-//    //require('postcss-mixins'),
-//    require('postcss-import'),
-//    require('tailwindcss'),
-//    require('postcss-flexbugs-fixes'),
-//    require('postcss-preset-env')({
-//      autoprefixer: {
-//        flexbox: 'no-2009',
-//      },
-//      features: {
-//        'nesting-rules': true,
-//      },
-//      stage: 3,
-//    }),
-//  ],
-//}

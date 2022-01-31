3fe3b2ef57a94eb7fb9a75e64278da8fd68223c7
Jeremy Walker <jez.walker@gmail.com>
2022-01-31 18:16:52 +0000

    Restructure about section

---
+++ app/helpers/view_components/about_nav.rb
@@ -0,0 +1,43 @@
+module ViewComponents
+  class AboutNav < ViewComponent
+    extend Mandate::Memoize
+
+    def initialize(selected_section = nil)
+      super()
+
+      @selected_section = selected_section
+    end
+
+    def to_s
+      tag.div(class: "c-about-nav") do
+        tag.div(lhs, class: "lg-container container")
+      end
+    end
+
+    def lhs
+      tag.ul do
+        safe_join(
+          [
+            li_link("About Exercism"),
+            li_link("Team", :team),
+            li_link("Jobs", :hiring),
+            li_link("Supporters", :individual_supporters),
+            li_link("Supporting Orgs", :organisation_supporters)
+            # li_link("Community", :community),
+            # li_link("Not-for-profit", :organisation),
+            # li_link("Track-specific", :tracks)
+          ]
+        )
+      end
+    end
+
+    def li_link(title, section = nil)
+      css_class = section == selected_section ? "selected" : nil
+      url = Exercism::Routes.send([section, "about_path"].compact.join("_"))
+      tag.li(link_to(title, url), class: css_class)
+    end
+
+    private
+    attr_reader :selected_section
+  end
+end

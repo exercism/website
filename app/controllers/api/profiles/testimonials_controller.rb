bb5a7bcd958949f44925c41f6cd293286f1f9e1e
Jeremy Walker <jez.walker@gmail.com>
2022-02-11 12:20:54 +0000

    Add temporary hiring endpoint

--- app/controllers/api/profiles/testimonials_controller.rb
+++ app/controllers/api/profiles/testimonials_controller.rb
@@ -3,5 +3,44 @@ module API::Profiles
     def index
       render json: AssembleProfileTestimonialsList.(@user)
     end
+
+    def hiring
+      testimonials = @user.mentor_testimonials.published.joins(solution: { exercise: :track })
+      testimonials = testimonials.where('exercises.track_id': Track.find(params[:track])) if params[:track]
+      testimonials = testimonials.where('exercises.title LIKE ?': "%#{params[:track]}%") if params[:exercise]
+      testimonials = testimonials.order(id: params[:order] == "newest_first" ? :desc : :asc)
+      testimonials = testimonials.page(params[:page]).per(20)
+
+      serialized = {
+        results: testimonials.map do |t|
+                   {
+                     id: t.id,
+                     track: {
+                       slug: t.track.slug,
+                       title: t.track.title,
+                       icon_url: t.track.icon_url
+                     },
+                     exercise: {
+                       slug: t.exercise.slug,
+                       title: t.exercise.title,
+                       icon_url: t.exercise.icon_url
+                     },
+                     author: {
+                       handle: t.student.handle,
+                       avatar_url: t.student.avatar_url
+                     },
+                     content: t.content,
+                     created_at: t.created_at
+                   }
+                 end,
+        meta: {
+          current_page: testimonials.current_page,
+          total_count: testimonials.total_count,
+          total_pages: testimonials.total_pages
+        }
+      }
+
+      render json: { testimonials: serialized }
+    end
   end
 end

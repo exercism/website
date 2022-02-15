0d3e1b9ee189847377d617d5aea42efcbfcd3e3f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-02-15 15:11:57 +0100

    Fix tests

--- test/system/components/editor_test.rb
+++ test/system/components/editor_test.rb
@@ -184,7 +184,7 @@ module Components
           }
         Submission::TestRunsChannel.broadcast!(test_run)
 
-        assert_text "2/3 TASKS COMPLETED"
+        assert_text "2 / 3 TASKS COMPLETED"
       end
     end
 
@@ -371,7 +371,7 @@ module Components
         sign_in!(user)
         visit edit_track_exercise_path(track, exercise)
 
-        assert_text "ALL TASKS COMPLETED"
+        assert_text "ALL TASKS PASSED"
       end
     end
 

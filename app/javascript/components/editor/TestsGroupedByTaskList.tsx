84538dcf82934d1edde2971e83d91921b965341c
Erik Schierboom <erik_schierboom@hotmail.com>
2022-02-23 12:34:57 +0100

    List tasks by id

--- app/javascript/components/editor/TestsGroupedByTaskList.tsx
+++ app/javascript/components/editor/TestsGroupedByTaskList.tsx
@@ -8,6 +8,8 @@ import { TasksContext } from '../Editor'
 type TaskWithTestsWithToggle = AssignmentTask & {
   id: number
   tests: TestWithToggle[]
+  defaultOpen: boolean
+  passing: boolean
 }
 
 const Tests = ({
@@ -63,65 +65,47 @@ export function TestsGroupedByTaskList({
   tasks: AssignmentTask[]
 }): JSX.Element {
   const testsWithIndex = tests.map((test, i) => ({ index: i + 1, ...test }))
-  const tasksWithTests = tasks.map((task, i) => ({
-    ...task,
-    id: i + 1,
-    tests: testsWithIndex.filter((test) => test.taskId === i + 1),
-  }))
-  const passed: TaskWithTestsWithToggle[] = tasksWithTests
-    .filter((task) =>
-      task.tests.every((test) => test.status === TestStatus.PASS)
-    )
-    .map((task) => ({
-      ...task,
-      tests: task.tests.map((test) => {
-        return { ...test, defaultOpen: false }
-      }),
-    }))
-
-  const failed: TaskWithTestsWithToggle[] = tasksWithTests
-    .filter((task) =>
-      task.tests.some(
-        (test) =>
-          test.status === TestStatus.FAIL || test.status === TestStatus.ERROR
-      )
-    )
+  const tasksWithTests = tasks
     .map((task, i) => ({
       ...task,
-      tests: task.tests.map((test, j) => {
-        return {
-          ...test,
-          defaultOpen:
-            i === 0 &&
-            j ==
-              task.tests.findIndex(
-                (test) =>
-                  test.status === TestStatus.FAIL ||
-                  test.status === TestStatus.ERROR
-              ),
-        }
-      }),
+      id: i + 1,
+      tests: testsWithIndex.filter((test) => test.taskId === i + 1),
+    }))
+    .map((task) => ({
+      ...task,
+      passing: task.tests.every((test) => test.status === TestStatus.PASS),
+    }))
+
+  const openTaskIdx = tasksWithTests.findIndex((t) => !t.passing)
+
+  const tasksWithTestsWithToggle: TaskWithTestsWithToggle[] =
+    tasksWithTests.map((task, i) => ({
+      ...task,
+      defaultOpen: i === openTaskIdx,
+      tests: task.tests.map((test, j) => ({
+        ...test,
+        defaultOpen:
+          i === openTaskIdx &&
+          j ==
+            task.tests.findIndex(
+              (test) =>
+                test.status === TestStatus.FAIL ||
+                test.status === TestStatus.ERROR
+            ),
+      })),
     }))
 
   return (
     <div className="tests-list">
-      {passed.map((task) => (
-        <TestsGroup key={task.id} tests={task.tests} open={false}>
+      {tasksWithTestsWithToggle.map((task, i) => (
+        <TestsGroup key={i} tests={task.tests} open={task.defaultOpen}>
           <TestsGroup.Header>
-            <GraphicalIcon icon="passed-check-circle" className="indicator" />
-            <Title task={task} />
-            <GraphicalIcon icon="chevron-right" className="--closed-icon" />
-            <GraphicalIcon icon="chevron-down" className="--open-icon" />
-          </TestsGroup.Header>
-          <Tests tests={task.tests} language={language} />
-          <JumpToInstructionButton taskId={task.id} />
-        </TestsGroup>
-      ))}
-
-      {failed.map((task, i) => (
-        <TestsGroup key={task.id} tests={task.tests} open={i === 0}>
-          <TestsGroup.Header>
-            <GraphicalIcon icon="failed-check-circle" className="indicator" />
+            <GraphicalIcon
+              icon={
+                task.passing ? 'passed-check-circle' : 'failed-check-circle'
+              }
+              className="indicator"
+            />
             <Title task={task} />
             <GraphicalIcon icon="chevron-right" className="--closed-icon" />
             <GraphicalIcon icon="chevron-down" className="--open-icon" />

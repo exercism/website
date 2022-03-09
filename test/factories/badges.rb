81d8a19231014942a5c6b6f82e600019dee62d10
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-09 16:21:38 +0100

    Add badge for v1 users

--- test/factories/badges.rb
+++ test/factories/badges.rb
@@ -2,10 +2,9 @@ FactoryBot.define do
   factory :badge, class: 'Badges::RookieBadge' do
   end
 
-  %i[
-    member rookie supporter contributor
-    anybody_there growth_mindset
-  ].each do |type|
+  %i[ # rubocop:disable Naming/VariableNumber
+      member rookie supporter contributor
+      anybody_there growth_mindset v1].each do |type| # rubocop:disable Naming/VariableNumber
     factory "#{type}_badge", class: "Badges::#{type.to_s.camelize}Badge" do
     end
   end

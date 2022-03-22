6b15d10e5e01b601712e345d43ea0421edd52cb8
Jeremy Walker <jez.walker@gmail.com>
2022-03-22 13:15:08 +0000

    Add more logs to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,12 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = :debug
   config.colorize_logging = false
+
+  # This uses too much memory in production
+  # Maybe we should set it in Bastion using an ENV var?
+  config.active_record.verbose_query_logs = false
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]

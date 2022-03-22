7a0c82262464758f88eb55d588a09daf40fc6666
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-22 14:44:23 +0100

    Add v1 and v2 badges (#2500)
    * Add badge for v1 users
    
    * Add badge for v1 users
    
    * Add badge for v1 users
    
    * Add badge for v2 users
    
    * Fix pluralization of badge modal
    
    * Add v1 icon
    
    * Add v2 icon
    
    * Add test for v1/v2 badges when registered now
    
    * Update v1 badge

--- test/factories/badges.rb
+++ test/factories/badges.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :badge, class: 'Badges::RookieBadge' do
   end
 
+  # rubocop:disable Naming/VariableNumber
   %i[
     member rookie supporter contributor
-    anybody_there growth_mindset
+    anybody_there growth_mindset v1 v2
   ].each do |type|
     factory "#{type}_badge", class: "Badges::#{type.to_s.camelize}Badge" do
     end
   end
+  # rubocop:enable Naming/VariableNumber
 end

a9eae44909a6440b4f30415fa8ac5e0eedf486f7
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-23 08:28:35 +0100

    Add researcher badge

---
+++ app/models/badges/researcher_badge.rb
@@ -0,0 +1,40 @@
+module Badges
+  class ResearcherBadge < Badge
+    seed "Researcher",
+      :ultimate,
+      'researcher',
+      'Awarded for having helped with Exercism Research'
+
+    def award_to?(user)
+      RESEARCHERS.include?(user.handle.downcase)
+    end
+
+    def send_email_on_acquisition?
+      true
+    end
+
+    RESEARCHERS = %w[
+      bergjohan
+      bkhl
+      bubo-py
+      ceddlyburge
+      coriolinus
+      ee7
+      erikschierboom
+      goalaleo
+      ihid
+      kntsoriano
+      kytrinyx
+      mmmmmrob
+      mpizenberg
+      nicolechalmers
+      pvcarrera
+      sleeplessbyte
+      sshine
+      tehsphinx
+      thelostlambda
+      yawpitch
+    ].freeze
+    private_constant :RESEARCHERS
+  end
+end
---
+++ test/models/badges/researcher_badge_test.rb
@@ -0,0 +1,55 @@
+require "test_helper"
+
+class Badge::ResearcherBadgeTest < ActiveSupport::TestCase
+  test "attributes" do
+    badge = create :researcher_badge
+    assert_equal "Researcher", badge.name
+    assert_equal :ultimate, badge.rarity
+    assert_equal :researcher, badge.icon
+    assert_equal 'Awarded for having helped with Exercism Research', badge.description
+    assert badge.send_email_on_acquisition?
+    assert_nil badge.notification_key
+  end
+
+  test "award_to?" do
+    badge = create :researcher_badge
+
+    non_researcher_user = create :user
+    refute badge.award_to?(non_researcher_user)
+
+    # Checks username case-insensitive
+    %w[erikschierboom ERIKSCHIERBOOM ErikSchierboom].each do |handle|
+      user = create :user, handle: handle
+      assert badge.award_to?(user)
+      user.destroy
+    end
+
+    %w[
+      bergjohan
+      bkhl
+      bubo-py
+      ceddlyburge
+      coriolinus
+      ee7
+      ErikSchierboom
+      goalaleo
+      iHiD
+      kntsoriano
+      kytrinyx
+      mmmmmrob
+      mpizenberg
+      nicolechalmers
+      pvcarrera
+      SleeplessByte
+      sshine
+      tehsphinx
+      TheLostLambda
+      yawpitch
+    ].each do |handle|
+      researcher_user = create :user, handle: handle
+
+      # Award to researcher user
+      assert badge.award_to?(researcher_user)
+    end
+  end
+end

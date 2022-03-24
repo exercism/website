75b1802bc4b73f24bc5268c11f03ca953e75d80c
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-24 12:15:40 +0100

    Remove updating of new status columns from migration

--- db/migrate/20220324090941_add_tooling_status_columns_to_tracks.rb
+++ db/migrate/20220324090941_add_tooling_status_columns_to_tracks.rb
@@ -2,12 +2,5 @@ class AddToolingStatusColumnsToTracks < ActiveRecord::Migration[7.0]
   def change
     add_column :tracks, :has_representer, :boolean, null: false, default: false
     add_column :tracks, :has_analyzer, :boolean, null: false, default: false
-
-    Track.find_each do |track|
-      track.update(
-         has_representer: track.git.has_representer?,
-         has_analyzer: track.git.has_analyzer?,
-      )
-    end
   end
 end

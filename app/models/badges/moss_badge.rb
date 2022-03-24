5c1d500ad93240b5c4847795793804d6c9725c8d
Erik Schierboom <erik_schierboom@hotmail.com>
2022-03-24 16:16:48 +0100

    Add moss badge

---
+++ app/models/badges/moss_badge.rb
@@ -0,0 +1,23 @@
+module Badges
+  class MossBadge < Badge
+    seed "Moss",
+      :ultimate,
+      'moss',
+      'Provided support to new users'
+
+    def award_to?(user)
+      return false unless user.github_username
+
+      SUPPORTERS.include?(user.github_username.downcase)
+    end
+
+    def send_email_on_acquisition?
+      true
+    end
+
+    SUPPORTERS = %w[
+      todo
+    ].freeze
+    private_constant :SUPPORTERS
+  end
+end

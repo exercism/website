8bd57afb1b5d3a9b2e85c2af83e6286ac1f35f28
Erik Schierboom <erik_schierboom@hotmail.com>
2022-04-21 11:20:49 +0200

    Add tests for reputation token award awarding

--- test/commands/user/reputation_token/create_test.rb
+++ test/commands/user/reputation_token/create_test.rb
@@ -6,7 +6,6 @@ class User::ReputationToken::CreateTest < ActiveSupport::TestCase
     contributorship = create :exercise_contributorship, contributor: user
 
     User::ReputationPeriod::MarkForToken.expects(:call)
-    AwardBadgeJob.expects(:perform_later).with(user, :contributor)
 
     2.times do
       User::ReputationToken::Create.(
@@ -58,5 +57,102 @@ class User::ReputationToken::CreateTest < ActiveSupport::TestCase
         }
       )
     end
+  end
+
+  test "awards contributor token if category is authoring" do
+    user = create :user, handle: "User22", github_username: "user22"
+    contributorship = create :exercise_contributorship, contributor: user
+
+    # The exercise contribution reputation token's category is authoring
+    User::ReputationToken::Create.(user, :exercise_contribution, { contributorship: contributorship })
+
+    perform_enqueued_jobs
+    assert_includes user.reload.badges.map(&:class), Badges::ContributorBadge
+  end
+
+  test "awards contributor token if category is building" do
+    user = create :user, handle: "User22", github_username: "user22"
+
+    # The code contribution reputation token's category is building
+    User::ReputationToken::Create.(
+      user,
+      :code_contribution,
+      repo: 'exercism/ruby',
+      level: :large,
+      pr_node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
+      pr_number: 1347,
+      pr_title: 'The cat sat on the mat',
+      merged_at: Time.parse('2020-04-03T14:54:57Z').utc,
+      external_url: 'https://api.github.com/repos/exercism/v3/pulls/1347'
+    )
+
+    perform_enqueued_jobs
+    assert_includes user.reload.badges.map(&:class), Badges::ContributorBadge
+  end
+
+  test "awards contributor token if category is maintaining" do
+    user = create :user, handle: "User22", github_username: "user22"
+
+    # The code merge reputation token's category is maintaining
+    User::ReputationToken::Create.(
+      user,
+      :code_merge,
+      repo: 'exercism/ruby',
+      level: :janitorial,
+      pr_node_id: 'MDExOlB1bGxSZXF1ZXN0NTgzMTI1NTaQ',
+      pr_number: 1347,
+      pr_title: 'The cat sat on the mat',
+      merged_at: Time.parse('2020-04-03T14:54:57Z').utc,
+      external_url: 'https://api.github.com/repos/exercism/v3/pulls/1347'
+    )
+
+    perform_enqueued_jobs
+    assert_includes user.reload.badges.map(&:class), Badges::ContributorBadge
+  end
+
+  test "awards contributor token if category is mentoring" do
+    user = create :user
+    discussion = create :mentor_discussion, :mentor_finished, mentor: user
+
+    # The mentor reputation token's category is building
+    User::ReputationToken::Create.(
+      user,
+      :mentored,
+      discussion: discussion
+    )
+
+    perform_enqueued_jobs
+    assert_includes user.reload.badges.map(&:class), Badges::ContributorBadge
+  end
+
+  test "does not award contributor token if category is publishing" do
+    user = create :user
+    solution = create :practice_solution, :published, user: user
+
+    # The published reputation token's category is publishing
+    User::ReputationToken::Create.(
+      user,
+      :published_solution,
+      solution: solution,
+      level: :medium
+    )
+
+    perform_enqueued_jobs
+    refute_includes user.reload.badges.map(&:class), Badges::ContributorBadge
+  end
+
+  test "does not award contributor token if category is misc" do
+    user = create :user
+
+    # The arbitrary reputation token's category is misc
+    User::ReputationToken::Create.(
+      user,
+      :arbitrary,
+      arbitrary_value: 23,
+      arbitrary_reason: 'For helping troubleshoot'
+    )
+
+    perform_enqueued_jobs
+    refute_includes user.reload.badges.map(&:class), Badges::ContributorBadge
   end
 end

a8b8bcd3038b2343e5d25ef2269ecb1ba206972c
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-04 11:10:44 +0200

    Add tests for docs section

--- test/controllers/docs_controller_test.rb
+++ test/controllers/docs_controller_test.rb
@@ -18,4 +18,25 @@ class DocsControllerTest < ActionDispatch::IntegrationTest
     get docs_path
     assert_response 200
   end
+
+  test "section shows when logged out" do
+    create :document, section: :mentoring, slug: 'APEX'
+    get docs_section_path(:mentoring)
+    assert_response 200
+  end
+
+  test "section shows when logged in" do
+    create :document, section: :mentoring, slug: 'APEX'
+    sign_in!
+    get docs_section_path(:mentoring)
+    assert_response 200
+  end
+
+  test "section shows when not onboarded" do
+    create :document, section: :mentoring, slug: 'APEX'
+    user = create :user, :not_onboarded
+    sign_in!(user)
+    get docs_section_path(:mentoring)
+    assert_response 200
+  end
 end

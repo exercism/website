71abed8b8e51ebe64e952e2ecf78c66c1e570fac
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-06 08:37:22 +0200

    Undo refactoring

--- app/models/user/communication_preferences.rb
+++ app/models/user/communication_preferences.rb
@@ -2,10 +2,6 @@ class User::CommunicationPreferences < ApplicationRecord
   belongs_to :user
 
   def self.keys
-    mentoring_keys + solution_keys + general_keys
-  end
-
-  def self.mentoring_keys
     %i[
       email_on_mentor_started_discussion_notification
       email_on_mentor_replied_to_discussion_notification
@@ -15,18 +11,8 @@ class User::CommunicationPreferences < ApplicationRecord
       email_on_student_finished_discussion_notification
       email_on_remind_mentor
       email_on_mentor_heartbeat
-    ]
-  end
-
-  def self.solution_keys
-    %i[
       email_on_new_solution_comment_for_solution_user_notification
       email_on_new_solution_comment_for_other_commenter_notification
-    ]
-  end
-
-  def self.general_keys
-    %i[
       email_on_general_update_notification
       email_on_acquired_badge_notification
       email_on_nudge_notification

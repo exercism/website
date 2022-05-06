a6e50b32e334eeea635a7ef2e237e36444b1fc21
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-06 14:43:26 +0200

    Only change what is in this PR

--- app/models/user/communication_preferences.rb
+++ app/models/user/communication_preferences.rb
@@ -7,14 +7,10 @@ class User::CommunicationPreferences < ApplicationRecord
       email_on_mentor_replied_to_discussion_notification
       email_on_mentor_finished_discussion_notification
       email_on_student_replied_to_discussion_notification
-      email_on_student_added_iteration_notification
       email_on_student_finished_discussion_notification
-      email_on_remind_mentor
-      email_on_mentor_heartbeat
-      email_on_new_solution_comment_for_solution_user_notification
-      email_on_new_solution_comment_for_other_commenter_notification
-      email_on_general_update_notification
+      email_on_student_added_iteration_notification
       email_on_acquired_badge_notification
+      email_on_general_update_notification
       email_on_nudge_notification
       receive_product_updates
     ]

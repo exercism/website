c2348d7e15f1c62e1679dd927b8c7c8ce239e4c0
Erik Schierboom <erik_schierboom@hotmail.com>
2022-05-20 13:49:46 +0200

    Increment num_comments when a new comment is added

--- app/models/solution/comment.rb
+++ app/models/solution/comment.rb
@@ -1,5 +1,5 @@
 class Solution::Comment < ApplicationRecord
-  belongs_to :solution
+  belongs_to :solution, counter_cache: :num_comments
   belongs_to :author, class_name: "User", foreign_key: :user_id, inverse_of: :solution_comments
 
   validates :content_markdown, presence: true

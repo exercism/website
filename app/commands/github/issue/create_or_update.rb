226bfc8bc9ef4956e23a35d7ddd6c7dcf4859237
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-16 14:35:23 +0200

    Add metric for open issue

--- app/commands/github/issue/create_or_update.rb
+++ app/commands/github/issue/create_or_update.rb
@@ -6,7 +6,7 @@ module Github
       initialize_with :node_id, :attributes
 
       def call
-        issue = ::Github::Issue.create_or_find_by!(node_id:) do |i|
+        @issue = ::Github::Issue.create_or_find_by!(node_id:) do |i|
           i.number = attributes[:number]
           i.title = attributes[:title]
           i.status = status
@@ -14,6 +14,8 @@ module Github
           i.opened_at = attributes[:opened_at]
           i.opened_by_username = attributes[:opened_by_username]
         end
+
+        log_metric! if issue.just_created?
 
         issue.update!(
           number: attributes[:number],
@@ -29,15 +31,23 @@ module Github
       end
 
       private
+      attr_reader :issue
+
       def labels(issue)
         attributes[:labels].to_a.map do |label|
           Github::IssueLabel::CreateOrUpdate.(issue, label)
         end
       end
 
-      def status
-        attributes[:state].downcase.to_sym
+      def status = attributes[:state].downcase.to_sym
+
+      def log_metric!
+        LogMetricJob.perform_later(:open_issue, issue.opened_at, track:, user: opened_by_username)
       end
+
+      def repo_url = "https://github.com/#{issue.repo}"
+      def track = Track.find_by(repo_url:)
+      def opened_by_username = User.find_by(github_username: issue.opened_by_username)
     end
   end
 end

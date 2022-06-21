b8fd9a1a58e408efabf1d4c658b207d36f436c55
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-21 14:46:28 +0200

    Fix cohort_membership factory tests

--- test/factories/cohort_memberships.rb
+++ test/factories/cohort_memberships.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :cohort_membership do
     user { create :user }
+    cohort_slug { "cohort-#{SecureRandom.hex(4)}" }
+    introduction { "Welcome to the #{cohort_slug} cohort" }
   end
 end

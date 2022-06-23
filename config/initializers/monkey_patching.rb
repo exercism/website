89d9ef573c95f74d3a11c9b8aa2f879c737191e5
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-23 16:30:07 +0200

    Simplify monkey_patching

--- config/initializers/monkey_patching.rb
+++
@@ -1,1 +0,0 @@
-require "time_extensions"
--- test/initializers/time_test.rb
+++ test/initializers/time_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 require "hcaptcha"
 
-class TimeExtensionsTest < ActiveSupport::TestCase
+class TimeTest < ActiveSupport::TestCase
   # rubocop:disable Rails/TimeZone
   test "min_of_day on Time instance" do
-    assert_equal 0, Time.now.change(hour: 0, min: 0).min_of_day
-    assert_equal 59, Time.now.change(hour: 0, min: 59).min_of_day
-    assert_equal 60, Time.now.change(hour: 1, min: 0).min_of_day
-    assert_equal 1439, Time.now.change(hour: 23, min: 59).min_of_day
+    assert_equal 0, Time.current.change(hour: 0, min: 0).min_of_day
+    assert_equal 59, Time.current.change(hour: 0, min: 59).min_of_day
+    assert_equal 60, Time.current.change(hour: 1, min: 0).min_of_day
+    assert_equal 1439, Time.current.change(hour: 23, min: 59).min_of_day
   end
   # rubocop:enable Rails/TimeZone
 
@@ -27,9 +27,9 @@ class TimeExtensionsTest < ActiveSupport::TestCase
 
   # rubocop:disable Rails/TimeZone
   test "prev_min on Time instance" do
-    assert_equal Time.now.change(hour: 0, min: 0), Time.now.change(hour: 0, min: 1).prev_min
-    assert_equal Time.now.change(hour: 0, min: 0) - 1.minute, Time.now.change(hour: 0, min: 0).prev_min
-    assert_equal Time.now.change(hour: 16, min: 43, sec: 57), Time.now.change(hour: 16, min: 44, sec: 57).prev_min
+    assert_equal Time.current.change(hour: 0, min: 0), Time.current.change(hour: 0, min: 1).prev_min
+    assert_equal Time.current.change(hour: 0, min: 0) - 1.minute, Time.current.change(hour: 0, min: 0).prev_min
+    assert_equal Time.current.change(hour: 16, min: 43, sec: 57), Time.current.change(hour: 16, min: 44, sec: 57).prev_min
   end
   # rubocop:enable Rails/TimeZone
 
@@ -47,9 +47,9 @@ class TimeExtensionsTest < ActiveSupport::TestCase
 
   # rubocop:disable Rails/TimeZone
   test "next_min on Time instance" do
-    assert_equal Time.now.change(hour: 0, min: 1), Time.now.change(hour: 0, min: 0).next_min
-    assert_equal Time.now.change(hour: 23, min: 59) + 1.minute, Time.now.change(hour: 23, min: 59).next_min
-    assert_equal Time.now.change(hour: 16, min: 44, sec: 57), Time.now.change(hour: 16, min: 43, sec: 57).next_min
+    assert_equal Time.current.change(hour: 0, min: 1), Time.current.change(hour: 0, min: 0).next_min
+    assert_equal Time.current.change(hour: 23, min: 59) + 1.minute, Time.current.change(hour: 23, min: 59).next_min
+    assert_equal Time.current.change(hour: 16, min: 44, sec: 57), Time.current.change(hour: 16, min: 43, sec: 57).next_min
   end
   # rubocop:enable Rails/TimeZone
 

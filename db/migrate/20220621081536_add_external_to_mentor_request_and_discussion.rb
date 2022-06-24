321346b9656749253da6ce0d89b35ee41886677c
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-24 08:17:37 +0200

    Don't run migration in production

--- db/migrate/20220621081536_add_external_to_mentor_request_and_discussion.rb
+++ db/migrate/20220621081536_add_external_to_mentor_request_and_discussion.rb
@@ -3,7 +3,9 @@ class AddExternalToMentorRequestAndDiscussion < ActiveRecord::Migration[7.0]
     add_column :mentor_requests, :external, :boolean, default: false, null: false, if_not_exists: true
     add_column :mentor_discussions, :external, :boolean, default: false, null: false, if_not_exists: true
 
-    Mentor::Request.where(comment_markdown: "This is a private review session").update_all(external: true)
-    Mentor::Discussion.joins(:request).where('request.external': true).update_all(external: true)
+    unless Rails.env.production?
+      Mentor::Request.where(comment_markdown: "This is a private review session").update_all(external: true)
+      Mentor::Discussion.joins(:request).where('request.external': true).update_all(external: true)
+    end
   end
 end

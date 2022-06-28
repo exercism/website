75c7e39496a577cb35bf237beae4afab6dc7d0ee
Jeremy Walker <jez.walker@gmail.com>
2022-06-28 14:21:38 +0100

    DRY it up and add track selection

--- app/controllers/impact_controller.rb
+++ app/controllers/impact_controller.rb
@@ -2,28 +2,34 @@ class ImpactController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @track = Track.find(params[:track_slug]) if params[:track_slug].present?
     @last_24_hours = last_24_hours
     @last_month = last_month
   end
 
   private
+  METRICS = {
+    num_solutions_submitted: :submit_solution,
+    num_solutions_completed: :complete_solution,
+    num_opened_pull_requests: :open_pull_request
+  }.freeze
   def last_24_hours
-    {
-      num_solutions_submitted: count_last_24_hours(:submit_solution),
-      num_solutions_completed: count_last_24_hours(:complete_solution),
-      num_opened_pull_requests: count_last_24_hours(:open_pull_request)
-    }
+    METRICS.transform_values { |v| count_last_24_hours(v) }
   end
-
-  def count_last_24_hours(metric_action) = MetricPeriod::Minute.where(metric_action:).sum(:count)
 
   def last_month
-    {
-      num_solutions_submitted: count_last_month(:submit_solution),
-      num_solutions_completed: count_last_month(:complete_solution),
-      num_opened_pull_requests: count_last_month(:open_pull_request)
-    }
+    METRICS.transform_values { |v| count_last_month(v) }
   end
 
-  def count_last_month(metric_action) = MetricPeriod::Day.where(metric_action:).sum(:count)
+  def count_last_24_hours(metric_action)
+    query = MetricPeriod::Minute.where(metric_action:)
+    query = query.where(track_id: @track.id) if @track
+    query.sum(:count)
+  end
+
+  def count_last_month(metric_action)
+    query = MetricPeriod::Day.where(metric_action:)
+    query = query.where(track_id: @track.id) if @track
+    query.sum(:count)
+  end
 end

256a29f98481819f01fe91f6fa14249385043fb1
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-28 11:00:33 +0200

    Fix failing unit journey test

--- test/system/flows/journey/overview_test.rb
+++ test/system/flows/journey/overview_test.rb
@@ -7,33 +7,31 @@ module Flows
       include CapybaraHelpers
 
       test "user sees track learning details" do
-        travel_to(Time.utc(2017, 12, 25)) do
-          user = create :user
-          track = create :track
-          create :user_track, user: user, track: track, created_at: Time.utc(2016, 12, 25)
+        user = create :user
+        track = create :track
+        user_track = create :user_track, user: user, track: track, created_at: Time.utc(2016, 12, 25)
 
-          exercise = create :concept_exercise, track: track
-          solution = create :concept_solution, exercise: exercise, user: user
-          create :mentor_discussion, student: user, solution: solution, status: :finished
+        exercise = create :concept_exercise, track: track
+        solution = create :concept_solution, exercise: exercise, user: user
+        create :mentor_discussion, student: user, solution: solution, status: :finished
 
-          exercise = create :concept_exercise, track: track
-          solution = create :concept_solution, exercise: exercise, user: user
-          create :mentor_discussion, student: user, solution: solution, status: :awaiting_student
+        exercise = create :concept_exercise, track: track
+        solution = create :concept_solution, exercise: exercise, user: user
+        create :mentor_discussion, student: user, solution: solution, status: :awaiting_student
 
-          exercise = create :concept_exercise, track: track
-          solution = create :concept_solution, exercise: exercise, user: user
-          create :mentor_request, student: user, solution: solution, status: :pending
+        exercise = create :concept_exercise, track: track
+        solution = create :concept_solution, exercise: exercise, user: user
+        create :mentor_request, student: user, solution: solution, status: :pending
 
-          use_capybara_host do
-            sign_in!(user)
-            visit journey_url
+        use_capybara_host do
+          sign_in!(user)
+          visit journey_url
 
-            assert_text "25 Dec 2016"
-            assert_text "When you joined the Ruby Track"
-            assert_text "You started working through the Ruby Track 5 years ago."
-            assert_text "1\nMentoring session completed"
-            assert_text "You have 1 discussion in progress and 1 solution in the queue."
-          end
+          assert_text "25 Dec 2016"
+          assert_text "When you joined the Ruby Track"
+          assert_text "You started working through the Ruby Track #{Time.current.year - user_track.created_at.year} years ago."
+          assert_text "1\nMentoring session completed"
+          assert_text "You have 1 discussion in progress and 1 solution in the queue."
         end
       end
 

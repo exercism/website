cb1f72247f33e82b3b8c53af26b0bb7a4d0b4a44
Erik Schierboom <erik_schierboom@hotmail.com>
2022-06-28 14:18:49 +0200

    Fix flaky badge test (#2743)
    * Add badge test for ordered_by_rarity scope
    
    * Fix flake test by explicitly ordering by badge id
    
    [skip ci]

--- test/models/badge_test.rb
+++ test/models/badge_test.rb
@@ -5,4 +5,12 @@ class BadgeTest < ActiveSupport::TestCase
     badge = create :badge, num_awardees: 123_456
     assert_equal 15.44, badge.percentage_awardees
   end
+
+  test "ordered_by_rarity" do
+    rare = create :mentor_badge
+    ultimate = create :completer_badge
+    common = create :member_badge
+    legendary = create :architect_badge
+    assert_equal [legendary, ultimate, rare, common], Badge.ordered_by_rarity
+  end
 end

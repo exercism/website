40dc1c4eb54271824de28655915a83504b4af7d4
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-14 15:44:31 +0200

    Add country code field to metrics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_24_090353) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_131833) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -478,6 +478,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_090353) do
     t.datetime "occurred_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "country_code", limit: 2
     t.index ["track_id"], name: "index_metrics_on_track_id"
     t.index ["type", "track_id", "occurred_at"], name: "index_metrics_on_type_and_track_id_and_occurred_at"
     t.index ["uniqueness_key"], name: "index_metrics_on_uniqueness_key", unique: true

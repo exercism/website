10594b434527c2529b34bf02a9a62c468eae50c7
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-19 14:40:20 +0200

    Parameterize cohorts

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,17 +1,29 @@
 class CohortsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :use_cohort
 
   def show
-    @membership = current_user.cohort_memberships.first if user_signed_in?
+    @membership = current_user.cohort_memberships.find_by(cohort_slug: @cohort.slug) if user_signed_in?
   end
 
   def join
     begin
-      current_user.cohort_memberships.create!(cohort_slug: "gohort", introduction: params[:introduction])
+      current_user.cohort_memberships.create!(cohort_slug: @cohort.slug, introduction: params[:introduction])
     rescue ActiveRecord::RecordNotUnique
       # This is fine
     end
 
     redirect_to action: :show, anchor: "register"
   end
+
+  private
+  def use_cohort
+    @cohort = COHORTS[params[:id].to_sym]
+
+    render_404 if @cohort.blank?
+  end
+
+  Cohort = Struct.new(:slug, :name)
+  COHORTS = { gohort: Cohort.new(:gohort, "Go-hort") }.freeze
+  private_constant
 end

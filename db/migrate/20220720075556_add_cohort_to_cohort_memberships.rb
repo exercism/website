2ea56aaafa0d6c402b07b6e10d76bbd1e85185e5
Jeremy Walker <jez.walker@gmail.com>
2022-07-20 13:24:40 +0100

    Add migration guard

--- db/migrate/20220720075556_add_cohort_to_cohort_memberships.rb
+++ db/migrate/20220720075556_add_cohort_to_cohort_memberships.rb
@@ -2,10 +2,12 @@ class AddCohortToCohortMemberships < ActiveRecord::Migration[7.0]
   def change
     add_reference :cohort_memberships, :cohort, null: true, foreign_key: true, if_not_exists: true
 
+    track = Track.find_by(slug: 'go') || Track.first # Guard against dev db not having Golang
+
     Cohort.find_create_or_find_by!(slug: 'gohort') do |c|
       c.name = 'Go-Hort'
       c.capacity = 100
-      c.track = Track.find_by(slug: 'go')
+      c.track = track
       c.begins_at = Time.utc(2022, 07, 01)
       c.ends_at = Time.utc(2022, 07, 30)
     end

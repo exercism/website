09339498970ae82c18f4308d4509d9167fbb6392
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-21 16:16:52 +0200

    Setup geoip2

--- Gemfile
+++ Gemfile
@@ -111,4 +111,6 @@ gem 'addressable', '~> 2.7'
 gem 'mini_magick'
 
 gem 'elasticsearch', '6.8.3'
+
 gem 'geocoder', '~> 1.8'
+gem "maxminddb", "~> 0.1.22"
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,17 @@
+# rubocop:disable Naming/VariableNumber
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
-  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
   # api_key: nil,               # API key for geocoding service
+  ip_lookup: :geoip2, # name of IP address geocoding service (symbol)
+  geoip2: {
+    file: File.join('/usr', 'share', 'GeoIP', 'GeoLite2-City.mmdb')
+  },
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
@@ -25,3 +29,4 @@ Geocoder.configure(
   #   prefix: 'geocoder:'
   # }
 )
+# rubocop:enable Naming/VariableNumber

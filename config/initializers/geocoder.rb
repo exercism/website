af3a60cb80e4c1f5f0d63c10899227f54ada0253
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-22 11:25:56 +0200

    Add comments to geocoder initializer

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,10 +1,14 @@
 # rubocop:disable Naming/VariableNumber
 Geocoder.configure(
-  timeout: 1, # in seconds
   ip_lookup: :geoip2,
   geoip2: {
     file: ENV.fetch('GEOIP_FILE', File.join('/usr', 'share', 'GeoIP', 'GeoLite2-Country.mmdb'))
   },
-  cache: Redis.new
+
+  # Use Rails' own cache
+  cache: Geocoder::CacheStore::Generic.new(Rails.cache, {}),
+
+  # Lowest possible value; should never be hit due to using file-based lookup
+  timeout: 1
 )
 # rubocop:enable Naming/VariableNumber

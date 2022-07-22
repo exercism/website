6144ca1bc73dd5410c66edfaeef97c963f032e3c
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-22 11:23:18 +0200

    Use safe file join

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,12 +3,8 @@ Geocoder.configure(
   timeout: 1, # in seconds
   ip_lookup: :geoip2,
   geoip2: {
-    file: ENV.fetch("GEOIP_FILE", '/usr/share/GeoIP/GeoLite2-Country.mmdb')
+    file: ENV.fetch('GEOIP_FILE', File.join('/usr', 'share', 'GeoIP', 'GeoLite2-Country.mmdb'))
   },
   cache: Redis.new
-  # cache_options: {
-  #   expiration: 2.days,
-  #   prefix: 'geocoder:'
-  # }
 )
 # rubocop:enable Naming/VariableNumber

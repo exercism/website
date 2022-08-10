d5a1ca3afb453ff960915912eff83c59e7460de9
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-10 12:07:45 +0200

    Simplify export

--- app/commands/exercise/export_solutions_to_zip_file.rb
+++ app/commands/exercise/export_solutions_to_zip_file.rb
@@ -4,33 +4,27 @@ class Exercise::ExportSolutionsToZipFile
   initialize_with :exercise
 
   def call
-    # TODO
-
-    solutions = exercise.solutions.
-      includes(iterations: :files).
-      where(status: %i[iterated completed published]).
-      order('id DESC').
-      limit(500)
-
-    dir = Rails.root / "tmp" / "export_solutions_data" / "#{Time.now.to_i}-#{SecureRandom.uuid}"
-    FileUtils.mkdir_p(dir)
-
-    solutions.each.with_index do |solution, idx|
-      solution_dir = dir / idx.to_s
-      FileUtils.mkdir_p(solution_dir)
-
-      solution.latest_iteration.files.each do |iteration_file|
-        file_dir = solution_dir / iteration_file.filename.split('/').tap(&:pop).join('/')
-
-        FileUtils.mkdir_p(file_dir) unless Dir.exist?(file_dir)
-        File.open(solution_dir / iteration_file.filename, "w+") do |file|
-          file << iteration_file.content.force_encoding("utf-8")
+    Zip::File.open(zip_file_path, create: true) do |zip_file|
+      solutions.each.with_index do |solution, idx|
+        solution.latest_iteration.files.each do |iteration_file|
+          zip_file.get_output_stream("#{idx}/#{iteration_file.filename}") do |f|
+            f.puts(iteration_file.content.force_encoding("utf-8"))
+          end
         end
       end
     end
 
-    (dir / "archive.zip").tap do |zip_path|
-      Directory::ZipRecursively.(dir, zip_path)
-    end
+    zip_file_path
+  end
+
+  private
+  memoize
+  def zip_file_path = Rails.root / "tmp" / "export_solutions_data" / "#{Time.now.to_i}-#{SecureRandom.uuid}.zip"
+
+  def solutions
+    exercise.solutions.includes(iterations: :files).
+      where(status: %i[iterated completed published]).
+      order('id DESC').
+      limit(500)
   end
 end
--- test/commands/exercise/export_solutions_to_zip_file_test.rb
+++ test/commands/exercise/export_solutions_to_zip_file_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class Exercise::ExportSolutionsToZipFileTest < ActiveSupport::TestCase
-  test "exercise without solutions" do
+  test "export exercise without solutions" do
     exercise = create :practice_exercise
 
     zip_file_path = Exercise::ExportSolutionsToZipFile.(exercise)
@@ -12,7 +12,7 @@ class Exercise::ExportSolutionsToZipFileTest < ActiveSupport::TestCase
     end
   end
 
-  test "exports solutions in correct zip format" do
+  test "exports solutions in zip format" do
     num_solutions = 5
     exercise = create :practice_exercise
 
@@ -31,7 +31,25 @@ class Exercise::ExportSolutionsToZipFileTest < ActiveSupport::TestCase
     end
   end
 
-  test "supports files that are not in exercise root" do
+  test "exports last 500 solutions" do
+    exercise = create :practice_exercise
+
+    501.times do |idx|
+      iteration = create :iteration, exercise: exercise
+      create :submission_file, submission: iteration.submission, filename: "stub.rb", content: "Stub #{idx}"
+    end
+
+    zip_file_path = Exercise::ExportSolutionsToZipFile.(exercise)
+
+    assert File.exist?(zip_file_path)
+    Zip::File.open(zip_file_path) do |zip_file|
+      assert 500, zip_file.size
+      assert "Stub 1", zip_file.read("0/stub.rb")
+      assert "Stub 499", zip_file.read("499/stub.rb")
+    end
+  end
+
+  test "supports solution files in nested directory" do
     iteration = create :iteration
     create :submission_file, submission: iteration.submission, filename: "src/stub.rb", content: "Stub"
 
@@ -40,6 +58,22 @@ class Exercise::ExportSolutionsToZipFileTest < ActiveSupport::TestCase
     assert File.exist?(zip_file_path)
     Zip::File.open(zip_file_path) do |zip_file|
       assert "Stub", zip_file.read("0/src/stub.rb")
+    end
+  end
+
+  test "supports multiple solution files" do
+    iteration = create :iteration
+    create :submission_file, submission: iteration.submission, filename: "src/stub.rb", content: "Stub"
+    create :submission_file, submission: iteration.submission, filename: "test/test.rb", content: "Test"
+    create :submission_file, submission: iteration.submission, filename: "helper.rb", content: "Helper"
+
+    zip_file_path = Exercise::ExportSolutionsToZipFile.(iteration.exercise)
+
+    assert File.exist?(zip_file_path)
+    Zip::File.open(zip_file_path) do |zip_file|
+      assert "Stub", zip_file.read("0/src/stub.rb")
+      assert "Test", zip_file.read("0/test/test.rb")
+      assert "Helper", zip_file.read("0/helper.rb")
     end
   end
 end

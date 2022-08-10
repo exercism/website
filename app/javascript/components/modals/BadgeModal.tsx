867498fc4aed2a3fbceb55630a0f5be8d0773c52
dem4ron <demaaron88@gmail.com>
2022-08-10 12:57:39 +0200

    Make BadgeModal screen-readable. Wrap BadgeModal in a div wrapper

--- app/javascript/components/modals/BadgeModal.tsx
+++ app/javascript/components/modals/BadgeModal.tsx
@@ -6,14 +6,16 @@ import { Modal, ModalProps } from './Modal'
 import { timeFormat } from '../../utils/time'
 import pluralize from 'pluralize'
 
+type BadgeModalProps = Omit<ModalProps, 'className'> & {
+  badge: BadgeProps
+  wasUnrevealed?: boolean
+}
+
 export const BadgeModal = ({
   badge,
   wasUnrevealed,
   ...props
-}: Omit<ModalProps, 'className'> & {
-  badge: BadgeProps
-  wasUnrevealed?: boolean
-}): JSX.Element => {
+}: BadgeModalProps): JSX.Element => {
   const classNames = ['m-badge', `--${badge.rarity}`, 'theme-dark']
 
   return (
@@ -22,28 +24,39 @@ export const BadgeModal = ({
       closeButton={true}
       className={classNames.join(' ')}
       cover={true}
+      aria={{
+        describedby: 'a11y-badge-modal-description',
+        labelledby: 'a11y-badge-modal-label',
+      }}
     >
-      <BadgeMedallion badge={badge} />
-      {wasUnrevealed ? <h2>New Badge Earned</h2> : null}
-      <div className="name">{badge.name}</div>
-      <div className="rarity">{badge.rarity}</div>
-      <hr className="c-divider --small" />
-      <div className="reason">{badge.description}</div>
-      <div className="earned-at">
-        Earned on{' '}
-        <time dateTime={badge.unlockedAt}>
-          {timeFormat(badge.unlockedAt, 'DD MMM YYYY')}
-        </time>
-      </div>
-      <div className="num-awardees text-p-base">
-        <GraphicalIcon icon="students" />
-        <strong>{badge.numAwardees}</strong>{' '}
-        {pluralize('member', badge.numAwardees)}{' '}
-        {pluralize('has', badge.numAwardees)} earned this badge.
-      </div>
-      <div className="percentage-awardees">
-        That's <strong>{badge.percentageAwardees}%</strong> of all Exercism
-        users
+      <div
+        id="a11y-badge-modal-description"
+        className="flex flex-col items-center"
+      >
+        <BadgeMedallion badge={badge} />
+        {wasUnrevealed ? <h2>New Badge Earned</h2> : null}
+        <div id="a11y-badge-modal-label" className="name">
+          {badge.name}
+        </div>
+        <div className="rarity">{badge.rarity}</div>
+        <hr className="c-divider --small" />
+        <div className="reason">{badge.description}</div>
+        <div className="earned-at">
+          Earned on{' '}
+          <time dateTime={badge.unlockedAt}>
+            {timeFormat(badge.unlockedAt, 'DD MMM YYYY')}
+          </time>
+        </div>
+        <div className="num-awardees text-p-base">
+          <GraphicalIcon icon="students" />
+          <strong>{badge.numAwardees}</strong>{' '}
+          {pluralize('member', badge.numAwardees)}{' '}
+          {pluralize('has', badge.numAwardees)} earned this badge.
+        </div>
+        <div className="percentage-awardees">
+          That&apos;s <strong>{badge.percentageAwardees}%</strong> of all
+          Exercism users
+        </div>
       </div>
     </Modal>
   )

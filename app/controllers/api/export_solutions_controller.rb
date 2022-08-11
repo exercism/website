fdaa427ecad2c9984ee522d389b323b291e01da2
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-11 13:10:38 +0200

    Move maintainer check to action filter

--- app/controllers/api/export_solutions_controller.rb
+++ app/controllers/api/export_solutions_controller.rb
@@ -1,17 +1,22 @@
 module API
   class ExportSolutionsController < BaseController
+    before_action :ensure_maintainer!
     before_action :use_track
     before_action :use_exercise
 
     def index
-      return render_403(:solutions_export_not_accessible) unless current_user.maintainer? || current_user.admin?
-
       zip_file_stream = Exercise::ExportSolutionsToZipFile.(@exercise)
-
       send_data zip_file_stream, type: 'application/zip', filename: 'solutions.zip'
     end
 
     private
+    def ensure_maintainer!
+      return if current_user&.maintainer?
+      return if current_user&.admin? # Admins have maintainer permissions
+
+      render_403(:solutions_export_not_accessible)
+    end
+
     def use_track
       @track = Track.find_by(slug: params[:track_slug])
       return render_track_not_found if @track.blank?

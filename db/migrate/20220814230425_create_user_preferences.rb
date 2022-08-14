589ddaeb321a6c80edd096c978bf4fb9d6dca9f9
Jeremy Walker <jez.walker@gmail.com>
2022-08-15 00:39:29 +0100

    Update db/migrate/20220814230425_create_user_preferences.rb

--- db/migrate/20220814230425_create_user_preferences.rb
+++ db/migrate/20220814230425_create_user_preferences.rb
@@ -1,6 +1,6 @@
 class CreateUserPreferences < ActiveRecord::Migration[7.0]
   def change
-    create_table :user_preferences do |t|
+    create_table :user_preferences, if_not_exists: true do |t|
       t.belongs_to :user, foreign_key: true, index: { unique: true }
       t.boolean :auto_update_exercises, default: true, null: false
 

4b59bb117649ac2883f2fd703facd4495e49b788
Jeremy Walker <jez.walker@gmail.com>
2022-08-15 00:45:34 +0100

    Fix tests further

--- test/factories/user/preferences.rb
+++ test/factories/user/preferences.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user_preferences, class: 'User::Preferences' do
-    user { create :user }
+    # Creating a user makes preferences, so this dance
+    # deletes the one that gets created by defualt and
+    # attaches this factory instead.
+    user { create(:user).tap { |u| u.preferences.destroy } }
   end
 end

3e280942f1bbab22b45bc536460b4f6a9bc8de36
dem4ron <demaaron88@gmail.com>
2022-08-18 17:34:41 +0200

    Finish mocking up right pane of Representation Session

--- app/javascript/components/mentoring/Representation.tsx
+++ app/javascript/components/mentoring/Representation.tsx
@@ -1,6 +1,9 @@
 import React, { useCallback, useState } from 'react'
+import { QueryStatus } from 'react-query'
 import { GraphicalIcon, Icon, SplitPane } from '../common'
 import { Accordion } from '../common/Accordion'
+// import { MarkdownEditor } from '../common/MarkdownEditor'
+import { MarkdownEditorForm } from '../common/MarkdownEditorForm'
 
 export function Representation(): JSX.Element {
   return (
@@ -157,11 +160,41 @@ function AlertText({ text }: { text: string }): JSX.Element {
 }
 
 function MentoringConversation() {
+  const [value, setValue] = useState('')
+
+  const handleCancel = useCallback(() => console.log('Cancelled!'), [])
+  const handleChange = useCallback((value) => setValue(value), [setValue])
   return (
     <div className="p-[24px] pt-[0px]">
       <h2 className="text-h4 mb-[10.5px]">How important is this?</h2>
       <RadioGroup />
+      <MarkdownEditorForm
+        value={value}
+        onChange={handleChange}
+        onCancel={handleCancel}
+        expanded
+        action="edit"
+        defaultError={new Error('ERROR!')}
+        error="Error"
+        onSubmit={() => console.log('Submitted')}
+        status={'success' as QueryStatus}
+      />
+      <div className="mt-12 mb-20 text-textColor6 bg-veryLightBlue py-4 px-8 rounded-5">
+        We imported your last mentoring feedback to this solution above
+      </div>
+      <Button />
+      <div className="text-textColor6">
+        Remember, you can edit this feedback anytime after submission.
+      </div>
     </div>
+  )
+}
+
+function Button() {
+  return (
+    <button className="border-1 border-primaryBtnBorder shadow-xsZ1v3 mb-16 bg-purple text-white text-16 font-semibold px-[68px] py-[12px] rounded-8">
+      Preview & Submit
+    </button>
   )
 }
 

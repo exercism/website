aa5eaf6d21bd75c3b20bccabee3c396f6323b3fb
Jeremy Walker <jez.walker@gmail.com>
2022-08-18 13:43:54 +0100

    Stop auto updating for now

--- app/commands/solution/auto_update_to_latest_exercise_version.rb
+++ app/commands/solution/auto_update_to_latest_exercise_version.rb
@@ -5,6 +5,7 @@ class Solution
     initialize_with :solution
 
     def call
+      return
       return if solution.git_sha == solution.exercise.git_sha
       return unless solution.latest_iteration_head_tests_status == :passed
       return unless solution.user.preferences.auto_update_exercises?

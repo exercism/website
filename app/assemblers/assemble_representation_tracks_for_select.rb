72f12353ce6895db7bd53beef165fe0cf7ffd820
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-19 10:08:34 +0200

    Add assembler for track select of representations

---
+++ app/assemblers/assemble_representation_tracks_for_select.rb
@@ -0,0 +1,19 @@
+class AssembleRepresentationTracksForSelect
+  include Mandate
+
+  initialize_with :representations
+
+  def call
+    [
+      SerializeTrackForSelect::ALL_TRACK.merge(num_submissions: track_counts.values.sum),
+      *tracks.map { |track| SerializeTrackForSelect.(track).merge(num_submissions: track_counts[track.id]) }
+    ]
+  end
+
+  private
+  memoize
+  def track_counts = representations.group(:track_id).count
+
+  memoize
+  def tracks = Track.where(id: track_counts.keys).order(title: :asc)
+end

ba008d09c33913388754f2a132ac45bcaee5e175
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-19 13:30:29 +0200

    Don't cause track exercises data to become stale due to criteria being empty

--- app/helpers/react_components/student/exercise_list.rb
+++ app/helpers/react_components/student/exercise_list.rb
@@ -12,18 +12,20 @@ module ReactComponents
 
       private
       def request
-        query = {
-          criteria: params[:criteria],
-          sideload: ["solutions"]
-        }.compact
-
         {
           endpoint: Exercism::Routes.api_track_exercises_path(track),
           options: {
-            initial_data: AssembleExerciseList.(current_user, track, query),
-            stale_time: 0
+            initial_data: AssembleExerciseList.(current_user, track, query)
           },
           query:
+        }
+      end
+
+      memoize
+      def query
+        {
+          criteria: params.fetch(:criteria, ''),
+          sideload: ["solutions"]
         }
       end
     end

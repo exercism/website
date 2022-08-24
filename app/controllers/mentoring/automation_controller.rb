a4e046c8560eebdad526683d3273136f8daf6fa9
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-24 09:25:12 +0200

    Allow only_mentored_solution in automation controller

--- app/controllers/mentoring/automation_controller.rb
+++ app/controllers/mentoring/automation_controller.rb
@@ -2,7 +2,7 @@ class Mentoring::AutomationController < ApplicationController
   before_action :ensure_supermentor!
 
   def index
-    @automation_params = params.permit(:order, :criteria, :page, :track_slug)
+    @automation_params = params.permit(:order, :criteria, :page, :track_slug, :only_mentored_solutions)
   end
 
   def with_feedback

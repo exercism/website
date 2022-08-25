d02d1268cbadbad3a1cc6109282d9729db8735de
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-25 14:29:27 +0200

    Inline representations list serializing

--- app/serializers/serialize_exercise_representations.rb
+++ app/serializers/serialize_exercise_representations.rb
@@ -4,6 +4,36 @@ class SerializeExerciseRepresentations
   initialize_with :representations
 
   def call
-    representations.map { |representation| SerializeExerciseRepresentation.(representation) }
+    representations.map { |representation| SerializeInstance.(representation) }
+  end
+
+  class SerializeInstance
+    include Mandate
+
+    initialize_with :representation
+
+    delegate :exercise, :track, to: :representation
+
+    def call
+      {
+        id: representation.id,
+        exercise: {
+          icon_url: exercise.icon_url,
+          title: exercise.title
+        },
+        track: {
+          icon_url: track.icon_url,
+          title: track.title
+        },
+        num_submissions: representation.num_submissions,
+        appears_frequently: representation.appears_frequently?,
+        feedback_html: representation.feedback_html,
+        last_submitted_at: representation.last_submitted_at,
+        links: {
+          # TODO: link to edit page
+          # self: Exercism::Routes.track_exercise_path(exercise.track, exercise)
+        }
+      }
+    end
   end
 end

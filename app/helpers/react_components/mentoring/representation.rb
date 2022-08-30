d7b22f150171d08e2acfae724cd4d5857ec01049
Erik Schierboom <erik_schierboom@hotmail.com>
2022-08-30 15:44:07 +0200

    Add mentor

--- app/helpers/react_components/mentoring/representation.rb
+++ app/helpers/react_components/mentoring/representation.rb
@@ -1,13 +1,30 @@
 module ReactComponents
   module Mentoring
     class Representation < ReactComponent
-      initialize_with :representation, :example_submissions
+      initialize_with :mentor, :representation, :example_submissions
 
       def to_s
         super("mentoring-representation", {
-          representation: SerializeExerciseRepresentation.(representation),
-          examples: example_submissions.map { |submission| SerializeFiles.(submission.files_for_editor) }
+          representation: representation_data,
+          examples: examples_data,
+          mentor: mentor_data
         })
+      end
+
+      def representation_data
+        SerializeExerciseRepresentation.(representation)
+      end
+
+      def examples_data
+        example_submissions.map { |submission| SerializeFiles.(submission.files_for_editor) }
+      end
+
+      def mentor_data
+        {
+          name: mentor.name,
+          handle: mentor.handle,
+          avatar_url: mentor.avatar_url
+        }
       end
     end
   end

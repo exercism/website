b6b68e377126902c8074788fbc88cd73bf03bfb0
dem4ron <demaaron88@gmail.com>
2022-08-31 10:42:45 +0200

    Remove unused import

--- app/javascript/components/mentoring/Representation.tsx
+++ app/javascript/components/mentoring/Representation.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { SplitPane } from '../common'
 import { LeftPane } from './representation/left-pane'
 import { RightPane } from './representation/right-pane'

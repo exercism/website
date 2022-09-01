aef029065c590634cc6a53317694c8b2c1b1f1ea
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-01 07:28:44 +0200

    Update track_id in transaction

--- db/migrate/20220831065513_add_track_to_exercise_representations.rb
+++ db/migrate/20220831065513_add_track_to_exercise_representations.rb
@@ -3,8 +3,13 @@ class AddTrackToExerciseRepresentations < ActiveRecord::Migration[7.0]
     add_reference :exercise_representations, :track, null: true, foreign_key: true, if_not_exists: true
 
     unless Rails.env.production?
-      exercise_track_id_sql = Arel.sql(Exercise.where('id = exercise_id').select(:track_id).to_sql)
-      Exercise::Representation.update_all("track_id = (#{exercise_track_id_sql})")
+      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
+        Exercise::Representation.includes(:exercise).find_each do |representation|
+          Exercise::Representation
+            .where(id: representation.id)
+            .update_all(track_id: representation.exercise.track_id)
+        end
+      end
     end
   end
 end

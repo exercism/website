8952889288eb34dac38fab00c86649a435e0bfaa
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-01 07:38:02 +0200

    Add migration for mentor

--- db/migrate/20220824135136_add_mentor_to_submission_representations.rb
+++ db/migrate/20220824135136_add_mentor_to_submission_representations.rb
@@ -3,10 +3,11 @@ class AddMentorToSubmissionRepresentations < ActiveRecord::Migration[7.0]
     add_reference :submission_representations, :mentor, null: true, foreign_key: { to_table: :users }, if_not_exists: true
 
     unless Rails.env.production?
-      # TODO: add migration
-      # Submission::Representation.find_each do |representation|
-      #   representation
-      # end      
+      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
+        Submission::Representation.includes(submission: %i[solution iteration]).find_each do |representation|
+          Submission::Representation::UpdateMentor.(representation.submission)
+        end
+      end
     end
   end
 end

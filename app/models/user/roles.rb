fbc502ab476edfc62047fd7850a47e836b8b04d7
Jeremy Walker <jez.walker@gmail.com>
2022-09-01 18:11:54 +0100

    No-one is a supermentor during dark deploy

--- app/models/user/roles.rb
+++ app/models/user/roles.rb
@@ -3,6 +3,7 @@ module User::Roles
   def admin? = roles.include?(:admin)
   def staff? = roles.include?(:staff)
   def maintainer? = roles.include?(:maintainer)
-  def supermentor? = roles.include?(:supermentor)
+  # def supermentor? = roles.include?(:supermentor)
+  def supermentor? = false
   def roles = super.to_a.map(&:to_sym).to_set
 end

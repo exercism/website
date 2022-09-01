b745521b8bb272a0849772daf50bdaf8c876886b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-01 07:50:12 +0200

    Migrate last_submitted_at

--- db/migrate/20220817075919_add_last_submitted_at_to_exercise_representations.rb
+++ db/migrate/20220817075919_add_last_submitted_at_to_exercise_representations.rb
@@ -3,8 +3,12 @@ class AddLastSubmittedAtToExerciseRepresentations < ActiveRecord::Migration[7.0]
     add_column :exercise_representations, :last_submitted_at, :datetime, null: false, default: Time.now, if_not_exists: true
 
     unless Rails.env.production?
-      Exercise::Representation.find_each do |rep|
-        rep.update(last_submitted_at: rep.submission_representations.last.created_at)
+      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
+        Exercise::Representation.find_each do |representation|
+          Exercise::Representation
+            .where(id: representation.id)
+            .update_all(last_submitted_at: representation.submission_representations.last.created_at)
+        end
       end
     end
   end

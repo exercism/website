a7ba1ce05499ffdc8560a19f70148e534181aa35
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-01 09:09:11 +0200

    Move supermentor logic to separate module

--- app/commands/mentor/update_roles.rb
+++ app/commands/mentor/update_roles.rb
@@ -6,22 +6,13 @@ module Mentor
 
     def call
       if supermentor?
-        mentor.update(roles: mentor.roles.add(SUPERMENTOR_ROLE))
+        mentor.update(roles: mentor.roles.add(Mentor::Supermentor::ROLE))
       else
-        mentor.update(roles: mentor.roles.delete(SUPERMENTOR_ROLE))
+        mentor.update(roles: mentor.roles.delete(Mentor::Supermentor::ROLE))
       end
     end
 
     private
-    def supermentor?
-      mentor.mentor? &&
-        mentor.mentor_satisfaction_percentage.to_i >= MIN_SATISFACTION_PERCENTAGE &&
-        mentor.mentor_discussions.finished.count >= MIN_FINISHED_MENTORING_SESSIONS
-    end
-
-    SUPERMENTOR_ROLE = :supermentor
-    MIN_FINISHED_MENTORING_SESSIONS = 100
-    MIN_SATISFACTION_PERCENTAGE = 95
-    private_constant :SUPERMENTOR_ROLE, :MIN_FINISHED_MENTORING_SESSIONS, :MIN_SATISFACTION_PERCENTAGE
+    def supermentor? = Mentor::Supermentor.eligible?(mentor)
   end
 end

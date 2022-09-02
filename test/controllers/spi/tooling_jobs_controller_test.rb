999a81cc716313439ae19bbc719f10aa0b7e46d2
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-02 17:11:20 +0200

    Revert "Temporarily run representer jobs via sidekiq (#2922)" (#2936)
    This reverts commit 2b546cb6abb4f738c2c3a761e64760b023ae3e03.

--- test/controllers/spi/tooling_jobs_controller_test.rb
+++ test/controllers/spi/tooling_jobs_controller_test.rb
@@ -1,22 +1,10 @@
 require "test_helper"
 
 class SPI::ToolingJobsControllerTest < ActionDispatch::IntegrationTest
-  test "update proxies to command for test runner" do
-    job = create_test_runner_job!(create(:submission))
+  test "update proxies to command" do
+    id = SecureRandom.uuid
+    ToolingJob::Process.expects(:call).with(id)
 
-    Submission::TestRun::Process.expects(:call).with(job)
-
-    patch spi_tooling_job_url(job.id)
-  end
-
-  test "update proxies to command for representer" do
-    job = create_representer_job!(create(:submission))
-
-    # Shouldn't call command immediately
-    patch spi_tooling_job_url(job.id)
-
-    # But should via sidekiq
-    Submission::Representation::Process.expects(:call).with(job)
-    perform_enqueued_jobs
+    patch spi_tooling_job_url(id)
   end
 end

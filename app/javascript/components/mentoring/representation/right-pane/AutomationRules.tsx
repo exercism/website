09cec1636d1e9e258288c1a79306af9dcef2dc1b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-02 16:43:06 +0200

    Don't show representation heading if it is not there

--- app/javascript/components/mentoring/representation/right-pane/AutomationRules.tsx
+++ app/javascript/components/mentoring/representation/right-pane/AutomationRules.tsx
@@ -3,7 +3,11 @@ import { CompleteRepresentationData } from '../../../types'
 
 export default function AutomationRules({
   guidance,
-}: Pick<CompleteRepresentationData, 'guidance'>): JSX.Element {
+}: Pick<CompleteRepresentationData, 'guidance'>): JSX.Element | null {
+  if (guidance.representationsHtml === null) {
+    return null
+  }
+
   return (
     <div className="px-24 shadow-xsZ1v2 mb-24">
       <h2 className="text-h4 mb-12">Please read before giving feedback</h2>

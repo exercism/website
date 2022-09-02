42e9f1f81e2c6b4039d5ef08ec18747d916703ef
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-02 16:40:53 +0200

    Update styling of representation header

--- app/javascript/components/mentoring/representation/right-pane/AutomationRules.tsx
+++ app/javascript/components/mentoring/representation/right-pane/AutomationRules.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { CompleteRepresentationData } from '../../../types'
-import AlertText from './AlertText'
 
 export default function AutomationRules({
   guidance,
 }: Pick<CompleteRepresentationData, 'guidance'>): JSX.Element {
   return (
     <div className="px-24 shadow-xsZ1v2 mb-24">
-      <AlertText text="Important rules when giving feedback" />
+      <h2 className="text-h4 mb-12">Please read before giving feedback</h2>
       <div
         dangerouslySetInnerHTML={{
           __html: `<div class="c-textual-content --base">${guidance.representationsHtml}</div>`,

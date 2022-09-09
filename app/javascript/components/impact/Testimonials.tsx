6fe8d43a2a44e9651cd2122b52f041a55437daa3
dem4ron <demaaron88@gmail.com>
2022-09-09 11:00:05 +0200

    Fix types in Testimonial resolved data

--- app/javascript/components/impact/Testimonials.tsx
+++ app/javascript/components/impact/Testimonials.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TestimonialsList } from '../profile'
 
 export default function Testimonials({ data }: { data: any }): JSX.Element {
   return (
@@ -7,6 +8,8 @@ export default function Testimonials({ data }: { data: any }): JSX.Element {
         87,305 people testified to the impact Exercism has had on their
         learning.
       </h2>
+
+      {/* <TestimonialsList data = {data can come here} /> */}
     </div>
   )
 }

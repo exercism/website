5c06335bdd8128487cb65bf450d8fd9f9e6b3dc5
dem4ron <demaaron88@gmail.com>
2022-09-09 12:56:31 +0200

    Add modified c-shapes

--- app/javascript/components/impact/Testimonials.tsx
+++ app/javascript/components/impact/Testimonials.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
-import { TestimonialsList } from '../profile'
+// import { TestimonialsList } from '../profile'
 
 export default function Testimonials({ data }: { data: any }): JSX.Element {
-  return (
-    <div>
-      <h2 className="text-h1 text-center">
-        87,305 people testified to the impact Exercism has had on their
-        learning.
-      </h2>
-
-      {/* <TestimonialsList data = {data can come here} /> */}
-    </div>
-  )
+  return <div>maybe we&apos;ll use this</div>
 }

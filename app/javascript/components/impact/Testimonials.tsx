27ff2056b30c2201b7d1f96e9c110e789e27506c
dem4ron <demaaron88@gmail.com>
2022-09-09 09:45:28 +0200

    Remove console logs

--- app/javascript/components/impact/Testimonials.tsx
+++ app/javascript/components/impact/Testimonials.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
 export default function Testimonials({ data }: { data: any }): JSX.Element {
-  return <div>hi</div>
+  return (
+    <div>
+      <h2 className="text-h1 text-center">
+        87,305 people testified to the impact Exercism has had on their
+        learning.
+      </h2>
+    </div>
+  )
 }

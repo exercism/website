5f7dcc826fb330b8d0853f666c9d8349d1b99d3b
Jeremy Walker <jez.walker@gmail.com>
2022-09-19 12:06:16 +0100

    Pass milestones from component

--- app/helpers/react_components/impact/chart.rb
+++ app/helpers/react_components/impact/chart.rb
@@ -1,16 +1,24 @@
 module ReactComponents
   module Impact
     class Chart < ReactComponent
-      # this needs to be changed
-      initialize_with :metrics
-
       def to_s
         super(
           "impact-chart",
           {
-            metrics: metrics.map(&:to_broadcast_hash)
+            milestones: milestones.to_json
           }
         )
+      end
+
+      def milestones
+        [
+          { date: '201309', text: 'Check tooltip radius', emoji: '\u{1f600}' },
+          { date: '202006', text: 'See if this emoji is centered', emoji: '\u{1f600}' },
+          { date: '202206', text: 'Reached 1M users!!', emoji: '⭐' },
+          { date: '201907', text: 'Exercism V2 launched', emoji: '🚀' },
+          { date: '201411', text: 'Exercism V1 launched', emoji: '🚀' },
+          { date: '201305', text: 'Exercism got launched', emoji: '🤯' }
+        ]
       end
     end
   end

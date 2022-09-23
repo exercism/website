0c8798ed48937d2e0d4d462c86516753cf5eec5a
dem4ron <demaaron88@gmail.com>
2022-09-23 17:39:20 +0200

    Fix typo

--- app/javascript/hooks/index.ts
+++ app/javascript/hooks/index.ts
@@ -1,3 +1,3 @@
 export { useLogger } from './use-logger'
 export { useList } from './use-list'
-export { useHighlighting } from './use-sytnax-highlighting'
+export { useHighlighting } from './use-syntax-highlighting'

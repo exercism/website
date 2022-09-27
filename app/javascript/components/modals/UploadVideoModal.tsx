3a56a40fc79d098585c965b72b0a3d8e1813584e
dem4ron <demaaron88@gmail.com>
2022-09-27 11:13:02 +0200

    Style first step of video upload modal

--- app/javascript/components/modals/UploadVideoModal.tsx
+++ app/javascript/components/modals/UploadVideoModal.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { ExerciseIcon, TrackIcon } from '../common'
 import { Modal } from './Modal'
 
 export function UploadVideoModal(): JSX.Element {
   return (
-    <Modal open onClose={() => console.log('hello')}>
-      <h2 className="text-h2">Submit a community workthrough</h2>
-      <p className="text-prose">
+    <Modal
+      open
+      onClose={() => console.log('hello')}
+      ReactModalClassName="w-1-2"
+    >
+      <h2 className="text-h2 mb-8">Submit a community workthrough</h2>
+      <p className="text-prose mb-24">
         Produced a video of working through this exercise yourself? Want to
         share it with the Exercism community?{' '}
         <strong className="font-medium text">
@@ -14,24 +19,42 @@ export function UploadVideoModal(): JSX.Element {
         </strong>
       </p>
 
-      <div className="bg-gray border-2 border-gradient rounded-100">
-        <div className="text-h5">Asteroid Exploration Co.</div>
-        <div className="textColor-6 font-normal leading-150 text-16">Ruby</div>
+      <div className="py-8 px-24 bg-gray border-2 border-gradient rounded-100 flex flex-row items-center mb-32">
+        <TrackIcon
+          iconUrl="https://dg8krxphbh767.cloudfront.net/tracks/rust.svg"
+          title="Rust"
+          className="h-[40px], w-[40px] mr-12"
+        />
+        <ExerciseIcon
+          iconUrl="https://dg8krxphbh767.cloudfront.net/exercises/amusement-park.svg"
+          className="h-48 mr-12"
+        />
+        <div className="flex flex-col">
+          <div className="text-h5">Amusement Park</div>
+          <div className="textColor-6 font-normal leading-150 text-16">
+            Rust
+          </div>
+        </div>
       </div>
 
       <label
         htmlFor="video-url"
-        className="text-label text-btnBorder flex flex-col"
+        className="text-label text-btnBorder flex flex-col mb-16"
       >
-        PASTE YOUR VIDEO URL (YOUTUBE / VIMEO)
+        <span className="mb-8">PASTE YOUR VIDEO URL (YOUTUBE / VIMEO)</span>
         <input
           type="text"
           name="video-url"
           placeholder="This is placeholder text"
+          className="font-body"
         />
       </label>
 
-      <button className="w-full btn-primary btn-l">Retrieve video</button>
+      <div className="flex">
+        <button className="w-full btn-primary btn-l grow">
+          Retrieve video
+        </button>
+      </div>
     </Modal>
   )
 }

70db5c235616c9a05bfa78fd804588f0690993fd
Erik Schierboom <erik_schierboom@hotmail.com>
2022-09-30 16:18:47 +0200

    Fix table name

--- app/models/exercise/approaches/introduction_authorship.rb
+++ app/models/exercise/approaches/introduction_authorship.rb
@@ -1,4 +1,7 @@
 class Exercise::Approaches::IntroductionAuthorship < ApplicationRecord
+  # TODO: figure out how to remove this
+  self.table_name = "exercise_approach_introduction_authorships"
+
   belongs_to :exercise
   belongs_to :author,
     class_name: "User",

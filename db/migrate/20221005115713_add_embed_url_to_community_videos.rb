8474827c49c1e6dc73b6802d4166eaf30c032918
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-05 15:31:26 +0200

    Only run migration update when not in production

--- db/migrate/20221005115713_add_embed_url_to_community_videos.rb
+++ db/migrate/20221005115713_add_embed_url_to_community_videos.rb
@@ -2,8 +2,10 @@ class AddEmbedUrlToCommunityVideos < ActiveRecord::Migration[7.0]
   def change
     add_column :community_videos, :embed_url, :string, null: false, if_not_exists: true
 
-    CommunityVideo.where(platform: :youtube).find_each do |video|
-      video.update(embed_url: "https://www.youtube.com/embed/#{video.embed_id}")
+    unless Rails.env.production?
+      CommunityVideo.where(platform: :youtube).find_each do |video|
+        video.update(embed_url: "https://www.youtube.com/embed/#{video.embed_id}")
+      end
     end
   end
 end

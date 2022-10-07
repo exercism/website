b6e9b6e020432b5d98146babb76e650e5f2dda92
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-07 11:33:44 +0200

    Add setting for new analysis feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_115400) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_091420) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -862,6 +862,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_115400) do
     t.boolean "email_on_nudge_notification", default: true, null: false
     t.boolean "email_on_student_finished_discussion_notification", default: true, null: false
     t.boolean "email_on_mentor_finished_discussion_notification", default: true, null: false
+    t.boolean "email_on_analysis_feedback_added_notification", default: true, null: false
     t.index ["token"], name: "index_user_communication_preferences_on_token", unique: true
     t.index ["user_id"], name: "index_user_communication_preferences_on_user_id"
   end

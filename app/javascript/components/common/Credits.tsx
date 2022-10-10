1de83d0a05868e1ce27278be787c2711d1be1054
dem4ron <demaaron88@gmail.com>
2022-10-10 14:34:03 +0200

    Create AvatarGroup component, Credits component

---
+++ app/javascript/components/common/Credits.tsx
@@ -0,0 +1,37 @@
+import { AvatarGroup, AvatarGroupProps } from '@/components/common'
+import { pluralizeWithNumber } from '@/utils/pluralizeWithNumber'
+import React from 'react'
+
+type CreditsProps = {
+  topLabel: string
+  topCount: number
+  bottomLabel?: string
+  bottomCount?: number
+  fontSize?: number
+} & AvatarGroupProps
+
+export function Credits({
+  topLabel,
+  topCount,
+  bottomLabel,
+  bottomCount,
+  max,
+  avatarUrls,
+  fontSize = 14,
+}: CreditsProps): JSX.Element {
+  return (
+    <div
+      className={`flex gap-x-16 text-textColor1 leading-150 whitespace-nowrap text-${fontSize} items-center`}
+    >
+      <AvatarGroup max={max} avatarUrls={avatarUrls} />
+      <div className="flex flex-col gap-y-2">
+        <div className="font-semibold">
+          {pluralizeWithNumber(topCount, topLabel)}
+        </div>
+        {bottomCount && bottomLabel && (
+          <div>{pluralizeWithNumber(bottomCount, bottomLabel)}</div>
+        )}
+      </div>
+    </div>
+  )
+}

3e0fd07c780486cffcfad265bcde95c4141e3d0b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-14 11:29:11 +0200

    Fix unlock_button

--- app/helpers/react_components/track/unlock_button.rb
+++ app/helpers/react_components/track/unlock_button.rb
@@ -1,12 +1,9 @@
 module ReactComponents
   module Track
     class UnlockButton < ReactComponent
-      # initialize_with unlock_url:unlock_help_api_solution_url(@solution.uuid)
-      initialize_with unlock_url: "https://exercism.org"
+      initialize_with :unlock_url
       def to_s
-        super("track-tooltip-unlock-button", {
-          unlock_url:
-        })
+        super("track-tooltip-unlock-button", { unlock_url: })
       end
     end
   end

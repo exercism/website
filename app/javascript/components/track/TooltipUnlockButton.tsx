48855504836b1abb6b93e46948aa8d6142fe77bc
dem4ron <demaaron88@gmail.com>
2022-10-18 11:31:22 +0200

    Change spacing and colour on tooltip

--- app/javascript/components/track/TooltipUnlockButton.tsx
+++ app/javascript/components/track/TooltipUnlockButton.tsx
@@ -8,7 +8,7 @@ export function TooltipUnlockButton({
 }): JSX.Element {
   return (
     // or flex container in haml
-    <div className="flex">
+    <div className="flex mb-10">
       <button className="btn-primary btn-s flex items-center grow w-[100%] text-14 py-4 px-16">
         <GraphicalIcon icon="unlock" width={14} height={14} className="mr-8" />
         Unlock this tab

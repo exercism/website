673652fd3cebb24ef299450dd218c5990754d9a0
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-20 16:31:42 +0200

    Serialize community videos

--- app/helpers/react_components/track/approaches.rb
+++ app/helpers/react_components/track/approaches.rb
@@ -29,23 +29,6 @@ class ReactComponents::Track::Approaches < ReactComponents::ReactComponent
     }
   end
 
-  def videos_data
-    videos.map do |video|
-      {
-        author: video.author.present? ? SerializeAuthorOrContributor.(video.author) : nil,
-        submitted_by: SerializeAuthorOrContributor.(video.submitted_by),
-        platform: video.platform,
-        title: video.title,
-        created_at: video.created_at,
-        links: {
-          watch: video.url,
-          embed: video.embed_url,
-          channel: video.channel_url,
-          thumbnail: video.thumbnail_url
-        }
-      }
-    end
-  end
-
+  def videos_data = SerializeCommunityVideos.(videos)
   def approaches_data = SerializeApproaches.(approaches)
 end

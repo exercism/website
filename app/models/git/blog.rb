9e94797d4d3e9a62a439a70922421bdd2ca3de7f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-21 14:28:32 +0200

    Sync community stories from blog (#3071)
    Sync community stories from blog

--- app/models/git/blog.rb
+++ app/models/git/blog.rb
@@ -5,31 +5,21 @@ module Git
     REPO_NAME = "exercism/blog".freeze
     DEFAULT_REPO_URL = "https://github.com/#{REPO_NAME}".freeze
 
-    def self.content_for(slug)
-      new.content_for(slug)
-    end
-
-    def self.update!
-      new.update!
-    end
+    def self.post_content_for(slug) = new.post_content_for(slug)
+    def self.story_content_for(slug) = new.story_content_for(slug)
+    def self.update! = new.update!
 
     def initialize(repo_url: DEFAULT_REPO_URL)
       @repo = Repository.new(repo_url:)
     end
 
     memoize
-    def config
-      repo.read_json_blob(repo.head_commit, "config.json")
-    end
+    def config = repo.read_json_blob(repo.head_commit, "config.json")
 
-    def content_for(slug)
-      filepath = "posts/#{slug}.md"
-      repo.read_text_blob(head_commit, filepath)
-    end
-
-    def update!
-      repo.fetch!
-    end
+    def post_content_for(slug) = content_for("posts", slug)
+    def story_content_for(slug) = content_for("stories", slug)
+    def content_for(dir, slug) = repo.read_text_blob(head_commit, "#{dir}/#{slug}.md")
+    def update! = repo.fetch!
 
     private
     attr_reader :repo

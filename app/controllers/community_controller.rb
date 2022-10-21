e0b708edc2d6732dc3ee84fedc7932326793b428
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-21 10:29:29 +0200

    Add supporter avatar urls

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,7 +1,15 @@
 class CommunityController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def index; end
+  def index
+    # TODO: add caching
+    @supporter_avatar_urls =
+      User::AcquiredBadge.joins(:user).
+        where(badge_id: Badge.find_by_slug!("supporter")). # rubocop:disable Rails/DynamicFindBy
+        where(users: { show_on_supporters_page: true }).
+        last(40).
+        map { |b| b.user.avatar_url }
+  end
 
   def show
     @title = 'How I Saved My Life with 3D Printing'

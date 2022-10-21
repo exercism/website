82828763b70f7eca4d0b74a7ac3df530bb61456e
Jeremy Walker <jez.walker@gmail.com>
2022-10-22 00:31:29 +0100

    Remove stubbing

--- test/javascript/components/mentoring/Session.test.jsx
+++ test/javascript/components/mentoring/Session.test.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-// jest.mock("react", () => ({
-//   ...jest.requireActual("react"),
-//   lazy: (func) => {func()},
-//   Suspense: (func) => {func()},
-// }))
-
 import userEvent from '@testing-library/user-event'
 import {
   render,

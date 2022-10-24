85ee7ed7b1f006e716c04af6247e35e0f14a3794
Katrina Owen <katrina.owen@gmail.com>
2022-10-24 20:51:13 +0200

    Link to forum, not gitter (#3081)
    [skip ci]

--- test/models/solution_test.rb
+++ test/models/solution_test.rb
@@ -346,7 +346,7 @@ class SolutionTest < ActiveSupport::TestCase
       If you'd like help solving the exercise, check the following pages:
 
       - The [Ruby track's documentation](https://exercism.org/docs/tracks/ruby)
-      - [Exercism's support channel on gitter](https://gitter.im/exercism/support)
+      - [Exercism's programming category on the forum](https://forum.exercism.org/c/programming/5)
       - The [Frequently Asked Questions](https://exercism.org/docs/using/faqs)
 
       Should those resources not suffice, you could submit your (incomplete) solution to request mentoring.
@@ -383,7 +383,7 @@ class SolutionTest < ActiveSupport::TestCase
       If you'd like help solving the exercise, check the following pages:
 
       - The [Ruby track's documentation](https://exercism.org/docs/tracks/ruby)
-      - [Exercism's support channel on gitter](https://gitter.im/exercism/support)
+      - [Exercism's programming category on the forum](https://forum.exercism.org/c/programming/5)
       - The [Frequently Asked Questions](https://exercism.org/docs/using/faqs)
 
       Should those resources not suffice, you could submit your (incomplete) solution to request mentoring.

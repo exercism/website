16963dcdb6ca67025588094b1af4d942a0d1ddf5
Jeremy Walker <jez.walker@gmail.com>
2022-10-24 01:30:58 +0100

    Fix JS errors

--- app/javascript/components/modals/CommunityLaunchModal.tsx
+++ app/javascript/components/modals/CommunityLaunchModal.tsx
@@ -59,11 +59,11 @@ export const CommunityLaunchModal = ({
           <h1>Exercism Community</h1>
 
           <p>
-            Exercism's always been about community - the volunteers who create
-            the tracks, the mentors that make learning magic, and everyone using
-            the platform to upskill. Now, for the first time, we're doubling
-            down on that community - bringing everyone together to learn and
-            support each other.
+            Exercism&apos;s always been about community - the volunteers who
+            create the tracks, the mentors that make learning magic, and
+            everyone using the platform to upskill. Now, for the first time,
+            we&apos;re doubling down on that community - bringing everyone
+            together to learn and support each other.
           </p>
         </header>
 
@@ -77,7 +77,7 @@ export const CommunityLaunchModal = ({
           <div className="improvement">
             <GraphicalIcon icon="exercise" category="graphics" />
             <div className="info">
-              <h3>Exercism's Forum</h3>
+              <h3>Exercism&apos;s Forum</h3>
               <p>
                 Want to explore a concept or exercise with others? Want to chat
                 about how Exercism can be improved? Want to discuss programming
@@ -102,10 +102,11 @@ export const CommunityLaunchModal = ({
             <div className="info">
               <h3>Community Stories</h3>
               <p>
-                When you're feeling frustrated or want some inspriation, there's
-                nothing better than hearing other people's stories. Well now you
-                can sit back and enjoy inspiring tales from Exercism's community
-                in our new Community Stories section.
+                When you&apos;re feeling frustrated or want some inspriation,
+                there&apos;s nothing better than hearing other people&apos;s
+                stories. Well now you can sit back and enjoy inspiring tales
+                from Exercism&apos;s community in our new Community Stories
+                section.
               </p>
             </div>
           </div>
@@ -114,10 +115,10 @@ export const CommunityLaunchModal = ({
             <div className="info">
               <h3>Dig Deeper</h3>
               <p>
-                There are thousands of videos about solving Exercism's
-                exercises. We're bringing them all into one place along with
-                commentry and guidance on exercises. Look out for the new Dig
-                Deeper tab when you complete an exercise!
+                There are thousands of videos about solving Exercism&apos;s
+                exercises. We&apos;re bringing them all into one place along
+                with commentry and guidance on exercises. Look out for the new
+                Dig Deeper tab when you complete an exercise!
               </p>
             </div>
           </div>
@@ -129,7 +130,7 @@ export const CommunityLaunchModal = ({
           type="button"
           onClick={handleClick}
         >
-          Sounds good! Let's go.
+          Sounds good! Let&apos;s go.
         </FormButton>
         <ErrorBoundary resetKeys={[status]}>
           <ErrorMessage error={error} defaultError={DEFAULT_ERROR} />
@@ -151,24 +152,26 @@ export const CommunityLaunchModal = ({
         <script src="https://player.vimeo.com/api/player.js"></script>
         <div className="thank-you">
           <h3>This wouldn't be possible without your kind donations 💙</h3>
-          <p class="mb-8">
+          <p className="mb-8">
             Thanks to your kind donations, we've been able to hire Jonathan, our
             new Community Manager, who can now dedicate the time and resource we
             need to properly nuture our community.
           </p>
-          <p class="mb-8">
+          <p className="mb-8">
             If you see Jonathan around, say hello! He'd love to chat, hear your
             story, and learn how we can make Exercism even better for you 👇
           </p>
-          <div class="flex flex-row items-center bg-white rounded-100 p-12">
+          <div className="flex flex-row items-center bg-white rounded-100 p-12">
             <img
               src={jonathanImageUrl}
               alt="Photo of Jonathan"
-              class="w-[48px] h-[48px] mr-16 rounded-circle"
+              className="w-[48px] h-[48px] mr-16 rounded-circle"
             />
-            <div class="flex flex-col">
-              <div class="text-h6 mb-4">Jonathan Middleton</div>
-              <div class="text-textColor6 font-medium">Community Manager</div>
+            <div className="flex flex-col">
+              <div className="text-h6 mb-4">Jonathan Middleton</div>
+              <div className="text-textColor6 font-medium">
+                Community Manager
+              </div>
             </div>
           </div>
         </div>

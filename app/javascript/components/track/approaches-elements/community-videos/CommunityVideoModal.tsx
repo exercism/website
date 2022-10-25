38028ae68e47a3f6cfcd1f8181daf2800e277118
dem4ron <demaaron88@gmail.com>
2022-10-25 10:23:41 +0200

    Don't show component if data is missing on video modal

--- app/javascript/components/track/approaches-elements/community-videos/CommunityVideoModal.tsx
+++ app/javascript/components/track/approaches-elements/community-videos/CommunityVideoModal.tsx
@@ -38,17 +38,26 @@ export function CommunityVideoModal({
       {video.author && (
         <VideoCredits links={video.links} author={video.author} />
       )}
-      <div className="text-center text-textColor6 leading-160 text-16">
-        Posted by{' '}
-        {video.submittedBy.links.profile ? (
-          <a href={video.submittedBy.links.profile} className="underline">
-            @{video.submittedBy.handle}
-          </a>
-        ) : (
-          `@${video.submittedBy.handle}`
-        )}{' '}
-        &middot; {dayjs(video.createdAt).format('D MMM YYYY')}
-      </div>
+
+      {video.submittedBy.handle.length > 0 ||
+        (video.createdAt.length > 0 && (
+          <div className="text-center text-textColor6 leading-160 text-16">
+            {video.submittedBy.links.profile &&
+              video.submittedBy.handle.length > 0 && (
+                <>
+                  Posted by{' '}
+                  <a
+                    href={video.submittedBy.links.profile}
+                    className="underline"
+                  >
+                    @{video.submittedBy.handle}
+                  </a>
+                </>
+              )}
+            {video.createdAt.length > 0 &&
+              `&middot; ${dayjs(video.createdAt).format('D MMM YYYY')}`}
+          </div>
+        ))}
     </Modal>
   )
 }

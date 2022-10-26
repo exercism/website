d5e858dd5aadcced678720d30064464ecff6c6bf
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-26 12:38:56 +0200

    Reduce nesting for Track::Create

--- app/commands/track/create.rb
+++ app/commands/track/create.rb
@@ -1,15 +1,13 @@
-class Track
-  class Create
-    include Mandate
+class Track::Create
+  include Mandate
 
-    initialize_with :slug, attributes: Mandate::KWARGS
+  initialize_with :slug, attributes: Mandate::KWARGS
 
-    def call
-      Track.create!(slug:, **attributes).tap do |track|
-        ContributorTeam::Create.(track.slug, type: :track_maintainers, track:)
-      end
-    rescue ActiveRecord::RecordNotUnique
-      Track.find_by!(slug:)
+  def call
+    Track.create!(slug:, **attributes).tap do |track|
+      ContributorTeam::Create.(track.slug, type: :track_maintainers, track:)
     end
+  rescue ActiveRecord::RecordNotUnique
+    Track.find_by!(slug:)
   end
 end

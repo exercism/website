236974e092c5025015347752b176145870394f36
Erik Schierboom <erik_schierboom@hotmail.com>
2022-10-27 10:13:59 +0200

    Add schema and migrations for exercise articles

---
+++ db/migrate/20221027074831_create_exercise_articles.rb
@@ -0,0 +1,19 @@
+class CreateExerciseArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exercise_articles do |t|
+      t.belongs_to :exercise, foreign_key: true, null: false, type: :bigint
+
+      t.string :uuid, null: false, index: true
+      
+      t.string :slug, null: false
+      t.string :title, null: false
+      t.string :blurb, null: false, limit: 350
+
+      t.string :synced_to_git_sha, null: false
+
+      t.timestamps
+
+      t.index [:exercise_id, :uuid], unique: true
+    end
+  end
+end

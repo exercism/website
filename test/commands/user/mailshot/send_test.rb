e20de8b9cb694f2ea84d11a4b12d8d106f30ad5a
Jeremy Walker <jez.walker@gmail.com>
2022-10-28 11:59:09 +0100

    Add community launch newsletter

--- test/commands/user/mailshot/send_test.rb
+++ test/commands/user/mailshot/send_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class User::Mailshot::SendTest < ActiveSupport::TestCase
-  test "sends v3_launch email" do
+  test "sends community_launch email" do
     user = create :user
 
     assert_enqueued_with(
       job: ActionMailer::MailDeliveryJob, args: [
         "MailshotsMailer",
-        "v3_launch",
+        "community_launch",
         "deliver_now",
         { params: { user: }, args: [] }
       ]
     ) do
-      User::Mailshot::Send.(user, :v3_launch)
+      User::Mailshot::Send.(user, :community_launch)
     end
   end
 end

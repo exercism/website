7573251143b381eef9171b6ff894a025ef19b27b
Jeremy Walker <jez.walker@gmail.com>
2022-11-02 18:23:56 +0000

    Improve unsubscribe (#3139)
    * Improve unsubscribe
    
    * Apply suggestions from code review

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -17,6 +17,28 @@ class UnsubscribeController < ApplicationController
     redirect_to({ action: :show, token: @token, key: @email_key }, notice: "You have been unsubscribed successfully")
   end
 
+  def all
+    @communication_preferences.update!(
+      email_on_mentor_started_discussion_notification: false,
+      email_on_mentor_replied_to_discussion_notification: false,
+      email_on_student_replied_to_discussion_notification: false,
+      email_on_student_added_iteration_notification: false,
+      email_on_new_solution_comment_for_solution_user_notification: false,
+      email_on_new_solution_comment_for_other_commenter_notification: false,
+      receive_product_updates: false,
+      email_on_remind_mentor: false,
+      email_on_mentor_heartbeat: false,
+      email_on_general_update_notification: false,
+      email_on_acquired_badge_notification: false,
+      email_on_nudge_notification: false,
+      email_on_student_finished_discussion_notification: false,
+      email_on_mentor_finished_discussion_notification: false,
+      email_on_automated_feedback_added_notification: false
+    )
+
+    redirect_to({ action: :show, token: @token }, notice: "You have been unsubscribed from all emails successfully")
+  end
+
   private
   def guard_token!
     @token = params[:token]
@@ -31,6 +53,6 @@ class UnsubscribeController < ApplicationController
 
   def guard_key!
     @email_key = params[:key]
-    redirect_to communication_preferences_settings_path unless @email_key
+    # redirect_to communication_preferences_settings_path unless @email_key
   end
 end

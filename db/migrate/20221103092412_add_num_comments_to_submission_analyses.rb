99460374344a4ef95df88e093ab53fe960ac0deb
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-03 12:32:56 +0100

    Update query

--- db/migrate/20221103092412_add_num_comments_to_submission_analyses.rb
+++ db/migrate/20221103092412_add_num_comments_to_submission_analyses.rb
@@ -1,10 +1,10 @@
 class AddNumCommentsToSubmissionAnalyses < ActiveRecord::Migration[7.0]
   def change
-    add_column :submission_analyses, :num_comments, :integer, limit: 1, default: 0, null: false
+    execute "ALTER TABLE submission_analyses ADD COLUMN num_comments TINYINT DEFAULT 0 NOT NULL, ALGORITHM=INPLACE, LOCK=NONE"
 
     unless Rails.env.production?
       Submission::Analysis.find_each do |analysis|
-        analysis.update(num_comments: analysis.comments.size)
+        analysis.update(num_comments: analysis.send(:comment_blocks).size)
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -351,10 +351,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_092412) do
     t.index ["exercise_id", "ast_digest"], name: "exercise_representations_unique", unique: true
     t.index ["exercise_id", "ast_digest"], name: "index_exercise_representations_on_exercise_id_and_ast_digest"
     t.index ["exercise_id"], name: "index_exercise_representations_on_exercise_id"
+    t.index ["feedback_author_id", "exercise_id", "last_submitted_at"], name: "index_exercise_representation_author_exercise_last_submitted_at", order: { last_submitted_at: :desc }
+    t.index ["feedback_author_id", "exercise_id", "num_submissions"], name: "index_exercise_representation_author_exercise_num_submissions", order: { num_submissions: :desc }
     t.index ["feedback_author_id", "track_id", "last_submitted_at"], name: "index_exercise_representation_author_track_last_submitted_at", order: { last_submitted_at: :desc }
     t.index ["feedback_author_id", "track_id", "num_submissions"], name: "index_exercise_representation_author_track_num_submissions", order: { num_submissions: :desc }
     t.index ["feedback_author_id"], name: "index_exercise_representations_on_feedback_author_id"
     t.index ["feedback_editor_id"], name: "index_exercise_representations_on_feedback_editor_id"
+    t.index ["feedback_type", "exercise_id", "last_submitted_at"], name: "index_exercise_representation_type_exercise_last_submitted_at", order: { last_submitted_at: :desc }
+    t.index ["feedback_type", "exercise_id", "num_submissions"], name: "index_exercise_representation_type_exercise_num_submissions", order: { num_submissions: :desc }
     t.index ["feedback_type", "track_id", "last_submitted_at"], name: "index_exercise_representation_type_track_last_submitted_at", order: { last_submitted_at: :desc }
     t.index ["feedback_type", "track_id", "num_submissions"], name: "index_exercise_representation_type_track_num_submissions", order: { num_submissions: :desc }
     t.index ["source_submission_id"], name: "index_exercise_representations_on_source_submission_id"

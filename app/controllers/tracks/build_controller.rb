f6432e85d8681c648b2fdb211fb630d85b23a081
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-04 16:22:11 +0100

    Use build status via filter

--- app/controllers/tracks/build_controller.rb
+++ app/controllers/tracks/build_controller.rb
@@ -2,14 +2,18 @@ class Tracks::BuildController < ApplicationController
   include UseTrackExerciseSolutionConcern
   before_action :use_track!
   skip_before_action :authenticate_user!
+  before_action :use_build_status
 
-  def show
-    @status = @track.build_status
-  end
+  def show; end
 
   def syllabus_tooltip = render_template_as_json
   def representer_tooltip = render_template_as_json
   def analyzer_tooltip = render_template_as_json
   def test_runner_tooltip = render_template_as_json
   def practice_exercises_tooltip = render_template_as_json
+
+  private
+  def use_build_status
+    @status = @track.build_status
+  end
 end

7d51bb436805120ff7b640266b515783adfdaec6
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-24 16:11:06 +0100

    Randomize properly

--- app/assemblers/combine_authors_and_contributors.rb
+++ app/assemblers/combine_authors_and_contributors.rb
@@ -15,8 +15,8 @@ class CombineAuthorsAndContributors
   private
   def randomize(users, num_users)
     if users.is_a?(ActiveRecord::Relation)
-      ids = users.distinct.pluck(user_id_column).shuffle.take(num_users)
-      User.with_attached_avatar.includes(:profile).where(id: ids).to_a
+      ids = users.distinct.order("RAND()").select(user_id_column)
+      User.with_attached_avatar.includes(:profile).where(id: ids).limit(num_users).to_a
     else
       users.shuffle.take(num_users)
     end

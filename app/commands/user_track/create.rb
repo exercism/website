67152b6f6965d942fe1cbecc4d02f4d269b93fcd
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-25 13:12:24 +0100

    Add join user track metric when joining track

--- app/commands/user_track/create.rb
+++ app/commands/user_track/create.rb
@@ -5,7 +5,13 @@ class UserTrack
     initialize_with :user, :track
 
     def call
-      ::UserTrack.create_or_find_by!(user:, track:)
+      ::UserTrack.create_or_find_by!(user:, track:).tap do |user_track|
+        log_metric!(user_track)
+      end
+    end
+
+    def log_metric!(user_track)
+      Metric::Queue.(:join_track, user_track.created_at, user_track:, track:, user:)
     end
   end
 end

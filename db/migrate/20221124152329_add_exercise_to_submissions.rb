6d5535530f49b875e01135fcee4131a59a478613
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-25 09:17:11 +0100

    Update batch script

--- db/migrate/20221124152329_add_exercise_to_submissions.rb
+++ db/migrate/20221124152329_add_exercise_to_submissions.rb
@@ -4,10 +4,10 @@ class AddExerciseToSubmissions < ActiveRecord::Migration[7.0]
       add_column :submissions, :exercise_id, :mediumint, null: true
       add_index :submissions, %i[track_id exercise_id], unique: false, if_not_exists: true
 
-      Submission.includes(solution: :exercise).find_in_batches do |batch|
+      Submission.includes(:solution).find_in_batches do |batch|
         ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
           batch.each do |submission|
-            submission.update(exercise_id: submission.solution.exercise_id)
+            submission.update_column(:exercise_id, submission.solution.exercise_id)
           end
         end
       end

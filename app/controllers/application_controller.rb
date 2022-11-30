3c941f55aa26195b2a161685bb318e49a5ff645e
Erik Schierboom <erik_schierboom@hotmail.com>
2022-11-30 13:46:44 +0100

    Fix rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -83,7 +83,7 @@ class ApplicationController < ActionController::Base
   def csp_policy
     websockets = "ws://#{Rails.env.production? ? 'exercism.org' : 'local.exercism.io:3334'}"
     stripe = "https://js.stripe.com"
-    captcha = %w(https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/)
+    captcha = %w[https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/]
     google_fonts_font = "https://fonts.gstatic.com"
     google_fonts_css = "https://fonts.googleapis.com"
     fontawesome = "https://maxcdn.bootstrapcdn.com"

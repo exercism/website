90994727cf18d5dcf4eb884679f77999a174aee6
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-01 16:28:34 +0100

    Add sanity checks

--- test/commands/mentor/discussion/update_num_finished_discussions_test.rb
+++ test/commands/mentor/discussion/update_num_finished_discussions_test.rb
@@ -1,28 +1,41 @@
 require 'test_helper'
 
 class Mentor::Discussion::UpdateNumFinishedDiscussionsTest < ActiveSupport::TestCase
-  test "updates count" do
+  test "updates num_finished_discussions" do
+    track = create :track, :random_slug
+    other_track = create :track, :random_slug
     mentor = create :user
-    mentorship = create :user_track_mentorship, user: mentor
+    other_mentor = create :user
+    mentorship = create :user_track_mentorship, user: mentor, track: track
 
-    discussion_1 = create :mentor_discussion, mentor: mentor, status: :awaiting_student
+    discussion_1 = create :mentor_discussion, mentor: mentor, status: :awaiting_student, track: track
     Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_1)
     assert_equal 0, mentorship.reload.num_finished_discussions
 
-    discussion_2 = create :mentor_discussion, mentor: mentor, status: :awaiting_mentor
+    discussion_2 = create :mentor_discussion, mentor: mentor, status: :awaiting_mentor, track: track
     Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_2)
     assert_equal 0, mentorship.reload.num_finished_discussions
 
-    discussion_3 = create :mentor_discussion, mentor: mentor, status: :mentor_finished
+    discussion_3 = create :mentor_discussion, mentor: mentor, status: :mentor_finished, track: track
     Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_3)
     assert_equal 0, mentorship.reload.num_finished_discussions
 
-    discussion_4 = create :mentor_discussion, mentor: mentor, status: :finished
+    discussion_4 = create :mentor_discussion, mentor: mentor, status: :finished, track: track
     Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_4)
     assert_equal 1, mentorship.reload.num_finished_discussions
 
-    discussion_5 = create :mentor_discussion, mentor: mentor, status: :finished
+    discussion_5 = create :mentor_discussion, mentor: mentor, status: :finished, track: track
     Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_5)
+    assert_equal 2, mentorship.reload.num_finished_discussions
+
+    # Sanity check: same mentor but different track
+    discussion_6 = create :mentor_discussion, mentor: mentor, status: :finished, track: other_track
+    Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_6)
+    assert_equal 2, mentorship.reload.num_finished_discussions
+
+    # Sanity check: same track but different mentor
+    discussion_7 = create :mentor_discussion, mentor: other_mentor, status: :finished, track: track
+    Mentor::Discussion::UpdateNumFinishedDiscussions.(discussion_7)
     assert_equal 2, mentorship.reload.num_finished_discussions
   end
 end

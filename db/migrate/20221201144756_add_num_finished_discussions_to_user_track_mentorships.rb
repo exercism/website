5e454e38dda2a4690d9ce330a2ec164ec175530b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-02 07:36:59 +0100

    Use mediumint

--- db/migrate/20221201144756_add_num_finished_discussions_to_user_track_mentorships.rb
+++ db/migrate/20221201144756_add_num_finished_discussions_to_user_track_mentorships.rb
@@ -1,7 +1,7 @@
 class AddNumFinishedDiscussionsToUserTrackMentorships < ActiveRecord::Migration[7.0]
   def change
     unless Rails.env.production?
-      add_column :user_track_mentorships, :num_finished_discussions, :integer, null: false, default: 0
+      add_column :user_track_mentorships, :num_finished_discussions, :mediumint, null: false, default: 0
     end
   end
 end

7b8d46c60a76384b1819b2fc0d0b1c00925bae84
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-06 10:43:34 +0100

    Allow testing blocked domain with email string

--- app/models/user/block_domain.rb
+++ app/models/user/block_domain.rb
@@ -1,6 +1,7 @@
 class User::BlockDomain < ApplicationRecord
-  def self.blocked?(user)
-    domain = Mail::Address.new(user.email).domain
+  def self.blocked?(user_or_email)
+    email = user_or_email.is_a?(User) ? user_or_email.email : user_or_email
+    domain = Mail::Address.new(email).domain
     User::BlockDomain.where(domain:).exists?
   end
 end

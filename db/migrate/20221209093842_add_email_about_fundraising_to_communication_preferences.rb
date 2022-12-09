6ae98700c5d973f998ebe1e2c05ec5cbbca065d7
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-09 14:33:28 +0100

    Update 20221209093842_add_email_about_fundraising_to_communication_preferences.rb

--- db/migrate/20221209093842_add_email_about_fundraising_to_communication_preferences.rb
+++ db/migrate/20221209093842_add_email_about_fundraising_to_communication_preferences.rb
@@ -1,5 +1,5 @@
 class AddEmailAboutFundraisingToCommunicationPreferences < ActiveRecord::Migration[7.0]
   def change
-    add_column :user_communication_preferences, :email_about_fundraising_campaigns, :boolean, default: true, null: false
+    add_column :user_communication_preferences, :email_about_fundraising_campaigns, :boolean, default: true, null: false, if_not_exists: true
   end
 end

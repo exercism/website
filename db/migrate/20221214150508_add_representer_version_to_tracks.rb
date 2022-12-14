3be0bec05d94ef23562484f62daf391b8a1bd4e4
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-14 16:09:51 +0100

    Initialize representer version to 1 for tracks with representer

--- db/migrate/20221214150508_add_representer_version_to_tracks.rb
+++ db/migrate/20221214150508_add_representer_version_to_tracks.rb
@@ -2,6 +2,10 @@ class AddRepresenterVersionToTracks < ActiveRecord::Migration[7.0]
   def change
     unless Rails.env.production?
       add_column :tracks, :representer_version, :smallint, null: true
+
+      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
+        Track.where(has_representer: true).update_all(representer_version: 1)
+      end
     end
   end
 end

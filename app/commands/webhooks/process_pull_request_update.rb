2ed811b76cae7d571d09d7e92047257e824cd39f
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-14 16:03:40 +0100

    Reduce nesting of webhooks commands

--- app/commands/webhooks/process_pull_request_update.rb
+++ app/commands/webhooks/process_pull_request_update.rb
@@ -1,9 +1,7 @@
-module Webhooks
-  class ProcessPullRequestUpdate
-    include Mandate
+class Webhooks::ProcessPullRequestUpdate
+  include Mandate
 
-    initialize_with pull_request_update: Mandate::KWARGS
+  initialize_with pull_request_update: Mandate::KWARGS
 
-    def call = ProcessPullRequestUpdateJob.perform_later(pull_request_update)
-  end
+  def call = ProcessPullRequestUpdateJob.perform_later(pull_request_update)
 end

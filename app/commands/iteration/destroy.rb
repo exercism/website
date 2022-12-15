e0acc3e7d37a59b5db522c5e4f82a8cae1b4293d
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-15 11:36:10 +0100

    Reduce nesting of iteration commands

--- app/commands/iteration/destroy.rb
+++ app/commands/iteration/destroy.rb
@@ -1,19 +1,17 @@
-class Iteration
-  class Destroy
-    include Mandate
+class Iteration::Destroy
+  include Mandate
 
-    initialize_with :iteration
+  initialize_with :iteration
 
-    def call
-      iteration.update!(deleted_at: Time.current)
+  def call
+    iteration.update!(deleted_at: Time.current)
 
-      Solution::Uncomplete.(solution) unless solution.iterations.not_deleted.exists?
-      Solution::Unpublish.(solution) if solution.published_iteration_id == iteration.id
-      Solution::UpdateNumLoc.(solution)
-    end
-
-    private
-    memoize
-    def solution = iteration.solution
+    Solution::Uncomplete.(solution) unless solution.iterations.not_deleted.exists?
+    Solution::Unpublish.(solution) if solution.published_iteration_id == iteration.id
+    Solution::UpdateNumLoc.(solution)
   end
+
+  private
+  memoize
+  def solution = iteration.solution
 end

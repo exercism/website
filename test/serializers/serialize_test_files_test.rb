6a635610c1b3c395920968370005c340a8f1e72b
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-15 11:55:03 +0100

    Fix old tests

--- test/serializers/serialize_test_files_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SerializeTestFilesTest < ActiveSupport::TestCase
-  test "serializes test files" do
-    files = { "bob_test.rb" => "test" }
-
-    assert_equal [{ filename: "bob_test.rb", content: "test" }], SerializeTestFiles.(files)
-  end
-end

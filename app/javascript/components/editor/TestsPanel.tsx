ff8f0cb3a2df5ee7bb9d0a12bee66ed0afe5f7c7
dem4ron <demaaron88@gmail.com>
2022-12-16 13:09:48 +0100

    Add comments

--- app/javascript/components/editor/TestsPanel.tsx
+++ app/javascript/components/editor/TestsPanel.tsx
@@ -17,23 +17,36 @@ export const TestsPanel = ({
   const [treeMap, setTreeMap] = useState<{ [key: string]: HTMLPreElement }>({})
   const [reusing, setReusing] = useState<boolean>(false)
 
+  // useLayoutEffect is necessary, because the highlighting must be completed _before_ render
   useLayoutEffect(() => {
     if (!testRef.current || !memoTestRef.current) {
       return
     }
 
+    // The highlighter fn is a bit expensive to run, therefore this block makes sure it won't be run on the same file again
+    // see https://github.com/exercism/website/pull/3320
+    // Checks if the filename has already been transformed into a highlighted representation
+    // if not, highlights it, and saves it in a hashMap
     if (!(testTab.filename in treeMap)) {
       setReusing(false)
       highlightAll(testRef.current)
+
+      // disable linter - if we get here, testRef.current is guaranteed to be here, otherwise it would've returned early
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       setTreeMap((t) => ({ ...t, [testTab.filename]: testRef.current! }))
     } else {
       setReusing(true)
+
+      // when testTab is updated, it removes the previously rendered dom element -  which comes from the treeMap hashmap
       while (memoTestRef.current.firstChild) {
         memoTestRef.current.removeChild(memoTestRef.current.firstChild)
       }
+
+      // ...and appends another child element to our memoTestRef container
       memoTestRef.current.appendChild(treeMap[testTab.filename])
     }
+
+    // we only want to rerun it if testTab changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [testTab])
 
@@ -44,6 +57,15 @@ export const TestsPanel = ({
         context={TestTabContext}
         className="tests c-code-pane"
       >
+        {/*
+        this cannot be removed, because if this pre tag doesn't exist,
+        and another testFile is loaded for the first time (not from the hashmap), the highlighter useEffect won't run,
+        because testRef.current will be missing, and `reusing` boolean won't be able to switch to true.
+
+        this can be further optimized by counting the testFiles, and comparing them to keys stored in the hashmap,
+        and ejecting this block if the two numbers are the same
+        but is not necessary at this point
+        */}
         <pre
           ref={testRef}
           className={reusing ? 'hidden' : ''}
@@ -57,7 +79,8 @@ export const TestsPanel = ({
             {testTab.content}
           </code>
         </pre>
-        <div className={!reusing ? 'hidden' : ''} ref={memoTestRef}></div>
+        {/* injecting cached DOM elements into this container */}
+        <div className={reusing ? '' : 'hidden'} ref={memoTestRef}></div>
       </Tab.Panel>
     </Tab.Panel>
   )

3c630d77689c985d4371b7bc87a5e2c90264b8ef
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-16 08:01:38 +0100

    Update db/migrate/20221214150508_add_representer_version_to_tracks.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- db/migrate/20221214150508_add_representer_version_to_tracks.rb
+++ db/migrate/20221214150508_add_representer_version_to_tracks.rb
@@ -1,11 +1,7 @@
 class AddRepresenterVersionToTracks < ActiveRecord::Migration[7.0]
   def change
     unless Rails.env.production?
-      add_column :tracks, :representer_version, :smallint, null: true
-
-      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
-        Track.where(has_representer: true).update_all(representer_version: 1)
-      end
+      add_column :tracks, :representer_version, :smallint, null: false, default: 1
     end
   end
 end

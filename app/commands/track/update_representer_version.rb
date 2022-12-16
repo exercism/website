5524a69c68f3b5425627d420575a31cb2495417b
Jeremy Walker <jez.walker@gmail.com>
2022-12-16 17:07:26 +0000

    Update representer repo before setting version (#3331)

--- app/commands/track/update_representer_version.rb
+++ app/commands/track/update_representer_version.rb
@@ -3,5 +3,8 @@ class Track::UpdateRepresenterVersion
 
   initialize_with :track
 
-  def call = track.update(representer_version: track.representer.version)
+  def call
+    track.representer.fetch!
+    track.update(representer_version: track.representer.version)
+  end
 end

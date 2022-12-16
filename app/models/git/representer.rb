1b8b16a84ab968e86acbe808dbde1d018e19e6a6
Jeremy Walker <jez.walker@gmail.com>
2022-12-16 15:55:25 +0000

    Update representer repo before setting version

--- app/models/git/representer.rb
+++ app/models/git/representer.rb
@@ -2,6 +2,8 @@ class Git::Representer
   extend Mandate::Memoize
   extend Git::HasGitFilepath
 
+  delegate :fetch!, :head_commit, to: :repo
+
   git_filepath :config, file: "config.json"
 
   def initialize(repo_url: nil, repo: nil)
@@ -10,12 +12,8 @@ class Git::Representer
     @repo = repo || Git::Repository.new(repo_url:)
   end
 
-  memoize
   def version = config[:version] || 1
-
-  memoize
-  def commit = repo.head_commit
-
+  def commit = head_commit
   def absolute_filepath(filepath) = filepath
 
   private

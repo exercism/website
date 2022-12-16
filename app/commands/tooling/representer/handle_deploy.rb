2f64550953d9bb028099068f2797b962e5eb74d5
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-16 08:09:21 +0100

    Fix tests

--- app/commands/tooling/representer/handle_deploy.rb
+++ app/commands/tooling/representer/handle_deploy.rb
@@ -6,14 +6,16 @@ class Tooling::Representer::HandleDeploy
   initialize_with :track
 
   def call
-    old_representer_version = track.representer_version
+    @old_representer_version = track.representer_version
     Track::UpdateRepresenterVersion.(track)
     reprocess_exercises!
   end
-    
+
+  private
+  attr_reader :old_representer_version
+
   def reprocess_exercises!
-    return if old_representer_version == track.representer_version
-    
+    return if @old_representer_version == track.representer_version
 
     Exercise::Representation.where(track:).with_feedback.includes(source_submission: :exercise).find_each do |representation|
       submission = representation.source_submission

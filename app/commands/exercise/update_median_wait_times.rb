93f1fb20ca8c8eab7202827621aaf0bd721b39ea
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:29:46 +0100

    Reduce nesting of exercise commands

--- app/commands/exercise/update_median_wait_times.rb
+++ app/commands/exercise/update_median_wait_times.rb
@@ -1,31 +1,29 @@
-class Exercise
-  class UpdateMedianWaitTimes
-    include Mandate
+class Exercise::UpdateMedianWaitTimes
+  include Mandate
 
-    def call
-      Exercise.find_each do |exercise|
-        exercise.update(median_wait_time: median_wait_time(exercise))
-      end
+  def call
+    Exercise.find_each do |exercise|
+      exercise.update(median_wait_time: median_wait_time(exercise))
     end
+  end
 
-    private
-    def median_wait_time(exercise)
-      wait_times = Mentor::Discussion.
-        joins(:request).
-        select('TIMESTAMPDIFF(SECOND, request.created_at, mentor_discussions.created_at) AS wait_time').
-        where('request.exercise_id': exercise.id).
-        where('mentor_discussions.created_at > ?', Time.current - 4.weeks).
-        map(&:wait_time).
-        reject { |seconds| seconds < 5 }
+  private
+  def median_wait_time(exercise)
+    wait_times = Mentor::Discussion.
+      joins(:request).
+      select('TIMESTAMPDIFF(SECOND, request.created_at, mentor_discussions.created_at) AS wait_time').
+      where('request.exercise_id': exercise.id).
+      where('mentor_discussions.created_at > ?', Time.current - 4.weeks).
+      map(&:wait_time).
+      reject { |seconds| seconds < 5 }
 
-      calculate_median(wait_times)
-    end
+    calculate_median(wait_times)
+  end
 
-    def calculate_median(vals)
-      return nil if vals.empty?
+  def calculate_median(vals)
+    return nil if vals.empty?
 
-      vals.sort!
-      (vals[(vals.length - 1) / 2] + vals[vals.length / 2]) / 2.0
-    end
+    vals.sort!
+    (vals[(vals.length - 1) / 2] + vals[vals.length / 2]) / 2.0
   end
 end

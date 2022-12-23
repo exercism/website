4e13bc2e8532896e593c3fab3e0b3e9d97d1ec36
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:23:44 +0100

    Reduce nesting of github commands

--- app/commands/github/dispatch_event_to_org_wide_files_repo.rb
+++ app/commands/github/dispatch_event_to_org_wide_files_repo.rb
@@ -1,27 +1,25 @@
-module Github
-  class DispatchEventToOrgWideFilesRepo
-    include Mandate
+class Github::DispatchEventToOrgWideFilesRepo
+  include Mandate
 
-    initialize_with :event_type, :repos, :pusher_github_username
+  initialize_with :event_type, :repos, :pusher_github_username
 
-    def call
-      raise "Unsupported event type" unless EVENT_TYPES.include?(event_type)
+  def call
+    raise "Unsupported event type" unless EVENT_TYPES.include?(event_type)
 
-      Exercism.octokit_client.post("https://api.github.com/repos/exercism/org-wide-files/dispatches", body)
-    end
-
-    private
-    def body
-      {
-        event_type: event_type.to_s,
-        client_payload: {
-          repos:,
-          pusher: pusher_github_username
-        }
-      }.to_json
-    end
-
-    EVENT_TYPES = %i[repo_update].freeze
-    private_constant :EVENT_TYPES
+    Exercism.octokit_client.post("https://api.github.com/repos/exercism/org-wide-files/dispatches", body)
   end
+
+  private
+  def body
+    {
+      event_type: event_type.to_s,
+      client_payload: {
+        repos:,
+        pusher: pusher_github_username
+      }
+    }.to_json
+  end
+
+  EVENT_TYPES = %i[repo_update].freeze
+  private_constant :EVENT_TYPES
 end

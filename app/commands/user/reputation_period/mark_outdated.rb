f27393ee953084153b1639a86b7b8542ae230be5
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:12:48 +0100

    Reduce nesting of user reputation_period commands

--- app/commands/user/reputation_period/mark_outdated.rb
+++ app/commands/user/reputation_period/mark_outdated.rb
@@ -1,26 +1,24 @@
-class User::ReputationPeriod
-  class MarkOutdated
-    include Mandate
+class User::ReputationPeriod::MarkOutdated
+  include Mandate
 
-    initialize_with period: Mandate::NO_DEFAULT, earned_on: Mandate::NO_DEFAULT
+  initialize_with period: Mandate::NO_DEFAULT, earned_on: Mandate::NO_DEFAULT
 
-    def call
-      rows = User::ReputationToken.where(earned_on:).find_each.flat_map do |token|
-        args = { user_id: token.user_id, period: }
+  def call
+    rows = User::ReputationToken.where(earned_on:).find_each.flat_map do |token|
+      args = { user_id: token.user_id, period: }
 
-        # Mark rows for :any and the category as long as the category isn't publishing
-        categories = [:any]
-        categories << token.category unless token.category.to_sym == :publishing
+      # Mark rows for :any and the category as long as the category isn't publishing
+      categories = [:any]
+      categories << token.category unless token.category.to_sym == :publishing
 
-        categories.flat_map do |category|
-          [
-            { category:, about: :everything, track_id: 0, **args },
-            (token.track_id ? { category:, about: :track, track_id: token.track_id, **args } : nil)
-          ].compact
-        end
+      categories.flat_map do |category|
+        [
+          { category:, about: :everything, track_id: 0, **args },
+          (token.track_id ? { category:, about: :track, track_id: token.track_id, **args } : nil)
+        ].compact
       end
-
-      rows.uniq.each { |row| User::ReputationPeriod.where(row).update_all(dirty: true) }
     end
+
+    rows.uniq.each { |row| User::ReputationPeriod.where(row).update_all(dirty: true) }
   end
 end
--- app/commands/user/reputation_period/sweep.rb
+++ app/commands/user/reputation_period/sweep.rb
@@ -1,11 +1,9 @@
-class User::ReputationPeriod
-  class Sweep
-    include Mandate
+class User::ReputationPeriod::Sweep
+  include Mandate
 
-    def call
-      User::ReputationPeriod.dirty.find_each do |period|
-        User::ReputationPeriod::UpdateReputation.(period)
-      end
+  def call
+    User::ReputationPeriod.dirty.find_each do |period|
+      User::ReputationPeriod::UpdateReputation.(period)
     end
   end
 end

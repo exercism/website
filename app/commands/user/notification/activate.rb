6c77dfe48151f567cf46344a762813119c05dc65
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:11:20 +0100

    Reduce nesting of user notification commands

--- app/commands/user/notification/activate.rb
+++ app/commands/user/notification/activate.rb
@@ -1,16 +1,14 @@
-class User::Notification
-  class Activate
-    include Mandate
+class User::Notification::Activate
+  include Mandate
 
-    initialize_with :notification
+  initialize_with :notification
 
-    def call
-      notification.with_lock do
-        if notification.pending?
-          notification.update_column(:status, :unread)
-          NotificationsChannel.broadcast_changed!(notification.user)
-          User::Notification::SendEmail.(notification)
-        end
+  def call
+    notification.with_lock do
+      if notification.pending?
+        notification.update_column(:status, :unread)
+        NotificationsChannel.broadcast_changed!(notification.user)
+        User::Notification::SendEmail.(notification)
       end
     end
   end
--- app/commands/user/notification/mark_all_as_read.rb
+++ app/commands/user/notification/mark_all_as_read.rb
@@ -1,16 +1,14 @@
-class User::Notification
-  class MarkAllAsRead
-    include Mandate
+class User::Notification::MarkAllAsRead
+  include Mandate
 
-    initialize_with :user
+  initialize_with :user
 
-    def call
-      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
-        num_changed = user.notifications.pending_or_unread.
-          update_all(status: :read, read_at: Time.current)
+  def call
+    ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
+      num_changed = user.notifications.pending_or_unread.
+        update_all(status: :read, read_at: Time.current)
 
-        NotificationsChannel.broadcast_changed!(user) if num_changed.positive?
-      end
+      NotificationsChannel.broadcast_changed!(user) if num_changed.positive?
     end
   end
 end

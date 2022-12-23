6cb74f344a09c05d279c0becab0f9cd481b002fa
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:09:24 +0100

    Reduce nesting of user mailshot commands

--- app/commands/user/mailshot/send.rb
+++ app/commands/user/mailshot/send.rb
@@ -3,19 +3,17 @@
 # users.each do |user|
 #   User::Mailshot::Send.(user, :community_launch)
 # end
-class User::Mailshot
-  class Send
-    include Mandate
+class User::Mailshot::Send
+  include Mandate
 
-    initialize_with :user, :mailshot_id
+  initialize_with :user, :mailshot_id
 
-    def call
-      mailshot = User::Mailshot.create!(mailshot_id:, user_id: user.id)
-      User::SendEmail.(mailshot) do
-        MailshotsMailer.with(user:).send(mailshot_id).deliver_later
-      end
-    rescue ActiveRecord::RecordNotUnique
-      # We're ok with silently catching this to avoid duplicates
+  def call
+    mailshot = User::Mailshot.create!(mailshot_id:, user_id: user.id)
+    User::SendEmail.(mailshot) do
+      MailshotsMailer.with(user:).send(mailshot_id).deliver_later
     end
+  rescue ActiveRecord::RecordNotUnique
+    # We're ok with silently catching this to avoid duplicates
   end
 end

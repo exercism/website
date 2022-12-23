1cf270fb043a05f2e3c7c3edf057b0cac30f5c59
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 11:30:57 +0100

    Add system test for switching tracks on automation page

--- test/system/flows/mentor/supermentor_gives_feedback_to_representation_test.rb
+++ test/system/flows/mentor/supermentor_gives_feedback_to_representation_test.rb
@@ -37,6 +37,38 @@ module Flows
           end
         end
       end
+
+      test "mentor changes track on need feedback page" do
+        mentor = create :user, :supermentor
+
+        csharp = create :track, slug: :csharp, title: 'C#'
+        ruby = create :track, slug: :ruby, title: 'Ruby'
+
+        csharp_exercise = create :practice_exercise, :random_slug, track: csharp
+        ruby_exercise = create :practice_exercise, :random_slug, track: ruby
+
+        create :exercise_representation, exercise: csharp_exercise, feedback_type: nil, num_submissions: 3
+        create :exercise_representation, exercise: ruby_exercise, feedback_type: nil, num_submissions: 2
+
+        create :user_track_mentorship, :supermentor_frequency, user: mentor, track: csharp
+        create :user_track_mentorship, :supermentor_frequency, user: mentor, track: ruby
+
+        use_capybara_host do
+          sign_in!(mentor)
+          visit mentoring_automation_index_path
+
+          assert_text csharp_exercise.title
+          refute_text ruby_exercise.title
+
+          within(".automation-header") do
+            click_on "C#"
+            find("label", text: "Ruby").click
+          end
+
+          assert_text ruby_exercise.title
+          refute_text csharp_exercise.title
+        end
+      end
     end
   end
 end

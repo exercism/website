26ed22192d8d61279ae890cab348d73a07ae26ce
Erik Schierboom <erik_schierboom@hotmail.com>
2022-12-23 14:19:50 +0100

    Reduce nesting of tooling_job commands

--- app/commands/tooling_job/create.rb
+++ app/commands/tooling_job/create.rb
@@ -1,56 +1,54 @@
-module ToolingJob
-  class Create
-    include Mandate
+class ToolingJob::Create
+  include Mandate
 
-    initialize_with :submission, :type
+  initialize_with :submission, :type
 
-    def initialize(submission, type, git_sha: nil, run_in_background: false, context: {})
-      @submission = submission
-      @type = type.to_sym
-      @git_sha = git_sha || submission.git_sha
-      @run_in_background = !!run_in_background
-      @context = context
+  def initialize(submission, type, git_sha: nil, run_in_background: false, context: {})
+    @submission = submission
+    @type = type.to_sym
+    @git_sha = git_sha || submission.git_sha
+    @run_in_background = !!run_in_background
+    @context = context
+  end
+
+  def call
+    Exercism::ToolingJob.create!(
+      type,
+      submission.uuid,
+      solution.track.slug,
+      solution.exercise.slug,
+      run_in_background:,
+      source: {
+        submission_efs_root: submission.uuid,
+        submission_filepaths: submission.valid_filepaths,
+        exercise_git_repo: solution.track.slug,
+        exercise_git_sha: git_sha,
+        exercise_git_dir: exercise_repo.dir,
+        exercise_filepaths:
+      },
+      context:
+    )
+  end
+
+  private
+  attr_reader :submission, :git_sha, :type, :run_in_background, :context
+
+  memoize
+  delegate :solution, to: :submission
+
+  def exercise_filepaths
+    exercise_repo.tooling_filepaths.reject do |filepath|
+      submission.valid_filepaths.include?(filepath)
     end
+  end
 
-    def call
-      Exercism::ToolingJob.create!(
-        type,
-        submission.uuid,
-        solution.track.slug,
-        solution.exercise.slug,
-        run_in_background:,
-        source: {
-          submission_efs_root: submission.uuid,
-          submission_filepaths: submission.valid_filepaths,
-          exercise_git_repo: solution.track.slug,
-          exercise_git_sha: git_sha,
-          exercise_git_dir: exercise_repo.dir,
-          exercise_filepaths:
-        },
-        context:
-      )
-    end
-
-    private
-    attr_reader :submission, :git_sha, :type, :run_in_background, :context
-
-    memoize
-    delegate :solution, to: :submission
-
-    def exercise_filepaths
-      exercise_repo.tooling_filepaths.reject do |filepath|
-        submission.valid_filepaths.include?(filepath)
-      end
-    end
-
-    memoize
-    def exercise_repo
-      Git::Exercise.new(
-        solution.git_slug,
-        solution.git_type,
-        git_sha,
-        repo_url: solution.track.repo_url
-      )
-    end
+  memoize
+  def exercise_repo
+    Git::Exercise.new(
+      solution.git_slug,
+      solution.git_type,
+      git_sha,
+      repo_url: solution.track.repo_url
+    )
   end
 end

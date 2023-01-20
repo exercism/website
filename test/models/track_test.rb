ae917e0ab2bd54526ef949f495a3300600f14db7
Jeremy Walker <jez.walker@gmail.com>
2023-01-20 11:32:20 +0000

    Fix issue with exercise updates not triggering concept recount

--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -176,44 +176,6 @@ class TrackTest < ActiveSupport::TestCase
     assert_nil Track.for_repo("exercism/configlet")
   end
 
-  test "recache_num_exercises!" do
-    track = create :track
-    track.recache_num_exercises!
-    assert_equal 0, track.num_exercises
-
-    create :practice_exercise, track: track, status: :beta
-    assert_equal 1, track.num_exercises
-
-    create :practice_exercise, track: track, status: :active
-    assert_equal 2, track.reload.num_exercises
-
-    # Ignore wip practice exercises
-    create :practice_exercise, track: track, status: :wip
-    assert_equal 2, track.reload.num_exercises
-
-    # Ignore deprecated practice exercises
-    create :practice_exercise, track: track, status: :deprecated
-    assert_equal 2, track.reload.num_exercises
-
-    # Ignore concept exercises when the track does not have a course
-    track.update(course: false)
-    create :concept_exercise, track: track, status: :active
-    assert_equal 2, track.reload.num_exercises
-
-    # Include concept exercises when the track has a course
-    track.update(course: true)
-    create :concept_exercise, track: track, status: :beta
-    assert_equal 4, track.reload.num_exercises
-
-    # Ignore wip concept exercises
-    create :concept_exercise, track: track, status: :wip
-    assert_equal 4, track.reload.num_exercises
-
-    # Ignore deprecated concept exercises
-    create :concept_exercise, track: track, status: :deprecated
-    assert_equal 4, track.reload.num_exercises
-  end
-
   test "representations" do
     track = create :track
     exercise_1 = create :practice_exercise, track: track

827fe7aaf5e977016eb548db8d729536a6ea80ed
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-01 15:02:39 +0100

    Update test/factories/badges.rb

--- test/factories/badges.rb
+++ test/factories/badges.rb
@@ -11,7 +11,6 @@ FactoryBot.define do
     mentor researcher v3_pioneer tooling_pioneer
     moss begetter bard architect troubleshooter
     completer conceptual supermentor
-
     functional_february
   ].each do |type|
     factory "#{type}_badge", class: "Badges::#{type.to_s.camelize}Badge" do

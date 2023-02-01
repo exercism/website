25c062ad8aea637f89368d29578ea58c604b7d24
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-01 15:32:20 +0100

    Add gleam highlightjs support (#3411)
    [skip ci]

--- app/javascript/declarations/index.d.ts
+++ app/javascript/declarations/index.d.ts
@@ -74,3 +74,10 @@ declare module 'highlightjs-bqn' {
 
   export default setup
 }
+
+declare module 'highlightjs-gleam' {
+  import { LanguageFn } from 'highlight.js'
+  const setup: LanguageFn
+
+  export default setup
+}

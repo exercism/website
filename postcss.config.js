d88930efcff75462ca94364473a6c4bc3eb9c9f3
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2023-02-15 16:53:31 +0100

    Configure css nesting (#3473)
    [skip ci]

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss/nesting'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),

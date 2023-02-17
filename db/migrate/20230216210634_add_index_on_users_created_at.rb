77085d6e02e5462432dc4c3d2daf37d832ee72a5
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-17 13:48:05 +0100

    Update db/migrate/20230216210634_add_index_on_users_created_at.rb
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

--- db/migrate/20230216210634_add_index_on_users_created_at.rb
+++ db/migrate/20230216210634_add_index_on_users_created_at.rb
@@ -1,5 +1,7 @@
 class AddIndexOnUsersCreatedAt < ActiveRecord::Migration[7.0]
   def change
-    add_index :users, :created_at
+    unless Rails.env.production?
+      add_index :users, :created_at
+    end
   end
 end

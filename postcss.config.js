1983838fad38a2cd128d58c73d59ab7b2b82bb9d
Jeremy Walker <jez.walker@gmail.com>
2023-02-21 10:14:32 +0000

    Add prefer-color-scheme default (#3477)
    * Add prefer-color-scheme default
    
    * Add another postcss plugin
    
    ---------
    
    Co-authored-by: dem4ron <demaaron88@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
+    require('@sector-labs/postcss-inline-class')(),
     process.env.NODE_ENV === 'production'
       ? require('cssnano')({
           preset: 'default',

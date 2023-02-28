3e2dbc0eb390ef797a1d403a068571704766dd15
Erik Schierboom <erik_schierboom@hotmail.com>
2023-02-28 12:45:18 +0100

    Show selected testimonial

--- app/assemblers/assemble_profile_testimonials_list.rb
+++ app/assemblers/assemble_profile_testimonials_list.rb
@@ -3,9 +3,11 @@ class AssembleProfileTestimonialsList
 
   initialize_with :user, :params
 
-  def self.keys
-    %i[page]
-  end
+  # Use class method rather than constant for
+  # easier stubbing during testing
+  def self.testimonials_per_page = 64
+
+  def self.keys = %i[page]
 
   def call
     SerializePaginatedCollection.(
@@ -19,17 +21,21 @@ class AssembleProfileTestimonialsList
 
   memoize
   def paginated_testimonials
-    page = testimonials.order(id: :desc).page(params[:page]).per(1)
+    page = testimonials.order(id: :desc).page(params[:page]).per(self.class.testimonials_per_page)
     return page unless params[:uuid]
-    return page if page.map(&:uuid).include?(params[:uuid])
+
+    page_uuids = page.map(&:uuid)
+    return page if page_uuids.include?(params[:uuid])
 
     selected = user.mentor_testimonials.published.find_by(uuid: params[:uuid])
     return page unless selected
 
-    (page + [selected])
+    page_uuids.unshift(selected.uuid) # Show selected testimonial first
+
+    testimonials.where(uuid: page_uuids).
+      order(Arel.sql("FIND_IN_SET(uuid, '#{page_uuids.join(',')}')")).
+      page(page.current_page).per(page.size + 1)
   end
 
-  def testimonials
-    user.mentor_testimonials.published
-  end
+  def testimonials = user.mentor_testimonials.published
 end

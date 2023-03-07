d58d5f60a0fc6d20062721f681592b2d2a19f4e7
Jeremy Walker <jez.walker@gmail.com>
2023-03-07 18:40:06 +0000

    Allow websockets from non-users

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,5 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    delegate :current_user, to: :connection
   end
 end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,10 +1,15 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
+    attr_accessor :current_user
+
+    identified_by :connection_identifier
 
     def connect
       self.current_user = env["warden"].user(:user)
-      self.current_user || reject_unauthorized_connection
+    end
+
+    def connection_identifier
+      current_user || SecureRandom.uuid
     end
   end
 end

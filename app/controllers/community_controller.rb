68cad30803d679c12c2764913ffc91c0ef2fcad8
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-08 15:27:54 +0100

    Use User.supporter for community supporters

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -4,13 +4,11 @@ class CommunityController < ApplicationController
   def show
     # TODO: add caching
     @supporter_avatar_urls =
-      User::AcquiredBadge.joins(:user).
-        where(badge_id: Badge.find_by_slug!("supporter")). # rubocop:disable Rails/DynamicFindBy
-        where(users: { show_on_supporters_page: true }).
-        merge(User.with_attached_avatar).
+      User.supporter.
+        with_attached_avatar.
         where.not('users.avatar_url': nil).
-        last(40).
-        map { |b| b.user.avatar_url }
+        pluck(:avatar_url).
+        last(40)
 
     @community_stories = CommunityStory.published.includes(:interviewee).ordered_by_recency.first(3)
     @forum_threads = Forum::RetrieveThreads.(type: :top, count: 1)

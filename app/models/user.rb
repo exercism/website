852247a23e48ade123507d52a38e0681a98f0ca6
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-08 14:10:28 +0100

    Add scope for supporter and donor

--- app/models/user.rb
+++ app/models/user.rb
@@ -120,6 +120,9 @@ class User < ApplicationRecord
 
   scope :random, -> { order('RAND()') }
 
+  scope :donor, -> { where.not(first_donated_at: nil) }
+  scope :supporter, -> { donor.where(show_on_supporters_page: true) }
+
   # TODO: Validate presence of name
 
   validates :handle, uniqueness: { case_sensitive: false }, handle_format: true

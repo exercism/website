da2b696a99e4e2251aa7ee884c119279d5afea4b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-08 11:32:39 +0100

    Show support prompt based on donated field

---
+++ test/helpers/view_components/site_header_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class ViewComponents::HeaderTest < ActionView::TestCase
+  test "show support message when not logged in" do
+    site_header_component = ViewComponents::SiteHeader.new
+    site_header_component.stubs(namespace_name: "mentoring")
+
+    html = render(site_header_component)
+
+    assert_includes html, "Please support us if you can!"
+  end
+
+  test "show support message when logged in but not a donor" do
+    site_header_component = ViewComponents::SiteHeader.new
+    site_header_component.stubs(namespace_name: "mentoring")
+    user = create :user, donated: false
+    sign_in! user
+
+    html = render(site_header_component)
+
+    assert_includes html, "Please support us if you can!"
+  end
+
+  test "don't show support message when logged in and a donor" do
+    site_header_component = ViewComponents::SiteHeader.new
+    site_header_component.stubs(namespace_name: "mentoring")
+    user = create :user, donated: true
+    sign_in! user
+
+    html = render(site_header_component)
+
+    refute_includes html, "Please support us if you can!"
+  end
+end

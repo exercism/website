5297133d722c5320a772b013706add22e354005e
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-11 14:15:01 +0100

    Add syntax highlighting for Zig in editor
    Closes: https://github.com/exercism/exercism/issues/6670

--- app/javascript/components/misc/CodeMirror/languageCompartment.ts
+++ app/javascript/components/misc/CodeMirror/languageCompartment.ts
@@ -40,6 +40,10 @@ export const loadLanguageCompartment = async (
       const { wren } = await import('@exercism/codemirror-lang-wren')
       return compartment.of(wren())
     }
+    case 'zig': {
+      const { rust: zig } = await import('@codemirror/lang-rust')
+      return compartment.of(zig())
+    }
 
     // Legacy
     case 'abap': {

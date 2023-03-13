587353419ddb39856ba9b2037f1994cd3cdbfb8b
dem4ron <demaaron88@gmail.com>
2023-03-13 15:41:25 +0100

    Fix apos in test

--- test/controllers/tracks/mentor_discussions_controller_test.rb
+++ test/controllers/tracks/mentor_discussions_controller_test.rb
@@ -42,7 +42,7 @@ class Tracks::MentorDiscussionsControllerTest < ActionDispatch::IntegrationTest
     get track_exercise_mentor_discussions_url(solution.track, solution.exercise)
 
     assert_response :ok
-    assert_includes @response.body, "You’ve requested mentoring"
+    assert_includes @response.body, "You&apos;ve requested mentoring"
     assert_includes @response.body, "You have no past code review sessions"
   end
 

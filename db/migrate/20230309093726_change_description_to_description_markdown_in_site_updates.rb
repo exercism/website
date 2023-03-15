1ea52e4533f1cd48d327dbcb857df07e55f31f37
Jeremy Walker <jez.walker@gmail.com>
2023-03-15 15:16:01 +0000

    Various tweaks for maintaining (#3571)

--- db/migrate/20230309093726_change_description_to_description_markdown_in_site_updates.rb
+++ db/migrate/20230309093726_change_description_to_description_markdown_in_site_updates.rb
@@ -1,8 +1,11 @@
 class ChangeDescriptionToDescriptionMarkdownInSiteUpdates < ActiveRecord::Migration[7.0]
   def change
     unless Rails.env.production?
-      rename_column :site_updates, :description, :description_markdown
+      add_column :site_updates, :description_markdown, :text, null: true
       add_column :site_updates, :description_html, :text, null: true
+      change_column_null :site_updates, :description, true
+
+      SiteUpdate.update_all('description_markdown = description')
 
       SiteUpdate.where.not(description_markdown: nil).each do |site_update|
         site_update.update(description_html: Markdown::Parse.(site_update.description_markdown))
@@ -10,4 +13,4 @@ class ChangeDescriptionToDescriptionMarkdownInSiteUpdates < ActiveRecord::Migrat
     end
   end
 end
-  
+

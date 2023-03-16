8d92a568ecda49b58b3961b975b1d29c1d4829d1
Jeremy Walker <jez.walker@gmail.com>
2023-03-16 19:20:38 +0000

    Add setting of discord roles (#3585)
    * Add setting of discord roles
    
    * Add missing files
    
    * Bad rubocop
    
    * Bad rubocop
    
    * Disable annoying rule

---
+++ test/commands/user/set_discord_roles_test.rb
@@ -0,0 +1,46 @@
+require "test_helper"
+
+class User::SetDiscordRolesTest < ActiveSupport::TestCase
+  test "noops with normal user" do
+    uid = '111'
+    user = create :user, discord_uid: uid
+
+    RestClient.expects(:put).never
+
+    User::SetDiscordRoles.(user)
+  end
+
+  test "noops for non-discord user" do
+    user = create :user, :maintainer
+
+    RestClient.expects(:put).never
+
+    User::SetDiscordRoles.(user)
+  end
+
+  test "writes maintainer role" do
+    uid = SecureRandom.hex
+    user = create :user, :maintainer, discord_uid: uid
+
+    RestClient.expects(:put).with(
+      "https://discord.com/api/guilds/854117591135027261/members/#{uid}/roles/1085196376058646559",
+      {},
+      Authorization: "Bot #{Exercism.secrets.discord_bot_token}"
+    )
+
+    User::SetDiscordRoles.(user)
+  end
+
+  test "writes supermentor role" do
+    uid = SecureRandom.hex
+    user = create :user, :supermentor, discord_uid: uid
+
+    RestClient.expects(:put).with(
+      "https://discord.com/api/guilds/854117591135027261/members/#{uid}/roles/1085196488436633681",
+      {},
+      Authorization: "Bot #{Exercism.secrets.discord_bot_token}"
+    )
+
+    User::SetDiscordRoles.(user)
+  end
+end

219e7c1d20a9dac7aaf35eb9e9c9728ee15ae57a
Jeremy Walker <jez.walker@gmail.com>
2023-03-17 12:47:02 +0000

    Allow configurable log level (#3590)
    [skip ci]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :error
+  config.log_level = (ENV['RAILS_LOG_LEVEL'].presence || :error).to_sym
   config.colorize_logging = false
 
   # This uses too much memory in production

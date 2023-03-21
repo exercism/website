2d8134394432d2f4762e81f1d61b77865169ea1c
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-21 12:59:46 +0100

    Add chatterbox badge

---
+++ app/models/badges/discourser_badge.rb
@@ -0,0 +1,20 @@
+module Badges
+  class DiscourserBadge < Badge
+    seed "Discourser",
+      :common,
+      'discourser',
+      'Linked your Exercism account to the Forum'
+
+    def award_to?(user) = has_discourse_account?(user)
+
+    def send_email_on_acquisition? = false
+
+    private
+    def has_discourse_account?(user)
+      Exercism.discourse_client.by_external_id(user.id)
+      true
+    rescue DiscourseApi::NotFoundError
+      false
+    end
+  end
+end

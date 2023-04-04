62e9f2b3dffc28b4fbbec2216dd9ae5dd7c8e4e1
Jeremy Walker <jez.walker@gmail.com>
2023-04-04 15:17:14 +0100

    Show all tracks to staff

--- app/assemblers/assemble_representation_tracks_for_select.rb
+++ app/assemblers/assemble_representation_tracks_for_select.rb
@@ -14,7 +14,11 @@ class AssembleRepresentationTracksForSelect
   def tracks = supermentored_tracks.select { |track| track_num_representations.key?(track.id) }
 
   memoize
-  def supermentored_tracks = Track.where(id: mentor.track_mentorships.supermentor_frequency.select(:track_id)).order(title: :asc)
+  def supermentored_tracks
+    return Track.all if mentor.staff?
+
+    Track.where(id: mentor.track_mentorships.supermentor_frequency.select(:track_id)).order(title: :asc)
+  end
 
   memoize
   def track_num_representations

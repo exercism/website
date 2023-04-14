0d23fabdd2cc852700e735fda5dd93ec03cfb502
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-14 14:54:26 +0200

    Add status column to subscriptions

--- app/models/donations/subscription.rb
+++ app/models/donations/subscription.rb
@@ -3,6 +3,8 @@ class Donations::Subscription < ApplicationRecord
 
   scope :active, -> { where(active: true) }
 
+  enum status: { canceled: 0, overdue: 1, active: 2 }
+
   def amount_in_dollars
     amount_in_cents / BigDecimal(100)
   end
@@ -10,4 +12,6 @@ class Donations::Subscription < ApplicationRecord
   def self.active_total_per_month_in_dollars
     active.sum(:amount_in_cents) / BigDecimal(100)
   end
+
+  def status = super.to_sym
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_075814) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_124641) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -176,6 +176,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_075814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "email_status", limit: 1, default: 0, null: false
+    t.integer "status", limit: 1, default: 0, null: false
     t.index ["stripe_id"], name: "index_donations_subscriptions_on_stripe_id", unique: true
     t.index ["user_id"], name: "index_donations_subscriptions_on_user_id"
   end

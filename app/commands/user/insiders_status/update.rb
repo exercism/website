bd147c0810f25cc770a5fee4ee86310df1286db0
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-20 11:10:13 +0200

    Don't send insiders notifications in lock

--- app/commands/user/insiders_status/update.rb
+++ app/commands/user/insiders_status/update.rb
@@ -6,17 +6,13 @@ class User::InsidersStatus::Update
   initialize_with :user, :status_before_unset
 
   def call
-    eligibility_status = User::InsidersStatus::DetermineEligibilityStatus.(user)
-
-    user.with_lock do
-      case eligibility_status
-      when :eligible_lifetime
-        update_eligible_lifetime
-      when :eligible
-        update_eligible
-      when :ineligible
-        update_ineligible
-      end
+    case User::InsidersStatus::DetermineEligibilityStatus.(user)
+    when :eligible_lifetime
+      update_eligible_lifetime
+    when :eligible
+      update_eligible
+    when :ineligible
+      update_ineligible
     end
   end
 
@@ -24,12 +20,12 @@ class User::InsidersStatus::Update
   def update_eligible_lifetime
     case status_before_unset
     when :active_lifetime
-      user.update(insiders_status: :active_lifetime)
+      update_insiders_status(:active_lifetime)
     when :active
-      user.update(insiders_status: :active_lifetime)
+      update_insiders_status(:active_lifetime)
       User::Notification::Create.(user, :joined_lifetime_insiders) if FeatureFlag::INSIDERS
     else
-      user.update(insiders_status: :eligible_lifetime)
+      update_insiders_status(:eligible_lifetime)
       User::Notification::Create.(user, :join_lifetime_insiders) if FeatureFlag::INSIDERS && status_before_unset != :eligible_lifetime
     end
   end
@@ -37,13 +33,13 @@ class User::InsidersStatus::Update
   def update_eligible
     case status_before_unset
     when :active_lifetime
-      user.update(insiders_status: :active_lifetime)
+      update_insiders_status(:active_lifetime)
     when :eligible_lifetime
-      user.update(insiders_status: :eligible_lifetime)
+      update_insiders_status(:eligible_lifetime)
     when :active
-      user.update(insiders_status: :active)
+      update_insiders_status(:active)
     else
-      user.update(insiders_status: :eligible)
+      update_insiders_status(:eligible)
       User::Notification::Create.(user, :join_insiders) if FeatureFlag::INSIDERS && status_before_unset != :eligible
     end
   end
@@ -51,12 +47,18 @@ class User::InsidersStatus::Update
   def update_ineligible
     case status_before_unset
     when :active_lifetime
-      user.update(insiders_status: :active_lifetime)
+      update_insiders_status(:active_lifetime)
     when :eligible_lifetime
-      user.update(insiders_status: :eligible_lifetime)
+      update_insiders_status(:eligible_lifetime)
     else
-      user.update(insiders_status: :ineligible)
+      update_insiders_status(:ineligible)
       User::Notification::Create.(user, :expired_insiders) if FeatureFlag::INSIDERS && status_before_unset == :active
+    end
+  end
+
+  def update_insiders_status(insiders_status)
+    user.with_lock do
+      user.update(insiders_status:)
     end
   end
 end

ee0ac1a118086ab6079053d48a779c35e45630b7
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-21 12:42:33 +0200

    Fix tests

--- app/commands/donations/subscription/update_amount.rb
+++ app/commands/donations/subscription/update_amount.rb
@@ -1,7 +1,7 @@
 class Donations::Subscription::UpdateAmount
   include Mandate
 
-  initialize_with :subscription, amount_in_cents
+  initialize_with :subscription, :amount_in_cents
 
   def call = subscription.update!(amount_in_cents:)
 end

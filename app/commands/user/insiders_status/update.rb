363a951ad17d84ea11ae06f3dd89e19471386680
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-25 15:43:35 +0200

    Remove status_before_unset

--- app/commands/user/insiders_status/update.rb
+++ app/commands/user/insiders_status/update.rb
@@ -3,10 +3,16 @@ class User::InsidersStatus::Update
 
   queue_as :default
 
-  initialize_with :user, :status_before_unset
+  initialize_with :user
 
   def call
+    # Lifetime statuses would not change
+    return if user.insiders_status_active_lifetime?
+    return if user.insiders_status_eligible_lifetime?
+
     new_status = User::InsidersStatus::DetermineEligibilityStatus.(user)
+    return if new_status == user.insiders_status
+
     user.with_lock do
       case new_status
       when :eligible_lifetime
@@ -23,45 +29,25 @@ class User::InsidersStatus::Update
 
   private
   def update_eligible_lifetime
-    case status_before_unset
-    when :active_lifetime
-      update_insiders_status(:active_lifetime)
+    case user.insiders_status
     when :active
-      update_insiders_status(:active_lifetime)
       @notification_key = :joined_lifetime_insiders if FeatureFlag::INSIDERS
+      user.update(insiders_status: :active_lifetime)
     else
-      update_insiders_status(:eligible_lifetime)
-      @notification_key = :join_lifetime_insiders if FeatureFlag::INSIDERS && status_before_unset != :eligible_lifetime
+      @notification_key = :join_lifetime_insiders if FeatureFlag::INSIDERS
+      user.update(insiders_status: :eligible_lifetime)
     end
   end
 
   def update_eligible
-    case status_before_unset
-    when :active_lifetime
-      update_insiders_status(:active_lifetime)
-    when :eligible_lifetime
-      update_insiders_status(:eligible_lifetime)
-    when :active
-      update_insiders_status(:active)
-    else
-      update_insiders_status(:eligible)
-      @notification_key =:join_insiders if FeatureFlag::INSIDERS && status_before_unset != :eligible
-    end
+    return if user.insiders_status_active?
+
+    @notification_key = :join_insiders if FeatureFlag::INSIDERS
+    user.update(insiders_status: :eligible)
   end
 
   def update_ineligible
-    case status_before_unset
-    when :active_lifetime
-      update_insiders_status(:active_lifetime)
-    when :eligible_lifetime
-      update_insiders_status(:eligible_lifetime)
-    else
-      update_insiders_status(:ineligible)
-      @notification_key = :expired_insiders if FeatureFlag::INSIDERS && status_before_unset == :active
-    end
-  end
-
-  def update_insiders_status(insiders_status)
-    user.update(insiders_status:)
+    @notification_key = :expired_insiders if FeatureFlag::INSIDERS && user.insiders_status_active?
+    user.update(insiders_status: :ineligible)
   end
 end

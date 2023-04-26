870c7f698c3a8dbaabfb99f15415eebbe7300b76
Jeremy Walker <jez.walker@gmail.com>
2023-04-26 23:48:24 +0100

    Update db/migrate/20230426212831_add_solution_stars_index.rb

--- db/migrate/20230426212831_add_solution_stars_index.rb
+++ db/migrate/20230426212831_add_solution_stars_index.rb
@@ -1,6 +1,6 @@
 class AddSolutionStarsIndex < ActiveRecord::Migration[7.0]
   def change
-    return unless Rails.env.production?
+    return if Rails.env.production?
 
     add_index :solutions, [:exercise_id, :status, :num_stars], order: {exercise_id: :asc, status: :desc, num_stars: :desc}, name: "solutions_ex_stat_stars"
     remove_index :solutions, "index_solutions_on_exercise_id_and_published_at"

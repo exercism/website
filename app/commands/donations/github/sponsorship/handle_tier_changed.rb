ee2ce0291a2a44307539334bd3a99005e9b43350
Erik Schierboom <erik_schierboom@hotmail.com>
2023-04-26 08:32:59 +0200

    Fix tests

--- app/commands/donations/github/sponsorship/handle_tier_changed.rb
+++ app/commands/donations/github/sponsorship/handle_tier_changed.rb
@@ -3,7 +3,7 @@
 class Donations::Github::Sponsorship::HandleTierChanged
   include Mandate
 
-  initialize_with :user, :node_id, :privacy_level, :is_one_time, :monthly_price_in_cents
+  initialize_with :user, :node_id, :is_one_time, :monthly_price_in_cents
 
   def call
     return if is_one_time

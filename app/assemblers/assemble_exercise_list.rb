f563d6c6180775e5f4eece9d08a60253606383f0
Jeremy Walker <jez.walker@gmail.com>
2023-04-27 02:12:03 +0100

    Add comment to refactor exercises lookup

--- app/assemblers/assemble_exercise_list.rb
+++ app/assemblers/assemble_exercise_list.rb
@@ -19,6 +19,9 @@ class AssembleExerciseList < Assembler
   def solutions
     return [] unless user
 
+    # TODO: It's *MUCH* quicker to get all the exercises for the user and the track
+    # and then to filter out the ones we don't want, than it is to use IN (...) with ids.
+    # So refactor this to do that. And check for other places too.
     SerializeSolutions.(user.solutions.where(exercise_id: exercises), user)
   end
 

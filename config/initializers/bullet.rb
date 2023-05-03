59dc8716b01c388f1a0580b9526e0841e20e929a
Jeremy Walker <jez.walker@gmail.com>
2023-05-03 14:44:55 +0100

    Fix bullet issues

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -14,5 +14,7 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.unused_eager_loading_enable = false
     Bullet.counter_cache_enable = false
+
+    Bullet.add_safelist type: :n_plus_one_query, class_name: "User", association: :preferences
   end
 end

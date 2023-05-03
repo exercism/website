6fb58db420fc33ec4e5d3f81dbe12b6b93ba3f02
Jeremy Walker <jez.walker@gmail.com>
2023-05-03 14:55:56 +0100

    Fix test

--- test/commands/user/link_with_discord_test.rb
+++ test/commands/user/link_with_discord_test.rb
@@ -44,8 +44,7 @@ class User::LinkWithDiscordTest < ActiveSupport::TestCase
     auth = stub(uid:)
     user = create :user, discord_uid: nil
 
-    User::SetDiscordRoles.stubs(:call).with(user)
-    User::SetDiscourseGroups.stubs(:call).with(user)
+    User::SetDiscordRoles.expects(:defer).with(user)
 
     perform_enqueued_jobs do
       User::LinkWithDiscord.(user, auth)

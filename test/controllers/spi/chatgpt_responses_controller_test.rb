e86e76b6ee028f54321cf002b0c5a1ef25c79717
Jeremy Walker <jez.walker@gmail.com>
2023-05-04 22:37:59 +0100

    Fix test

--- test/controllers/spi/chatgpt_responses_controller_test.rb
+++ test/controllers/spi/chatgpt_responses_controller_test.rb
@@ -4,14 +4,16 @@ class SPI::ChatGPTResponsesControllerTest < ActionDispatch::IntegrationTest
   test "proxies to process help request correctly" do
     submission = create :submission
     chatgpt_response = "Something #{SecureRandom.uuid}"
+    chatgpt_version = '4.0'
 
     Submission::AI::ChatGPT::ProcessHelpRequest.expects(:call).with(
-      submission, chatgpt_response
+      submission, chatgpt_version, chatgpt_response
     )
 
     post spi_chatgpt_responses_url(
       submission_uuid: submission.uuid,
       type: 'help',
+      chatgpt_version:,
       chatgpt_response:
     )
   end

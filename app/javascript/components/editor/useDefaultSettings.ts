eac764d5b4dad0d5340ba65933547051f1b369f5
dem4ron <demaaron88@gmail.com>
2023-05-04 20:07:19 +0200

    Fix editor

--- app/javascript/components/editor/useDefaultSettings.ts
+++ app/javascript/components/editor/useDefaultSettings.ts
@@ -1,27 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Keybindings, Themes, EditorSettings } from './types'
 
+function getThemeSetting() {
+  const body = document.querySelector('body')
+  const prefersDark =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  if (!body) return
+
+  const currentTheme = body.classList.value.match(/theme-\S+/)?.[0]
+  if (
+    (currentTheme === 'theme-system' && prefersDark) ||
+    currentTheme === 'theme-dark'
+  ) {
+    return Themes.DARK
+  } else return Themes.LIGHT
+}
+
 export const useDefaultSettings = (settings: Partial<EditorSettings>) => {
-  const [theme, setTheme] = useState<Themes>()
-  useEffect(() => {
-    const body = document.querySelector('body')
-    const prefersDark =
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    if (!body) return
-
-    const currentTheme = body.classList.value.match(/theme-\S+/)?.[0]
-    if (
-      (currentTheme === 'theme-system' && prefersDark) ||
-      currentTheme === 'theme-dark'
-    ) {
-      setTheme(Themes.DARK)
-    }
-    setTheme(Themes.LIGHT)
-  }, [])
-
   return useState<EditorSettings>({
-    theme: theme!,
+    theme: getThemeSetting()!,
     wrap: 'on',
     tabBehavior: 'captured',
     keybindings: Keybindings.DEFAULT,

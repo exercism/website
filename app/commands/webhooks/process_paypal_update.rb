f4d50b5cfcf17527881503a80c4c7150ff02b4d8
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-10 15:36:34 +0200

    Convert to paypal IPN

--- app/commands/webhooks/process_paypal_update.rb
+++ app/commands/webhooks/process_paypal_update.rb
@@ -1,98 +1,21 @@
 class Webhooks::ProcessPaypalUpdate
   include Mandate
 
-  initialize_with :event_type, :resource
+  initialize_with :payload
 
   def call
-    Webhooks::Paypal::Debug.(
-      <<~MESSAGE
-        [Webhooks::ProcessPaypalUpdate-1]
-          Event type: #{event_type}
-          Resource: #{resource.to_json}
-      MESSAGE
-    )
+    Webhooks::Paypal::Debug.("PAYLOAD: #{payload}")
 
-    case event_type
-    when "BILLING.SUBSCRIPTION.ACTIVATED", "BILLING.SUBSCRIPTION.RE-ACTIVATED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-2]
-            Donations::Paypal::Subscription::HandleActivated.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Subscription::HandleActivated.(resource)
-    when "BILLING.SUBSCRIPTION.CANCELLED", "BILLING.SUBSCRIPTION.EXPIRED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-3]
-            Donations::Paypal::Subscription::HandleCancelled.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Subscription::HandleCancelled.(resource)
-    when "BILLING.SUBSCRIPTION.CREATED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-4]
-            Donations::Paypal::Subscription::HandleCreated.(resource)#{' '}
-        MESSAGE
-      )
-      Donations::Paypal::Subscription::HandleCreated.(resource)
-    when "BILLING.SUBSCRIPTION.PAYMENT.FAILED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-5]
-            Donations::Paypal::Subscription::HandlePaymentFailed.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Subscription::HandlePaymentFailed.(resource)
-    when "BILLING.SUBSCRIPTION.UPDATED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-6]
-            Donations::Paypal::Subscription::HandleUpdated.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Subscription::HandleUpdated.(resource)
-    when "PAYMENT.SALE.COMPLETED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-7]
-            Donations::Paypal::Payment::HandleSaleCompleted.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Payment::HandleSaleCompleted.(resource)
-    when "PAYMENT.SALE.DENIED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-8]
-            Donations::Paypal::Payment::HandleSaleDenied.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Payment::HandleSaleDenied.(resource)
-    when "PAYMENT.SALE.REFUNDED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-9]
-            Donations::Paypal::Payment::HandleSaleRefunded.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Payment::HandleSaleRefunded.(resource)
-    when "PAYMENT.SALE.REVERSED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-10]
-            Donations::Paypal::Payment::HandleSaleReversed.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Payment::HandleSaleReversed.(resource)
-    when "PAYMENTS.PAYMENT.CREATED"
-      Webhooks::Paypal::Debug.(
-        <<~MESSAGE
-          [Webhooks::ProcessPaypalUpdate-11]
-            Donations::Paypal::Payment::HandlePaymentCreated.(resource)
-        MESSAGE
-      )
-      Donations::Paypal::Payment::HandlePaymentCreated.(resource)
+    response = RestClient.post('https://ipnpb.paypal.com/cgi-bin/webscr', "cmd=_notify-validate&#{payload}")
+    case response.body
+    when "VERIFIED"
+      Webhooks::Paypal::Debug.("VERIFIED")
+      # TODO: handle verified
+    when "INVALID"
+      Webhooks::Paypal::Debug.("INVALID")
+      # TODO: handle invalid
+    else
+      Webhooks::Paypal::Debug.("UNKNOWN")
     end
   end
 end

368bec4d5f6037cb279d46b009c4c1649c17c330
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-10 16:14:45 +0200

    Convert to paypal IPN (#5423)

--- test/commands/webhooks/process_paypal_update_test.rb
+++ test/commands/webhooks/process_paypal_update_test.rb
@@ -1,43 +1,4 @@
 require "test_helper"
 
 class Webhooks::ProcessPaypalUpdateTest < ActiveSupport::TestCase
-  [
-    ["BILLING.SUBSCRIPTION.ACTIVATED", Donations::Paypal::Subscription::HandleActivated],
-    ["BILLING.SUBSCRIPTION.CANCELLED", Donations::Paypal::Subscription::HandleCancelled],
-    ["BILLING.SUBSCRIPTION.CREATED", Donations::Paypal::Subscription::HandleCreated],
-    ["BILLING.SUBSCRIPTION.EXPIRED", Donations::Paypal::Subscription::HandleCancelled],
-    ["BILLING.SUBSCRIPTION.PAYMENT.FAILED", Donations::Paypal::Subscription::HandlePaymentFailed],
-    ["BILLING.SUBSCRIPTION.RE-ACTIVATED", Donations::Paypal::Subscription::HandleActivated],
-    ["BILLING.SUBSCRIPTION.UPDATED", Donations::Paypal::Subscription::HandleUpdated],
-    ["PAYMENT.SALE.COMPLETED", Donations::Paypal::Payment::HandleSaleCompleted],
-    ["PAYMENT.SALE.DENIED", Donations::Paypal::Payment::HandleSaleDenied],
-    ["PAYMENT.SALE.REFUNDED", Donations::Paypal::Payment::HandleSaleRefunded],
-    ["PAYMENT.SALE.REVERSED", Donations::Paypal::Payment::HandleSaleReversed],
-    ["PAYMENTS.PAYMENT.CREATED", Donations::Paypal::Payment::HandlePaymentCreated]
-  ].each do |event_type, expected_command|
-    test "process '#{event_type}' enqueues #{expected_command} job" do
-      resource = { amount: 500 }
-      expected_command.expects(:call).with(resource)
-
-      Webhooks::ProcessPaypalUpdate.(event_type, resource)
-    end
-  end
-
-  ignored_event_types = ['CHECKOUT.ORDER.APPROVED', 'RISK.DISPUTE.CREATED', 'VAULT.CREDIT-CARD.CREATED']
-  ignored_event_types.each do |invalid_event_type|
-    test "should ignore '#{invalid_event_type}' event" do
-      Donations::Paypal::Subscription::HandleActivated.expects(:call).never
-      Donations::Paypal::Subscription::HandleCancelled.expects(:call).never
-      Donations::Paypal::Subscription::HandleCreated.expects(:call).never
-      Donations::Paypal::Subscription::HandlePaymentFailed.expects(:call).never
-      Donations::Paypal::Subscription::HandleUpdated.expects(:call).never
-      Donations::Paypal::Payment::HandleSaleCompleted.expects(:call).never
-      Donations::Paypal::Payment::HandleSaleDenied.expects(:call).never
-      Donations::Paypal::Payment::HandleSaleRefunded.expects(:call).never
-      Donations::Paypal::Payment::HandleSaleReversed.expects(:call).never
-      Donations::Paypal::Payment::HandlePaymentCreated.expects(:call).never
-
-      Webhooks::ProcessPaypalUpdate.(invalid_event_type, {})
-    end
-  end
 end
--- test/controllers/webhooks/paypal_controller_test.rb
+++ test/controllers/webhooks/paypal_controller_test.rb
@@ -1,21 +1,12 @@
 require_relative './base_test_case'
 
 class Webhooks::PaypalControllerTest < Webhooks::BaseTestCase
-  test "create should return 403 when signature is invalid" do
-    payload = {
-      event_type: "BILLING.SUBSCRIPTION.ACTIVATED",
-      resource: {
-        quantity: "20"
-      }
-    }
-
-    Webhooks::Paypal::VerifySignature.expects(:call).raises(Webhooks::Paypal::VerifySignature::SignatureVerificationError)
-
-    post webhooks_paypal_path, as: :json, params: payload
-    assert_response :forbidden
+  test "create should return 200" do
+    post webhooks_paypal_path, as: :json
+    assert_response :ok
   end
 
-  test "create should return 200 when signature is valid" do
+  test "create should process payload" do
     payload = {
       event_type: "BILLING.SUBSCRIPTION.ACTIVATED",
       resource: {
@@ -23,14 +14,8 @@ class Webhooks::PaypalControllerTest < Webhooks::BaseTestCase
       }
     }
 
-    Webhooks::Paypal::VerifySignature.stubs(:call)
-
-    Webhooks::ProcessPaypalUpdate.expects(:call).with(
-      "BILLING.SUBSCRIPTION.ACTIVATED",
-      { "quantity" => "20" }
-    )
+    Webhooks::ProcessPaypalUpdate.expects(:defer).with(payload.to_json)
 
     post webhooks_paypal_path, headers: headers(payload), as: :json, params: payload
-    assert_response :ok
   end
 end

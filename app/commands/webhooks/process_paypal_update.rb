6575b07a2156d1ebe5f8677dc3931c1286807b9a
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-11 07:56:47 +0200

    Add error handlign to processing of paypal update

--- app/commands/webhooks/process_paypal_update.rb
+++ app/commands/webhooks/process_paypal_update.rb
@@ -1,21 +1,52 @@
 class Webhooks::ProcessPaypalUpdate
   include Mandate
 
+  class PaypalInvalidIpnError < RuntimeError; end
+  class PaypalIpnVerificationError < RuntimeError; end
+
   initialize_with :payload
 
   def call
     Webhooks::Paypal::Debug.("PAYLOAD: #{payload}")
 
-    response = RestClient.post('https://ipnpb.paypal.com/cgi-bin/webscr', "cmd=_notify-validate&#{payload}")
-    case response.body
+    case ipn_verification_status
     when "VERIFIED"
-      Webhooks::Paypal::Debug.("VERIFIED")
-      # TODO: handle verified
+      handle_verified
     when "INVALID"
-      Webhooks::Paypal::Debug.("INVALID")
-      # TODO: handle invalid
+      handle_invalid
     else
-      Webhooks::Paypal::Debug.("UNKNOWN")
+      handle_error
     end
+  rescue StandardError => e
+    Bugsnag.notify(e)
   end
+
+  private
+  memoize
+  def ipn_verification_status
+    response = RestClient.post(IPN_VERIFICATION_URL, ipn_verification_body)
+    response.body
+  end
+
+  def handle_verified
+    Webhooks::Paypal::Debug.("VERIFIED")
+    # TODO: handle verified
+
+    # params = Rack::Utils.parse_nested_query(payload)
+  end
+
+  def handle_invalid
+    Webhooks::Paypal::Debug.("INVALID")
+    raise PaypalInvalidIpnError
+  end
+
+  def handle_error
+    Webhooks::Paypal::Debug.("ERROR")
+    raise PaypalIpnVerificationError
+  end
+
+  def ipn_verification_body = "cmd=_notify-validate&#{payload}"
+
+  IPN_VERIFICATION_URL = 'https://ipnpb.paypal.com/cgi-bin/webscr'.freeze
+  private_constant :IPN_VERIFICATION_URL
 end
--- test/commands/webhooks/process_paypal_update_test.rb
+++ test/commands/webhooks/process_paypal_update_test.rb
@@ -1,4 +1,36 @@
 require "test_helper"
 
 class Webhooks::ProcessPaypalUpdateTest < ActiveSupport::TestCase
+  test "bugsnag is created when IPN is invalid" do
+    payload = "txn_id=5E125564UE4492634&txn_type=web_accept"
+
+    stub_request(:post, "https://ipnpb.paypal.com/cgi-bin/webscr").
+      to_return(status: 200, body: "INVALID", headers: {})
+
+    Bugsnag.expects(:notify).once
+
+    Webhooks::ProcessPaypalUpdate.(payload)
+  end
+
+  test "bugsnag is created when IPN verification has unknown result" do
+    payload = "txn_id=5E125564UE4492634&txn_type=web_accept"
+
+    stub_request(:post, "https://ipnpb.paypal.com/cgi-bin/webscr").
+      to_return(status: 200, body: "UNKNOWN", headers: {})
+
+    Bugsnag.expects(:notify).once
+
+    Webhooks::ProcessPaypalUpdate.(payload)
+  end
+
+  test "bugsnag is created when error occurs in verification request" do
+    payload = "txn_id=5E125564UE4492634&txn_type=web_accept"
+
+    stub_request(:post, "https://ipnpb.paypal.com/cgi-bin/webscr").
+      to_return(status: 500, body: "ERROR", headers: {})
+
+    Bugsnag.expects(:notify).once
+
+    Webhooks::ProcessPaypalUpdate.(payload)
+  end
 end

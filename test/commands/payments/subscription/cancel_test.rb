45a71c630cb791ef3b3f1df22f2c83ef388ccab4
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-17 16:24:05 +0200

    Update premium status when subscription is cancelled

--- test/commands/payments/subscription/cancel_test.rb
+++ test/commands/payments/subscription/cancel_test.rb
@@ -11,12 +11,40 @@ class Payments::Subscription::CancelTest < Payments::TestBase
     refute user.active_donation_subscription?
   end
 
-  test "triggers insiders_status update" do
+  test "updates insiders status when product is donation" do
     subscription_id = SecureRandom.uuid
     user = create :user, active_donation_subscription: true
-    subscription = create :payments_subscription, status: :active, user:, external_id: subscription_id
+    subscription = create :payments_subscription, status: :active, product: :donation, user:, external_id: subscription_id
 
-    User::InsidersStatus::TriggerUpdate.expects(:call).with(user).at_least_once
+    User::InsidersStatus::TriggerUpdate.expects(:call).with(user).once
+
+    Payments::Subscription::Cancel.(subscription)
+  end
+
+  test "does not update insiders status when product is premium" do
+    subscription_id = SecureRandom.uuid
+    user = create :user, active_donation_subscription: true
+    subscription = create :payments_subscription, status: :active, product: :premium, user:, external_id: subscription_id
+
+    User::InsidersStatus::TriggerUpdate.expects(:call).with(user).never
+
+    Payments::Subscription::Cancel.(subscription)
+  end
+
+  test "updates premium status when product is premium" do
+    user = create :user
+    subscription = create(:payments_subscription, product: :premium, user:)
+
+    User::Premium::Update.expects(:call).with(user).once
+
+    Payments::Subscription::Cancel.(subscription)
+  end
+
+  test "does not update premium status when product is donation" do
+    user = create :user
+    subscription = create(:payments_subscription, product: :donation, user:)
+
+    User::Premium::Update.expects(:call).with(user).never
 
     Payments::Subscription::Cancel.(subscription)
   end

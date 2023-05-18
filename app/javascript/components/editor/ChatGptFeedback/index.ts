cd3e6bd761299240541598fc8c4e3969f0454f84
dem4ron <demaaron88@gmail.com>
2023-05-18 15:19:30 +0200

    Tweak ChatGPT button behaviour

--- app/javascript/components/editor/ChatGptFeedback/index.ts
+++ app/javascript/components/editor/ChatGptFeedback/index.ts
@@ -1,5 +1,6 @@
 export { ChatGptTab as Tab } from './ChatGptTab'
 export { ChatGptPanel as Panel } from './ChatGptPanel'
+export { ChatGptUpsellPanel as UpsellPanel } from './ChatGptUpsellPanel'
 export { AskChatGptButton as Button } from './AskChatGptButton'
 export { useChatGptFeedback as Hook } from './useChatGptFeedback'
 export {

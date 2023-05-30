689641a5a9ee38d7e0b7952ff67239203f6a17cb
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-30 12:56:35 +0200

    Fix nil error

--- app/helpers/react_components/donations/premium_subscription_form.rb
+++ app/helpers/react_components/donations/premium_subscription_form.rb
@@ -18,7 +18,7 @@ class ReactComponents::Donations::PremiumSubscriptionForm < ReactComponents::Rea
     Exercism::Routes.cancel_api_payments_subscription_url(current_user.current_active_premium_subscription)
   end
 
-  def donation_attributes = current_user.current_active_donation_subscription.attributes.slice("provider", "amount_in_cents",
+  def donation_attributes = current_user.current_active_premium_subscription.attributes.slice("provider", "amount_in_cents",
     "interval")
   # TODO: add update link
 end

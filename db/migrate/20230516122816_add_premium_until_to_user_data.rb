ae54863ac106e8bf979b7d147c232a63d3e12419
Jeremy Walker <jez.walker@gmail.com>
2023-05-30 15:28:04 +0100

    Update db/migrate/20230516122816_add_premium_until_to_user_data.rb

--- db/migrate/20230516122816_add_premium_until_to_user_data.rb
+++ db/migrate/20230516122816_add_premium_until_to_user_data.rb
@@ -3,6 +3,6 @@ class AddPremiumUntilToUserData < ActiveRecord::Migration[7.0]
     return if Rails.env.production?
 
     add_column :user_data, :premium_until, :datetime, null: true
-    add_index :user_data, [:premium_until], if_not_exists: true
+    add_index :user_data, :premium_until, if_not_exists: true
   end
 end

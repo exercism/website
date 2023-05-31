6026b3503d8797b52669bdcb6dcd51c5ec868eeb
dem4ron <demaaron88@gmail.com>
2023-05-31 13:23:36 +0200

    Format tests, add one more case

--- test/system/flows/settings/user_changes_theme_preferences.rb
+++ test/system/flows/settings/user_changes_theme_preferences.rb
@@ -12,16 +12,61 @@ module Flows
         use_capybara_host do
           sign_in!(user)
           visit user_preferences_settings_path
+
           find("#dark-theme").click
           assert page.has_css?('body.theme-dark')
+
           find("#system-theme").click
           assert page.has_css?('body.theme-system')
+
           find("#sepia-theme").click
           assert page.has_css?('body.theme-sepia')
+
           find("#light-theme").click
           assert page.has_css?('body.theme-light')
 
           find("button.toggle-button").click
+          assert page.has_css?('body.theme-dark')
+        end
+      end
+
+      test "lifetime insider user updates theme preferences" do
+        user = create :user, premium_until: Time.current + 2.days
+
+        use_capybara_host do
+          sign_in!(user)
+          visit user_preferences_settings_path
+
+          find("#dark-theme").click
+          assert page.has_css?('body.theme-dark')
+
+          find("#system-theme").click
+          assert page.has_css?('body.theme-system')
+
+          find("#sepia-theme").click
+          assert page.has_css?('body.theme-sepia')
+
+          find("#light-theme").click
+          assert page.has_css?('body.theme-light')
+
+          find("button.toggle-button").click
+          assert page.has_css?('body.theme-dark')
+        end
+      end
+
+      test "lifetime insider user updates theme preferences and it persists" do
+        user = create :user, premium_until: Time.current + 2.days
+
+        use_capybara_host do
+          sign_in!(user)
+          visit user_preferences_settings_path
+
+          find("#dark-theme").click
+          assert page.has_css?('body.theme-dark')
+          # 1 second debounce
+          sleep 1
+          visit dashboard_path
+
           assert page.has_css?('body.theme-dark')
         end
       end
@@ -37,6 +82,7 @@ module Flows
 
           find("#sepia-theme").click
           assert page.has_css?('body.theme-sepia')
+
           find("#light-theme").click
           assert page.has_css?('body.theme-light')
 

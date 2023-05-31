686da44657341ad0110c16cd166c117bd93374b9
Erik Schierboom <erik_schierboom@hotmail.com>
2023-05-31 14:10:19 +0200

    Reset chatgpt usage for premium users

--- app/commands/user/reset_usage.rb
+++ app/commands/user/reset_usage.rb
@@ -1,12 +1,11 @@
-class User::IncrementUsage
+class User::ResetUsage
   include Mandate
 
   initialize_with :user, :group, :metric
 
   def call
     user.usages[group.to_s] ||= {}
-    user.usages[group.to_s][metric] ||= 0
-    user.usages[group.to_s][metric] += 1
+    user.usages[group.to_s][metric] = nil
     user.save!
   end
 end

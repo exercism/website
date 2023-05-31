883adc8f8f202f90c86f7553b6fc99cecdfb07e3
Jeremy Walker <jez.walker@gmail.com>
2023-05-31 13:04:58 +0100

    Add extra community preferences

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_130106) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_120146) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -1026,6 +1026,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_130106) do
     t.boolean "email_on_automated_feedback_added_notification", default: true, null: false
     t.boolean "email_about_fundraising_campaigns", default: true, null: false
     t.boolean "email_about_events", default: true, null: false
+    t.boolean "email_about_premium", default: true, null: false
+    t.boolean "email_about_insiders", default: true, null: false
     t.index ["token"], name: "index_user_communication_preferences_on_token", unique: true
     t.index ["user_id"], name: "index_user_communication_preferences_on_user_id"
   end

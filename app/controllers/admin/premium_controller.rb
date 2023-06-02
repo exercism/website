b4c76ef41572fc76acf79b01ae4aff5d1c28440f
Jeremy Walker <jez.walker@gmail.com>
2023-06-02 19:52:30 +0100

    Update dodgy SQL

--- app/controllers/admin/premium_controller.rb
+++ app/controllers/admin/premium_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PremiumController < Admin::BaseController
   def index
     @num_subscribers = Payments::Payment.premium.joins(user: :data).
-      where.not("JSON_CONTAINS(user_data.roles, ?, '$')", %("admin")).
+      where("user_data.roles IS NULL OR NOT JSON_CONTAINS(user_data.roles, ?, '$')", %("admin")).
       select(:user_id).distinct.count
   end
 end

8f2dba39e9d5e7369c561f620138bd984dccee16
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-02 07:37:58 +0200

    Fix react component

--- app/helpers/react_components/premium/price_option.rb
+++ app/helpers/react_components/premium/price_option.rb
@@ -18,7 +18,7 @@ module ReactComponents
       def data_for_monthly
         {
           period: 'month',
-          display_amount: Premium::MONTH_AMOUNT_IN_DOLLARS,
+          display_amount: ::Premium::MONTH_AMOUNT_IN_DOLLARS,
           payment_intent_type: 'premium_monthly_subscription',
           paypal_link: 'https://www.paypal.com/webapps/billing/plans/subscribe?plan_id=P-0EU999217Y297052UMRTXYEQ'
         }
@@ -27,7 +27,7 @@ module ReactComponents
       def data_for_yearly
         {
           period: 'year',
-          display_amount: Premium::YEAR_AMOUNT_IN_DOLLARS,
+          display_amount: ::Premium::YEAR_AMOUNT_IN_DOLLARS,
           payment_intent_type: 'premium_yearly_subscription',
           paypal_link: 'https://www.paypal.com/webapps/billing/plans/subscribe?plan_id=P-23J11871PJ838224HMRTXYTA'
         }
@@ -36,7 +36,7 @@ module ReactComponents
       def data_for_lifetime
         {
           period: 'lifetime',
-          display_amount: Premium::LIFETIME_AMOUNT_IN_DOLLARS.to_i,
+          display_amount: ::Premium::LIFETIME_AMOUNT_IN_DOLLARS.to_i,
           payment_intent_type: 'payment',
           paypal_link: "https://www.paypal.com/donate/?hosted_button_id=W898BNJR5JPDJ&custom=#{current_user.email}"
         }

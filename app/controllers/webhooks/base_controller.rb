53828f9ed0fbc45aab97b354d37ac9371c244dc2
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-06 16:29:29 +0200

    Skip filters around webhook actions

--- app/controllers/webhooks/base_controller.rb
+++ app/controllers/webhooks/base_controller.rb
@@ -9,6 +9,8 @@ module Webhooks
   class BaseController < ApplicationController
     skip_before_action :verify_authenticity_token
     skip_before_action :authenticate_user!
+    skip_before_action :set_body_class_header
+    skip_around_action :mark_notifications_as_read!
 
     layout false
 

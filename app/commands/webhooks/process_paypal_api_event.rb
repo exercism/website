539c816f20cf3a2f01d6658021b7c1d8a0fef9d1
Erik Schierboom <erik_schierboom@hotmail.com>
2023-06-06 13:47:15 +0200

    Add leading newline

--- app/commands/webhooks/process_paypal_api_event.rb
+++ app/commands/webhooks/process_paypal_api_event.rb
@@ -4,7 +4,7 @@ class Webhooks::ProcessPaypalAPIEvent
   initialize_with :payload, :headers
 
   def call
-    Payments::Paypal::Debug.("[Webhook] Headers:\n#{headers}")
+    Payments::Paypal::Debug.("\n[Webhook] Headers:\n#{headers}")
     Payments::Paypal::Debug.("[Webhook] Payload:\n#{payload}")
     Payments::Paypal::VerifyAPIEvent.(payload, headers)
 

79ae5df9a90142672a78a2c1a02779032ff84f9b
Jeremy Walker <jez.walker@gmail.com>
2023-06-07 19:32:58 +0100

    Rails is fine with nil ids

--- app/commands/payments/paypal/customer/find_or_update.rb
+++ app/commands/payments/paypal/customer/find_or_update.rb
@@ -14,6 +14,6 @@ class Payments::Paypal::Customer::FindOrUpdate
   def user_by_paypal_id = User.with_data.find_by(data: { paypal_payer_id: })
 
   def user = user_by_id || user_by_email
-  def user_by_id = user_id ? User.find_by(id: user_id) : nil
+  def user_by_id = User.find_by(id: user_id)
   def user_by_email = User.find_by(email: paypal_payer_email)
 end

61a5cca5adc58f9c0f233bc22e959719ace9368f
Jeremy Walker <jez.walker@gmail.com>
2023-06-17 15:26:53 +0100

    Split SMTP into transactional and bulk (#5581)
    * Use different SMTP credentials for bulk and transactional emails
    
    * Add seperate sending domains
    
    * Fix test

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   prepend_view_path "app/views/mailers"
-  # TODO: Change to exercism.org when moving to AWS SES
-  default from: "The Exercism Team <hello@mail.exercism.io>", reply_to: "hello@exercism.io"
+  default reply_to: "jonathan@exercism.org"
 
   layout "mailer"
   helper :email
@@ -14,6 +13,43 @@ class ApplicationMailer < ActionMailer::Base
     email_address_with_name(user.email, name)
   end
 
+  def transactional_mail(*args, **kwargs)
+    delivery_options = {
+      user_name: Exercism.secrets.transactional_smtp_username,
+      password: Exercism.secrets.transactional_smtp_password,
+      address: Exercism.secrets.transactional_smtp_address,
+      port: Exercism.secrets.transactional_smtp_port,
+      authentication: Exercism.secrets.transactional_smtp_authentication,
+      enable_starttls_auto: true
+    }
+    mail_to_user(
+      *args,
+      **kwargs.merge(
+        from: "Exercism <hello@#{Exercism.secrets.transactional_smtp_sending_domain}>",
+        delivery_options:
+      )
+    )
+  end
+
+  def bulk_mail(*args, **kwargs)
+    delivery_options = {
+      user_name: Exercism.secrets.bulk_smtp_username,
+      password: Exercism.secrets.bulk_smtp_password,
+      address: Exercism.secrets.bulk_smtp_address,
+      domain: Exercism.secrets.bulk_smtp_address,
+      port: Exercism.secrets.bulk_smtp_port,
+      authentication: Exercism.secrets.bulk_smtp_authentication
+    }
+    mail_to_user(
+      *args,
+      **kwargs.merge(
+        from: "Jeremy from Exercism <hello@#{Exercism.secrets.bulk_smtp_sending_domain}>",
+        delivery_options:
+      )
+    )
+  end
+
+  private
   def mail_to_user(user, subject, **options)
     return unless user.may_receive_emails?
 

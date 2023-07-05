bb05a3caeb8ba4c4f3807a1855874ff47d3418cd
Jeremy Walker <jez.walker@gmail.com>
2023-07-05 13:57:03 +0100

    Fix mismatch of logo and light logo (#5661)

--- app/models/partner/advert.rb
+++ app/models/partner/advert.rb
@@ -1,6 +1,7 @@
 class Partner::Advert < ApplicationRecord
   belongs_to :partner
-  has_one_attached :logo
+  has_one_attached :light_logo
+  has_one_attached :dark_logo
 
   enum status: { pending: 0, active: 1, out_of_budget: 2, retired: 3 }
 
--- test/factories/partner/adverts.rb
+++ test/factories/partner/adverts.rb
@@ -5,7 +5,16 @@ FactoryBot.define do
     base_text { "Try it" }
     emphasised_text { "It's great" }
 
-    logo do
+    light_logo do
+      # Ensure we have a file with a different filename each time
+      tempfile = Tempfile.new([SecureRandom.uuid, '.png'])
+      tempfile.write(File.read(Rails.root.join("app", "images", "favicon.png")))
+      tempfile.rewind
+
+      Rack::Test::UploadedFile.new(tempfile.path, 'image/png')
+    end
+
+    dark_logo do
       # Ensure we have a file with a different filename each time
       tempfile = Tempfile.new([SecureRandom.uuid, '.png'])
       tempfile.write(File.read(Rails.root.join("app", "images", "favicon.png")))

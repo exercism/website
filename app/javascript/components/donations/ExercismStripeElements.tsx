28824fc297e9f06cd3f2c6a6f838e73688775674
dem4ron <demaaron88@gmail.com>
2023-07-11 14:56:38 +0200

    Add different theme objects

--- app/javascript/components/donations/ExercismStripeElements.tsx
+++ app/javascript/components/donations/ExercismStripeElements.tsx
@@ -1,11 +1,12 @@
+import React from 'react'
 import {
   BaseStripeElementsOptions,
   StripeElementsOptions,
   loadStripe,
 } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
-import React from 'react'
 import Bugsnag from '@bugsnag/browser'
+import { useThemeObserver } from '@/hooks'
 
 const stripe = load()
 
@@ -23,13 +24,29 @@ function load() {
   return loadStripe(publishableKey)
 }
 
-const appearance: BaseStripeElementsOptions['appearance'] = {
+const lightAppearance: BaseStripeElementsOptions['appearance'] = {
   theme: 'none',
   variables: {
     fontSizeBase: '16px',
-    colorPrimary: 'grey',
-    colorText: 'grey',
-    colorBackground: 'transparent',
+    colorPrimary: '#130B43',
+    colorText: '#130B43',
+    colorBackground: '#FBFCFE',
+    fontFamily: 'Poppins, sans-serif',
+    fontSmooth: 'antialiased',
+    fontLineHeight: '32px',
+    fontWeightNormal: '500',
+    colorDanger: '#D03B3B',
+    colorTextPlaceholder: '#76709F',
+  },
+}
+
+const darkAppearance: BaseStripeElementsOptions['appearance'] = {
+  theme: 'none',
+  variables: {
+    fontSizeBase: '16px',
+    colorPrimary: '#f0f3f9',
+    colorText: '#f0f3f9',
+    colorBackground: '#211D2F',
     fontFamily: 'Poppins, sans-serif',
     fontSmooth: 'antialiased',
     fontLineHeight: '32px',
@@ -50,7 +67,6 @@ const OPTIONS: StripeElementsOptions = {
         'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
     },
   ],
-  appearance,
 }
 
 export const ExercismStripeElements = ({
@@ -60,10 +76,13 @@ export const ExercismStripeElements = ({
   children?: React.ReactNode
   amount: number
 }): JSX.Element | null => {
+  const { explicitTheme } = useThemeObserver()
   if (stripe === undefined) {
     return null
   }
 
+  OPTIONS.appearance =
+    explicitTheme === 'theme-light' ? lightAppearance : darkAppearance
   OPTIONS.amount = amount
 
   return (

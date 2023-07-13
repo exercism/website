93c685f8121197b1c02992e05c4151ccf0c52ad8
Jeremy Walker <jez.walker@gmail.com>
2023-07-13 14:19:35 +0100

    Improve performance of updating

--- app/commands/exercise/update_solution_git_data.rb
+++ app/commands/exercise/update_solution_git_data.rb
@@ -5,41 +5,42 @@ class Exercise
     initialize_with :exercise, :old_git_important_files_hash
 
     def call
-      ActiveRecord::Base.transaction(isolation: Exercism::READ_COMMITTED) do
-        # We use loop here and for the things below to avoid locking millions of records
-        # We just keep going until we run out of records to update.
-        loop do
-          num_results = Solution.where(exercise:, git_important_files_hash: old_git_important_files_hash).
-            limit(BATCH_UPDATE_SIZE).
-            update_all(
-              git_sha: new_git_sha,
-              git_slug: new_git_slug,
-              git_important_files_hash: new_git_important_files_hash
-            )
-          break if num_results < BATCH_UPDATE_SIZE
-        end
+      # We use loop here and for the things below to avoid locking millions of records
+      # We just keep going until we run out of records to update.
+      loop do
+        ids = Solution.where(exercise:, git_important_files_hash: old_git_important_files_hash).
+          limit(BATCH_UPDATE_SIZE).pluck(:id)
 
-        loop do
-          num_results = Submission.where(exercise:, git_important_files_hash: old_git_important_files_hash).
-            limit(BATCH_UPDATE_SIZE).
-            update_all(
-              git_sha: new_git_sha,
-              git_slug: new_git_slug,
-              git_important_files_hash: new_git_important_files_hash
-            )
-          break if num_results < BATCH_UPDATE_SIZE
-        end
+        Solution.where(id: ids).update_all(
+          git_sha: new_git_sha,
+          git_slug: new_git_slug,
+          git_important_files_hash: new_git_important_files_hash
+        )
+        break if ids.length < BATCH_UPDATE_SIZE
+      end
 
-        loop do
-          num_results = Submission::TestRun.joins(:submission).where(submissions: { exercise: },
-            git_important_files_hash: old_git_important_files_hash).
-            limit(BATCH_UPDATE_SIZE).
-            update_all(
-              git_sha: new_git_sha,
-              git_important_files_hash: new_git_important_files_hash
-            )
-          break if num_results < BATCH_UPDATE_SIZE
-        end
+      loop do
+        ids = Submission.where(exercise:, git_important_files_hash: old_git_important_files_hash).
+          limit(BATCH_UPDATE_SIZE)
+
+        Submission.where(id: ids).update_all(
+          git_sha: new_git_sha,
+          git_slug: new_git_slug,
+          git_important_files_hash: new_git_important_files_hash
+        )
+        break if ids.length < BATCH_UPDATE_SIZE
+      end
+
+      loop do
+        ids = Submission::TestRun.joins(:submission).where(submissions: { exercise: },
+          git_important_files_hash: old_git_important_files_hash).
+          limit(BATCH_UPDATE_SIZE)
+
+        Submission::TestRun.where(id: ids).update_all(
+          git_sha: new_git_sha,
+          git_important_files_hash: new_git_important_files_hash
+        )
+        break if ids.length < BATCH_UPDATE_SIZE
       end
     end
 

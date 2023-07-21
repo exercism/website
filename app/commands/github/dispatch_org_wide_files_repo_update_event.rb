a87febe09ed4820f969900b834cb0261ddf57e3d
Erik Schierboom <erik_schierboom@hotmail.com>
2023-07-21 11:44:58 +0200

    Simplify DispatchOrgWideFilesRepoUpdateEvent arguments

--- app/commands/github/dispatch_org_wide_files_repo_update_event.rb
+++ app/commands/github/dispatch_org_wide_files_repo_update_event.rb
@@ -1,25 +1,20 @@
 class Github::DispatchOrgWideFilesRepoUpdateEvent
   include Mandate
 
-  initialize_with :event_type, :repos, :pusher_github_username
+  initialize_with :repo, :pusher_github_username
 
   def call
-    raise "Unsupported event type" unless EVENT_TYPES.include?(event_type)
-
     Exercism.octokit_client.post("https://api.github.com/repos/exercism/org-wide-files/dispatches", body)
   end
 
   private
   def body
     {
-      event_type: event_type.to_s,
+      event_type: :repo_update,
       client_payload: {
-        repos:,
+        repos: [repo],
         pusher: pusher_github_username
       }
     }.to_json
   end
-
-  EVENT_TYPES = %i[repo_update].freeze
-  private_constant :EVENT_TYPES
 end
--- test/commands/github/dispatch_org_wide_files_repo_update_event_test.rb
+++ test/commands/github/dispatch_org_wide_files_repo_update_event_test.rb
@@ -1,31 +1,10 @@
 require "test_helper"
 
 class Github::DispatchOrgWideFilesRepoUpdateEventTest < ActiveJob::TestCase
-  test "dispatch repository event for single repo" do
+  test "dispatch repo update event" do
     stub_request(:post, "https://api.github.com/repos/exercism/org-wide-files/dispatches").
       with(body: '{"event_type":"repo_update","client_payload":{"repos":["exercism/ruby"],"pusher":"user17"}}')
 
-    Github::DispatchOrgWideFilesRepoUpdateEvent.(:repo_update, ["exercism/ruby"], 'user17')
-
-    assert_requested(:post, "https://api.github.com/repos/exercism/org-wide-files/dispatches", times: 1) do |req|
-      req.body == '{"event_type":"repo_update","client_payload":{"repos":["exercism/ruby"],"pusher":"user17"}}'
-    end
-  end
-
-  test "dispatch repository event for multiples repos" do
-    stub_request(:post, "https://api.github.com/repos/exercism/org-wide-files/dispatches").
-      with(body: '{"event_type":"repo_update","client_payload":{"repos":["exercism/website","exercism/configlet"],"pusher":"user17"}}')
-
-    Github::DispatchOrgWideFilesRepoUpdateEvent.(:repo_update, ["exercism/website", "exercism/configlet"], 'user17')
-
-    assert_requested(:post, "https://api.github.com/repos/exercism/org-wide-files/dispatches", times: 1) do |req|
-      req.body == '{"event_type":"repo_update","client_payload":{"repos":["exercism/website","exercism/configlet"],"pusher":"user17"}}'
-    end
-  end
-
-  test "raises for unknown event type" do
-    assert_raises do
-      Github::DispatchOrgWideFilesRepoUpdateEvent.(:unknown, ["exercism/website"], 'user17')
-    end
+    Github::DispatchOrgWideFilesRepoUpdateEvent.("exercism/ruby", 'user17')
   end
 end

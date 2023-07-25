2d4945785a23e7ec7f853f4789a1a2ccafb957cf
dem4ron <demaaron88@gmail.com>
2023-07-25 15:12:06 +0200

    Add confirmation modal to "Remove from Inbox" button

--- app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
+++ app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from 'react-query'
 import { sendRequest } from '@/utils'
 import { Loading } from '@/components/common'
 import { ErrorBoundary, useErrorHandler } from '@/components/ErrorBoundary'
+import { Modal } from '@/components/modals'
 
 type ComponentProps = {
   endpoint: string
@@ -19,6 +20,7 @@ export const MarkAsNothingToDoButton = (props: ComponentProps): JSX.Element => {
 const DEFAULT_ERROR = new Error('Unable to mark discussion as nothing to do')
 
 const Component = ({ endpoint }: ComponentProps): JSX.Element | null => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const [mutation, { status, error }] = useMutation(() => {
     const { fetch } = sendRequest({
       endpoint: endpoint,
@@ -34,19 +36,48 @@ const Component = ({ endpoint }: ComponentProps): JSX.Element | null => {
   switch (status) {
     case 'idle':
       return (
-        <button
-          onClick={() => {
-            mutation()
-          }}
-          type="button"
-          className="btn-xs btn-enhanced"
-        >
-          <div className="--hint">Remove from Inbox</div>
-        </button>
+        <>
+          <button
+            onClick={() => setIsModalOpen(true)}
+            type="button"
+            className="btn-xs btn-enhanced"
+          >
+            <div className="--hint">Remove from Inbox</div>
+          </button>
+          <ConfirmationModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            onConfirm={mutation}
+          />
+        </>
       )
     case 'loading':
       return <Loading />
     default:
       return null
   }
+}
+
+function ConfirmationModal({
+  isOpen,
+  onClose,
+  onConfirm,
+}: {
+  isOpen: boolean
+  onClose: () => void
+  onConfirm: () => void
+}) {
+  return (
+    <Modal open={isOpen} onClose={onClose}>
+      Are you sure?
+      <div className="flex gap-8 mt-32 ">
+        <button className="btn-s btn-primary" onClick={onConfirm}>
+          Yep
+        </button>
+        <button className="btn-s btn-default" onClick={onClose}>
+          No
+        </button>
+      </div>
+    </Modal>
+  )
 }

92bd2bffa007d15ceb5298e4e4403407dddcc382
dem4ron <demaaron88@gmail.com>
2023-07-25 14:25:41 +0200

    Change migration file

--- db/migrate/20230724113620_add_allow_comments_by_default_to_user_preferences.rb
+++ db/migrate/20230724113620_add_allow_comments_by_default_to_user_preferences.rb
@@ -1,5 +1,7 @@
 class AddAllowCommentsByDefaultToUserPreferences < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+
     add_column :user_preferences, :allow_comments_by_default, :boolean
   end
 end

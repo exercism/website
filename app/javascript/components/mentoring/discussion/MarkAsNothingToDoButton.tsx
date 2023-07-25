e76cc391e9ecfb231e3a887602e27eba5a01e275
Jeremy Walker <jez.walker@gmail.com>
2023-07-25 17:48:51 +0100

    Add a tooltip and some text to the modal

--- app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
+++ app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
@@ -4,6 +4,7 @@ import { sendRequest } from '@/utils'
 import { Loading } from '@/components/common'
 import { ErrorBoundary, useErrorHandler } from '@/components/ErrorBoundary'
 import { Modal } from '@/components/modals'
+import { GenericTooltip } from '../../misc/ExercismTippy'
 
 type ComponentProps = {
   endpoint: string
@@ -36,20 +37,26 @@ const Component = ({ endpoint }: ComponentProps): JSX.Element | null => {
   switch (status) {
     case 'idle':
       return (
-        <>
-          <button
-            onClick={() => setIsModalOpen(true)}
-            type="button"
-            className="btn-xs btn-enhanced"
-          >
-            <div className="--hint">Remove from Inbox</div>
-          </button>
-          <ConfirmationModal
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            onConfirm={mutation}
-          />
-        </>
+        <GenericTooltip
+          content={
+            "This button allows you to remove this discussion from your Inbox and return it to the student's Inbox. Use it when it's the student's turn but the system incorrectly thinks its yours."
+          }
+        >
+          <div>
+            <button
+              onClick={() => setIsModalOpen(true)}
+              type="button"
+              className="btn-xs btn-enhanced"
+            >
+              <div className="--hint">It's the student's turn...</div>
+            </button>
+            <ConfirmationModal
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+              onConfirm={mutation}
+            />
+          </div>
+        </GenericTooltip>
       )
     case 'loading':
       return <Loading />
@@ -69,13 +76,23 @@ function ConfirmationModal({
 }) {
   return (
     <Modal open={isOpen} onClose={onClose}>
-      Are you sure?
-      <div className="flex gap-8 mt-32 ">
+      <h2 class="text-h4 mb-8">Pass this discussion back to the student?</h2>
+      <p class="text-p-base max-w-[540px] mb-8">
+        Continuing will remove this from your Inbox and return it to the
+        student's Inbox. This feature is intended for when a student has left a
+        message for you such as "Sure, I'll do that" and the system now needs to
+        be told it's still the student's turn to act.
+      </p>
+      <p class="text-p-base max-w-[540px] font-medium color-textColor2">
+        If it may not be clear to the student that it's their turn to act,
+        please add a comment to the discussion in addition to using this button.
+      </p>
+      <div className="flex gap-8 mt-20 ">
         <button className="btn-s btn-primary" onClick={onConfirm}>
-          Yep
+          Continue
         </button>
         <button className="btn-s btn-default" onClick={onClose}>
-          No
+          Cancel
         </button>
       </div>
     </Modal>

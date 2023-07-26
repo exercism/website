8e3a8878dfb3c701f043862cfa3564473041b991
Jeremy Walker <jez.walker@gmail.com>
2023-07-26 15:42:57 +0100

    Speed up mentoring nudge job (#5803)
    * Speed up mentoring nudge job
    
    * Add distinct

--- app/jobs/nudge_users_to_request_mentoring_job.rb
+++ app/jobs/nudge_users_to_request_mentoring_job.rb
@@ -4,10 +4,11 @@ class NudgeUsersToRequestMentoringJob < ApplicationJob
   def perform
     # Don't change this without actively benchmarking it on bastion.
     # Things that logically feel like they should make it faster often don't.
-    # At time of writing it takes 4.8s.
-    user_ids = Iteration.joins(solution: :exercise).
-      # Iterated since v3 launch
-      where('iterations.created_at >= ?', Date.new(2021, 9, 1)).
+    # At time of writing it takes 450ms (down from 23s...).
+    user_ids = Iteration.joins(:solution).
+      # Iterated in the 3 days (this runs daily so it gives us one day of error)
+      where('iterations.created_at >= ?', Time.current - 3.days).
+
       # Iterated over a day ago (give time to request manually)
       where('iterations.created_at < ?', Time.current - 1.day).
 
@@ -15,22 +16,19 @@ class NudgeUsersToRequestMentoringJob < ApplicationJob
       where('solutions.type': 'PracticeSolution').
 
       # Don't include hello-world
-      where.not('exercises.slug': 'hello-world').
-
-      # Not for users who have mentor requests
-      where.not('solutions.user_id': Mentor::Request.select(:student_id)).
-
-      # Not for users who have already had the notification
-      where.not('solutions.user_id': User::Notifications::NudgeToRequestMentoringNotification.select(:user_id)).
+      where.not('solutions.exercise_id': Exercise.where(slug: 'hello-world').select(:id)).
 
       # Just the user ids
-      pluck('solutions.user_id').uniq
+      distinct.pluck('solutions.user_id')
 
     user_ids.in_groups_of(100) do |batch|
       User.find(batch).each do |user|
         track = PracticeSolution.where(user:).where.not(status: :started).
           joins(:exercise).where.not('exercises.slug': 'hello-world').
           last.track
+
+        # Not for users who have mentor requests
+        next if Mentor::Request.where(student_id: user.id).exists?
 
         User::Notification::Create.(
           user,

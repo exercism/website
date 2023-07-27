1635c722b91d5ed8781f10ca8646238333a657a3
dem4ron <demaaron88@gmail.com>
2023-07-27 16:58:56 +0200

    Clean up tsx

--- app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
+++ app/javascript/components/mentoring/discussion/MarkAsNothingToDoButton.tsx
@@ -48,7 +48,7 @@ const Component = ({ endpoint }: ComponentProps): JSX.Element | null => {
               type="button"
               className="btn-xs btn-enhanced"
             >
-              <div className="--hint">It's the student's turn...</div>
+              <div className="--hint">It&apos;s the student&apos;s turn...</div>
             </button>
             <ConfirmationModal
               isOpen={isModalOpen}
@@ -76,15 +76,18 @@ function ConfirmationModal({
 }) {
   return (
     <Modal open={isOpen} onClose={onClose}>
-      <h2 class="text-h4 mb-8">Pass this discussion back to the student?</h2>
-      <p class="text-p-base max-w-[540px] mb-8">
+      <h2 className="text-h4 mb-8">
+        Pass this discussion back to the student?
+      </h2>
+      <p className="text-p-base max-w-[540px] mb-8">
         Continuing will remove this from your Inbox and return it to the
-        student's Inbox. This feature is intended for when a student has left a
-        message for you such as "Sure, I'll do that" and the system now needs to
-        be told it's still the student's turn to act.
+        student&apos;s Inbox. This feature is intended for when a student has
+        left a message for you such as &quot;Sure, I&apos;ll do that&quot; and
+        the system now needs to be told it&apos;s still the student&apos;s turn
+        to act.
       </p>
-      <p class="text-p-base max-w-[540px] font-medium color-textColor2">
-        If it may not be clear to the student that it's their turn to act,
+      <p className="text-p-base max-w-[540px] font-medium color-textColor2">
+        If it may not be clear to the student that it&apos;s their turn to act,
         please add a comment to the discussion in addition to using this button.
       </p>
       <div className="flex gap-8 mt-20 ">

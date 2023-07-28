a6d82bfba1c6e355a197d7145be5dbce29cfd213
Jeremy Walker <jez.walker@gmail.com>
2023-07-28 20:47:17 +0100

    Handle double-deletion attempt more gracefully

--- app/commands/solution/sync_to_search_index.rb
+++ app/commands/solution/sync_to_search_index.rb
@@ -29,5 +29,7 @@ class Solution::SyncToSearchIndex
       type: 'solution',
       id: solution.id
     )
+  rescue Elasticsearch::Transport::Transport::Errors::NotFound
+    # The record has already been deleted.
   end
 end

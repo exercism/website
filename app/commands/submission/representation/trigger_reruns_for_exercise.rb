72f1a4be23712de6830c1ea788f851b849462df7
Jeremy Walker <jez.walker@gmail.com>
2023-07-28 19:47:07 +0100

    Tweak things slightly

--- app/commands/submission/representation/trigger_reruns_for_exercise.rb
+++ app/commands/submission/representation/trigger_reruns_for_exercise.rb
@@ -7,7 +7,11 @@ class Submission::Representation::TriggerRerunsForExercise
 
   def call
     Submission.where(exercise:).find_each do |submission|
-      Submission::Representation::Init.(submission, type: :exercise, git_sha: exercise.git_sha, run_in_background: true)
+      Submission::Representation::Init.(submission, type: :exercise, git_sha:, run_in_background: true)
     end
   end
+
+  # Always use the exercise git_sha here, not the submission one.
+  # We want to run against the latest version.
+  delegate :git_sha, to: :exercise
 end
--- db/migrate/20221222144232_add_representer_version_to_exercises.rb
+++ db/migrate/20221222144232_add_representer_version_to_exercises.rb
@@ -1,7 +1,7 @@
 class AddRepresenterVersionToExercises < ActiveRecord::Migration[7.0]
   def change
-    unless Rails.env.production?
-      add_column :exercises, :representer_version, :smallint, null: false, default: 1
-    end
+    return if  Rails.env.production?
+
+    add_column :exercises, :representer_version, :smallint, null: false, default: 1
   end
 end

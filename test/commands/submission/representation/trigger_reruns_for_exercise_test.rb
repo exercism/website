5719e8d822d69a8030fd07f4cb06901ef55c24da
Jeremy Walker <jez.walker@gmail.com>
2023-07-29 14:27:16 +0100

    Add comment

--- test/commands/submission/representation/trigger_reruns_for_exercise_test.rb
+++ test/commands/submission/representation/trigger_reruns_for_exercise_test.rb
@@ -5,6 +5,8 @@ class Submission::Representation::TriggerRerunsForExerciseTest < ActiveSupport::
     exercise = create :practice_exercise
     submission = create :submission, solution: create(:practice_solution, exercise:, user: create(:user))
     create(:iteration, submission:)
+
+    # This one doesn't have an iteration, so shouldn't be rerun
     create :submission, solution: create(:practice_solution, exercise:, user: create(:user))
 
     Submission::Representation::Init.expects(:call).with(submission, type: :exercise, git_sha: "HEAD", run_in_background: true)

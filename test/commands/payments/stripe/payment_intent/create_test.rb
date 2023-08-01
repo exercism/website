a30f262cffca23f94806879092546f54484d6b51
dem4ron <demaaron88@gmail.com>
2023-08-01 13:09:07 +0200

    Adjust tests

--- test/commands/payments/stripe/payment_intent/create_test.rb
+++ test/commands/payments/stripe/payment_intent/create_test.rb
@@ -12,7 +12,11 @@ class Payments::Stripe::PaymentIntent::CreateTest < Payments::TestBase
     Stripe::PaymentIntent.expects(:create).with(
       customer: customer_id,
       amount: amount_in_cents,
-      currency: 'usd'
+      currency: 'usd',
+      setup_future_usage: 'off_session',
+      automatic_payment_methods: {
+        enabled: true
+      }
     ).returns(payment_intent)
 
     actual = Payments::Stripe::PaymentIntent::Create.(user, type, amount_in_cents)

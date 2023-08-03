207787b8b91cb0c44d9e2eafb454fb9ecd6a49ef
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-03 11:34:02 +0200

    Remove unneeded test

--- app/models/user_track.rb
+++ app/models/user_track.rb
@@ -19,8 +19,6 @@ class UserTrack < ApplicationRecord
     primary_key: :user_id,
     inverse_of: :user_track
 
-  has_many :viewed_community_solutions, dependent: :destroy
-
   delegate :num_concepts, to: :track
   delegate :title, to: :track, prefix: true
 

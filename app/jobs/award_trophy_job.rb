665adb3de87b7fef4e5feffacc6f8f2b0f10932b
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-03 14:49:36 +0200

    Remove check

--- app/jobs/award_trophy_job.rb
+++ app/jobs/award_trophy_job.rb
@@ -4,7 +4,7 @@ class AwardTrophyJob < ApplicationJob
   discard_on TrophyCriteriaNotFulfilledError
 
   def self.perform_later(user, track, category, slug, send_email: true, context: nil)
-    return unless self.worth_queuing?(user, track, category, slug, context:)
+    return unless self.worth_queuing?(track, category, slug, context:)
 
     super(user, track, category, slug, send_email:)
   end
@@ -13,10 +13,13 @@ class AwardTrophyJob < ApplicationJob
     UserTrack::AcquiredTrophy::Create.(user, track, category, slug, send_email:)
   end
 
-  def self.worth_queuing?(user, track, category, slug, context:)
-    return true if category == :general
+  def self.worth_queuing?(track, category, slug, context:)
+    # General trophies apply to _all_ tracks, so there won't be any track filtering.
+    # If the context is also empty, there is no possibility of the trophy not being
+    # worth queuing
+    return true if category == :general && context.empty?
 
-    args = { user:, track: }
+    args = { track: }
     args[context.class.base_class.name.demodulize.underscore] = context if context.present?
 
     badge = "Track::Trophies::#{category.to_s.camelize}::#{slug.to_s.camelize}Trophy".safe_constantize

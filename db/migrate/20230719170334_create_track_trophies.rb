68b657b5794ecf82a85f972242ef933bcdc9c6f4
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-03 15:15:44 +0200

    Seed trophies

--- db/migrate/20230719170334_create_track_trophies.rb
+++ db/migrate/20230719170334_create_track_trophies.rb
@@ -1,5 +1,7 @@
 class CreateTrackTrophies < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+
     create_table :track_trophies do |t|
       t.string :type, null: false
       t.json :valid_track_slugs, null: true
@@ -7,5 +9,22 @@ class CreateTrackTrophies < ActiveRecord::Migration[7.0]
 
       t.index :type, unique: true
     end
+
+    TROPHIES.each do |trophy|
+      trophy.create!
+    end    
   end
+
+  TROPHIES = [
+    Track::Trophies::General::CompletedAllExercisesTrophy,
+    Track::Trophies::General::ReadFiftyCommunitySolutionsTrophy,
+    Track::Trophies::General::MentoredTrophy,
+    Track::Trophies::General::IteratedTwentyExercisesTrophy,
+    Track::Trophies::General::CompletedTwentyExercisesTrophy,
+    Track::Trophies::General::CompletedFiftyPercentOfExercisesTrophy,
+    Track::Trophies::Shared::FunctionalTrophy,
+    Track::Trophies::Shared::CompletedLearningModeTrophy,
+    Track::Trophies::Shared::CompletedFiveHardExercisesTrophy
+  ].freeze
+  private_constant :TROPHIES
 end
--- db/migrate/20230719170535_create_user_track_acquired_trophies.rb
+++ db/migrate/20230719170535_create_user_track_acquired_trophies.rb
@@ -1,5 +1,7 @@
 class CreateUserTrackAcquiredTrophies < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+
     create_table :user_track_acquired_trophies do |t|
       t.string :uuid, null: false
       t.bigint :user_id, null: false

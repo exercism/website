87df28ad9aaa685ec944f2678125985930fb0015
dem4ron <demaaron88@gmail.com>
2023-08-07 16:13:40 +0200

    Tweak margins

--- app/javascript/components/donations/ExercismStripeElements.tsx
+++ app/javascript/components/donations/ExercismStripeElements.tsx
@@ -63,8 +63,8 @@ export const ExercismStripeElements = ({
 
   const { stripe, error } = useLazyLoadStripe()
 
-  if (error) return <div className="c-alert--danger m-12">{error}</div>
-  if (!stripe) return <div className="c-alert m-12">Loading...</div>
+  if (error) return <div className="c-alert--danger my-12 mx-24">{error}</div>
+  if (!stripe) return <div className="c-alert my-12 mx-24">Loading...</div>
 
   return (
     <Elements stripe={stripe} options={OPTIONS}>

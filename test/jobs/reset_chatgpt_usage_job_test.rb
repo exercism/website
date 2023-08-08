4e6c804ea988a9ac85aff52139f202fa2f320d8a
Jeremy Walker <jez.walker@gmail.com>
2023-08-08 15:35:15 +0100

    Fix things

--- test/jobs/reset_chatgpt_usage_job_test.rb
+++ test/jobs/reset_chatgpt_usage_job_test.rb
@@ -2,36 +2,23 @@ require "test_helper"
 
 class ResetChatGPTUsageJobTest < ActiveJob::TestCase
   test "reset chatgpt 3.5 usage to nil for premium users" do
-    premium_user_1 = create :user, :premium
-    premium_user_2 = create :user, :premium
     insider_user = create :user, :insider
     user = create :user
 
-    User::IncrementUsage.(premium_user_1, :chatgpt, '3.5')
-    User::IncrementUsage.(premium_user_1, :chatgpt, '3.5')
-    User::IncrementUsage.(premium_user_1, :chatgpt, '4.0')
-    User::IncrementUsage.(premium_user_2, :chatgpt, '3.5')
     User::IncrementUsage.(insider_user, :chatgpt, '3.5')
+    User::IncrementUsage.(insider_user, :chatgpt, '3.5')
+    User::IncrementUsage.(insider_user, :chatgpt, '4.0')
     User::IncrementUsage.(user, :chatgpt, '3.5')
 
-    assert_equal 2, premium_user_1.usages['chatgpt']['3.5']
-    assert_equal 1, premium_user_1.usages['chatgpt']['4.0']
-    assert_equal 1, premium_user_2.usages['chatgpt']['3.5']
-    assert_equal 1, insider_user.usages['chatgpt']['3.5']
+    assert_equal 2, insider_user.usages['chatgpt']['3.5']
+    assert_equal 1, insider_user.usages['chatgpt']['4.0']
     assert_equal 1, user.usages['chatgpt']['3.5']
 
     ResetChatGPTUsageJob.perform_now
 
-    premium_user_1.reload
-    assert_nil premium_user_1.usages['chatgpt']['3.5']
-    assert_nil premium_user_1.usages['chatgpt']['4.0']
-
-    premium_user_2.reload
-    assert_nil premium_user_2.usages['chatgpt']['3.5']
-    assert_nil premium_user_2.usages['chatgpt']['4.0']
-
     insider_user.reload
-    assert_equal 1, insider_user.usages['chatgpt']['3.5']
+    assert_nil insider_user.usages['chatgpt']['3.5']
+    assert_nil insider_user.usages['chatgpt']['4.0']
 
     user.reload
     assert_equal 1, user.usages['chatgpt']['3.5']

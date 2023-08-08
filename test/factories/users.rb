090e482f8cd83612ce159e52321d355d7fa3a01f
Jeremy Walker <jez.walker@gmail.com>
2023-08-08 15:35:15 +0100

    Fix things

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -49,12 +49,6 @@ FactoryBot.define do
       end
     end
 
-    trait :premium do
-      after(:create) do |user, _evaluator|
-        user.data.update(premium_until: Time.current + 1.year)
-      end
-    end
-
     trait :not_mentor do
       after(:create) do |user, _evaluator|
         user.data.update(became_mentor_at: nil)

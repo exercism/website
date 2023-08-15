5767389341191853427c5e821bc5936e4f979620
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-15 13:47:56 +0200

    Fix tooltip

--- app/models/track/trophies/general/completed_fifty_percent_of_exercises_trophy.rb
+++ app/models/track/trophies/general/completed_fifty_percent_of_exercises_trophy.rb
@@ -3,25 +3,25 @@ class Track::Trophies::General::CompletedFiftyPercentOfExercisesTrophy < Track::
   def icon = 'trophy-completed-fifty-percent-of-exercises'
 
   def criteria(track)
-    "Awarded once you complete %<percentage_completed_exercises>i of the exercises in %<track_title>s" % {
+    "Awarded once you complete %<percentage_completed_exercises>i%% of the exercises in %<track_title>s" % {
       percentage_completed_exercises: PERCENTAGE_COMPLETED_EXERCISES,
       track_title: track.title
     }
   end
 
   def success_message(track)
-    "Congratulations on completing %<percentage_completed_exercises>i of the exercises in %<track_title>s" % {
+    "Congratulations on completing %<percentage_completed_exercises>i%% of the exercises in %<track_title>s" % {
       percentage_completed_exercises: PERCENTAGE_COMPLETED_EXERCISES,
       track_title: track.title
     }
   end
 
   def award?(user, track)
-    UserTrack.for(user, track).completed_percentage >= 50.0
+    UserTrack.for(user, track).completed_percentage >= PERCENTAGE_COMPLETED_EXERCISES
   end
 
   def send_email_on_acquisition? = true
 
-  PERCENTAGE_COMPLETED_EXERCISES = 20
+  PERCENTAGE_COMPLETED_EXERCISES = 50
   private_constant :PERCENTAGE_COMPLETED_EXERCISES
 end

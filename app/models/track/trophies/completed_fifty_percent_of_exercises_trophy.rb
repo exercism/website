b40afa56a92d94d7717ad88315959a8248edc667
Jeremy Walker <jez.walker@gmail.com>
2023-08-15 21:24:55 +0100

    Add halfway there reference

--- app/models/track/trophies/completed_fifty_percent_of_exercises_trophy.rb
+++ app/models/track/trophies/completed_fifty_percent_of_exercises_trophy.rb
@@ -4,25 +4,20 @@ class Track::Trophies::CompletedFiftyPercentOfExercisesTrophy < Track::Trophy
   def order = 2
 
   def criteria(track)
-    "Awarded once you complete %<percentage_completed_exercises>i%% of the exercises in %<track_title>s" % {
-      percentage_completed_exercises: PERCENTAGE_COMPLETED_EXERCISES,
+    "Awarded once you complete 50%% of the exercises in %<track_title>s" % {
       track_title: track.title
     }
   end
 
   def success_message(track)
-    "Congratulations on completing %<percentage_completed_exercises>i%% of the exercises in %<track_title>s" % {
-      percentage_completed_exercises: PERCENTAGE_COMPLETED_EXERCISES,
+    "Congratulations on getting half way there and completing 50%% of the exercises in %<track_title>s" % {
       track_title: track.title
     }
   end
 
   def award?(user, track)
-    UserTrack.for(user, track).completed_percentage >= PERCENTAGE_COMPLETED_EXERCISES
+    UserTrack.for(user, track).completed_percentage >= 50
   end
 
   def send_email_on_acquisition? = true
-
-  PERCENTAGE_COMPLETED_EXERCISES = 50
-  private_constant :PERCENTAGE_COMPLETED_EXERCISES
 end

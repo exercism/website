4cb6cf98c8deca8ba52a25572177a3c09b8055d9
dem4ron <demaaron88@gmail.com>
2023-08-15 12:54:59 +0200

    Add front end flow

--- app/javascript/components/track/Trophies.tsx
+++ app/javascript/components/track/Trophies.tsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction, useCallback, useState } from 'react'
+import { useMutation } from 'react-query'
+import { assetUrl, sendRequest } from '@/utils'
 import { GraphicalIcon } from '../common'
+import { Modal } from '../modals'
 
 export type TrophyStatus = 'not_earned' | 'unrevealed' | 'revealed'
 
@@ -19,26 +22,120 @@ export type TrophiesProps = {
   trophies: readonly Trophy[]
 }
 
-export const Trophies = ({ trophies }: TrophiesProps): JSX.Element => {
+export function Trophies({ trophies }: TrophiesProps): JSX.Element {
+  const [modalOpen, setModalOpen] = useState(false)
+  const [highlightedTrophy, setHighlightedTrophy] = useState<Trophy | null>(
+    null
+  )
+
   return (
     <div className="trophies">
       {trophies.map((trophy) => (
-        <Trophy {...trophy} />
+        <Trophy
+          key={trophy.name}
+          trophy={trophy}
+          setHighlightedTrophy={setHighlightedTrophy}
+          setModalOpen={setModalOpen}
+        />
       ))}
+      <Modal open={modalOpen} celebratory onClose={() => setModalOpen(false)}>
+        <div className="flex flex-col items-center">
+          <h2 className="text-h2 mb-12">
+            Congrats on earning {highlightedTrophy?.name}!
+          </h2>
+
+          {highlightedTrophy && (
+            <Trophy
+              trophy={highlightedTrophy}
+              disabled
+              setHighlightedTrophy={setHighlightedTrophy}
+              setModalOpen={setModalOpen}
+            />
+          )}
+          <p className="text-p-base mt-16">{highlightedTrophy?.criteria}</p>
+        </div>
+      </Modal>
     </div>
   )
 }
 
-const Trophy = (trophy: Trophy): JSX.Element => {
+const statusToClassName: Record<TrophyStatus, string> = {
+  not_earned: 'not-acquired',
+  revealed: 'acquired',
+  unrevealed: 'revealable',
+}
+
+const Trophy = ({
+  trophy,
+  setHighlightedTrophy,
+  setModalOpen,
+  disabled = false,
+}: {
+  trophy: Trophy
+  setHighlightedTrophy: Dispatch<SetStateAction<Trophy | null>>
+  setModalOpen: Dispatch<SetStateAction<boolean>>
+  disabled?: boolean
+}): JSX.Element => {
+  const [trophyStatus, setTrophyStatus] = useState<TrophyStatus>(trophy.status)
+  const [mutation, { status, error }] = useMutation(
+    () => {
+      if (!trophy.links.reveal) {
+        throw new Error('Reveal link is not available')
+      }
+      const { fetch } = sendRequest({
+        endpoint: trophy.links.reveal,
+        method: 'PATCH',
+        body: '',
+      })
+
+      return fetch
+    },
+    {
+      onSuccess: (data, trophy: Trophy) => {
+        console.log(data)
+        setModalOpen(true)
+        setHighlightedTrophy(trophy)
+        setTrophyStatus('revealed')
+      },
+    }
+  )
+
+  const handleReveal = useCallback(() => {
+    if (trophyStatus === 'unrevealed') {
+      mutation()
+    } else if (trophyStatus === 'revealed') {
+      setHighlightedTrophy(trophy)
+      setModalOpen(true)
+    }
+  }, [mutation, setHighlightedTrophy, setModalOpen, trophy, trophyStatus])
+
   return (
-    <div className="trophy">
+    <button
+      className={`trophy ${statusToClassName[trophyStatus]}`}
+      onClick={handleReveal}
+      disabled={trophy.status === 'not_earned' || disabled}
+    >
       <GraphicalIcon
         icon={trophy.iconName}
         category="graphics"
         width={128}
         height={128}
       />
-      <div className="title">{trophy.name}</div>
-    </div>
+      {trophy.status === 'unrevealed' ? (
+        <>
+          <div
+            className="shimmer"
+            style={{
+              backgroundImage: `url(${assetUrl(
+                `graphics/${trophy.iconName}.svg`
+              )})`,
+            }}
+          />
+          <div className="title !text-textColor1">Click to Reveal</div>
+        </>
+      ) : (
+        <div className="title">{trophy.name}</div>
+      )}
+    </button>
   )
 }

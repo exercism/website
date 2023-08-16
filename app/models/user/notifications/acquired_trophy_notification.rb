99ed652ffbbcbb8d5e60f0faa1d09f6a771336e4
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-16 10:43:38 +0200

    Materialize trophy notification

--- app/models/user/notifications/acquired_trophy_notification.rb
+++ app/models/user/notifications/acquired_trophy_notification.rb
@@ -1,11 +1,17 @@
 class User::Notifications::AcquiredTrophyNotification < User::Notification
   params :user_track_acquired_trophy
 
-  delegate :user_id, :track_id, :trophy_id, to: :user_track_acquired_trophy
+  delegate :track, :trophy, to: :user_track_acquired_trophy
 
-  def url = Exercism::Routes.profile_url(user_id)
-  def image_type; end
-  def image_url; end
+  def url = Exercism::Routes.track_url(track, anchor: "trophy-cabinet")
+  def image_type = :icon
+  def image_url = trophy.icon
+  def guard_params = "Track##{track.id}|Trophy##{trophy.id}"
 
-  def guard_params = "Track##{track_id}|Trophy##{trophy_id}"
+  def i18n_params
+    {
+      track_title: track.title,
+      trophy_name: trophy.name(track)
+    }
+  end
 end
--- test/models/user/notifications/acquired_trophy_notification_test.rb
+++ test/models/user/notifications/acquired_trophy_notification_test.rb
@@ -5,11 +5,16 @@ class User::Notifications::AcquiredTrophyNotificationTest < ActiveSupport::TestC
 
   test "keys are valid" do
     user = create :user
-    track = create :track
-    trophy = create :trophy
+    track = create :track, slug: 'ruby', title: 'Ruby'
+    trophy = create :mentored_trophy
     user_track_acquired_trophy = create(:user_track_acquired_trophy, user:, track:, trophy:)
 
     notification = User::Notifications::AcquiredTrophyNotification.create!(user:, params: { user_track_acquired_trophy: })
     assert_equal "#{user.id}|acquired_trophy|Track##{track.id}|Trophy##{trophy.id}", notification.uniqueness_key
+    assert_equal "You've been awarded the Magnificent Mentee trophy on the Ruby track", notification.text
+    assert_equal :icon, notification.image_type
+    assert_equal trophy.icon, notification.image_url
+    assert_equal "https://test.exercism.org/tracks/ruby#trophy-cabinet", notification.url
+    assert_equal "/tracks/#{track.slug}#trophy-cabinet", notification.path
   end
 end

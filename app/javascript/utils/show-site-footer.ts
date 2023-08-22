2a20efd84fd7aaa1bd922a6960418c2a5b02c5d0
dem4ron <demaaron88@gmail.com>
2023-08-22 17:47:49 +0200

    Add max retries to recursion, typescriptize things

--- app/javascript/utils/show-site-footer.ts
+++ app/javascript/utils/show-site-footer.ts
@@ -1,12 +1,21 @@
+let retryCount = 0
+const MAX_RETRIES = 50
+
 export function showSiteFooter(): void {
-  elems = document.body.getElementsByClassName('c-react-component')
-  for (let elem of elems) {
-    if (elem.children.length == 0) {
+  const elems = document.body.getElementsByClassName('c-react-component')
+  for (const elem of elems) {
+    if (elem.childElementCount === 0 && retryCount < MAX_RETRIES) {
+      retryCount++
       setTimeout(showSiteFooter, 50)
       return
     }
   }
 
+  displayFooter()
+  retryCount = 0
+}
+
+const displayFooter = () => {
   const footerElement = document.getElementById('site-footer')
   if (footerElement !== null) {
     footerElement.style.display = 'block'

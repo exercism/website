81db08356c08294dd199c91f4331d81f6c356f25
Jeremy Walker <jez.walker@gmail.com>
2023-08-22 16:25:28 +0100

    Remove footer CLS

--- app/javascript/utils/show-site-footer.ts
+++ app/javascript/utils/show-site-footer.ts
@@ -1,4 +1,12 @@
 export function showSiteFooter(): void {
+  elems = document.body.getElementsByClassName('c-react-component')
+  for (let elem of elems) {
+    if (elem.children.length == 0) {
+      setTimeout(showSiteFooter, 50)
+      return
+    }
+  }
+
   const footerElement = document.getElementById('site-footer')
   if (footerElement !== null) {
     footerElement.style.display = 'block'

f67d51e5b2ee3429437c5e9a0a03a1cf30eb0156
Jeremy Walker <jez.walker@gmail.com>
2023-08-22 16:53:18 +0100

    Defer final footer load

--- app/javascript/utils/show-site-footer.ts
+++ app/javascript/utils/show-site-footer.ts
@@ -4,6 +4,8 @@ const MAX_RETRIES = 50
 export function showSiteFooter(): void {
   const elems = document.body.getElementsByClassName('c-react-component')
   for (const elem of elems) {
+    console.log(elem.childElementCount)
+
     if (elem.childElementCount === 0 && retryCount < MAX_RETRIES) {
       retryCount++
       setTimeout(showSiteFooter, 50)
@@ -11,7 +13,10 @@ export function showSiteFooter(): void {
     }
   }
 
-  displayFooter()
+  /* Now everything is rendered, it might take the browser
+   * a few ms to actually paint everything. In my testing, it's
+   * always between 50-100ms, so I've added a little extra to be safe*/
+  setTimeout(displayFooter, 150)
   retryCount = 0
 }
 

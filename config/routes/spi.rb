9814c5b46609d3304e7be85c665b397a3b1b83bc
Jeremy Walker <jez.walker@gmail.com>
2023-08-23 18:02:59 +0100

    Add SPI endpoint for avatars

--- config/routes/spi.rb
+++ config/routes/spi.rb
@@ -2,6 +2,7 @@
 # SPI #
 # ### #
 namespace :spi do
+  resources :avatars, only: :show
   resources :tooling_jobs, only: :update
   resources :chatgpt_responses, only: :create
   get "solution_image_data/:track_slug/:exercise_slug/:user_handle" => "solution_image_data#show"

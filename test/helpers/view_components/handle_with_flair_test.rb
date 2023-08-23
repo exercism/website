14c7d84f6646a6345b6ebbd95338ceb8e1cb27e1
dem4ron <demaaron88@gmail.com>
2023-08-23 13:50:34 +0200

    Fix test

--- test/helpers/view_components/handle_with_flair_test.rb
+++ test/helpers/view_components/handle_with_flair_test.rb
@@ -7,7 +7,7 @@ class ViewComponents::HandleWithFlairTest < ActionView::TestCase
   test "nothing with no flair" do
     handle = "iHiD"
 
-    expected = %(<span class="inline-flex items-center"><span>#{handle}</span></span>)
+    expected = %(<span class="inline-flex items-center leading-150">#{handle}</span>)
     actual = render(ViewComponents::HandleWithFlair.new(handle, nil))
     assert_equal expected, actual
   end
@@ -18,9 +18,11 @@ class ViewComponents::HandleWithFlairTest < ActionView::TestCase
     title = 'An Exercism Insider'
     alt = "#{title}'s flair"
 
-    expected = tag.span(class: 'inline-flex items-center') do
-      tag.span(handle) +
-        icon(:insiders, alt, style: "all:unset; height: 13px; width: 13px; margin-left: 4px; margin-bottom: 1px", title:).to_s
+    expected = tag.span(class: 'inline-flex items-center leading-150') do
+      safe_join([
+        handle,
+        icon(:insiders, alt, style: "all:unset; height: 13px; width: 13px; margin-left: 4px; margin-bottom: 1px", title:)
+      ].compact)
     end
 
     actual = render(ViewComponents::HandleWithFlair.new(handle, flair))

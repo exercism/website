7278b4b4f296ad07b0d9ccc3991266d910594638
Erik Schierboom <erik_schierboom@hotmail.com>
2023-08-30 14:18:18 +0200

    Add notification for student reply nudge

---
+++ app/models/user/notifications/nudge_student_to_reply_in_discussion_notification.rb
@@ -0,0 +1,20 @@
+class User::Notifications::NudgeStudentToReplyInDiscussionNotification < User::Notification
+  params :discussion, :num_days_waiting
+
+  delegate :mentor, :exercise, :track, to: :discussion
+
+  def url = discussion.student_url
+
+  def i18n_params
+    {
+      mentor_name: mentor.handle,
+      track_title: track.title,
+      exercise_title: exercise.title
+    }
+  end
+
+  def image_type = :avatar
+  def image_url = mentor.avatar_url
+  def guard_params = "Discussion##{discussion.id}##{nudge_day}"
+  def nudge_day = (discussion.awaiting_student_since + num_days_waiting.days).to_date
+end

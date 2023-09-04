abebf45faa16feb3af3a196d098a6711f7e81d3a
Jeremy Walker <jez.walker@gmail.com>
2023-09-04 16:27:59 +0100

    Apply suggestions from code review

--- db/migrate/20230216210634_add_index_on_users_created_at.rb
+++ db/migrate/20230216210634_add_index_on_users_created_at.rb
@@ -1,7 +1,7 @@
 class AddIndexOnUsersCreatedAt < ActiveRecord::Migration[7.0]
   def change
-    unless Rails.env.production?
-      add_index :users, :created_at
-    end
+    return if Rails.env.production?
+    
+    add_index :users, :created_at
   end
 end

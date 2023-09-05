d033c2135622c5b49af286e9959a766a4a931449
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-05 13:34:25 +0200

    Remove viewed community solutions when resetting track

--- app/models/user_track/viewed_community_solution.rb
+++ app/models/user_track/viewed_community_solution.rb
@@ -2,4 +2,10 @@ class UserTrack::ViewedCommunitySolution < ApplicationRecord
   belongs_to :user
   belongs_to :track
   belongs_to :solution
+
+  has_one :user_track, # rubocop:disable Rails/HasManyOrHasOneDependent
+    ->(vcs) { where(track_id: vcs.track_id) },
+    foreign_key: :user_id,
+    primary_key: :user_id,
+    inverse_of: :viewed_community_solutions
 end

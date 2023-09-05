d4909ec94473a4b9018015fb87bc3b1cda2302b5
dem4ron <demaaron88@gmail.com>
2023-09-05 14:06:58 +0200

    Refactor

--- app/javascript/utils/lazy-highlight-all.ts
+++ app/javascript/utils/lazy-highlight-all.ts
@@ -1,7 +1,7 @@
 export function lazyHighlightAll(): void {
   let highlighted = false
 
-  function loadAndHighlight() {
+  function applySyntaxHighlighting() {
     import('@/utils/highlight')
       .then((m) => {
         m.highlightAll()
@@ -13,34 +13,40 @@ export function lazyHighlightAll(): void {
       })
   }
 
-  function checkAndHighlight(): void {
-    // only load highlightjs when a code block exists in the DOM
-    if (highlighted || !hasHighlightableCodeBlock(document)) return
-
+  function initAndApplyHighlighting() {
     // to avoid race conditions, set this optimistically to true before the async loading
     highlighted = true
-    loadAndHighlight()
+    applySyntaxHighlighting()
   }
 
-  function mutationCallback(mutationsList: MutationRecord[]): void {
+  function checkAndInitHighlighting() {
+    if (!shouldApplySyntaxHighlighting(document, highlighted)) return
+    initAndApplyHighlighting()
+  }
+
+  function handleDOMChanges(mutationsList: MutationRecord[]): void {
     for (const mutation of mutationsList) {
       if (mutation.type !== 'childList') continue
       for (const node of mutation.addedNodes) {
         if (node.nodeType !== Node.ELEMENT_NODE) continue
-        if (hasHighlightableCodeBlock(node as Element)) {
-          checkAndHighlight()
+        if (shouldApplySyntaxHighlighting(node as Element, highlighted)) {
+          initAndApplyHighlighting()
           return
         }
       }
     }
   }
 
-  checkAndHighlight()
+  checkAndInitHighlighting()
 
-  const observer = new MutationObserver(mutationCallback)
+  const observer = new MutationObserver(handleDOMChanges)
   observer.observe(document.body, { childList: true, subtree: true })
 }
 
-function hasHighlightableCodeBlock(rootNode: Document | Element): boolean {
-  return rootNode.querySelector('code') !== null
+// only load highlightjs when a code block exists in the DOM and it hasn't been highlighted yet
+function shouldApplySyntaxHighlighting(
+  rootNode: Document | Element,
+  highlighted: boolean
+): boolean {
+  return rootNode.querySelector('code') !== null && !highlighted
 }

ed3669c4f1d5b3b99b47d7985f1826f399568aee
Jeremy Walker <jez.walker@gmail.com>
2023-09-07 11:48:30 +0100

    Add canonical urls (#6095)
    * Add canonical url
    
    * Remove whitespace
    
    * Hardcode landing page
    
    * Add canonical url for Impact page
    
    * Add canonical url for docs pages
    
    * Add canonical url for about pages
    
    * Add canonical url for track pages
    
    * Add canonical url for blog pages
    
    * Add canonical url for /perks
    
    * Add canonical url for 12in23 challenge
    
    * Add canonical url for community pages
    
    * Add canonical url for insiders
    
    * Add canonical url for contributing pages
    
    * Add canonical url for donate
    
    * Add canonical url for mentoring
    
    * Update app/views/layouts/_meta_tags.html.haml
    
    * Remove unnecessary module prefixes
    
    * Fix published solution URL
    
    ---------
    
    Co-authored-by: dem4ron <demaaron88@gmail.com>
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -17,21 +17,28 @@ module MetaTagsHelper
     content_for?(:meta_url) ? content_for(:meta_url) : request.original_url.gsub(%r{/$}, "")
   end
 
+  def canonical_url
+    content_for(:canonical_url).presence
+  end
+
   def track_meta_tags(user_track)
     content_for :meta_title, "#{user_track.track_title} on Exercism"
     content_for :meta_description,
       "Get fluent in #{user_track.track_title} by solving #{user_track.num_exercises} exercises. And then level up with mentoring from our world-class team." # rubocop:disable Layout/LineLength
+    content_for :canonical_url, track_url(user_track.track)
   end
 
   def concept_meta_tags(concept, user_track)
     content_for :meta_title, "#{concept.name} in #{user_track.track_title} on Exercism"
     content_for :meta_description,
       "Master #{concept.name} in #{user_track.track_title} by solving #{user_track.num_exercises_for_concept(concept)} exercises, with support from our world-class team." # rubocop:disable Layout/LineLength
+    content_for :canonical_url, track_concept_url(user_track.track, concept)
   end
 
   def exercise_meta_tags(exercise, user_track)
     content_for :meta_title, "#{exercise.title} in #{user_track.track_title} on Exercism"
     content_for :meta_description,
       "Can you solve #{exercise.title} in #{user_track.track_title}? Improve your #{user_track.track_title} skills with support from our world-class team of mentors." # rubocop:disable Layout/LineLength
+    content_for :canonical_url, track_exercise_url(user_track.track, exercise)
   end
 end

075de39aefc389eb57517a9500ff08325e2715c2
Jeremy Walker <jez.walker@gmail.com>
2023-09-07 15:46:36 +0200

    Fix test

--- test/models/user/notifications/onboarding_insiders_notification_test.rb
+++ test/models/user/notifications/onboarding_insiders_notification_test.rb
@@ -9,8 +9,8 @@ class User::Notifications::OnboardingInsidersNotificationTest < ActiveSupport::T
     assert_equal "#{user.id}|onboarding_insiders|", notification.uniqueness_key
     assert_equal I18n.t('notifications.onboarding_insiders')[1].strip, notification.text
     assert_equal :icon, notification.image_type
-    assert_match(%r{^/assets/icons/donate-[a-z0-9]+\.svg$}, notification.image_url)
-    assert_equal "https://test.exercism.org/donate", notification.url
+    assert_match(%r{^/assets/icons/insiders-[a-z0-9]+\.svg$}, notification.image_url)
+    assert_equal "https://test.exercism.org/insiders", notification.url
     assert_equal "/donate", notification.path
   end
 end

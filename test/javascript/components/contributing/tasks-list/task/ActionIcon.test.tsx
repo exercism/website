c3a0476f0edbca15738d565c9166e6e937537a7b
dem4ron <demaaron88@gmail.com>
2023-09-08 10:36:35 +0200

    Fix icons_helper

--- test/javascript/components/contributing/tasks-list/task/ActionIcon.test.tsx
+++ test/javascript/components/contributing/tasks-list/task/ActionIcon.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { ActionIcon } from '../../../../../../app/javascript/components/contributing/tasks-list/task/ActionIcon'
 
-test('renders an empty div when action is empty', async () => {
+test('renders an empty div when action is empty', () => {
   render(<ActionIcon />)
 
   expect(screen.queryByRole('img')).not.toBeInTheDocument()

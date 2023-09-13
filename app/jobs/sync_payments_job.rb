d862396084150960f0aaa2e750238f880d9ad7fe
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-13 12:49:02 +0200

    Rename SyncSponsorsJob to SyncPaymentsJob

--- app/jobs/sync_payments_job.rb
+++ app/jobs/sync_payments_job.rb
@@ -1,6 +1,6 @@
 # The goal of this job is to re-sync sponsors to ensure
 # that our payment and subscription data are correct
-class SyncSponsorsJob < ApplicationJob
+class SyncPaymentsJob < ApplicationJob
   queue_as :dribble
 
   def perform

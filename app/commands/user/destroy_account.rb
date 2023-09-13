769d150d6282668ab160b32379aed59c0252a899
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-13 13:23:36 +0200

    Destroy payments and subscriptions

--- app/commands/user/destroy_account.rb
+++ app/commands/user/destroy_account.rb
@@ -7,8 +7,8 @@ class User::DestroyAccount
     User::ResetAccount.(user)
     user.student_relationships.destroy_all
     user.mentor_relationships.destroy_all
-    user.subscriptions.update_all(user_id: User::GHOST_USER_ID)
-    user.payments.update_all(user_id: User::GHOST_USER_ID)
+    user.subscriptions.destroy_all
+    user.payments.destroy_all
     user.destroy
   end
 end

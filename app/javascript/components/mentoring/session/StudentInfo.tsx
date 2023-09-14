50a550fcbffc9d7bc96e67e7cca1c21420605d09
dem4ron <demaaron88@gmail.com>
2023-09-14 15:52:38 +0200

    Format code

--- app/javascript/components/mentoring/session/StudentInfo.tsx
+++ app/javascript/components/mentoring/session/StudentInfo.tsx
@@ -15,37 +15,37 @@ export const StudentInfo = ({
 }): JSX.Element => {
   return (
     <div className="student-info">
-    <div className="flex mb-8">
-      <div className="flex-grow">
-        <div className="subtitle">Who you&apos;re mentoring</div>
-        <div className="handle-block">
-          <div className="handle">
-            <HandleWithFlair
-              handle={student.handle}
-              flair={student.flair}
-              size="medium"
+      <div className="flex mb-8">
+        <div className="flex-grow">
+          <div className="subtitle">Who you&apos;re mentoring</div>
+          <div className="handle-block">
+            <div className="handle">
+              <HandleWithFlair
+                handle={student.handle}
+                flair={student.flair}
+                size="medium"
+              />
+            </div>
+            <Reputation
+              value={student.reputation.toString()}
+              type="primary"
+              size="small"
             />
           </div>
-          <Reputation
-            value={student.reputation.toString()}
-            type="primary"
-            size="small"
-          />
+          <div className="name">{student.name}</div>
+          <Pronouns handle={student.handle} pronouns={student.pronouns} />
         </div>
-        <div className="name">{student.name}</div>
-        <Pronouns handle={student.handle} pronouns={student.pronouns} />
+        <Avatar src={student.avatarUrl} handle={student.handle} />
       </div>
-      <Avatar src={student.avatarUrl} handle={student.handle} />
+      <div className="bio">{student.bio}</div>
+      <div className="options">
+        {student.links ? (
+          <StudentInfoActions student={student} setStudent={setStudent} />
+        ) : null}
+        <PreviousSessionsLink student={student} setStudent={setStudent} />
+      </div>
+      <StudentTrackObjectives student={student} />
     </div>
-    <div className="bio">{student.bio}</div>
-    <div className="options">
-      {student.links ? (
-        <StudentInfoActions student={student} setStudent={setStudent} />
-      ) : null}
-      <PreviousSessionsLink student={student} setStudent={setStudent} />
-    </div>
-    <StudentTrackObjectives student={student}/>
-  </div>
   )
 }
 

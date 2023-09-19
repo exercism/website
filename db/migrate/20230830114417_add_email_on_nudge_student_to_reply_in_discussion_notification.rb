f00c3c7dbf0d4162a0730d8fc6548a23a15308d3
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-19 09:21:11 +0200

    Remove comment

--- db/migrate/20230830114417_add_email_on_nudge_student_to_reply_in_discussion_notification.rb
+++ db/migrate/20230830114417_add_email_on_nudge_student_to_reply_in_discussion_notification.rb
@@ -3,7 +3,5 @@ class AddEmailOnNudgeStudentToReplyInDiscussionNotification < ActiveRecord::Migr
     return if Rails.env.production?
 
     add_column :user_communication_preferences, :email_on_nudge_student_to_reply_in_discussion_notification, :boolean, default: true, null: false
-
-    # TODO: do we want to set this value to a default based on an existing preference?
   end
 end
--- db/migrate/20230831092351_add_email_on_student_timed_out_discussion_notification.rb
+++ db/migrate/20230831092351_add_email_on_student_timed_out_discussion_notification.rb
@@ -3,7 +3,5 @@ class AddEmailOnStudentTimedOutDiscussionNotification < ActiveRecord::Migration[
     return if Rails.env.production?
 
     add_column :user_communication_preferences, :email_on_student_timed_out_discussion_notification, :boolean, default: true, null: false
-
-    # TODO: do we want to set this value to a default based on an existing preference?
   end
 end

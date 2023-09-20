4db348a4b3d5462e9228541994e354d73d8f3c31
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-20 16:32:07 +0200

    Another rename

--- app/jobs/finish_abandoned_mentor_discussions_job.rb
+++ app/jobs/finish_abandoned_mentor_discussions_job.rb
@@ -1,6 +1,6 @@
 # The goal of this job is to re-sync sponsors to ensure
 # that our payment and subscription data are correct
-class FinishAbandonedDiscussionsJob < ApplicationJob
+class FinishAbandonedMentorDiscussionsJob < ApplicationJob
   queue_as :dribble
 
   def perform = Mentor::Discussion::FinishAbandoned.()

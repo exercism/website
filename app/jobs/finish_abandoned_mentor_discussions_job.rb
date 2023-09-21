ba45e4a63f41e432c3d87e029695e1054765ade4
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-21 10:43:41 +0200

    Update timed out in job

--- app/jobs/finish_abandoned_mentor_discussions_job.rb
+++
@@ -1,7 +0,0 @@
-# The goal of this job is to re-sync sponsors to ensure
-# that our payment and subscription data are correct
-class FinishAbandonedMentorDiscussionsJob < ApplicationJob
-  queue_as :dribble
-
-  def perform = Mentor::Discussion::FinishAbandoned.()
-end

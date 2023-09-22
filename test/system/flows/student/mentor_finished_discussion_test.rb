3f9ca80cbf3ae6424501c443c48b9136772d1fb2
dem4ron <demaaron88@gmail.com>
2023-09-22 12:43:28 +0200

    Add tests for frontend flow

--- test/system/flows/student/mentor_finished_discussion_test.rb
+++ test/system/flows/student/mentor_finished_discussion_test.rb
@@ -44,6 +44,7 @@ module Flows
           sign_in!(user)
           visit track_exercise_mentor_discussion_path(solution.track, solution.exercise, discussion)
           click_on "Review & finish discussion"
+          assert_css ".m-confirm-finish-student-mentor-discussion"
           within(".m-confirm-finish-student-mentor-discussion") { click_on "Review and end discussion" }
           click_on "It was good!"
           fill_in "Leave #{discussion.mentor.handle} a testimonial (optional)", with: "Good mentor!"
@@ -51,6 +52,114 @@ module Flows
           click_on "Continue without donating"
 
           assert_text "Nice, it looks like you're done here! "
+        end
+      end
+
+      test "student reviews timed out discussion from discussion header" do
+        user = create :user
+        track = create :track
+        create(:user_track, user:, track:)
+        exercise = create(:concept_exercise, track:)
+        solution = create(:concept_solution, exercise:, user:)
+        submission = create :submission, solution:,
+          tests_status: :passed,
+          representation_status: :generated,
+          analysis_status: :completed
+        create(:iteration, idx: 1, solution:, submission:)
+        discussion = create(:mentor_discussion,
+          status: :mentor_timed_out,
+          finished_by: :mentor_timed_out,
+          solution:)
+
+        use_capybara_host do
+          sign_in!(user)
+          visit track_exercise_mentor_discussion_path(solution.track, solution.exercise, discussion)
+          within("#panel-discussion") do
+            refute_text "Review and end discussion"
+            assert_text "Review discussion"
+          end
+          within(".discussion-header") do
+            refute_text "Review & finish discussion"
+            assert_text "Review discussion"
+            click_on "Review discussion"
+          end
+          refute_css ".m-confirm-finish-student-mentor-discussion"
+          assert_text "Your discussion timed out"
+          click_on "It was good!"
+          fill_in "Leave #{discussion.mentor.handle} a testimonial (optional)", with: "Good mentor!"
+          click_on "Finish"
+          click_on "Continue without donating"
+
+          assert_text "Nice, it looks like you're done here!"
+        end
+      end
+
+      test "student reviews timed out discussion from side-panel" do
+        user = create :user
+        track = create :track
+        create(:user_track, user:, track:)
+        exercise = create(:concept_exercise, track:)
+        solution = create(:concept_solution, exercise:, user:)
+        submission = create :submission, solution:,
+          tests_status: :passed,
+          representation_status: :generated,
+          analysis_status: :completed
+        create(:iteration, idx: 1, solution:, submission:)
+        discussion = create(:mentor_discussion,
+          status: :mentor_timed_out,
+          finished_by: :mentor_timed_out,
+          solution:)
+
+        use_capybara_host do
+          sign_in!(user)
+          visit track_exercise_mentor_discussion_path(solution.track, solution.exercise, discussion)
+          within(".discussion-header") do
+            refute_text "Review & finish discussion"
+            assert_text "Review discussion"
+          end
+          within("#panel-discussion") do
+            refute_text "Review and end discussion"
+            assert_text "Review discussion"
+            click_on "Review discussion"
+          end
+          assert_text "Your discussion timed out"
+          click_on "It was good!"
+          fill_in "Leave #{discussion.mentor.handle} a testimonial (optional)", with: "Good mentor!"
+          click_on "Finish"
+          click_on "Continue without donating"
+
+          assert_text "Nice, it looks like you're done here!"
+        end
+      end
+
+      test "student revisits reviewed timed out discussion" do
+        user = create :user
+        track = create :track
+        create(:user_track, user:, track:)
+        exercise = create(:concept_exercise, track:)
+        solution = create(:concept_solution, exercise:, user:)
+        submission = create :submission, solution:,
+          tests_status: :passed,
+          representation_status: :generated,
+          analysis_status: :completed
+        create(:iteration, idx: 1, solution:, submission:)
+        discussion = create(:mentor_discussion,
+          status: :finished,
+          finished_by: :mentor_timed_out,
+          solution:)
+
+        use_capybara_host do
+          sign_in!(user)
+          visit track_exercise_mentor_discussion_path(solution.track, solution.exercise, discussion)
+          within(".discussion-header") do
+            refute_text "Review & finish discussion"
+            refute_text "Review discussion"
+            assert_text "Ended"
+          end
+          within("#panel-discussion") do
+            refute_text "Review and end discussion"
+            refute_text "Review discussion"
+          end
         end
       end
     end

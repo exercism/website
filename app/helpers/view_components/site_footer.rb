17c06b0d6e0bf5f01fd12b7709e01fab8aa6f8a2
dem4ron <demaaron88@gmail.com>
2023-09-22 09:43:15 +0200

    Generate new cache key for site footer

--- app/helpers/view_components/site_footer.rb
+++ app/helpers/view_components/site_footer.rb
@@ -19,7 +19,7 @@ module ViewComponents
       parts << ::Track.active.count
       parts << user_part
       parts << stripe_version
-      parts << "v2"
+      parts << "v3"
 
       parts.join(':')
     end

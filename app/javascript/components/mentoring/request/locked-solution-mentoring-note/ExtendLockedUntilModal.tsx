0ec161d24d23dc4122b29e5f5d8d47712d98bcf1
Jeremy Walker <jez.walker@gmail.com>
2023-09-26 14:51:25 +0100

    Add styling

--- app/javascript/components/mentoring/request/locked-solution-mentoring-note/ExtendLockedUntilModal.tsx
+++ app/javascript/components/mentoring/request/locked-solution-mentoring-note/ExtendLockedUntilModal.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Modal } from '@/components/modals'
+import { GraphicalIcon } from '@/components/common'
+
 export function ExtendLockedUntilModal({
   open,
   onClose,
@@ -21,20 +23,35 @@ export function ExtendLockedUntilModal({
         describedby: 'cancelled-mentoring-request-description',
       }}
     >
-      <h3
-        id="cancelled-mentoring-request-description"
-        className="text-h3 mb-16 text-center"
-      >
-        You only have 10 minutes left to submit your comment. Would you like
-        more time?
-      </h3>
-      <div className="flex justify-center gap-16">
-        <button onClick={onExtend} className="btn-m btn-primary">
-          Yes, extend for 30 minutes
-        </button>
-        <button className="btn-m btn-secondary" onClick={onClose}>
-          No, thank you
-        </button>
+      <div class="flex items-start">
+        <div className="flex flex-col mr-32">
+          <h3
+            id="cancelled-mentoring-request-description"
+            className="text-h3 mb-6"
+          >
+            Mentor Lock close to expiring
+          </h3>
+          <p className="text-p-large mb-8">
+            You only have 10 minutes remaining to submit your comment before
+            this mentor request is unlocked for other mentors to reply to.
+          </p>
+          <p className="text-p-large mb-20">Would you like more time?</p>
+          <div className="flex gap-16">
+            <button onClick={onExtend} className="btn-m btn-primary">
+              Yes, extend for 30 minutes
+            </button>
+            <button className="btn-m btn-secondary" onClick={onClose}>
+              No, thank you
+            </button>
+          </div>
+        </div>
+        <GraphicalIcon
+          icon="alarm-alert"
+          category="graphics"
+          className="ml-auto"
+          height={128}
+          width={128}
+        />
       </div>
     </Modal>
   )

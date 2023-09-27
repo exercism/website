7583b1246c0d0d576cff2df938151aec5e685dc1
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-27 14:27:05 +0200

    Fix secret

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Rails is very pinickity over this being set and really
 # really wants it to be an ENV var. So let's just give it
 # its way, even though this is pretty ugly.
-ENV["SECRET_KEY_BASE"] = Exercism.secrets.website_secret_key_base
+ENV["SECRET_KEY_BASE"] = 'qeqwe'
 
 begin
   ref = File.read(Rails.root.join(".git", "HEAD")) # This gives `ref: refs/heads/main`

cd17e9ba8d077bbb02f5035f9346f3702e882fff
Erik Schierboom <erik_schierboom@hotmail.com>
2023-09-27 11:14:42 +0200

    Minor refactoring

--- app/commands/mentor/discussion/finish_abandoned.rb
+++ app/commands/mentor/discussion/finish_abandoned.rb
@@ -2,10 +2,7 @@ class Mentor::Discussion::FinishAbandoned
   include Mandate
 
   def call
-    Mentor::Discussion.
-      where(status: %i[mentor_finished student_timed_out]).
-      where('finished_at <= ?', Time.current.utc - 1.week).
-      each do |discussion|
+    discussions_to_abandon.each do |discussion|
       finish!(discussion)
     rescue StandardError => e
       Bugsnag.notify(e)
@@ -13,6 +10,12 @@ class Mentor::Discussion::FinishAbandoned
   end
 
   private
+  def discussions_to_abandon
+    Mentor::Discussion.
+      where(status: %i[mentor_finished student_timed_out]).
+      where('finished_at <= ?', Time.current.utc - 1.week)
+  end
+
   def finish!(discussion)
     discussion.update!(
       status: :finished,

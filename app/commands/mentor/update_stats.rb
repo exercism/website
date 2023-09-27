2f3bc3b02dc92d3e7483532b397ffcde34c9f944
Jeremy Walker <jez.walker@gmail.com>
2023-09-27 21:06:42 +0100

    Reorder code

--- app/commands/mentor/update_stats.rb
+++ app/commands/mentor/update_stats.rb
@@ -10,23 +10,15 @@ class Mentor::UpdateStats
   # - Sets the new values
   # - Triggers some extra checks if they've changed
   def call
-    # Actively use the cache to get these values as we don't want
-    # to follow the automatic setting pathway we normally use.
-    old_values = [
-      mentor.data.cache['num_students_mentored'],
-      mentor.data.cache['num_solutions_mentored'],
-      mentor.data.cache['mentor_satisfaction_percentage']
-    ]
-    new_values = old_values.clone
-
-    new_values[0] = User::ResetCache.(mentor, :num_students_mentored)
-    new_values[1] = User::ResetCache.(mentor, :num_solutions_mentored)
-    new_values[2] = Mentor::UpdateSatisfactionPercentage.(mentor)
-
-    # This should happen in band at the same time as the above code
+    # Firstly we want to update the number of finished discussions
+    # for this track. It's important to do this first as the other
+    # methods below us this for quicker calculation
     Mentor::UpdateNumFinishedDiscussions.(mentor, track)
 
-    return unless old_values != new_values
+    # Now we do the other essential checks
+    User::ResetCache.(mentor, :num_students_mentored)
+    User::ResetCache.(mentor, :num_solutions_mentored)
+    Mentor::UpdateSatisfactionPercentage.(mentor)
 
     # These can safely be defered into other jobs
     User::UpdateSupermentorRole.defer(mentor)

7c5c6771f786558f08d833c53809a50462aff549
Jeremy Walker <jez.walker@gmail.com>
2023-09-28 13:32:03 +0100

    Fix more tests

--- app/commands/user/update_automator_role.rb
+++ app/commands/user/update_automator_role.rb
@@ -3,20 +3,24 @@ class User::UpdateAutomatorRole
 
   initialize_with :user, :track
 
-  def call = mentorship.update!(automator: eligible?)
+  def call
+    mentorship&.update!(automator: eligible?)
+  end
 
   private
   def eligible?
     return true if explicit?
     return false unless user.mentor?
-    return false unless mentorship
     return false if user.mentor_satisfaction_percentage.to_i < MIN_SATISFACTION_PERCENTAGE
     return false if mentorship.num_finished_discussions < MIN_FINISHED_MENTORING_SESSIONS
 
     true
   end
 
+  memoize
   def mentorship = user.track_mentorships.find_by(track:)
+
+  memoize
   def explicit? = automators.fetch(user.handle, []).include?(track.slug)
 
   memoize

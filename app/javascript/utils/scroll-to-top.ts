39e688c568072601fc9659b2ac0062de00d9bc7f
dem4ron <demaaron88@gmail.com>
2023-09-29 11:20:41 +0200

    Add JSDoc to function

--- app/javascript/utils/scroll-to-top.ts
+++ app/javascript/utils/scroll-to-top.ts
@@ -1,3 +1,22 @@
+/**
+ * Scrolls the page to a specific element identified by the `data-scroll-top-anchor` attribute.
+ * If no anchor is provided, the function defaults to scrolling to the top of the page.
+ *
+ * @param {string} [anchorName] - The value for the `data-scroll-top-anchor` attribute.
+ *                                Ensure the element has this attribute format: `data-scroll-top-anchor="${anchorName}"`.
+ * @param {number} [yOffset=0] - The vertical offset from the element. A positive value scrolls above the element,
+ *                               while a negative value scrolls below.
+ *
+ * @example
+ * HTML element:
+ * ```html
+ * <div data-scroll-top-anchor="contributors-list"></div>
+ * ```
+ * JavaScript:
+ * ```javascript
+ * scrollToTop("contributors-list", 32);  // Scrolls to 32 pixels above the element.
+ * ```
+ */
 export function scrollToTop(anchorName?: string, yOffset?: number): void {
   if (!anchorName) {
     window.scrollTo(0, 0)

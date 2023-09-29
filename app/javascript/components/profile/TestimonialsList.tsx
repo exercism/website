aa2308ab364e8021ca8c3e15fa4b7cff9e3ee494
dem4ron <demaaron88@gmail.com>
2023-09-29 10:46:57 +0200

    Add Profile/TestimonialsList anchor + 32px yOffset

--- app/javascript/components/profile/TestimonialsList.tsx
+++ app/javascript/components/profile/TestimonialsList.tsx
@@ -70,7 +70,7 @@ export default function TestimonialsList({
               total={resolvedData.meta.totalPages}
               setPage={(p) => {
                 setPage(p)
-                scrollToTop()
+                scrollToTop('profile-testimonials', 32)
               }}
             />
           </>

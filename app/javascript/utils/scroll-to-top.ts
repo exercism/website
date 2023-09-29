ac97a3927e46e458aab798fecf9fcbb0b778820d
dem4ron <demaaron88@gmail.com>
2023-09-29 10:58:52 +0200

    Adjust scrollToTop function

--- app/javascript/utils/scroll-to-top.ts
+++ app/javascript/utils/scroll-to-top.ts
@@ -1,12 +1,17 @@
-export function scrollToTop(anchorName?: string): void {
+export function scrollToTop(anchorName?: string, yOffset?: number): void {
   if (!anchorName) {
     window.scrollTo(0, 0)
   } else {
     const element = document.querySelector(
       `[data-scroll-top-anchor="${anchorName}"]`
     )
-    if (element) {
-      element.scrollIntoView(true)
-    }
+    if (!element) return
+    const rect = element.getBoundingClientRect()
+    const currentScrollPosition =
+      window.scrollY || document.documentElement.scrollTop
+    window.scrollTo({
+      top: currentScrollPosition + rect.top - (yOffset || 0),
+      behavior: 'instant',
+    })
   }
 }

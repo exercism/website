57b34ecc175ea32adb7444a0a23a6204abf7aee7
dem4ron <demaaron88@gmail.com>
2023-10-06 12:07:24 +0200

    Refactor

--- app/javascript/utils/anchor-docs-headers.ts
+++ app/javascript/utils/anchor-docs-headers.ts
@@ -2,48 +2,64 @@ import { assetUrl } from './assets'
 import { copyToClipboard } from './copyToClipboard'
 
 export function appendAnchorButtonsToDocsHeaders() {
-  const currentPath = window.location.pathname
-
-  if (!currentPath.includes('/docs')) return
+  if (!window.location.pathname.includes('/docs')) return
 
   const docsHeaders = document.querySelectorAll('[id^="h-"')
 
-  const anchorIcon = document.createElement('img')
-  anchorIcon.src = assetUrl('icons/link.svg')
-  anchorIcon.height = 16
-  anchorIcon.width = 16
+  docsHeaders.forEach(attachAnchorButton)
+}
 
+function attachAnchorButton(headerElement: Element) {
+  const header = headerElement as HTMLElement
+  header.classList.add('anchored-header')
+
+  const button = createAnchorButton(header)
+  header.insertBefore(button, header.firstChild)
+
+  button.addEventListener('click', (event) => handleButtonClick(event, header))
+}
+
+function createAnchorButton(header: HTMLElement): HTMLElement {
   const button = document.createElement('button')
   button.className = 'copy-header-link-button'
-  button.appendChild(anchorIcon)
 
+  const icon = createAnchorIcon()
+  button.appendChild(icon)
+
+  const headerText = header.textContent || header.innerText
+  button.setAttribute('aria-label', `Copy link for header: ${headerText}`)
+
+  return button
+}
+
+function createAnchorIcon(): HTMLImageElement {
+  const icon = document.createElement('img')
+  icon.src = assetUrl('icons/link.svg')
+  icon.height = 16
+  icon.width = 16
+
+  return icon
+}
+
+function handleButtonClick(event: Event, header: HTMLElement) {
+  event.preventDefault()
+
+  const baseURL = window.location.href.split('#')[0]
   const copyFeedback = document.createElement('span')
   copyFeedback.className = 'copy-header-link-feedback'
 
-  docsHeaders.forEach((headerElement) => {
-    const header = headerElement as HTMLElement
-    header.classList.add('anchored-header')
+  copyToClipboard(baseURL + `#${header.id}`)
+    .then(() => displayFeedback(copyFeedback, header, 'Copied!'))
+    .catch(() => displayFeedback(copyFeedback, header, 'Failed to copy!'))
+}
 
-    const buttonClone = button.cloneNode(true) as HTMLElement
-    const headerText = header.textContent || header.innerText
-    buttonClone.setAttribute(
-      'aria-label',
-      `Copy link for header: ${headerText}`
-    )
-    header.insertBefore(buttonClone, header.firstChild)
+function displayFeedback(
+  feedbackElem: HTMLElement,
+  header: HTMLElement,
+  message: string
+) {
+  feedbackElem.innerText = message
+  header.insertBefore(feedbackElem, header.firstChild)
 
-    function displayFeedback(message: string = 'Copied!') {
-      header.insertBefore(copyFeedback, buttonClone)
-      copyFeedback.innerText = message
-      setTimeout(() => header.removeChild(copyFeedback), 1000)
-    }
-
-    buttonClone.addEventListener('click', (event) => {
-      event.preventDefault()
-      const baseURL = window.location.href.split('#')[0]
-      copyToClipboard(baseURL + `#${header.id}`)
-        .then(() => displayFeedback())
-        .catch(() => displayFeedback('Failed to copy!'))
-    })
-  })
+  setTimeout(() => header.removeChild(feedbackElem), 1000)
 }

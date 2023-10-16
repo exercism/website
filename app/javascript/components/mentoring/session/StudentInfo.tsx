d838c7439542dd76ced1c78a6c78ce22a8dda4ce
dem4ron <demaaron88@gmail.com>
2023-10-16 12:21:19 +0200

    Refactor StudentInfo

--- app/javascript/components/mentoring/session/StudentInfo.tsx
+++ app/javascript/components/mentoring/session/StudentInfo.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { Avatar, Reputation } from '@/components/common'
 import type { Student } from '@/components/types'
-import { Avatar, GraphicalIcon, Icon, Reputation } from '@/components/common'
-import { FavoritableStudent, FavoriteButton } from './FavoriteButton'
 import { PreviousSessionsLink } from './PreviousSessionsLink'
 import { HandleWithFlair } from '@/components/common/HandleWithFlair'
 import { Pronouns } from '@/components/common/Pronouns'
+import { ScreenSizeContext } from './ScreenSizeContext'
+import { ToggleMoreInformationButton } from './student-info/ToggleMoreInformationButton'
+import { StudentInfoActions } from './student-info/StudentInfoActions'
+import { StudentTrackObjectives } from './student-info/StudentTrackObjectives'
+import { ReducedStudentInfo } from './student-info/ReducedStudentInfo'
 
 export const StudentInfo = ({
   student,
@@ -13,36 +17,15 @@ export const StudentInfo = ({
   student: Student
   setStudent: (student: Student) => void
 }): JSX.Element => {
+  const { isBelowLgWidth = false } = useContext(ScreenSizeContext) || {}
   const [showMoreInformation, setShowMoreInformation] = useState(false)
 
-  if (!showMoreInformation)
+  if (!showMoreInformation && isBelowLgWidth)
     return (
-      <div className="student-info">
-        <div className="flex">
-          <div className="flex-grow">
-            <div className="handle-block">
-              <div className="handle">
-                <HandleWithFlair
-                  handle={student.handle}
-                  flair={student.flair}
-                  size="medium"
-                />
-              </div>
-              <Reputation
-                value={student.reputation.toString()}
-                type="primary"
-                size="small"
-              />
-            </div>
-            <div className="name">{student.name}</div>
-          </div>
-          <Avatar src={student.avatarUrl} handle={student.handle} />
-        </div>
-
-        <ToggleMoreInformationButton
-          onClick={() => setShowMoreInformation(true)}
-        />
-      </div>
+      <ReducedStudentInfo
+        student={student}
+        onClick={() => setShowMoreInformation(true)}
+      />
     )
   else
     return (
@@ -78,70 +61,12 @@ export const StudentInfo = ({
         </div>
         <StudentTrackObjectives student={student} />
 
-        <ToggleMoreInformationButton
-          rotate
-          onClick={() => setShowMoreInformation(false)}
-        />
+        {isBelowLgWidth && (
+          <ToggleMoreInformationButton
+            rotate
+            onClick={() => setShowMoreInformation(false)}
+          />
+        )}
       </div>
     )
-}
-
-function ToggleMoreInformationButton({
-  onClick,
-  rotate,
-}: {
-  onClick: () => void
-  rotate?: boolean
-}) {
-  return (
-    <button className="self-center mt-8" onClick={onClick}>
-      <Icon
-        icon="chevron-down"
-        alt="expand"
-        height={16}
-        width={16}
-        className={rotate ? 'rotate-180' : ''}
-      />
-    </button>
-  )
-}
-
-const StudentInfoActions = ({
-  student,
-  setStudent,
-}: {
-  student: Student
-  setStudent: (student: Student) => void
-}) => {
-  return (
-    <React.Fragment>
-      {student.links.favorite ? (
-        <FavoriteButton
-          student={student as FavoritableStudent}
-          onSuccess={(student) => setStudent(student)}
-        />
-      ) : null}
-    </React.Fragment>
-  )
-}
-
-function StudentTrackObjectives({
-  student,
-}: {
-  student: Student
-}): JSX.Element | null {
-  if (!student.trackObjectives) return null
-
-  return (
-    <details className="track-objectives c-details">
-      <summary>
-        <div className="--summary-inner justify-between select-none">
-          Explore {student.handle}&apos;s track goal(s)
-          <GraphicalIcon icon="chevron-right" className="--closed-icon" />
-          <GraphicalIcon icon="chevron-down" className="--open-icon" />
-        </div>
-      </summary>
-      <p>{student.trackObjectives}</p>
-    </details>
-  )
 }

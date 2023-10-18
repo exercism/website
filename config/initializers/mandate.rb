14150e5bbb3c3d25c9cfed95c8f709cebe59b604
Jeremy Walker <jez.walker@gmail.com>
2023-10-18 09:55:31 +0100

    Update config/initializers/mandate.rb

--- config/initializers/mandate.rb
+++ config/initializers/mandate.rb
@@ -53,6 +53,8 @@ module Mandate
     end
 
     def defer(*args, wait: nil, **kwargs)
+      # We need to convert the jobs to a hash before we serialize as there's no serialization format
+      # for a job. We do this here to avoid cluttering the codebase with this logic.
       if kwargs[:prereq_jobs]
         kwargs[:prereq_jobs] = kwargs[:prereq_jobs].map do |job|
           {

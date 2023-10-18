2270895a8d2fb0a89437eee812b1d87b85a1c249
Jeremy Walker <jez.walker@gmail.com>
2023-10-18 23:39:54 +0900

    Guard against double processing

--- app/commands/tooling_job/process.rb
+++ app/commands/tooling_job/process.rb
@@ -1,4 +1,7 @@
 class ToolingJob::Process
+  class AlreadyProcessedJobError < RuntimeError
+  end
+
   include Mandate
 
   initialize_with :id
@@ -6,19 +9,31 @@ class ToolingJob::Process
   def call
     send("process_#{job.type}_job!")
     job.processed!
+  rescue AlreadyProcessedJobError
+    # We can silently ignore this
   end
 
   private
   def process_test_runner_job!
+    guard_duplicate!(Submission::TestRun)
+
     Submission::TestRun::Process.(job)
   end
 
   def process_representer_job!
+    guard_duplicate!(Submission::Representation)
+
     Submission::Representation::Process.(job)
   end
 
   def process_analyzer_job!
+    guard_duplicate!(Submission::Analysis)
+
     Submission::Analysis::Process.(job)
+  end
+
+  def guard_duplicate!(klass)
+    raise AlreadyProcessedJobError if klass.where(tooling_job_id: job.id).exists?
   end
 
   memoize

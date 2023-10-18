1f2dce795d23b9dc5056e1631b83c8e651f9bd1f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-10-18 08:58:24 +0200

    Change order of columns in migration

--- db/migrate/20231017072611_create_solution_tags.rb
+++ db/migrate/20231017072611_create_solution_tags.rb
@@ -3,11 +3,11 @@ class CreateSolutionTags < ActiveRecord::Migration[7.0]
     return if Rails.env.production?
 
     create_table :solution_tags do |t|
-      t.string :tag, null: false
-
       t.references :solution, null: false, foreign_key: true
       t.references :exercise, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
+
+      t.string :tag, null: false
 
       t.index %i[solution_id tag], unique: true
 
--- db/migrate/20231017101049_create_exercise_tags.rb
+++ db/migrate/20231017101049_create_exercise_tags.rb
@@ -3,10 +3,10 @@ class CreateExerciseTags < ActiveRecord::Migration[7.0]
     return if Rails.env.production?
 
     create_table :exercise_tags do |t|
+      t.references :exercise, null: false, foreign_key: true
+
       t.string :tag, null: false
       t.boolean :filterable, null: false, default: true
-
-      t.references :exercise, null: false, foreign_key: true
 
       t.index %i[exercise_id tag], unique: true
 

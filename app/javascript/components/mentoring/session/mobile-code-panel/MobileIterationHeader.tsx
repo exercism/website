6f1e91a2b892bfa609c3535866ca50ea852a9e4b
dem4ron <demaaron88@gmail.com>
2023-10-19 12:35:43 +0200

    Refactor components, add missing finished class

--- app/javascript/components/mentoring/session/mobile-code-panel/MobileIterationHeader.tsx
+++ app/javascript/components/mentoring/session/mobile-code-panel/MobileIterationHeader.tsx
@@ -1,13 +1,11 @@
-import React, { useCallback, useContext, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { default as IterationSummaryWithWebsockets } from '@/components/track/IterationSummary'
-import type { Iteration, File } from '@/components/types'
 import { CloseButton } from '../CloseButton'
 import { OutOfDateNotice } from '@/components/track/iteration-summary/OutOfDateNotice'
 import { CopyButton } from '../iteration-view/iteration-header/CopyButton'
-import { SessionProps } from '../../Session'
-import { Modal } from '@/components/modals'
-import { DiscussionActions } from '../../discussion/DiscussionActions'
-import { Avatar, Icon, TrackIcon } from '@/components/common'
+import { SessionInfoModalProps, SessionInfoModal } from './SessionInfoModal'
+import { SessionInfoHamburgerButton } from './SessionInfoHamburgerButton'
+import type { Iteration, File } from '@/components/types'
 
 export type MobileIterationHeaderProps = {
   iteration: Iteration
@@ -47,7 +45,7 @@ export const MobileIterationHeader = ({
           showFeedbackIndicator={false}
         />
         {files ? <CopyButton files={files} /> : null}
-        <SessionActionButton onClick={handleModalOpen} />
+        <SessionInfoHamburgerButton onClick={handleModalOpen} />
       </header>
       <SessionInfoModal
         open={modalOpen}
@@ -61,67 +59,5 @@ export const MobileIterationHeader = ({
         track={track}
       />
     </>
-  )
-}
-
-type SessionInfoModalProps = {
-  open: boolean
-  onClose: () => void
-  session: SessionProps
-  setSession: (session: SessionProps) => void
-} & Pick<
-  SessionProps,
-  'discussion' | 'links' | 'student' | 'exercise' | 'track'
->
-function SessionInfoModal({
-  open,
-  onClose,
-  student,
-  track,
-  exercise,
-  discussion,
-  session,
-  setSession,
-}: SessionInfoModalProps) {
-  return (
-    <Modal open={open} onClose={onClose} className="m-session-info">
-      <div className="session-info-header">
-        <TrackIcon title={track.title} iconUrl={track.iconUrl} />
-        <div className="student">
-          <Avatar src={student.avatarUrl} handle={student.handle} />
-          <div className="info">
-            <div className="handle">{student.handle}</div>
-            <div className="exercise">
-              on {exercise.title} in {track.title}{' '}
-            </div>
-          </div>
-        </div>
-      </div>
-      {discussion ? (
-        <div className="flex flex-col gap-8 items-center">
-          <DiscussionActions
-            {...discussion}
-            session={session}
-            setSession={setSession}
-          />
-        </div>
-      ) : null}
-    </Modal>
-  )
-}
-
-export function SessionActionButton({
-  onClick,
-}: {
-  onClick: () => void
-}): JSX.Element {
-  return (
-    <button
-      className="btn-s btn-default download-btn"
-      type="button"
-      onClick={onClick}
-    >
-      <Icon icon="hamburger" alt="Download solution" />
-    </button>
   )
 }

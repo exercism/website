420627f9b140c33faae50fc0ee74bfae43b53548
dem4ron <demaaron88@gmail.com>
2023-10-20 15:46:14 +0200

    Change highlighter

--- app/javascript/components/mentoring/session/FileViewer.tsx
+++ app/javascript/components/mentoring/session/FileViewer.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useContinuousHighlighting } from '@/hooks/use-syntax-highlighting'
+import { useHighlighting } from '@/hooks/use-syntax-highlighting'
 import type { File } from '@/components/types'
 
 export const FileViewer = ({
@@ -11,7 +11,7 @@ export const FileViewer = ({
   indentSize: number
   file: File
 }): JSX.Element => {
-  const parentRef = useContinuousHighlighting<HTMLPreElement>(file.content)
+  const parentRef = useHighlighting<HTMLPreElement>(file.content)
 
   return (
     <pre ref={parentRef}>
@@ -20,9 +20,8 @@ export const FileViewer = ({
         data-highlight-line-numbers={true}
         data-highlight-line-number-start={1}
         style={{ tabSize: indentSize }}
-      >
-        {file.content}
-      </code>
+        dangerouslySetInnerHTML={{ __html: file.content }}
+      />
     </pre>
   )
 }

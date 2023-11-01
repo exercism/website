8937ce03dc2cb94813e77b0d0993785466571f9c
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-01 09:15:04 +0100

    Extract update_published_iteration_head_tests_status! from Solution to command

---
+++ app/commands/solution/update_published_iteration_head_tests_status.rb
@@ -0,0 +1,12 @@
+class Solution::UpdatePublishedIterationHeadTestsStatus
+  include Mandate
+
+  initialize_with :solution, :status
+
+  def call
+    return if solution.published_iteration_head_tests_status == status.to_sym
+
+    solution.update_column(:published_iteration_head_tests_status, status)
+    Solution::SyncToSearchIndex.defer(solution)
+  end
+end

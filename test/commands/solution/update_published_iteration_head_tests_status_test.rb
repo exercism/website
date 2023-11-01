9d617247f5c55a544386fe7c48bb26d4356cf114
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-01 09:28:02 +0100

    Update tags when updating published_iteration_head_tests_status

--- test/commands/solution/update_published_iteration_head_tests_status_test.rb
+++ test/commands/solution/update_published_iteration_head_tests_status_test.rb
@@ -18,6 +18,14 @@ class Solution::UpdatePublishedIterationHeadTestsStatusTest < ActiveSupport::Tes
     Solution::UpdatePublishedIterationHeadTestsStatus.(solution, :passed)
   end
 
+  test "updates solution tags when status is different" do
+    solution = create :practice_solution, published_iteration_head_tests_status: :queued
+
+    Solution::UpdateTags.expects(:defer).with(solution).once
+
+    Solution::UpdatePublishedIterationHeadTestsStatus.(solution, :passed)
+  end
+
   test "does not update solution when status is the same" do
     updated_at = Time.current - 1.week
     solution = create(:practice_solution, updated_at:)
@@ -31,6 +39,14 @@ class Solution::UpdatePublishedIterationHeadTestsStatusTest < ActiveSupport::Tes
     solution = create :practice_solution
 
     Solution::SyncToSearchIndex.expects(:defer).never
+
+    Solution::UpdatePublishedIterationHeadTestsStatus.(solution, solution.published_iteration_head_tests_status)
+  end
+
+  test "does not update solution tags when status is the same" do
+    solution = create :practice_solution
+
+    Solution::UpdateTags.expects(:defer).never
 
     Solution::UpdatePublishedIterationHeadTestsStatus.(solution, solution.published_iteration_head_tests_status)
   end

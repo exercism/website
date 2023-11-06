b8516c88e68aadf922fa85c8fb9f10432a5d1aff
dem4ron <demaaron88@gmail.com>
2023-11-06 13:47:14 +0100

    Clean up DashboardHeader

--- app/javascript/components/training-data/dashboard/DashboardHeader.tsx
+++ app/javascript/components/training-data/dashboard/DashboardHeader.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { TextFilter } from '@/components/mentoring/TextFilter'
 import { TrackFilter } from '@/components/mentoring/inbox/TrackFilter'
-import { Sorter } from '@/components/mentoring/Sorter'
 import { DashboardHeaderProps } from './Dashboard.types'
 
 export function DashboardHeader({
@@ -10,8 +9,6 @@ export function DashboardHeader({
   setTrack,
   criteria,
   setCriteria,
-  setOrder,
-  setPage,
 }: DashboardHeaderProps) {
   return (
     <header className="c-search-bar inbox-header">
@@ -29,13 +26,6 @@ export function DashboardHeader({
         id="discussion-filter"
         placeholder="Filter by student or exercise name"
       />
-      {/* we may not need a sorter */}
-      {/* <Sorter
-        sortOptions={sortOptions}
-        order={request.query.order}
-        setOrder={setOrder}
-        setPage={setPage}
-      /> */}
     </header>
   )
 }

f2ab3be45f7b5fb183b5eef0bb044d3fc9f1d326
Jeremy Walker <jez.walker@gmail.com>
2023-11-07 13:14:53 +0900

    Fix the tests

--- db/migrate/20231106122534_add_multiple_files_to_samples.rb
+++ db/migrate/20231106122534_add_multiple_files_to_samples.rb
@@ -1,5 +1,7 @@
 class AddMultipleFilesToSamples < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+
     add_column :training_data_code_tags_samples, :files, :text, null: false
 
     TrainingData::CodeTagsSample.find_each do |sample|

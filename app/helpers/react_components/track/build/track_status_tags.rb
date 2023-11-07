bfbdf1655bd3104c227db8c7e0c12bfcc4882c2d
Jeremy Walker <jez.walker@gmail.com>
2023-11-07 18:01:37 +0900

    Combine tags ruby-side

--- app/helpers/react_components/track/build/track_status_tags.rb
+++ app/helpers/react_components/track/build/track_status_tags.rb
@@ -4,10 +4,20 @@ module ReactComponents
       initialize_with :tags, :solution_counts
       def to_s
         super("track-build-track-status-tags", {
-          tags:,
-          solution_counts:,
+          tags: combined_tags,
           editor: current_user.maintainer?
         })
+      end
+
+      def combined_tags
+        tags.map do |tag|
+          {
+            tag: tag.tag,
+            enabled: tag.enabled?,
+            filterable: tag.filterable?,
+            num_solution: solution_counts[tag.tag]
+          }
+        end
       end
     end
   end

4fc48068259e644c97e078dbb888936d0ea2c3fb
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-07 16:09:23 +0100

    Fix migrations test

--- test/misc/migrations_test.rb
+++ test/misc/migrations_test.rb
@@ -4,7 +4,7 @@ class MigrationsTest < ActiveSupport::TestCase
   test "don't modify tables in production" do
     ApplicationRecord.connection.migration_context.migrations.each do |migration_proxy|
       # We only started running migrations manually after the below time
-      next if migration_proxy.version <= 20231017101049 # rubocop:disable Style/NumericLiterals
+      next if migration_proxy.version <= 20231103102049 # rubocop:disable Style/NumericLiterals
 
       migration = File.read(migration_proxy.filename)
       assert_includes migration, "return if Rails.env.production?"

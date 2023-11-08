081a53e7958448ea83cd6d36a08891e910ca3821
dem4ron <demaaron88@gmail.com>
2023-11-08 13:52:07 +0100

    Simplify logic

--- app/javascript/hooks/use-syntax-highlighting.tsx
+++ app/javascript/hooks/use-syntax-highlighting.tsx
@@ -19,27 +19,16 @@ export function useContinuousHighlighting<T extends HTMLElement>(
   html?: string
 ): RefObject<T> {
   const parentRef = useRef<T | null>(null)
-  const hasRunHighlighting = useRef(false)
   const prevHtml = useRef<string | undefined>(undefined)
 
   useLayoutEffect(() => {
-    if (hasRunHighlighting.current && prevHtml.current === html) {
-      return
-    }
-
     if (!parentRef.current || prevHtml.current === html) {
       return
     }
 
     highlightAllAlways(parentRef.current as unknown as ParentNode)
 
-    hasRunHighlighting.current = true
     prevHtml.current = html
-
-    // Reset the hasRunHighlighting flag on unmount to ensure it runs again after re-mounting
-    return () => {
-      hasRunHighlighting.current = false
-    }
   }, [html])
 
   return parentRef

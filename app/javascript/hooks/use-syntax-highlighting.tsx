12d03cf8e82995bffc7e21da3b8a88efca3a9173
dem4ron <demaaron88@gmail.com>
2023-11-08 13:39:14 +0100

    Fix highlighting bug

--- app/javascript/hooks/use-syntax-highlighting.tsx
+++ app/javascript/hooks/use-syntax-highlighting.tsx
@@ -19,11 +19,27 @@ export function useContinuousHighlighting<T extends HTMLElement>(
   html?: string
 ): RefObject<T> {
   const parentRef = useRef<T | null>(null)
+  const hasRunHighlighting = useRef(false)
+  const prevHtml = useRef<string | undefined>(undefined)
 
   useLayoutEffect(() => {
-    if (!parentRef.current) return
+    if (hasRunHighlighting.current && prevHtml.current === html) {
+      return
+    }
+
+    if (!parentRef.current || prevHtml.current === html) {
+      return
+    }
 
     highlightAllAlways(parentRef.current as unknown as ParentNode)
+
+    hasRunHighlighting.current = true
+    prevHtml.current = html
+
+    // Reset the hasRunHighlighting flag on unmount to ensure it runs again after re-mounting
+    return () => {
+      hasRunHighlighting.current = false
+    }
   }, [html])
 
   return parentRef

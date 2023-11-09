e2d1074a77736c3f94ff41811957060c5f4d6248
dem4ron <demaaron88@gmail.com>
2023-11-09 17:26:46 +0100

    Adjust css

--- app/javascript/components/student/solution-summary/LatestIterationLink.tsx
+++ app/javascript/components/student/solution-summary/LatestIterationLink.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { IterationSummary } from '../../track/IterationSummary'
 import { Iteration } from '../../types'
-import { useLogger } from '@/hooks'
 
 export default function LatestIterationLink({
   iteration,
 }: {
   iteration: Iteration
 }): JSX.Element {
-  useLogger('iteration', iteration)
   return (
-    <a className="iteration" href={iteration.links.self}>
+    <a className="latest-iteration-link mt-16" href={iteration.links.self}>
       <IterationSummary
         iteration={iteration}
         showSubmissionMethod={true}

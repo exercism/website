8259921c3f185eedcab984076307bab6c237c94e
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-16 13:58:42 +0100

    Update user_track reputation in migration

--- db/migrate/20231116124259_add_reputation_to_user_track.rb
+++ db/migrate/20231116124259_add_reputation_to_user_track.rb
@@ -3,5 +3,9 @@ class AddReputationToUserTrack < ActiveRecord::Migration[7.0]
     return if Rails.env.production?
 
     add_column :user_tracks, :reputation, :integer, null: false, default: 0
+
+    UserTrack.find_each do |user_track|
+      UserTrack::UpdateReputation.(user_track)
+    end
   end
 end

51732101037f6006ddea0ee317f261169556831f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-16 09:44:21 +0100

    Reduce indentation

--- app/controllers/training_data/code_tags_samples_controller.rb
+++ app/controllers/training_data/code_tags_samples_controller.rb
@@ -1,31 +1,29 @@
-module TrainingData
-  class CodeTagsSamplesController < ApplicationController
-    before_action :use_track, only: [:next]
+class TrainingData::CodeTagsSamplesController < ApplicationController
+  before_action :use_track, only: [:next]
 
-    def index; end
+  def index; end
 
-    def show
-      @sample = TrainingData::CodeTagsSample.first
-    end
+  def show
+    @sample = TrainingData::CodeTagsSample.first
+  end
 
-    def next
-      status = params[:status] || :untagged
-      sample = TrainingData::CodeTagsSample.unlocked.where(
-        track: @track,
-        status:
-      ).first
+  def next
+    status = params[:status] || :untagged
+    sample = TrainingData::CodeTagsSample.unlocked.where(
+      track: @track,
+      status:
+    ).first
 
-      raise unless sample
+    raise unless sample
 
-      sample.lock_for_editing!(current_user)
+    sample.lock_for_editing!(current_user)
 
-      redirect_to action: :show, id: sample.id
-    end
+    redirect_to action: :show, id: sample.id
+  end
 
-    def use_track
-      @track = Track.find(params[:track_id])
-    rescue StandardError
-      redirect_to(action: :index)
-    end
+  def use_track
+    @track = Track.find(params[:track_id])
+  rescue StandardError
+    redirect_to(action: :index)
   end
 end
--- app/controllers/training_data/dashboard_controller.rb
+++ app/controllers/training_data/dashboard_controller.rb
@@ -1,16 +1,14 @@
-module TrainingData
-  class DashboardController < ApplicationController
-    before_action :guard_trainer!
+class TrainingData::DashboardController < ApplicationController
+  before_action :guard_trainer!
 
-    def index
-      @training_data_dashboard_params = params.permit(:status, :order, :criteria, :page, :track_slug)
-      @statuses = TrainingData::CodeTagsSample.statuses.keys.map(&:to_s)
-    end
+  def index
+    @training_data_dashboard_params = params.permit(:status, :order, :criteria, :page, :track_slug)
+    @statuses = TrainingData::CodeTagsSample.statuses.keys.map(&:to_s)
+  end
 
-    def guard_trainer!
-      return true if current_user&.trainer?
+  def guard_trainer!
+    return true if current_user&.trainer?
 
-      redirect_to training_data_external_path
-    end
+    redirect_to training_data_external_path
   end
 end

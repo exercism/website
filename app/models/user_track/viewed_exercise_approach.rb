1828e77645af66fd8c112eeafe3a65fde53f6c84
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-17 10:40:11 +0100

    Keep track of viewed approaches (#6519)
    * Create table
    
    * Add model for viewed exercise approach
    
    * Add command to create viewed exercise approach
    
    * Keep track of viewed approaches
    
    * Remove viewed exercise approaches when resetting
    
    * Apply suggestions from code review
    
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>
    
    * Update db/migrate/20231116111312_create_user_track_viewed_exercise_approach.rb
    
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>
    
    * Fix column name
    
    * Fix model
    
    * Update db/migrate/20231116111312_create_user_track_viewed_exercise_approach.rb
    
    ---------
    
    Co-authored-by: Jeremy Walker <jez.walker@gmail.com>

---
+++ app/models/user_track/viewed_exercise_approach.rb
@@ -0,0 +1,11 @@
+class UserTrack::ViewedExerciseApproach < ApplicationRecord
+  belongs_to :user
+  belongs_to :track
+  belongs_to :approach, class_name: "Exercise::Approach"
+
+  has_one :user_track, # rubocop:disable Rails/HasManyOrHasOneDependent
+    ->(vcs) { where(track_id: vcs.track_id) },
+    foreign_key: :user_id,
+    primary_key: :user_id,
+    inverse_of: :viewed_exercise_approaches
+end
---
+++ test/models/user_track/viewed_exercise_approach_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserTrack::ViewedExerciseApproachTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

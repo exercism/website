59b830bf879cba3b9052af83573abd857ac6d5e1
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-17 11:48:02 +0100

    Materialize exercise

--- test/models/user_track/viewed_exercise_approach_test.rb
+++ test/models/user_track/viewed_exercise_approach_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTrack::ViewedExerciseApproachTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "materializes exercise from approach" do
+    user = create :user
+    track = create :track
+    exercise = create(:practice_exercise, track:)
+    approach = create(:exercise_approach, exercise:)
+
+    viewed_approach = UserTrack::ViewedExerciseApproach.create!(user:, track:, approach:)
+    assert_equal exercise, viewed_approach.exercise
+  end
 end

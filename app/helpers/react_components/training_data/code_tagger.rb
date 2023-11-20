17145bfb8dcea60524165d74e5ecfd0a71fa3fb6
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-20 15:02:50 +0100

    Add status

--- app/helpers/react_components/training_data/code_tagger.rb
+++ app/helpers/react_components/training_data/code_tagger.rb
@@ -3,7 +3,7 @@ module ReactComponents
     class CodeTagger < ReactComponent
       include Mandate
 
-      initialize_with :sample
+      initialize_with :sample, :status
 
       def to_s
         super("training-data-code-tagger", {
@@ -24,7 +24,7 @@ module ReactComponents
           },
           links: {
             confirm_tags_api: Exercism::Routes.update_tags_api_training_data_code_tags_sample_path(sample.uuid),
-            next_sample: Exercism::Routes.next_training_data_code_tags_samples_path(track:, status: sample.status),
+            next_sample: Exercism::Routes.next_training_data_code_tags_samples_path(track:, status:),
             training_data_dashboard: Exercism::Routes.training_data_root_path
           },
           sample: {

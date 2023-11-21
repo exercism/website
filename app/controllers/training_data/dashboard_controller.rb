bea51982c82f36a389bf3a6ed268c2e4d3a16194
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-21 09:34:43 +0100

    Move trainer filter to application controller

--- app/controllers/training_data/dashboard_controller.rb
+++ app/controllers/training_data/dashboard_controller.rb
@@ -1,14 +1,8 @@
 class TrainingData::DashboardController < ApplicationController
-  before_action :guard_trainer!
+  before_action :ensure_trainer!
 
   def index
     @training_data_dashboard_params = params.permit(:status, :order, :criteria, :page, :track_slug)
     @statuses = %w[needs_tagging needs_checking needs_checking_admin]
-  end
-
-  def guard_trainer!
-    return true if current_user.trainer?
-
-    redirect_to training_data_external_path
   end
 end

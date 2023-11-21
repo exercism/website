abd693cde898b3bc964f577e8f7b9af91e3b89d1
Erik Schierboom <erik_schierboom@hotmail.com>
2023-11-21 12:46:27 +0100

    Add tests for external controller

--- app/controllers/training_data/external_controller.rb
+++ app/controllers/training_data/external_controller.rb
@@ -4,8 +4,8 @@ class TrainingData::ExternalController < ApplicationController
   def index
     redirect_to training_data_root_path if current_user&.trainer?
 
-    @eligible = current_user.eligible_for_trainer?
-    @max_reputation_track = current_user.user_tracks.order(reputation: :desc).first
+    @eligible = current_user&.eligible_for_trainer?
+    @max_reputation_track = current_user && current_user.user_tracks.order(reputation: :desc).first
   end
 
   def become_trainer

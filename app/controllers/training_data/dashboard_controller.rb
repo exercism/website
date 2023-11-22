bd01ac17cab46d10ff1e75f30f2a1d0893625dc9
dem4ron <demaaron88@gmail.com>
2023-11-22 09:54:29 +0100

    Remove `app/views/training_data/dashboard` folder and its controller

--- app/controllers/training_data/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class TrainingData::DashboardController < ApplicationController
-  skip_before_action :authenticate_user!
-  before_action :ensure_trainer!
-
-  def index
-    redirect_to training_data_code_tags_samples_path
-  end
-end

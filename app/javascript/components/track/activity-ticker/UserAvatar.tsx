8f244759b5ea5eeebdff480c168f654ae4a435d1
dem4ron <demaaron88@gmail.com>
2023-11-24 14:23:36 +0100

    Wrap avatar in a fixed-dimensioned div

--- app/javascript/components/track/activity-ticker/UserAvatar.tsx
+++ app/javascript/components/track/activity-ticker/UserAvatar.tsx
@@ -8,10 +8,12 @@ export function UserAvatar({
 }): JSX.Element | null {
   if (!user) return null
   return (
-    <img
-      src={user.avatarUrl}
-      alt={`${user.handle}'s avatar`}
-      className="w-[36px] h-[36px] rounded-circle mr-12 flex-shrink-0"
-    />
+    <div className="w-[36px] h-[36px] mr-12">
+      <img
+        src={user.avatarUrl}
+        alt={`${user.handle}'s avatar`}
+        className="w-[36px] h-[36px] rounded-circle"
+      />
+    </div>
   )
 }

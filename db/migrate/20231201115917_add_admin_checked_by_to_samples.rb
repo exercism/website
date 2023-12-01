e233a1cfaddf9574d32e1cc8d8fcb13454bb3e8f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-12-01 14:09:46 +0100

    Fix typoe

--- db/migrate/20231201115917_add_admin_checked_by_to_samples.rb
+++ db/migrate/20231201115917_add_admin_checked_by_to_samples.rb
@@ -2,6 +2,6 @@ class AddAdminCheckedByToSamples < ActiveRecord::Migration[7.0]
   def change
     return if Rails.env.production?
 
-    add_reference :training_data_code_tags_samples, :admin_checked_by, null: true, foreign_key: { to_table: :users }, index: { name: "index_code_tags_samples_on_admin_tagged_by_id" }, if_not_exists: true
+    add_reference :training_data_code_tags_samples, :admin_checked_by, null: true, foreign_key: { to_table: :users }, index: { name: "index_code_tags_samples_on_admin_checked_by_id" }, if_not_exists: true
   end
 end

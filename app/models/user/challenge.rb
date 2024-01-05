578de79e9c7db47fd1c33ed1a0a7739055daf18b
Jeremy Walker <jez.walker@gmail.com>
2024-01-05 12:38:45 +0000

    Add new external 48in24 page

--- app/models/user/challenge.rb
+++ app/models/user/challenge.rb
@@ -2,5 +2,9 @@ class User::Challenge < ApplicationRecord
   belongs_to :user
 
   CHALLENGE_12_IN_23 = "12in23".freeze
-  CHALLENGES = [CHALLENGE_12_IN_23].freeze
+  CHALLENGE_48_IN_24 = "48in24".freeze
+  CHALLENGES = [
+    CHALLENGE_12_IN_23,
+    CHALLENGE_48_IN_24
+  ].freeze
 end

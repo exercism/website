00efdd873d2e739c5a13a90a8112baba1e09139e
dem4ron <demaaron88@gmail.com>
2024-01-17 14:35:59 +0100

    Add CopyToClipboard button

--- app/javascript/components/modals/track-welcome-modal/LHS/steps/SelectedLocalMachineStep.tsx
+++ app/javascript/components/modals/track-welcome-modal/LHS/steps/SelectedLocalMachineStep.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StepButton } from './components/StepButton'
 import { ButtonContainer } from './components/ButtonContainer'
+import CopyToClipboardButton from '@/components/common/CopyToClipboardButton'
+import { TrackContext } from '../..'
 
 export function SelectedLocalMachineStep({
   onContinueToLocalMachine,
   onReset,
 }: Record<'onReset' | 'onContinueToLocalMachine', () => void>): JSX.Element {
+  const { track } = useContext(TrackContext)
   return (
     <>
-      <header>
-        <p>
-          I&apos;ll write the text out later including a link to the CLI
-          walkthrough, and the copy-to-clipboard widget for Hello World. If you
-          get those links in, I&apos;ll do the rest.
-        </p>
-      </header>
+      <p className="mb-8">
+        I&apos;ll write the text out later including a link to the CLI
+        walkthrough, and the copy-to-clipboard widget for Hello World. If you
+        get those links in, I&apos;ll do the rest.
+      </p>
+      <CopyToClipboardButton
+        textToCopy={`exercism download --exercise=hello-world --track=${track.title}`}
+      />
       <ButtonContainer>
         <StepButton onClick={onReset}>Reset</StepButton>
         <StepButton onClick={onContinueToLocalMachine}>Continue</StepButton>

00eacbdc48e7538dc13baaf1cb6ac2973cf8dd65
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-18 10:10:18 +0100

    Use generic exercise

--- app/controllers/generic_exercises_controller.rb
+++ app/controllers/generic_exercises_controller.rb
@@ -5,7 +5,7 @@ class GenericExercisesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :start
 
   def show
-    @ps_data = Git::ProblemSpecifications::Exercise.new(params[:id])
+    @ps_data = GenericExercise.find_by!(slug: params[:id])
     @track_variants = Exercise.available.where(
       slug: params[:id],
       track_id: Track.active.select(:id)
--- app/models/generic_exercise.rb
+++ app/models/generic_exercise.rb
@@ -1,5 +1,12 @@
 class GenericExercise < ApplicationRecord
+  extend Mandate::Memoize
+
   enum status: { active: 0, deprecated: 1 }
 
+  delegate :description_html, to: :git
+
   def status = super.to_sym
+
+  memoize
+  def git = Git::ProblemSpecifications::Exercise.new(slug)
 end

21c040920bc27dc0010f6a7a3be28bed54d253db
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-18 11:27:57 +0100

    Don't run migration when not production

--- db/migrate/20240117152211_create_generic_exercises.rb
+++ db/migrate/20240117152211_create_generic_exercises.rb
@@ -1,5 +1,7 @@
 class CreateGenericExercises < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+
     create_table :generic_exercises do |t|
       t.string :slug, null: false
       t.string :title, null: false

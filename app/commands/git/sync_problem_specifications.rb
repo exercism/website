896b30379c513c4edd5c4f098c801f5b0c61ed4e
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-18 09:57:44 +0100

    Sync exercises from prob-specs

--- app/commands/git/sync_problem_specifications.rb
+++ app/commands/git/sync_problem_specifications.rb
@@ -6,13 +6,15 @@ class Git::SyncProblemSpecifications
   def call
     repo.update!
 
-    # repo.config[:posts].to_a.each do |data|
-    #   create_or_update_post(data)
-    # end
-
-    # repo.config[:stories].to_a.each do |data|
-    #   create_or_update_story(data)
-    # end
+    repo.exercises.each do |exercise|
+      GenericExercise::CreateOrUpdate.(
+        exercise.slug, exercise.title, exercise.blurb,
+        exercise.source, exercise.source_url, exercise.deep_dive_youtube_id,
+        exercise.deprecated? ? :deprecated : :active
+      )
+    rescue StandardError => e
+      Bugsnag.notify(e)
+    end
   end
 
   memoize

d95698a7c907d9d827fd053438cabcdf5e307830
Erik Schierboom <erik_schierboom@hotmail.com>
2024-01-18 10:19:57 +0100

    Use generic exercise to determine unimplemented

--- app/commands/track/retrieve_unimplemented_practice_exercises.rb
+++ app/commands/track/retrieve_unimplemented_practice_exercises.rb
@@ -4,19 +4,10 @@ class Track::RetrieveUnimplementedPracticeExercises
   initialize_with :track
 
   def call
-    prob_specs.active_exercises.reject do |exercise|
-      implemented_exercises.include?(exercise.slug) ||
-        foregone_exercises.include?(exercise.slug)
-    end
+    GenericExercise.active.where.not(slug: implemented_exercise_slugs + foregone_exercise_slugs)
   end
 
   private
-  memoize
-  def prob_specs = ProblemSpecifications.new
-
-  memoize
-  def implemented_exercises = track.practice_exercises.pluck(:slug).to_set
-
-  memoize
-  def foregone_exercises = track.git.foregone_exercises.to_set
+  def implemented_exercise_slugs = track.practice_exercises.pluck(:slug)
+  def foregone_exercise_slugs = track.git.foregone_exercises
 end

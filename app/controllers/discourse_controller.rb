f6d6d4e47d70ed93caa24a153bfe9fd51683c814
Jeremy Walker <jez.walker@gmail.com>
2024-01-24 14:18:04 +0000

    Add comment

--- app/controllers/discourse_controller.rb
+++ app/controllers/discourse_controller.rb
@@ -11,6 +11,8 @@ class DiscourseController < ApplicationController
     sso.bio = current_user.bio
     sso.sso_secret = secret
 
+    # Wait 30s on these so we don't get a race condition from Discourse
+    # not having updated its data by the time we run things.
     User::SetDiscourseGroups.defer(current_user, wait: 30.seconds)
     AwardBadgeJob.set(wait: 30.seconds).perform_later(current_user, :discourser)
 

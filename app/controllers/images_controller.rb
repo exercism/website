593d95d725a0dbaa6440d9685ba1dd78f38e9b10
Jeremy Walker <jez.walker@gmail.com>
2024-01-31 16:35:41 +0000

    Add endpoint for profile image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,6 +3,28 @@ class ImagesController < ApplicationController
 
   layout 'images'
 
+  def profile
+    @user = User.find_by!(handle: params[:user_handle])
+    @profile = @user&.profile
+
+    return render_404 unless @profile
+
+    track_ids = User::ReputationPeriod.where(
+      user: @user,
+      period: :forever,
+      about: :track,
+      category: :any
+    ).order(reputation: :desc).limit(3).pluck(:track_id)
+
+    @top_three_tracks = ::Track.active.where(id: track_ids).sort_by { |t| track_ids.index(t.id) }
+
+    @header_tags = []
+    @header_tags << { class: "tag staff", icon: :logo, title: "Exercism Staff" } if @user.staff?
+    @header_tags << { class: "tag maintainer", icon: :maintaining, title: "Maintainer" } if @user.maintainer?
+    @header_tags << { class: "tag insider", icon: :insiders, title: "Insider" } if @user.insider?
+    @header_tags = @header_tags.take(2)
+  end
+
   def solution
     @solution = Solution.for!(params[:user_handle], params[:track_slug], params[:exercise_slug])
   end

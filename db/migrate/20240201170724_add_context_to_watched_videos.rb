6c8d6752447ab8fd14d7bb33f91016db6048db55
Jeremy Walker <jez.walker@gmail.com>
2024-02-02 13:24:58 +0000

    Let context be blank

--- db/migrate/20240201170724_add_context_to_watched_videos.rb
+++ db/migrate/20240201170724_add_context_to_watched_videos.rb
@@ -2,7 +2,7 @@ class AddContextToWatchedVideos < ActiveRecord::Migration[7.0]
   def change
     return if Rails.env.production?
 
-    add_column :user_watched_videos, :context, :string, null: false, default: 0
+    add_column :user_watched_videos, :context, :string, null: true
     add_index :user_watched_videos, :context
   end
 end

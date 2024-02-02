36033dd202384a23a1605a2f25318504cd125456
dem4ron <demaaron88@gmail.com>
2024-02-02 11:08:54 +0100

    Replace containers with react component

--- app/models/user/watched_video.rb
+++ app/models/user/watched_video.rb
@@ -6,6 +6,16 @@ class User::WatchedVideo < ApplicationRecord
     dashboard: 1,
     editor: 2,
     solution_main: 3,
-    solution_dig_deeper: 4
+    solution_dig_deeper: 4,
+    blog: 5,
+    challenge: 6,
+    challenge_external: 7,
+    brief_introduction: 8,
+    community_interviews: 9,
+    community: 10,
+    generic_exercises: 11,
+    tracks_about: 12,
+    track_summary: 13,
+    exercises_external: 14
   }
 end

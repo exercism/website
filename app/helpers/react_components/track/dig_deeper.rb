5e9846097c85c63e37b5ae7a9dcef7de145f9bce
dem4ron <demaaron88@gmail.com>
2024-02-02 13:34:28 +0100

    Update dig_deeper

--- app/helpers/react_components/track/dig_deeper.rb
+++ app/helpers/react_components/track/dig_deeper.rb
@@ -30,7 +30,7 @@ class ReactComponents::Track::DigDeeper < ReactComponents::ReactComponent
       deep_dive_youtube_id: exercise.deep_dive_youtube_id,
       deep_dive_blurb: exercise.deep_dive_blurb,
       deep_dive_mark_as_seen_endpoint: Exercism::Routes.api_watched_videos_path(video_provider: :youtube,
-        video_id: exercise.deep_dive_youtube_id, context: :solution_dig_deeper)
+        video_id: exercise.deep_dive_youtube_id, context: 'solution_dig_deeper')
     }
   end
 

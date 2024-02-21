cbccd8b15032ccf4222ea55f1ec5c414acdc4fa3
dem4ron <demaaron88@gmail.com>
2024-02-21 16:31:20 +0100

    Return lazyHighlightAll if important regex is not suppoted by the browser

--- app/javascript/utils/lazy-highlight-all.ts
+++ app/javascript/utils/lazy-highlight-all.ts
@@ -1,4 +1,7 @@
+import { areAllRegExpFeaturesSupported } from './regex-check'
+
 export function lazyHighlightAll(): void {
+  if (!areAllRegExpFeaturesSupported()) return
   function applySyntaxHighlighting() {
     import('@/utils/highlight')
       .then((m) => {

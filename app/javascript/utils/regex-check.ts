b0f0dd88589476a1308bf12477178f8f51b76575
dem4ron <demaaron88@gmail.com>
2024-02-21 15:19:05 +0100

    Remove a couple well-supported tests

--- app/javascript/utils/regex-check.ts
+++ app/javascript/utils/regex-check.ts
@@ -6,34 +6,10 @@ declare global {
 
 export function areAllRegExpFeaturesSupported(): boolean {
   try {
-    const namedCaptureGroupsTest = new RegExp('(?<name>.*)')
-    if (!namedCaptureGroupsTest.exec('test')?.groups?.name) return false
-  } catch (e) {
-    console.log('Named capture groups are not supported:', e)
-    return false
-  }
-
-  try {
     const lookbehindAssertionsTest = new RegExp('(?<=a)b')
     if ('ab'.search(lookbehindAssertionsTest) === -1) return false
   } catch (e) {
     console.log('Lookbehind is not supported:', e)
-    return false
-  }
-
-  try {
-    const unicodePropertyEscapesTest = new RegExp('\\p{Script=Latin}', 'u')
-    if (!'a'.match(unicodePropertyEscapesTest)) return false
-  } catch (e) {
-    console.log('Unicode prop escape is not supported:', e)
-    return false
-  }
-
-  try {
-    const dotAllFlagTest = new RegExp('.', 's')
-    if (!dotAllFlagTest.test('\n')) return false
-  } catch (e) {
-    console.log('dotAll flag is not supported:', e)
     return false
   }
 

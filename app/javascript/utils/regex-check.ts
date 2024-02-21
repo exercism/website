2cb31a860468771abfd4ff4b59fcc90c5faa8c07
dem4ron <demaaron88@gmail.com>
2024-02-21 14:57:32 +0100

    Add logs

--- app/javascript/utils/regex-check.ts
+++ app/javascript/utils/regex-check.ts
@@ -9,6 +9,7 @@ export function areAllRegExpFeaturesSupported(): boolean {
     const namedCaptureGroupsTest = new RegExp('(?<name>.*)')
     if (!namedCaptureGroupsTest.exec('test')?.groups?.name) return false
   } catch (e) {
+    console.log('Named capture groups are not supported:', e)
     return false
   }
 
@@ -16,6 +17,7 @@ export function areAllRegExpFeaturesSupported(): boolean {
     const lookbehindAssertionsTest = new RegExp('(?<=a)b')
     if ('ab'.search(lookbehindAssertionsTest) === -1) return false
   } catch (e) {
+    console.log('Lookbehind is not supported:', e)
     return false
   }
 
@@ -23,6 +25,7 @@ export function areAllRegExpFeaturesSupported(): boolean {
     const unicodePropertyEscapesTest = new RegExp('\\p{Script=Latin}', 'u')
     if (!'a'.match(unicodePropertyEscapesTest)) return false
   } catch (e) {
+    console.log('Unicode prop escape is not supported:', e)
     return false
   }
 
@@ -30,6 +33,7 @@ export function areAllRegExpFeaturesSupported(): boolean {
     const dotAllFlagTest = new RegExp('.', 's')
     if (!dotAllFlagTest.test('\n')) return false
   } catch (e) {
+    console.log('dotAll flag is not supported:', e)
     return false
   }
 
@@ -37,6 +41,7 @@ export function areAllRegExpFeaturesSupported(): boolean {
     const hasIndicesFlagTest = new RegExp('.', 'd')
     if ('a'.match(hasIndicesFlagTest)?.indices === undefined) return false
   } catch (e) {
+    console.log('Has indices flag is not supported:', e)
     return false
   }
 

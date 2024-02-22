34e5a13daf848298c1a444a3b07bc58d2a576acc
Erik Schierboom <erik_schierboom@hotmail.com>
2024-02-22 08:05:02 +0100

    Enable syntax highlight for red (#6802)
    * Enable syntax highlight for red
    
    * Update to highlightjs-redbol 2.1.1
    
    * Update `highlightjs-redbol` 2.1.1 -> 2.1.2
    
    ---------
    
    Co-authored-by: dem4ron <demaaron88@gmail.com>

--- app/javascript/utils/highlight.ts
+++ app/javascript/utils/highlight.ts
@@ -1,12 +1,13 @@
 import * as highlighter from 'highlight.js'
 import { useLayoutEffect, useRef } from 'react'
+import { areAllRegExpFeaturesSupported } from './regex-check'
 import setupABAP from 'highlightjs-sap-abap'
 import setupCobol from 'highlightjs-cobol'
 import setupBqn from 'highlightjs-bqn'
 import setupZig from 'highlightjs-zig'
 import setupGleam from '@gleam-lang/highlight.js-gleam'
 import setupBallerina from '@ballerina/highlightjs-ballerina'
-import { areAllRegExpFeaturesSupported } from './regex-check'
+import setupRed from 'highlightjs-redbol'
 
 if (areAllRegExpFeaturesSupported()) {
   highlighter.default.registerLanguage('abap', setupABAP)
@@ -15,6 +16,7 @@ if (areAllRegExpFeaturesSupported()) {
   highlighter.default.registerLanguage('zig', setupZig)
   highlighter.default.registerLanguage('gleam', setupGleam)
   highlighter.default.registerLanguage('ballerina', setupBallerina)
+  highlighter.default.registerLanguage('red', setupRed)
 }
 
 highlighter.default.configure({

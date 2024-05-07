c85d5f0fea88936e8afc07342b69052236d54d54
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2024-05-07 12:04:41 +0200

    Restirct hljs by browser version number (#6901)

--- app/javascript/utils/regex-check.ts
+++ app/javascript/utils/regex-check.ts
@@ -1,3 +1,5 @@
+import UAParser from 'ua-parser-js'
+
 declare global {
   interface RegExpMatchArray {
     indices?: number[][]
@@ -14,4 +16,73 @@ export function areAllRegExpFeaturesSupported(): boolean {
   }
 
   return true
+}
+
+function compareVersions(versionA: string, versionB: string): number {
+  const partsA = versionA.split('.').map(Number)
+  const partsB = versionB.split('.').map(Number)
+
+  for (let i = 0; i < Math.max(partsA.length, partsB.length); i++) {
+    const partA = partsA[i] || 0
+    const partB = partsB[i] || 0
+
+    if (partA > partB) {
+      return 1
+    } else if (partA < partB) {
+      return -1
+    }
+  }
+
+  return 0
+}
+
+export function isLookbehindSupported(): boolean {
+  const parser = new UAParser().getBrowser()
+
+  switch (parser.name) {
+    case 'IE':
+    case 'Opera mini':
+      return false
+    case 'Chrome': {
+      if (parser.version) return compareVersions(parser.version, '62') >= 0
+    }
+    case 'Edge': {
+      if (parser.version) return compareVersions(parser.version, '79') >= 0
+    }
+    case 'Firefox': {
+      if (parser.version) return compareVersions(parser.version, '78') >= 0
+    }
+    case 'Safari': {
+      if (parser.version) return compareVersions(parser.version, '16.4') >= 0
+    }
+    case 'Mobile Safari': {
+      if (parser.version) return compareVersions(parser.version, '16.4') >= 0
+    }
+    case 'Opera': {
+      if (parser.version) return compareVersions(parser.version, '49') >= 0
+    }
+    case 'Opera Mobile': {
+      if (parser.version) return compareVersions(parser.version, '80') >= 0
+    }
+    case 'Samsung Internet': {
+      if (parser.version) return compareVersions(parser.version, '8.2') >= 0
+    }
+    case 'UCBrowser': {
+      if (parser.version) return compareVersions(parser.version, '15.5') >= 0
+    }
+    case 'Android Browser': {
+      if (parser.version) return compareVersions(parser.version, '124') >= 0
+    }
+    case 'QQ Browser': {
+      if (parser.version) return compareVersions(parser.version, '14.9') >= 0
+    }
+    case 'Baidu Browser': {
+      if (parser.version) return compareVersions(parser.version, '13.52') >= 0
+    }
+    case 'KaiOS Browser': {
+      if (parser.version) return compareVersions(parser.version, '3.1') >= 0
+    }
+    default:
+      return true
+  }
 }

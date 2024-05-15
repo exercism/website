6d3eade1fb3b813f234edea5522401730c20fa3f
Erik Schierboom <erik_schierboom@hotmail.com>
2024-05-15 13:40:11 +0200

    Handle tracks without exercises gracefully on journey page (#6914)
    * Handle tracks without exercises gracefully on journey page
    
    * Update app/javascript/components/journey/types.ts

--- app/javascript/components/journey/types.ts
+++ app/javascript/components/journey/types.ts
@@ -30,6 +30,8 @@ export class TrackProgress {
   progressChart: TrackProgressChart
 
   get completion(): number {
+    if (this.numExercises === 0) return 0
+
     return (100 * this.numCompletedExercises) / this.numExercises
   }
 

17733fcc2e784b36bf113714e11427bbfcd50c0e
dem4ron <demaaron88@gmail.com>
2024-06-26 14:16:19 +0200

    Fix mistakes

--- app/javascript/utils/handle-accessibility-fullscreen.ts
+++ app/javascript/utils/handle-accessibility-fullscreen.ts
@@ -1,14 +1,19 @@
+let shouldToggleDarkThemeOnFullscreenChange = false
+
 export function toggleDarkThemeOnFullscreenChange() {
-  const fullscreenElement = document.fullscreenElement
-  const youtubeIframe = document.querySelector('iframe')
+  const fullscreenElement =
+    document.fullscreenElement as HTMLIFrameElement | null
 
   if (
-    fullscreenElement === youtubeIframe &&
+    fullscreenElement &&
+    fullscreenElement.src.includes('youtube.com') &&
     document.body.classList.contains('theme-accessibility-dark')
   ) {
     document.body.classList.remove('theme-accessibility-dark')
-  } else {
+    shouldToggleDarkThemeOnFullscreenChange = true
+  } else if (!fullscreenElement && shouldToggleDarkThemeOnFullscreenChange) {
     document.body.classList.add('theme-accessibility-dark')
+    shouldToggleDarkThemeOnFullscreenChange = false
   }
 }
 

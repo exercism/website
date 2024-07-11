7851c9d0a4c83daa7ce2f0f121030528ccb6ccd7
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-11 15:49:29 +0200

    Auto update maintainership

--- app/commands/github/team_member/create.rb
+++ app/commands/github/team_member/create.rb
@@ -3,5 +3,12 @@ class Github::TeamMember::Create
 
   initialize_with :user_id, :team_name
 
-  def call = ::Github::TeamMember.find_create_or_find_by!(user_id:, team_name:)
+  def call
+    ::Github::TeamMember.find_create_or_find_by!(user_id:, team_name:).tap do
+      User::UpdateMaintainer.(user) if user
+    end
+  end
+
+  memoize
+  def user = User.find_by(uid: user_id)
 end
--- app/commands/github/team_member/destroy.rb
+++ app/commands/github/team_member/destroy.rb
@@ -3,5 +3,12 @@ class Github::TeamMember::Destroy
 
   initialize_with :user_id, :team_name
 
-  def call = ::Github::TeamMember.where(user_id:, team_name:).destroy_all
+  def call
+    ::Github::TeamMember.where(user_id:, team_name:).destroy_all.tap do
+      User::UpdateMaintainer.(user) if user
+    end
+  end
+
+  memoize
+  def user = User.find_by(uid: user_id)
 end

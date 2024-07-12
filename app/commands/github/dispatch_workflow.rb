7ecc0335778e112ef4a0a5e047b17218e057eb1b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-12 09:28:00 +0200

    Allow dispatching workflow

---
+++ app/commands/github/dispatch_workflow.rb
@@ -0,0 +1,11 @@
+class Github::DispatchWorkflow
+  include Mandate
+
+  initialize_with :repo, :workflow_id
+
+  def call = Exercism.octokit_client.post(api_url, body)
+
+  private
+  def api_url = "https://api.github.com/repos/exercism/#{repo}/actions/workflows/configlet.yml/dispatches"
+  def body = { ref: "main" }.to_json
+end

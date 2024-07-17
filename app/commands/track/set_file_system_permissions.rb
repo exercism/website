ce69323bfb0de7217b2cdd9c7cbfb665f762b5ad
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-17 15:36:18 +0200

    Don't set safe directory if already set

--- app/commands/track/set_file_system_permissions.rb
+++ app/commands/track/set_file_system_permissions.rb
@@ -4,6 +4,8 @@ class Track::SetFileSystemPermissions
   initialize_with :track
 
   def call
+    return if safe_directory_already_set?
+
     add_safe_directory!
 
     # A redeploy of the website is required to pick up the new safe directory
@@ -11,11 +13,17 @@ class Track::SetFileSystemPermissions
   end
 
   private
+  def safe_directory_already_set?
+    Kernel.system("git config --global --get safe.directory #{safe_directory}")
+  end
+
   def add_safe_directory!
-    Kernel.system("git config --global --add safe.directory /mnt/efs/repos/#{track.slug}")
+    Kernel.system("git config --global --add safe.directory #{safe_directory}")
   end
 
   def trigger_redeploy!
     Github::DispatchWorkflow.("website", "deploy.yml")
   end
+
+  def safe_directory = "/mnt/efs/repos/#{track.slug}"
 end

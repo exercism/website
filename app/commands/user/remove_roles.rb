4f862f99a8c11ad1610422863603b43539e2cc35
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-23 13:28:59 +0200

    Early return when updating roles (#6994)

--- app/commands/user/remove_roles.rb
+++ app/commands/user/remove_roles.rb
@@ -1,9 +1,13 @@
 class User::RemoveRoles
   include Mandate
 
-  initialize_with :user, :roles
+  initialize_with :user, :roles do
+    @roles = roles.to_set
+  end
 
   def call
+    return if roles.disjoint?(user.roles)
+
     user.update(roles: user.roles - roles)
 
     User::SetDiscordRoles.defer(user)
--- test/commands/user/remove_roles_test.rb
+++ test/commands/user/remove_roles_test.rb
@@ -12,8 +12,29 @@ class User::RemoveRolesTest < ActiveSupport::TestCase
     refute_includes user.roles, :supermentor
   end
 
+  test "noop when user does not have roles" do
+    updated_at = Time.current - 5.days
+    user = create :user, roles: %i[staff]
+    user.update_column(:updated_at, updated_at)
+
+    # Sanity check
+    assert_includes user.roles, :staff
+    refute_includes user.roles, :maintainer
+    refute_includes user.roles, :supermentor
+
+    User::SetDiscordRoles.expects(:defer).never
+    User::InsidersStatus::TriggerUpdate.expects(:call).never
+
+    User::RemoveRoles.(user, %i[maintainer supermentor])
+
+    assert_includes user.roles, :staff
+    refute_includes user.roles, :maintainer
+    refute_includes user.roles, :supermentor
+    assert_equal updated_at, user.updated_at
+  end
+
   test "updates discourse roles" do
-    user = create :user, roles: []
+    user = create :user, roles: [:staff]
 
     User::SetDiscordRoles.expects(:defer).with(user)
 
@@ -21,7 +42,7 @@ class User::RemoveRolesTest < ActiveSupport::TestCase
   end
 
   test "triggers insiders_status update" do
-    user = create :user, roles: []
+    user = create :user, roles: [:staff]
 
     User::InsidersStatus::TriggerUpdate.expects(:call).with(user)
 

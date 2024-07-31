e51fd92c6ba674638c28667eb6480ed91d1d244b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-07-31 12:35:34 +0200

    Simplify

--- app/models/github/team_member.rb
+++ app/models/github/team_member.rb
@@ -1,6 +1,5 @@
 class Github::TeamMember < ApplicationRecord
   belongs_to :user,
     primary_key: :uid,
-    inverse_of: :github_team_memberships,
-    optional: true
+    inverse_of: :github_team_memberships
 end

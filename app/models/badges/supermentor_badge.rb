55cc041b7485cd715661f0af5950e3f205110ed3
Jeremy Walker <jez.walker@gmail.com>
2024-08-09 12:39:24 +0100

    Update app/models/badges/supermentor_badge.rb

--- app/models/badges/supermentor_badge.rb
+++ app/models/badges/supermentor_badge.rb
@@ -2,7 +2,7 @@ class Badges::SupermentorBadge < Badge
   seed 'Supermentor',
     :legendary,
     'supermentor',
-    'Mentored 100 students on any track'
+    'Mentored 100 students'
 
   def award_to?(user) = user.supermentor?
   def send_email_on_acquisition? = true

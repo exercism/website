85c156072cbb1634fd0efd48410182c90e5b6b04
Erik Schierboom <erik_schierboom@hotmail.com>
2024-08-14 10:06:26 +0200

    Fix missing group

--- app/commands/github/team_member/sync_members.rb
+++ app/commands/github/team_member/sync_members.rb
@@ -17,7 +17,7 @@ class Github::TeamMember::SyncMembers
 
   def delete_team_members!
     Github::TeamMember.includes(:user).find_each do |team_member|
-      next if org_team_members[team_member.team_name].include?(team_member.user.uid)
+      next if org_team_members[team_member.team_name].to_a.include?(team_member.user.uid)
 
       Github::TeamMember::Destroy.(team_member.user.uid, team_member.team_name)
     end
--- test/commands/github/team_member/sync_members_test.rb
+++ test/commands/github/team_member/sync_members_test.rb
@@ -45,4 +45,21 @@ class Github::TeamMember::SyncMembersTest < ActiveSupport::TestCase
     assert ::Github::TeamMember.where(team_name: 'ruby', user: user_2).exists?
     assert ::Github::TeamMember.where(team_name: 'fsharp', user: user_1).exists?
   end
+
+  test "delete when group does not exist" do
+    team_members = { 'ruby' => [82_462], 'fsharp' => [12_412] }
+    Github::Organization.any_instance.stubs(:team_members).returns(team_members)
+
+    user_1 = create(:user, uid: '12412')
+    user_2 = create(:user, uid: '82462')
+    user_3 = create(:user, uid: '56653')
+    create :github_team_member, team_name: 'prolog', user: user_3
+
+    Github::TeamMember::SyncMembers.()
+
+    assert_equal 2, ::Github::TeamMember.count
+    refute ::Github::TeamMember.where(team_name: 'prolog', user: user_3).exists?
+    assert ::Github::TeamMember.where(team_name: 'ruby', user_id: user_2).exists?
+    assert ::Github::TeamMember.where(team_name: 'fsharp', user_id: user_1).exists?
+  end
 end

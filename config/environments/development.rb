9dbee02cbfe8b61641e8220ed60f000b566b1534
Jeremy Walker <jez.walker@gmail.com>
2024-08-28 13:58:04 +0100

    Improve caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.cache_classes = true
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 

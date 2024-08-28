fc1a992df88cc92edd79db8059143a1fa0ac9377
Erik Schierboom <erik_schierboom@hotmail.com>
2024-08-28 14:29:08 +0200

    Show cutoffs

--- app/commands/track/retrieve_maintainers_status.rb
+++ app/commands/track/retrieve_maintainers_status.rb
@@ -1,117 +1,90 @@
 class Track::RetrieveMaintainersStatus
   include Mandate
 
-  def call
-    { Track.for!('csharp') =>
-  { active: [{ handle: "erikSchierboom", github_username: "ErikSchierboom", reputation: 6130 }],
-    inactive: [],
-    candidates: [{ handle: "iHiD", github_username: "iHiD", reputation: 60 },
-                 { handle: "dem4ron", github_username: "dem4ron", reputation: 3 }],
-    unlinked: ["wolf99"] },
-      Track.for!("elixir") =>
-  { active: [],
-    inactive: [],
-    candidates: [{ handle: "erikSchierboom", github_username: "ErikSchierboom", reputation: 393 },
-                 { handle: "iHiD", github_username: "iHiD", reputation: 65 },
-                 { handle: "meatball", github_username: "meatball133", reputation: 29 }],
-    unlinked: %w[Cohen-Carlisle angelikatyborska neenjaw Br1ght0ne jiegillet] },
-      Track.for!("javascript") =>
-  { active: [],
-    inactive: [],
-    candidates: [{ handle: "erikSchierboom", github_username: "ErikSchierboom", reputation: 375 },
-                 { handle: "iHiD", github_username: "iHiD", reputation: 96 },
-                 { handle: "dem4ron", github_username: "dem4ron", reputation: 12 }],
-    unlinked: %w[joshgoebel IsaacG tejasbubane SleeplessByte junedev] },
-      Track.for!("prolog") =>
-  { active: [],
-    inactive: [],
-    candidates: [{ handle: "erikSchierboom", github_username: "ErikSchierboom", reputation: 1803 },
-                 { handle: "iHiD", github_username: "iHiD", reputation: 12 }],
-    unlinked: ["Average-user"] } }
+  MIN_REP_FOR_MEMBER = 50
+  MIN_REP_FOR_CANDIDATE = 100
+  LAST_NUMBER_OF_MONTHS_FOR_REP = 9
+
+  def call = tracks.index_with { |track| track_maintainers(track) }
+
+  private
+  def track_maintainers(track)
+    contributors = track_contributors[track.slug].to_a
+    team_members = track_team_members[track.slug].to_a
+
+    unlinked = team_members.reject do |github_username|
+      contributors.find { |data| data[:github_username] == github_username }
+    end
+
+    maintainers = { active: [], inactive: [], candidates: [], unlinked: }
+    contributors.each do |contributor|
+      if team_members.include?(contributor[:github_username])
+        category = contributor[:reputation] >= MIN_REP_FOR_MEMBER ? :active : :inactive
+        maintainers[category] << contributor
+      elsif contributor[:reputation] >= MIN_REP_FOR_CANDIDATE
+        maintainers[:candidates] << contributor
+      end
+    end
+
+    maintainers
   end
+
+  memoize
+  def track_team_members
+    query = <<~GRAPHQL
+      query ($endCursor: String) {
+        organization(login: "exercism") {
+          team(slug: "track-maintainers") {
+            childTeams(after: $endCursor, first: 100) {
+              nodes {
+                name
+                members(first: 100) {
+                  nodes {
+                    login
+                  }
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
+            }
+          }
+        }
+      }
+    GRAPHQL
+
+    response = Github::Graphql::ExecuteQuery.(query, %i[organization team childTeams])
+    response[0][:nodes].filter_map do |node|
+      next unless track_slugs.include?(node[:name])
+
+      [node[:name], node.dig(:members, :nodes).pluck(:login)]
+    end.sort.to_h
+  end
+
+  memoize
+  def track_contributors
+    rep_cutoff_date = Time.zone.today - LAST_NUMBER_OF_MONTHS_FOR_REP.months
+
+    track_slugs.map do |track_slug|
+      users_rep = User::ReputationToken.
+        includes(user: :data).
+        joins(:track).
+        where('tracks.slug': track_slug).
+        where(category: :building).
+        where('user_reputation_tokens.created_at > ?', rep_cutoff_date).
+        group(:user).
+        sum(:value).
+        map { |user, reputation| { handle: user.handle, github_username: user.data&.github_username, reputation: } }.
+        sort_by { |data| -data[:reputation] }
+
+      [track_slug, users_rep]
+    end.to_h
+  end
+
+  memoize
+  def tracks = Track.active
+
+  memoize
+  def track_slugs = tracks.pluck(:slug)
 end
-
-# def fetch_track_team_members(track_slugs)
-#   query = <<~GRAPHQL
-#   query ($endCursor: String) {
-#     organization(login: "exercism") {
-#       team(slug: "track-maintainers") {
-#         childTeams(after: $endCursor, first: 100) {
-#           nodes {
-#             name
-#             members(first: 100) {
-#               nodes {
-#                 login
-#               }
-#             }
-#           }
-#           pageInfo {
-#             endCursor
-#             hasNextPage
-#           }
-#         }
-#       }
-#     }
-#   }
-#   GRAPHQL
-
-#   response = Github::Graphql::ExecuteQuery.(query, %i[organization team childTeams])
-#   response[0][:nodes].filter_map do |node|
-#     next unless track_slugs.include?(node[:name])
-
-#     [node[:name], node.dig(:members, :nodes).pluck(:login)]
-#   end.sort.to_h
-# end
-
-# LAST_NUMBER_OF_MONTHS_FOR_REP = 9
-
-# def fetch_track_contributors(track_slugs)
-#   rep_cutoff_date = Time.now.to_date - LAST_NUMBER_OF_MONTHS_FOR_REP.months
-
-#   track_slugs.map do |track_slug|
-#     users_rep = User::ReputationToken.
-#             includes(user: :data).
-#             joins(:track).
-#             where('tracks.slug': track_slug).
-#             where(category: :building).
-#             where('user_reputation_tokens.created_at > ?', rep_cutoff_date).
-#             group(:user).
-#             sum(:value).
-#             map {|user, reputation| { handle: user.handle, github_username: user.data&.github_username, reputation:}}.
-#             sort_by {|data| -data[:reputation]}
-
-#     [track_slug, users_rep]
-#   end.to_h
-# end
-
-# MIN_REP = 1
-
-# def fetch_tracks
-#   slugs = Track.active.map(&:slug)
-#   track_team_members = fetch_track_team_members(slugs)
-#   track_contributors = fetch_track_contributors(slugs)
-
-#   slugs.map do |slug|
-#     contributors = track_contributors[slug].to_a
-#     team_members = track_team_members[slug].to_a
-
-#     linked_members, unlinked_members = team_members.partition do |github_username|
-#       contributors.find {|data| data[:github_username] == github_username}
-#     end
-
-#     users = { active: [], inactive: [], candidates: [], unlinked: unlinked_members }
-#     contributors.each do |contributor|
-#       if team_members.include?(contributor[:github_username])
-#         if contributor[:reputation] >= MIN_REP
-#           users[:active] << contributor
-#         else
-#           users[:inactive] << contributor
-#         end
-#       else
-#         users[:candidates] << contributor
-#       end
-#     end
-
-#     [slug, users]
-#   end.to_h
-# end
--- test/controllers/maintaining/contributors_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Maintaining::ContributorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end

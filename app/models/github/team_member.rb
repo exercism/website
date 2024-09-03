b1fa999aa04085ad635b4ceebe07a9bae0eb6396
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-03 15:55:14 +0200

    Add model

--- app/models/github/team_member.rb
+++ app/models/github/team_member.rb
@@ -1,3 +1,8 @@
 class Github::TeamMember < ApplicationRecord
   belongs_to :user
+  belongs_to :track, optional: true
+
+  before_validation on: :create do
+    self.track_id = Track.where(slug: team_name).pick(:id) unless track
+  end
 end

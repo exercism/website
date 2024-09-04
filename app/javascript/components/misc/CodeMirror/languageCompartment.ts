0e1dcf07e889441812043df00659f652b77ff009
Daniel Mita <966706+m-dango@users.noreply.github.com>
2024-09-04 15:52:47 +0100

    Add YAML highlighting for YAMLScript

--- app/javascript/components/misc/CodeMirror/languageCompartment.ts
+++ app/javascript/components/misc/CodeMirror/languageCompartment.ts
@@ -56,6 +56,10 @@ export const loadLanguageCompartment = async (
       const { wren } = await import('@exercism/codemirror-lang-wren')
       return compartment.of(wren())
     }
+    case 'yamlscript': {
+      const { yamlScript } = await import('@codemirror/lang-yaml')
+      return compartment.of(yamlScript())
+    }
     case 'zig': {
       const { rust: zig } = await import('@codemirror/lang-rust')
       return compartment.of(zig())

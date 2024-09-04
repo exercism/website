b401d4893e9a280b07c93227965fcdcd50cbbe4d
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-04 15:37:05 +0200

    Only update maintainer status when added to track team (#7054)

--- app/commands/github/team_member/create.rb
+++ app/commands/github/team_member/create.rb
@@ -7,7 +7,7 @@ class Github::TeamMember::Create
     return unless user
 
     user.github_team_memberships.find_or_create_by!(team_name:).tap do |team_member|
-      User::UpdateMaintainer.(user) if team_member.previously_new_record?
+      User::UpdateMaintainer.(user) if team_member.previously_new_record? && team_member.track_id
     end
   end
 

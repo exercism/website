a85a9c3ddd198e9d61ebd76b68e53312022763cc
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-05 11:41:43 +0200

    More sync members speedup (#7057)

--- app/commands/github/team_member/create.rb
+++ app/commands/github/team_member/create.rb
@@ -1,16 +1,11 @@
 class Github::TeamMember::Create
   include Mandate
 
-  initialize_with :github_uid, :team_name
+  initialize_with :user, :team_name
 
   def call
-    return unless user
-
     user.github_team_memberships.find_or_create_by!(team_name:).tap do |team_member|
       User::UpdateMaintainer.(user) if team_member.previously_new_record? && team_member.track_id
     end
   end
-
-  memoize
-  def user = User.find_by(uid: github_uid)
 end

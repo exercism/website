a0861e21fd0a5fa1df18fa3d2c51f2ea3f36641b
dem4ron <demaaron88@gmail.com>
2024-11-05 18:07:42 +0100

    Add TrackSlugselector, Save values in a hidden field

---
+++ app/javascript/components/common/TrackSlugsMultiselector.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useRef } from 'react'
+import CreatableSelect from 'react-select/creatable'
+
+export default function TrackSlugsMultiselector({
+  trackSlugs,
+}: {
+  trackSlugs: string[]
+}): JSX.Element {
+  const trackSlugsHiddenInputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    trackSlugsHiddenInputRef.current = document.querySelector(
+      '#partner_advert_track_slugs'
+    ) as HTMLInputElement
+
+    if (trackSlugsHiddenInputRef.current) {
+      trackSlugsHiddenInputRef.current.value = JSON.stringify(trackSlugs)
+    }
+  }, [trackSlugs])
+
+  return (
+    <CreatableSelect
+      isMulti
+      // defaultValue={formatTags(tags)}
+      options={formatTags(trackSlugs)}
+      isClearable={false}
+      maxMenuHeight={100}
+      styles={{
+        valueContainer: (base) => ({
+          ...base,
+          padding: '8px',
+        }),
+
+        indicatorSeparator: (base) => ({
+          ...base,
+          display: 'none',
+          '&:focus': {
+            border: 'none',
+            outline: 'none',
+          },
+        }),
+        indicatorsContainer: (base) => ({
+          ...base,
+          display: 'none',
+          border: 'none',
+          '&:focus': {
+            border: 'none',
+            outline: 'none',
+          },
+        }),
+        input: (base) => ({
+          ...base,
+          border: 'none',
+          outline: 'none',
+          boxShadow: 'none',
+          '&.c-faux-input': {
+            boxShadow: 'none',
+          },
+        }),
+
+        multiValue: (base) => ({
+          ...base,
+          borderRadius: '8px',
+          overflow: 'hidden',
+          backgroundColor: 'var(--backgroundColorI)',
+          border: '1px solid',
+          borderColor: '#2E57E8',
+          margin: '4px',
+        }),
+        multiValueLabel: (base) => ({
+          ...base,
+          fontSize: '16px',
+          padding: '4px',
+          paddingLeft: '8px',
+        }),
+        multiValueRemove: (base) => ({
+          ...base,
+          paddingRight: '8px',
+          color: 'var(--textColor6)',
+          '&:hover': {
+            background: '#E27979',
+            color: 'var(--textColor1)',
+          },
+        }),
+      }}
+      onChange={(selected): void => {
+        if (trackSlugsHiddenInputRef.current) {
+          trackSlugsHiddenInputRef.current.value = JSON.stringify(
+            selected.map((s) => s.value)
+          )
+        }
+      }}
+    />
+  )
+}
+
+function formatTags(tags: string[]) {
+  return tags.map((tag) => ({
+    label: tag,
+    value: tag,
+  }))
+}

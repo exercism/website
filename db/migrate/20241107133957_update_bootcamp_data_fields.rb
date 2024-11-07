49ba77715e25069823903df5b2287532e27e999b
Jeremy Walker <jez.walker@gmail.com>
2024-11-08 01:13:53 +0900

    Add production guard

--- db/migrate/20241107133957_update_bootcamp_data_fields.rb
+++ db/migrate/20241107133957_update_bootcamp_data_fields.rb
@@ -1,5 +1,7 @@
 class UpdateBootcampDataFields < ActiveRecord::Migration[7.0]
   def change
+    return if Rails.env.production?
+    
     change_column_null :user_bootcamp_data, :user_id, true
     add_column :user_bootcamp_data, :checkout_session_id, :string
   end

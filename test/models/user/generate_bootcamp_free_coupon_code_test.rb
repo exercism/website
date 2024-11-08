5f37655df7d676a791ec95637349997544f351dd
Jeremy Walker <jez.walker@gmail.com>
2024-11-08 17:49:22 +0900

    Add insider discounts

---
+++ test/models/user/generate_bootcamp_free_coupon_code_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class User::GenerateBootcampFreeCouponCodeTest < ActiveSupport::TestCase
+  test "creates correctly" do
+    user = create :user
+    code = SecureRandom.hex(6)
+    Stripe::PromotionCode.expects(:create).with(
+      coupon: "OarhoKrd", metadata: { user_id: user.id }
+    ).returns(OpenStruct.new(code:))
+
+    User::GenerateBootcampFreeCouponCode.(user)
+
+    assert_equal code, user.bootcamp_free_coupon_code
+  end
+
+  test "immutable" do
+    code = SecureRandom.hex(6)
+    user = create :user, bootcamp_free_coupon_code: code
+    Stripe::PromotionCode.expects(:create).never
+
+    User::GenerateBootcampFreeCouponCode.(user)
+
+    assert_equal code, user.bootcamp_free_coupon_code
+  end
+end

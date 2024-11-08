c6a5a542725065878d41db403f2f81c46b7c3ab5
Jeremy Walker <jez.walker@gmail.com>
2024-11-08 18:56:33 +0900

    Remove typo

--- app/commands/user/generate_bootcamp_affiliate_coupon_code.rb
+++ app/commands/user/generate_bootcamp_affiliate_coupon_code.rb
@@ -3,9 +3,6 @@ class User::GenerateBootcampAffiliateCouponCode
 
   initialize_with :user
 
-  current_user.bootcamp_affiliate_coupon_code.present?
-  current_user.bootcamp_free_coupon_code.present?
-
   def call
     # Easy cheap guard
     return if user_data.bootcamp_affiliate_coupon_code.present?

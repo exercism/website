100c08bab1511c4c2fb509883080c06905a06dc3
dem4ron <demaaron88@gmail.com>
2024-11-19 14:36:21 +0100

    Adjust all tests

--- test/system/flows/settings/user_visits_insiders_settings_test.rb
+++ test/system/flows/settings/user_visits_insiders_settings_test.rb
@@ -19,7 +19,7 @@ module Flows
           assert_text "Donate to Exercism"
 
           links = all("a", text: "Donate to Exercism")
-          assert_equal 2, links.size, "Expected exactly 2 'Donate to Exercism' links but found #{links.size}"
+          assert_equal 1, links.size, "Expected exactly 1 'Donate to Exercism' links but found #{links.size}"
 
           expected_hrefs = [Exercism::Routes.insiders_url, Exercism::Routes.insiders_url]
 
@@ -31,7 +31,7 @@ module Flows
           assert change_button[:disabled]
 
           assert_text "Bootcamp Affiliate Coupon"
-          generate_button = find("#generate-affiliate-coupon-code-button", text: "Click to generate code")
+          generate_button = find("#generate-affiliate-coupon-code-button", text: "Generate your Affiliate Discount code")
           assert generate_button[:disabled]
           refute_text FREE_BOOTCAMP_CANARY
         end
@@ -51,7 +51,7 @@ module Flows
           assert change_button[:disabled]
 
           assert_text "Bootcamp Affiliate Coupon"
-          generate_button = find("button", text: "Click to generate code")
+          generate_button = find("button", text: "Generate your Affiliate Discount code")
           assert generate_button[:disabled]
         end
       end
@@ -65,17 +65,16 @@ module Flows
           visit insiders_settings_path
           sleep(2)
 
-          assert_text "These are exclusive options"
-          assert_text "You've not yet generated your affiliate code"
+          assert_text "we are giving all Insiders an Discount Affiliate code"
 
           assert_text "Bootcamp Affiliate Coupon"
 
-          assert has_no_css?("button[disabled]", text: "Change preferences"),
+          assert has_no_css?("button[disabled]", text: "Generate your Affiliate Discount code"),
             "Change preference button should be enabled but it is disabled"
-          assert has_no_css?("button[disabled]", text: "Click to generate code"),
+          assert has_no_css?("button[disabled]", text: "Generate your Affiliate Discount code"),
             "Generate button should be enabled but it is disabled"
           assert_button("Change preferences")
-          assert_button("Click to generate code")
+          assert_button("Generate your Affiliate Discount code")
 
           refute_text FREE_BOOTCAMP_CANARY
         end
@@ -90,17 +89,16 @@ module Flows
           visit insiders_settings_path
           sleep(2)
 
-          assert_text "These are exclusive options"
-          assert_text "You've not yet generated your affiliate code"
+          assert_text "we are giving all Insiders an Discount Affiliate code"
 
           assert_text "Bootcamp Affiliate Coupon"
 
           assert has_no_css?("button[disabled]", text: "Change preferences"),
             "Change preference button should be enabled but it is disabled"
-          assert has_no_css?("button[disabled]", text: "Click to generate code"),
+          assert has_no_css?("button[disabled]", text: "Generate your Affiliate Discount code"),
             "Generate button should be enabled but it is disabled"
           assert_button("Change preferences")
-          assert_button("Click to generate code")
+          assert_button("Generate your Affiliate Discount code")
 
           assert_text FREE_BOOTCAMP_CANARY
         end

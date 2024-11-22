b946b68b10c5bc26559178d5c204c068f7f5d268
Jeremy Walker <jez.walker@gmail.com>
2024-11-22 13:14:18 +0900

    Add insiders video

--- app/controllers/insiders_controller.rb
+++ app/controllers/insiders_controller.rb
@@ -21,6 +21,7 @@ class InsidersController < ApplicationController
   def paypal_cancelled; end
 
   VIDEOS = [
+    [:vimeo, "1032194219?h=50693f7d99", "insiders-14.jpg"],
     [:vimeo, "1027581747?h=5a2ccb608c", "insiders-13.jpg"],
     [:vimeo, "1024765646?h=772526ae1d", "insiders-12.jpg"],
     [:vimeo, "904597260?h=0322718fa5", "insiders-11.jpg"],

9edb3ee7d8548cc810476b2b1a35bcebd78629c3
Jeremy Walker <jez.walker@gmail.com>
2024-12-04 20:10:46 +0900

    Tweak copy

--- app/javascript/components/modals/track-welcome-modal/LHS/BootcampRecommendationView.tsx
+++ app/javascript/components/modals/track-welcome-modal/LHS/BootcampRecommendationView.tsx
@@ -1,28 +1,72 @@
 import React, { useContext } from 'react'
 import { TrackContext } from '../TrackWelcomeModal'
+import { GraphicalIcon } from '@/components/common'
 
 export function BootcampRecommendationView() {
   const { hideBootcampRecommendationView, track, links } =
     useContext(TrackContext)
   return (
     <>
-      <h4 className="text-h4">You might find the Bootcamp is a better fit</h4>
+      <h4 className="text-h4 mb-8">Our Bootcamp might be better for you…</h4>
 
-      {/* TODO: Add copy */}
-      <p className="mb-16">Here's why:</p>
-      <ul className="flex flex-col gap-4 text-14 font-medium">
-        <li>✅ You're new to {track.title}</li>
-        <li>✅ You want a structured learning path</li>
-        <li>✅ You want to build projects</li>
+      <p className="mb-8">
+        Exercism's tracks are designed for people who{' '}
+        <strong className="font-medium">already know how to code</strong> and
+        are practicing or learning new languages.
+      </p>
+      <p className="mb-8">
+        If you're just starting out on your coding journey,{' '}
+        <strong className="font-semibold">
+          our Bootcamp might be a better fit for you.
+        </strong>{' '}
+        It offers:
+      </p>
+      <ul className="flex flex-col gap-2 text-14 font-medium mb-8">
+        <li className="flex items-center">
+          <GraphicalIcon
+            icon="wave"
+            category="bootcamp"
+            className="mr-8 w-[20px]"
+          />
+          Expert teaching and mentoring support
+        </li>
+        <li className="flex items-center">
+          <GraphicalIcon
+            icon="fun"
+            category="bootcamp"
+            className="mr-8 w-[20px]"
+          />
+          Hands-on project based learning
+        </li>
+        <li className="flex items-center">
+          <GraphicalIcon
+            icon="complete"
+            category="bootcamp"
+            className="mr-8 w-[20px]"
+          />
+          A complete Learn to Code syllabus
+        </li>
+        <li className="flex items-center">
+          <GraphicalIcon
+            icon="certificate"
+            category="bootcamp"
+            className="mr-8 w-[20px]"
+          />
+          A formal certificate on completion
+        </li>
       </ul>
+      <p className="mb-16">
+        It's part time, remote, and priced affordably, with discounts available
+        for students, people who are unemployed, and those living in emerging
+        economies.
+      </p>
 
-      <div className="grid grid-cols-2 gap-12 items-center">
-        <a
+      <div className="flex gap-12 items-center w-full">
           data-capy-element="go-to-bootcamp-button"
           href={links.bootcampLanding}
           className="btn-m btn-primary"
-        >
-          Go to bootcamp
+        <a  href={links.bootcampLanding} className="btn-m btn-primary flex-grow">
+          Check out the Bootcamp
         </a>
         <button
           onClick={hideBootcampRecommendationView}
--- app/javascript/components/modals/track-welcome-modal/RHS/WhoIsThisTrackForRHS.tsx
+++ app/javascript/components/modals/track-welcome-modal/RHS/WhoIsThisTrackForRHS.tsx
@@ -1,18 +1,34 @@
+import { GraphicalIcon, Icon } from '@/components/common'
+import VimeoEmbed from '@/components/common/VimeoEmbed'
 import { Track } from '@/components/types'
 import React from 'react'
 
 export function WhoIsThisTrackForRHS({ track }: { track: Track }): JSX.Element {
   return (
-    <div data-capy-element="who-is-this-track-for-rhs" className="rhs">
-      <h2 className="text-h4 mb-16">Who is this track for?</h2>
-      <ul className="flex flex-col gap-8 text-16 font-medium">
-        <li> ✅ Developers Learning {track.title}</li>
-        <li>✅ Developers Practicing {track.title}</li>
-
-        <li>✅ Beginners Practicing {track.title} (but learning elsewhere).</li>
-
-        <li>❌ Beginners Learning {track.title}</li>
-      </ul>
+    <div className="rhs" data-capy-element="who-is-this-track-for-rhs">
+      <div className="rounded-8 p-20 bg-backgroundColorD border-1 border-borderColor7 mb-16">
+        <div className="flex flex-row gap-8 items-center justify-center text-16 text-textColor1 mb-16">
+          <Icon
+            icon="exercism-face"
+            className="filter-textColor1"
+            alt="exercism-face"
+            height={16}
+            width={16}
+          />
+          <div>
+            <strong className="font-semibold"> Exercism </strong>
+            Bootcamp
+          </div>
+        </div>
+        <VimeoEmbed className="rounded-8 mb-16" id="1024390839?h=c2b3bdce14" />
+        <span className="text-16 leading-150 text-textColor2">
+          <strong className="font-medium">
+            🗓️ The Bootcamp starts in January.{' '}
+          </strong>
+          Check out our introduction video (☝️) to see how it will work and if
+          it's the right fit for you!
+        </span>
+      </div>
     </div>
   )
 }

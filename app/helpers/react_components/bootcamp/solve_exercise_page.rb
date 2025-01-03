db26751a9ac448ccb6156edbbd051dcdd241963d
dem4ron <demaaron88@gmail.com>
2025-01-03 11:36:09 +0100

    Adjust endpoints

--- app/helpers/react_components/bootcamp/solve_exercise_page.rb
+++ app/helpers/react_components/bootcamp/solve_exercise_page.rb
@@ -36,8 +36,8 @@ module ReactComponents
           readonly_ranges:
         },
         links: {
-          # post_submission: Exercism::Routes.api_solution_submissions_url(solution_uuid: solution.uuid, only_path: true),
-          # complete_solution: Exercism::Routes.complete_api_solution_url(solution.uuid, only_path: true),
+          post_submission: Exercism::Routes.api_bootcamp_solution_submissions_url(solution_uuid: solution.uuid, only_path: true),
+          complete_solution: Exercism::Routes.complete_api_bootcamp_solution_url(solution.uuid, only_path: true),
           projects_index: Exercism::Routes.bootcamp_projects_url(only_path: true)
         }
       }

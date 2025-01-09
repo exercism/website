c2805f1676ac2253b65ed948493cd6bb20edd42f
Jeremy Walker <jez.walker@gmail.com>
2025-01-09 00:42:00 +0000

    Use a number for missing user id

--- app/commands/user/bootcamp/subscribe_to_onboarding_emails.rb
+++ app/commands/user/bootcamp/subscribe_to_onboarding_emails.rb
@@ -30,7 +30,7 @@ class User
               "price": bootcamp_data.price,
               "has_discount": bootcamp_data.has_discount?,
               "discount_percentage": bootcamp_data.discount_percentage,
-              "user_id": bootcamp_data.user_id,
+              "user_id": bootcamp_data.user_id || 0,
               "access_code": bootcamp_data.access_code
             }
           },

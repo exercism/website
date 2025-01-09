72a48d67b07fd1a47e7ada0e6e6d3601064bde7a
dem4ron <demaaron88@gmail.com>
2025-01-09 15:53:26 +0100

    Break out setup hook

--- app/javascript/components/bootcamp/DrawingPage/DrawingPage.tsx
+++ app/javascript/components/bootcamp/DrawingPage/DrawingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Header, StudentCodeGetter } from './Header/Header'
 import {
   Resizer,
@@ -8,9 +8,9 @@ import { CodeMirror } from '../SolveExercisePage/CodeMirror/CodeMirror'
 import ErrorBoundary from '../common/ErrorBoundary/ErrorBoundary'
 import { useDrawingEditorHandler } from './useDrawingEditorHandler'
 import { useLocalStorage } from '@uidotdev/usehooks'
-import useEditorStore from '../SolveExercisePage/store/editorStore'
 import Scrubber from './Scrubber/Scrubber'
 import { debounce } from 'lodash'
+import { useSetupDrawingPage } from './useSetupDrawingPage'
 
 export default function DrawingPage({
   drawing,
@@ -44,24 +44,11 @@ export default function DrawingPage({
     { code: code.code, storedAt: code.storedAt }
   )
 
-  const { setDefaultCode, setShouldAutoRunCode } = useEditorStore()
-
-  // Setup hook
-  useEffect(() => {
-    if (
-      editorLocalStorageValue.storedAt &&
-      code.storedAt &&
-      // if the code on the server is newer than in localstorage, update the storage and load the code from the server
-      editorLocalStorageValue.storedAt < code.storedAt
-    ) {
-      setEditorLocalStorageValue({ code: code.code, storedAt: code.storedAt })
-      setDefaultCode(code.code)
-    } else {
-      // otherwise we are using the code from the storage
-      setDefaultCode(editorLocalStorageValue.code)
-    }
-    setShouldAutoRunCode(true)
-  }, [code, setDefaultCode, setEditorLocalStorageValue])
+  useSetupDrawingPage({
+    code,
+    editorLocalStorageValue,
+    setEditorLocalStorageValue,
+  })
 
   const patchCodeOnDebounce = useMemo(() => {
     return debounce(() => {
@@ -96,7 +83,6 @@ export default function DrawingPage({
         <Resizer direction="vertical" handleMouseDown={handleMouseDown} />
         {/* RHS */}
         <div className="page-body-rhs" style={{ width: RHSWidth }}>
-          {/* DRAWING HERE */}
           <div ref={viewContainerRef} id="view-container" />
         </div>
       </div>

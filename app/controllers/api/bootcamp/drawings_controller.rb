00ffe609f0f4675f06f1f6e1b01c8559cd60d349
Jeremy Walker <jez.walker@gmail.com>
2025-01-10 13:06:48 +0000

    Add background drawing slug

--- app/controllers/api/bootcamp/drawings_controller.rb
+++ app/controllers/api/bootcamp/drawings_controller.rb
@@ -4,6 +4,7 @@ class API::Bootcamp::DrawingsController < API::Bootcamp::BaseController
   def update
     @drawing.update(code: params[:code]) if params[:code].present?
     @drawing.update(title: params[:title]) if params[:title].present?
+    @drawing.update(background_slug: params[:background_slug]) if params[:background_slug].present?
 
     render json: {}, status: :ok
   end

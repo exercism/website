09132f484da4376fa6f2044cffa05a72dc62dd08
dem4ron <demaaron88@gmail.com>
2025-01-10 11:43:18 +0100

    Adjust more types

--- app/javascript/components/bootcamp/types/SolveExercisePage.d.ts
+++ app/javascript/components/bootcamp/types/SolveExercisePage.d.ts
@@ -1,29 +1,5 @@
 import { LanguageFeatures } from '@/interpreter/interpreter'
 
-type Code = {
-  stub: string
-  code: string
-  storedAt: Date | string | null
-  readonlyRanges?: { from: number; to: number }[]
-}
-
-type Solution = {
-  uuid: string
-  status: 'completed' | 'in_progress'
-}
-
-interface Exercise {
-  part: number
-  introductionHtml: string
-  config: Config
-  tasks: Task[]
-  testResults: Pick<TestSuiteResult<NewTestResult>, 'status'> & {
-    tests: (Pick<NewTestResult, 'status' | 'slug'> & { actual: string })[]
-  }
-}
-
-type Project = { slug: string }
-
 declare global {
   type SolveExercisePageProps = {
     solution: Solution
@@ -37,13 +13,37 @@ declare global {
       dashboardIndex: string
     }
   }
-}
 
-declare type Config = {
-  description: string
-  title: string
-  projectType: string
-  // tasks: Task[];
-  testsType: 'io' | 'state'
-  interpreterOptions: LanguageFeatures
+  type Code = {
+    stub: string
+    code: string
+    storedAt: Date | string | null
+    readonlyRanges?: { from: number; to: number }[]
+  }
+
+  type Solution = {
+    uuid: string
+    status: 'completed' | 'in_progress'
+  }
+
+  interface Exercise {
+    part: number
+    introductionHtml: string
+    config: Config
+    tasks: Task[]
+    testResults: Pick<TestSuiteResult<NewTestResult>, 'status'> & {
+      tests: (Pick<NewTestResult, 'status' | 'slug'> & { actual: string })[]
+    }
+  }
+
+  type Project = { slug: string }
+
+  type Config = {
+    description: string
+    title: string
+    projectType: string
+    // tasks: Task[];
+    testsType: 'io' | 'state'
+    interpreterOptions: LanguageFeatures
+  }
 }

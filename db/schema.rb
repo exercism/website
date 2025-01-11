22ce96160d4dc59e05769f7a53480b5c7f47b723
Jeremy Walker <jez.walker@gmail.com>
2025-01-11 21:44:05 +0000

    Add youtube embed for levels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_09_143039) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_11_213608) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -126,6 +126,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_09_143039) do
     t.text "content_html", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "youtube_id"
   end
 
   create_table "bootcamp_projects", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|

62f7b478041452ec6d6b18a3e54ab587b6771642
dem4ron <demaaron88@gmail.com>
2025-01-16 14:30:28 +0100

    Remove unused code

--- app/javascript/components/bootcamp/SolveExercisePage/hooks/useSetupStores.ts
+++ app/javascript/components/bootcamp/SolveExercisePage/hooks/useSetupStores.ts
@@ -1,88 +1,16 @@
 import { useLayoutEffect } from 'react'
-import useEditorStore from '../store/editorStore'
 import useTaskStore from '../store/taskStore/taskStore'
 import useTestStore from '../store/testStore'
-import { useLocalStorage } from '@uidotdev/usehooks'
 
 export function useSetupStores({
   exercise,
   code,
 }: Pick<SolveExercisePageProps, 'exercise' | 'code'>) {
-  const [editorLocalStorageValue, setEditorLocalStorageValue] = useLocalStorage(
-    'bootcamp-editor-value-' + exercise.config.title,
-    {
-      code: code.code,
-      storedAt: code.storedAt,
-      readonlyRanges: code.readonlyRanges,
-    }
-  )
   const { initializeTasks } = useTaskStore()
-  const {
-    setPreviousTestSuiteResult,
-    setInspectedPreviousTestResult,
-    setFlatPreviewTaskTests,
-  } = useTestStore()
+  const { setFlatPreviewTaskTests } = useTestStore()
 
   useLayoutEffect(() => {
-    let previousTestSuiteResult: TestSuiteResult<PreviousTestResult> | null =
-      null
-    if (exercise.testResults) {
-      const tests = (() => {
-        const testArr: {
-          name: string
-          testIndex: number
-          slug: string
-          status: 'pass' | 'fail'
-          actual: string
-          expected: string | undefined
-          errorHtml?: string
-          codeRun: string
-          testsType: 'io' | 'state'
-        }[] = []
-
-        let i = 0
-        for (let taskInConfig of exercise.tasks) {
-          for (let testInConfig of taskInConfig.tests) {
-            const prevTest = exercise.testResults.tests[i]
-            if (testInConfig.slug === prevTest.slug) {
-              testArr.push({
-                name: testInConfig.name,
-                testIndex: i,
-                slug: testInConfig.slug,
-                status: prevTest.status,
-                actual: prevTest.actual,
-                errorHtml:
-                  exercise.config.testsType === 'state' &&
-                  prevTest.status === 'fail'
-                    ? prevTest.actual
-                    : undefined,
-                expected: testInConfig.expected,
-                codeRun: generateCodeRunString(
-                  testInConfig.function,
-                  testInConfig.params
-                ),
-
-                testsType: exercise.config.testsType,
-              })
-            }
-            i++
-          }
-        }
-        return testArr
-      })()
-
-      const firstFailingTest = tests.find((test) => test.status === 'fail')
-      setInspectedPreviousTestResult(firstFailingTest ?? tests[0])
-
-      previousTestSuiteResult = {
-        suiteName: exercise.config.title,
-        status: exercise.testResults.status,
-        tests,
-      }
-      setPreviousTestSuiteResult(previousTestSuiteResult)
-    }
-
-    initializeTasks(exercise.tasks, previousTestSuiteResult)
+    initializeTasks(exercise.tasks, null)
     setFlatPreviewTaskTests(exercise.tasks.flatMap((task) => task.tests))
   }, [exercise, code])
 }

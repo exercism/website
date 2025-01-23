2f99a2c241dc3a5bd97d5e3da1d02fe5f8bbd8bc
Jeremy Walker <jez.walker@gmail.com>
2025-01-23 14:31:33 +0000

    Only award 48in24 badge to participants (#7373)

--- app/models/badges/participant_in_48_in_24_badge.rb
+++ app/models/badges/participant_in_48_in_24_badge.rb
@@ -6,6 +6,8 @@ module Badges
       'Participated in the #48in24 challenge and achieved a medal'
 
     def award_to?(user)
+      return false unless user.challenges.where(challenge_id: "48in24").exists?
+
       exercises = User::Challenges::FeaturedExercisesProgress48In24.(user)
       exercises.any? { |e| e.status != :in_progress }
     end

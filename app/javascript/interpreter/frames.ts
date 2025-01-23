192d42de14821b1c1acbfbaaedb45a94cd0bb990
Jeremy Walker <jez.walker@gmail.com>
2025-01-23 15:03:33 +0000

    Print out env to test if it works

--- app/javascript/interpreter/frames.ts
+++ app/javascript/interpreter/frames.ts
@@ -52,6 +52,9 @@ export function describeFrame(
   externalFunctions: ExternalFunction[]
 ): string {
   try {
+    console.log(process.env.NODE_ENV)
+  } catch {}
+  try {
     // These need to come from the exercise.
     const functionDescriptions: Record<string, string> =
       externalFunctions.reduce(

12fadebc4eb3614215be9f34b904897bdd18854d
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2025-01-24 19:00:37 +0100

    Allow pasting and deleting multiple lines when there are readonly ranges (#7383)

--- app/javascript/components/bootcamp/SolveExercisePage/CodeMirror/extensions/read-only-ranges/readOnlyRanges.ts
+++ app/javascript/components/bootcamp/SolveExercisePage/CodeMirror/extensions/read-only-ranges/readOnlyRanges.ts
@@ -8,26 +8,30 @@ export const readOnlyRangesStateField = StateField.define<ReadOnlyRange[]>({
     return []
   },
   update(ranges, tr) {
+    // if we are adding lines
     if (tr.startState.doc.lines < tr.state.doc.lines) {
       const cursor = tr.state.selection.main.head
       const newLine = tr.state.doc.lineAt(cursor).number
+      const diff = tr.state.doc.lines - tr.startState.doc.lines
       return ranges.map((r) => {
         const rangeLine = r.from
         if (rangeLine >= newLine) {
-          return { from: r.from + 1, to: r.to + 1 }
+          return { from: r.from + diff, to: r.to + diff }
         } else return r
       })
     }
+    // if we are deleting lines
     if (tr.startState.doc.lines > tr.state.doc.lines) {
       const cursor = tr.state.selection.main.head
       const lineAtCursor = tr.state.doc.lineAt(cursor)
+      const diff = tr.startState.doc.lines - tr.state.doc.lines
+
       const lineDeletedAbove = lineAtCursor.number - 1
 
       return ranges.map((r) => {
         if (r.from > lineDeletedAbove) {
-          return { from: r.from - 1, to: r.to - 1 }
+          return { from: r.from - diff, to: r.to - diff }
         }
-
         return r
       })
     }

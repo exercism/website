04e03dcddf4002f6f099e4e709fbce86c6b698e1
dem4ron <demaaron88@gmail.com>
2025-01-27 11:28:35 +0100

    Remove unused import

--- app/javascript/components/bootcamp/SolveExercisePage/TestResultsView/TestResultInfo.tsx
+++ app/javascript/components/bootcamp/SolveExercisePage/TestResultsView/TestResultInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { CodeRun } from './CodeRun'
 import { IOTestResultView } from './IOTestResultView'
 import { StateTestResultView } from './StateTestResultView'

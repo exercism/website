6d84d28de65fe12f041e542c3d15ca3dabb96d88
Jeremy Walker <jez.walker@gmail.com>
2025-02-01 16:21:07 +0000

    Split out active/inactive on bootcamp admin

--- app/controllers/bootcamp/admin/exercises_controller.rb
+++ app/controllers/bootcamp/admin/exercises_controller.rb
@@ -1,7 +1,11 @@
 class Bootcamp::Admin::ExercisesController < Bootcamp::Admin::BaseController
   def index
     @projects_with_exercises = Bootcamp::Exercise.includes(:project).group_by(&:project)
-    @in_progress_exercise_counts = Bootcamp::Solution.in_progress.group(:exercise_id).count
+    in_progress = Bootcamp::Solution.in_progress
+    threshold = Time.current - 10.days
+    @active_exercise_counts = in_progress.where('updated_at >= ?', threshold).group(:exercise_id).count
+    @stale_exercise_counts = in_progress.where('updated_at < ?', threshold).group(:exercise_id).count
+
     @completed_exercise_counts = Bootcamp::Solution.completed.group(:exercise_id).count
     @current_level_idx = Bootcamp::Settings.instance.level_idx
   end

d7f7571c680dbdaf8b88514b3224596df8f4eb4a
Jeremy Walker <jez.walker@gmail.com>
2025-02-07 22:00:35 +0000

    Fix test

--- test/commands/bootcamp/solution/create_test.rb
+++ test/commands/bootcamp/solution/create_test.rb
@@ -28,16 +28,17 @@ class Bootcamp::Solution::CreateTest < ActiveSupport::TestCase
   end
 
   test "uses solutions from previous exercises" do
+    project_slug = "project-34"
     exercise_1_slug = "part-1"
     exercise_2_slug = "ex-2"
 
     solution_1_code = "first\nbit"
     solution_2_code = "second\ppart"
-    template = "Hello {{EXERCISE_#{exercise_1_slug}}} World {{EXERCISE_#{exercise_2_slug}}} Yay!"
+    template = "Hello {{EXERCISE:#{project_slug}/#{exercise_1_slug}}} World {{EXERCISE:#{project_slug}/#{exercise_2_slug}}} Yay!"
     expected = "Hello #{solution_1_code} World #{solution_2_code} Yay!"
 
     user = create :user, :with_bootcamp_data
-    project = create :bootcamp_project
+    project = create :bootcamp_project, slug: project_slug
     create :bootcamp_user_project, user:, project:, status: :available
     exercise_1 = create :bootcamp_exercise, project:, slug: exercise_1_slug
     exercise_2 = create :bootcamp_exercise, project:, slug: exercise_2_slug

e94a5745627149e781d4f8720f7225cc226cb180
dem4ron <demaaron88@gmail.com>
2025-02-19 18:14:15 +0100

    Add multi select

--- app/javascript/components/bootcamp/CustomFunctionEditor/Header/Header.tsx
+++ app/javascript/components/bootcamp/CustomFunctionEditor/Header/Header.tsx
@@ -3,9 +3,7 @@ import { wrapWithErrorBoundary } from '@/components/bootcamp/common/ErrorBoundar
 import { assembleClassNames } from '@/utils/assemble-classnames'
 
 import { GraphicalIcon } from '@/components/common/GraphicalIcon'
-import { MultipleSelect } from '@/components/common/MultipleSelect'
-import { TaskKnowledge } from '@/components/types'
-import { KnowledgeIcon } from '@/components/contributing/tasks-list/task/KnowledgeIcon'
+import CreatableSelect from 'react-select/creatable'
 
 export type StudentCodeGetter = () => string | undefined
 
@@ -18,7 +16,7 @@ function _Header({
   handleSaveChanges: () => void
   someTestsAreFailing: boolean
 }) {
-  const [value, setValue] = useState([])
+  const [value, setValue] = useState<{ label: string; value: string }[]>([])
 
   return (
     <div className="page-header justify-between">
@@ -30,6 +28,7 @@ function _Header({
       </div>
 
       <div className="flex items-center gap-8">
+        <CustomFnMultiSelect value={value} setValue={setValue} />
         <button
           className="btn-primary btn-xxs"
           disabled={someTestsAreFailing}
@@ -50,3 +49,73 @@ function _Header({
 }
 
 export const Header = wrapWithErrorBoundary(_Header)
+
+function CustomFnMultiSelect({ value, setValue }) {
+  return (
+    <CreatableSelect
+      isMulti
+      // ref={selectRef}
+      defaultValue={[{ label: 'len', value: 'my#len' }]}
+      value={value}
+      options={[
+        { label: 'len', value: 'my#len' },
+        { label: 'index_of', value: 'my#index_of' },
+      ]}
+      isClearable={false}
+      maxMenuHeight={100}
+      styles={{
+        valueContainer: (base) => ({
+          ...base,
+          minWidth: '120px',
+          padding: '4px',
+        }),
+
+        indicatorSeparator: (base) => ({
+          ...base,
+          display: 'none',
+          '&:focus': {
+            border: 'none',
+            outline: 'none',
+          },
+        }),
+        indicatorsContainer: (base) => ({
+          ...base,
+          display: 'none',
+          border: 'none',
+          '&:focus': {
+            border: 'none',
+            outline: 'none',
+          },
+        }),
+
+        multiValue: (base) => ({
+          ...base,
+          borderRadius: '4px',
+          overflow: 'hidden',
+          backgroundColor: 'var(--backgroundColorI)',
+          border: '1px solid',
+          borderColor: '#2E57E8',
+          margin: '2px',
+        }),
+        multiValueLabel: (base) => ({
+          ...base,
+          fontSize: '14px',
+          padding: '2px',
+          paddingLeft: '4px',
+        }),
+        multiValueRemove: (base) => ({
+          ...base,
+          paddingRight: '4px',
+          color: 'var(--textColor6)',
+          '&:hover': {
+            background: '#E27979',
+            color: 'var(--textColor1)',
+          },
+        }),
+      }}
+      onChange={(selected): void => {
+        setValue(selected as { label: string; value: string }[])
+      }}
+    />
+  )
+}

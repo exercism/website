f8a5a1b118ca8dc40ac8e4405cd1daa9b3c16e33
Jeremy Walker <jez.walker@gmail.com>
2025-03-11 22:21:20 +0000

    Add less restrictive default

--- app/javascript/interpreter/interpreter.ts
+++ app/javascript/interpreter/interpreter.ts
@@ -155,8 +155,8 @@ export class Interpreter {
       excludeList: undefined,
       timePerFrame: 0.01,
       repeatDelay: 0,
-      maxRepeatUntilGameOverIterations: 1000,
-      maxTotalLoopIterations: 1000,
+      maxRepeatUntilGameOverIterations: 100,
+      maxTotalLoopIterations: 10000,
       maxTotalExecutionTime: 10 * 1000, // 10 seconds
       allowGlobals: false,
       customFunctionDefinitionMode: false,

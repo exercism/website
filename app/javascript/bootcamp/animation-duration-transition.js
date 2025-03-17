9062feaef29641f6cea2a7e97630f527c929fc2c
dem4ron <demaaron88@gmail.com>
2025-03-17 19:56:39 +0100

    Working speed up

--- app/javascript/bootcamp/animation-duration-transition.js
+++ app/javascript/bootcamp/animation-duration-transition.js
@@ -1,35 +1,59 @@
-import { animate } from '@juliangarnierorg/anime-beta'
+document.addEventListener('DOMContentLoaded', () => {
+  const marqueeElement = document.querySelector('.scrolling-testimonials ul')
+  const container = document.querySelector('.scrolling-testimonials')
 
-const scrollingTestimonials = document.querySelector('.scrolling-testimonials')
-const ul = scrollingTestimonials.querySelector('ul')
+  const computedStyle = getComputedStyle(container)
+  const gap = parseFloat(computedStyle.getPropertyValue('--gap')) || 1
 
-if (ul) {
-  const scroll = { durationTime: 60 }
+  const normalSpeed = 0.1
+  const fastSpeed = 0.3
+  let currentSpeed = normalSpeed
+  let animationPosition = 0
+  let lastTimestamp = null
+  let isHovering = false
 
-  ul.addEventListener('mouseenter', () => {
-    console.log('entering mouse')
-    animate(scroll, {
-      durationTime: 20,
-      duration: 1000,
-      loop: false,
-      ease: 'linear',
-      onUpdate: () => {
-        console.log('duration', scroll)
-        ul.style.animation = `${scroll.durationTime}s`
-      },
-    })
+  function animateMarquee(timestamp) {
+    if (!lastTimestamp) {
+      lastTimestamp = timestamp
+    }
+
+    const elapsed = timestamp - lastTimestamp
+    lastTimestamp = timestamp
+
+    animationPosition += elapsed * 0.01 * currentSpeed
+
+    if (animationPosition >= 100) {
+      animationPosition = animationPosition % 100
+    }
+
+    marqueeElement.style.transform = `translateX(${-animationPosition}%)`
+
+    requestAnimationFrame(animateMarquee)
+  }
+
+  requestAnimationFrame(animateMarquee)
+
+  container.addEventListener('mouseenter', () => {
+    isHovering = true
+
+    const speedTransition = setInterval(() => {
+      currentSpeed += 0.1
+      if (currentSpeed >= fastSpeed) {
+        currentSpeed = fastSpeed
+        clearInterval(speedTransition)
+      }
+    }, 20)
   })
-  ul.addEventListener('mouseleave', () => {
-    console.log('entering mouse')
-    animate(scroll, {
-      durationTime: 60,
-      duration: 1000,
-      loop: false,
-      ease: 'linear',
-      onUpdate: () => {
-        console.log('duration', scroll)
-        ul.style.animationDuration = `${scroll.durationTime}s`
-      },
-    })
+
+  container.addEventListener('mouseleave', () => {
+    isHovering = false
+
+    const speedTransition = setInterval(() => {
+      currentSpeed -= 0.1
+      if (currentSpeed <= normalSpeed) {
+        currentSpeed = normalSpeed
+        clearInterval(speedTransition)
+      }
+    }, 20)
   })
-}
+})
--- app/javascript/packs/courses.tsx
+++ app/javascript/packs/courses.tsx
@@ -1,4 +1,5 @@
 import '../bootcamp/nav-intersection-animations'
+import '../bootcamp/animation-duration-transition'
 import '../bootcamp/annotate-intersection-animations'
 import '../bootcamp/confetti-at-footer-animation'
 import '../bootcamp/arrow-animation'

93075b09c19058b0b9975bbbe5bbdc3a3f4bd099
Jeremy Walker <jez.walker@gmail.com>
2025-03-24 02:03:24 +0000

    Fix

--- test/models/courses/coding_fundamentals_test.rb
+++ test/models/courses/coding_fundamentals_test.rb
@@ -3,13 +3,11 @@ require "test_helper"
 class Courses::CodingFundamentalsTest < ActiveSupport::TestCase
   test "enable_for_user!" do
     user = create :user
-    
+
     User::SetDiscordRoles.expects(:defer).with(user)
     User::SetDiscourseGroups.expects(:defer).with(user)
 
-
     # TODO: Assert email
-
 
     Courses::CodingFundamentals.instance.enable_for_user!(user)
 

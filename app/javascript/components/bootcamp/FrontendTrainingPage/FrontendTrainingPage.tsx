095f1d0676f891d0485f38d302b74fa85188066a
dem4ron <demaaron88@gmail.com>
2025-03-25 17:05:51 +0100

    Get rid of interpreter-related code

--- app/javascript/components/bootcamp/FrontendTrainingPage/FrontendTrainingPage.tsx
+++ app/javascript/components/bootcamp/FrontendTrainingPage/FrontendTrainingPage.tsx
@@ -1,10 +1,9 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { Resizer } from '../SolveExercisePage/hooks/useResize'
 import { CodeMirror } from '../SolveExercisePage/CodeMirror/CodeMirror'
 import { Instructions } from './Instructions'
 import { html } from '@codemirror/lang-html'
 import { css } from '@codemirror/lang-css'
-import interact from '@replit/codemirror-interact'
 import { basicLight } from 'cm6-theme-basic-light'
 import { Prec } from '@codemirror/state'
 import { ActualOutput } from './ActualOutput'
@@ -16,19 +15,8 @@ import { useInitResizablePanels } from './useInitResizablePanels'
 import { useSetupEditors } from './useSetupEditors'
 import { useSetupIFrames } from './useSetupIFrames'
 import { FrontendTrainingPageContext } from './FrontendTrainingPageContext'
-import { interpret } from '@/css-interpreter/interpreter'
-import { Frame } from '@/css-interpreter/frames'
-import { showError } from '../SolveExercisePage/utils/showError'
-import useEditorStore from '../SolveExercisePage/store/editorStore'
 import SolveExercisePageContextWrapper from '../SolveExercisePage/SolveExercisePageContextWrapper'
-import {
-  Animation,
-  AnimationTimeline,
-} from '../SolveExercisePage/AnimationTimeline/AnimationTimeline'
-import { DEFAULT_BROWSER_STYLES } from './defaultStyles'
 import { interactionExtension } from './extensions/interaction'
-
-let colorInput: HTMLInputElement | null = null
 
 export default function FrontendTrainingPage() {
   const actualOutputRef = useRef<HTMLDivElement>(null)
@@ -55,59 +43,6 @@ export default function FrontendTrainingPage() {
     handleHtmlEditorDidMount,
     setEditorCodeLocalStorage,
   } = useSetupEditors()
-
-  const {
-    setUnderlineRange,
-    setHighlightedLine,
-    setHighlightedLineColor,
-    setInformationWidgetData,
-    setShouldShowInformationWidget,
-    setHasCodeBeenEdited,
-  } = useEditorStore()
-
-  const [frames, setFrames] = useState<Frame[] | undefined>(undefined)
-  const [cssAnimationTimeline, setCssAnimationTimeline] = useState()
-
-  const interpreterCssCode = useCallback(() => {
-    if (actualIFrameRef.current) {
-      // Reload iframe
-      actualIFrameRef.current.contentWindow?.location.reload()
-
-      updateIFrame(
-        actualIFrameRef,
-        htmlEditorViewRef?.current?.state.doc.toString() || ''
-      )
-    }
-
-    const cssCode = cssEditorViewRef.current?.state.doc.toString() || ''
-    const interpretation = interpret(cssCode)
-    if (interpretation.error) {
-      showError({
-        error: interpretation.error,
-        editorView: cssEditorViewRef.current,
-        setHighlightedLine,
-        setHighlightedLineColor,
-        setInformationWidgetData,
-        setShouldShowInformationWidget,
-        setUnderlineRange,
-      })
-    }
-
-    setHasCodeBeenEdited(false)
-    const { frames } = interpretation
-    if (frames && frames.length > 0) {
-      setFrames(frames)
-      const animationTimeline = buildCssAnimationTimeline(
-        actualIFrameRef.current,
-        frames
-      )
-      console.log(animationTimeline)
-      // @ts-ignore
-      setCssAnimationTimeline(animationTimeline)
-    }
-
-    console.log(interpretation)
-  }, [])
 
   return (
     <SolveExercisePageContextWrapper
@@ -208,57 +143,5 @@ export default function FrontendTrainingPage() {
         </div>
       </FrontendTrainingPageContext.Provider>
     </SolveExercisePageContextWrapper>
-  )
-}
-
-export function buildCssAnimationTimeline(
-  domElement: HTMLIFrameElement | null,
-  frames: Frame[]
-) {
-  let animations: Animation[] = []
-  let placeholder = false
-
-  if (!domElement) return
-
-  const document = domElement.contentDocument
-  if (!document) return
-
-  frames.forEach((frame) => {
-    if (frame.animations) {
-      frame.animations.forEach((animation) => {
-        const target = document.querySelector(animation.selector)
-        if (!target) return
-
-        const defaultStyle =
-          DEFAULT_BROWSER_STYLES[target.tagName.toLowerCase()][
-            animation.property
-          ]
-
-        // // if there is a default style, prepend it to the animation, so the student can undo their css
-        if (defaultStyle && typeof defaultStyle === 'string') {
-          const newAnim: Animation = {
-            targets: target,
-            [animation.property]: defaultStyle,
-            duration: 0,
-            offset: frame.time - 1,
-          }
-          animations.push(newAnim)
-        }
-
-        const newAnim: Animation = {
-          targets: target,
-          [animation.property]: animation.value,
-          duration: 0,
-          offset: frame.time,
-        }
-        animations.push(newAnim)
-      })
-    }
-  })
-
-  // @ts-ignore
-  return new AnimationTimeline({}, frames).populateTimeline(
-    animations,
-    placeholder
   )
 }

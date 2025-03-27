93b10ca7f785dd38a725278e7b548d4d9764ab34
dem4ron <demaaron88@gmail.com>
2025-03-27 13:58:08 +0100

    Run the js code outside of iframe body tag

--- app/javascript/components/bootcamp/FrontendTrainingPage/utils/updateIFrame.ts
+++ app/javascript/components/bootcamp/FrontendTrainingPage/utils/updateIFrame.ts
@@ -1,5 +1,5 @@
 export function updateIFrame(
-  iFrameRef:
+  iframeRef:
     | React.RefObject<HTMLIFrameElement>
     | React.ForwardedRef<HTMLIFrameElement>,
   {
@@ -10,12 +10,12 @@ export function updateIFrame(
 ) {
   let iframeElement: HTMLIFrameElement | null = null
 
-  if (iFrameRef) {
-    if (typeof iFrameRef === 'function') {
+  if (iframeRef) {
+    if (typeof iframeRef === 'function') {
       console.warn('Function refs are not supported in updateIFrame')
       return
-    } else if ('current' in iFrameRef) {
-      iframeElement = iFrameRef.current
+    } else if ('current' in iframeRef) {
+      iframeElement = iframeRef.current
     }
   }
 
@@ -25,24 +25,16 @@ export function updateIFrame(
     iframeElement.contentDocument || iframeElement.contentWindow?.document
   if (!iframeDoc) return
 
-  const existingHtml = iframeDoc.body?.innerHTML || ''
-  const existingCss = iframeDoc.head?.querySelector('style')?.textContent || ''
-  const existingJs = iframeDoc.head?.querySelector('script')?.textContent || ''
-
-  const finalHtml = html ?? existingHtml
-  const finalCss = css ?? existingCss
-  const finalJs = javascript ?? existingJs
-
   iframeDoc.open()
   iframeDoc.write(`
     <!DOCTYPE html>
     <html>
       <head>
-        <style>${finalCss}</style>
+        <style>${css}</style>
         <script>
           window.runCode = function() {
             try {
-              ${finalJs}
+              ${javascript}
             } catch (error) {
               console.error('User script error:', error)
             }
@@ -50,10 +42,25 @@ export function updateIFrame(
         </script>
       </head>
       <body>
-        ${finalHtml}
-        <script>window.runCode?.()</script>
-      </body>
-    </html>
-  `)
+        ${html}
+        </body>
+        </html>
+        `)
   iframeDoc.close()
+
+  iframeElement.onload = () => {
+    try {
+      // @ts-ignore
+      const runCode = iframeElement?.contentWindow?.runCode
+      if (typeof runCode === 'function') {
+        runCode()
+      } else {
+        console.warn('runCode is not defined on iframe')
+      }
+    } catch (err) {
+      console.error('Failed to execute runCode:', err)
+    }
+  }
 }
+
+/* <script>window.runCode?.()</script> */

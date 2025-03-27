83e2952767e1366a1a2ea1f643c716444ca39fc1
Jeremy Walker <jez.walker@gmail.com>
2025-03-27 22:56:01 +0000

    WIP

--- app/javascript/components/bootcamp/JikiscriptExercisePage/test-runner/generateAndRunTestSuite/generateAndRunTestSuite.ts
+++ app/javascript/components/bootcamp/JikiscriptExercisePage/test-runner/generateAndRunTestSuite/generateAndRunTestSuite.ts
@@ -5,31 +5,37 @@ import exerciseMap, {
 import { execTest } from './execTest'
 import { type TestRunnerOptions } from '@/components/bootcamp/types/TestRunner'
 
-export default (options: TestRunnerOptions) => {
-  return describe(options.config.title, (test) => {
+export async function generateAndRunTestSuite(options: TestRunnerOptions) {
+  return await describe(options.config.title, async (test) => {
     let project: Project | undefined
     if (options.config.projectType) {
       project = exerciseMap.get(options.config.projectType)
     }
-    options.tasks.map((taskData) => {
-      taskData.tests.map((testData) => {
-        test(testData.name, testData.descriptionHtml, () => {
-          const result = execTest(testData, options, project)
 
-          const { frames, expects } = result
-
-          // make sure a test is only successful if all frames are successful
-          expects.push({
-            actual: 'running',
-            matcher: 'toBe',
-            errorHtml: 'Your code has an error in it.',
-            expected: true,
-            pass: frames.every((frame) => frame.status === 'SUCCESS'),
-          })
-
-          return { ...result, expects }
-        })
-      })
-    })
+    await mapTasks(test, options, project)
   })
 }
+const mapTasks = async (test, options, project) => {
+  for (const taskData of options.tasks) {
+    for (const testData of taskData.tests) {
+      await test(testData.name, testData.descriptionHtml, async () => {
+        const result = await execTest(testData, options, project)
+
+        const { frames, expects } = result
+
+        // make sure a test is only successful if all frames are successful
+        expects.push({
+          actual: 'running',
+          matcher: 'toBe',
+          errorHtml: 'Your code has an error in it.',
+          expected: true,
+          pass: frames.every((frame) => frame.status === 'SUCCESS'),
+        })
+
+        return { ...result, expects }
+      })
+    }
+  }
+}
+
+export default generateAndRunTestSuite

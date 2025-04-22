79bbff963159dd98c40265d787b0f16f63b2fbd2
dem4ron <demaaron88@gmail.com>
2025-04-22 11:33:06 +0200

    Simplify undefined check

--- app/javascript/components/bootcamp/JikiscriptExercisePage/utils/generateCodeRunString.ts
+++ app/javascript/components/bootcamp/JikiscriptExercisePage/utils/generateCodeRunString.ts
@@ -1,7 +1,7 @@
 import { formatJikiObject } from '@/interpreter/helpers'
 
 export function generateCodeRunString(fn: string | undefined, args: any[]) {
-  if (!fn || fn === undefined) return ''
+  if (!fn) return ''
   if (!args) return `${fn}()`
   args = args.map((p) => formatJikiObject(p))
   return `${fn}(${args.join(', ')})`

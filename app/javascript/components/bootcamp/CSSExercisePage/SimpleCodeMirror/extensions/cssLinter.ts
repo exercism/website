e59fecb341e1a386629a40a9196561be37c2f352
dem4ron <demaaron88@gmail.com>
2025-04-23 10:54:52 +0200

    Add all rules, each set to false

--- app/javascript/components/bootcamp/CSSExercisePage/SimpleCodeMirror/extensions/cssLinter.ts
+++ app/javascript/components/bootcamp/CSSExercisePage/SimpleCodeMirror/extensions/cssLinter.ts
@@ -3,7 +3,42 @@ import { linter as cmLinter, Diagnostic } from '@codemirror/lint'
 
 export const cssLinter = cmLinter((view) => {
   const code = view.state.doc.toString()
-  const messages = CSSLint.verify(code)
+  // check https://csslint.net/about.html
+  const messages = CSSLint.verify(code, {
+    'adjoining-classes': false,
+    'box-model': false,
+    'box-sizing': false,
+    'compatible-vendor-prefixes': false,
+    'display-property-grouping': false,
+    'duplicate-background-images': false,
+    'duplicate-properties': false,
+    'empty-rules': false,
+    errors: false,
+    'fallback-colors': false,
+    floats: false,
+    'font-faces': false,
+    'font-sizes': false,
+    gradients: false,
+    ids: false,
+    import: false,
+    important: false,
+    'known-properties': false,
+    'outline-none': false,
+    'overqualified-elements': false,
+    'qualified-headings': false,
+    'regex-selectors': false,
+    'rules-count': false,
+    'selector-max-approaching': false,
+    shorthand: false,
+    'star-property-hack': false,
+    'text-indent': false,
+    'underscore-property-hack': false,
+    'unique-headings': false,
+    'universal-selector': false,
+    'unqualified-attributes': false,
+    'vendor-prefix': false,
+    'zero-units': false,
+  })
 
   const diagnostics: Diagnostic[] = messages.messages.map((msg) => ({
     from: view.state.doc.line(msg.line).from + msg.col - 1,

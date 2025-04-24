635c2e0ef18f5d78dfa3b39d69cf466b80d7c776
dem4ron <demaaron88@gmail.com>
2025-04-24 14:42:12 +0200

    Update expectedReferenceIFrame only once, adjust curtain visually

--- app/javascript/components/bootcamp/CSSExercisePage/ActualOutput.tsx
+++ app/javascript/components/bootcamp/CSSExercisePage/ActualOutput.tsx
@@ -67,40 +67,50 @@ export function ActualOutput() {
               : 'none',
           }}
         />
-        {/* Don't show it at all if curtainMode or diffMode is not on */}
-        {(curtainMode || diffMode) && (
-          <>
-            {/* the reference iframe - visually the same as `expected` */}
-            <iframe
-              className="absolute top-0 left-0 h-full w-full"
-              ref={expectedReferenceIFrameRef}
-              style={{
-                opacity: 1,
-                zIndex: 10,
-              }}
-            />
-            {curtainMode && (
-              <>
-                {/* the curtain itself */}
-                <div
-                  className="absolute top-0 right-0 h-full"
-                  style={{
-                    boxShadow: `-${curtainWidth}px 0 0 1px #f22, 0 0 0 ${curtainWidth}px rgba(255, 255, 255, ${curtainOpacity})`,
-                    zIndex: 20,
-                  }}
-                />
-                {/* mouse-capture area - otherwise mouse-move couldn't be properly captured */}
-                <div
-                  className="absolute top-0 left-0 w-full h-full"
-                  onMouseMove={handleMouseMove}
-                  onMouseLeave={handleOnMouseLeave}
-                  onMouseEnter={handleOnMouseEnter}
-                  style={{ zIndex: 40 }}
-                />
-              </>
-            )}
-          </>
-        )}
+        <>
+          {/* the reference iframe - visually the same as `expected` */}
+          <iframe
+            className="absolute top-0 left-0 h-full w-full"
+            ref={expectedReferenceIFrameRef}
+            style={{
+              opacity: 1,
+              zIndex: 10,
+              display: curtainMode || diffMode ? 'block' : 'none',
+            }}
+          />
+          {curtainMode && (
+            <>
+              {/* the curtain itself */}
+              <div
+                className="absolute top-0 right-0 h-full"
+                style={{
+                  width: `${curtainWidth}px`,
+                  backgroundColor: `rgba(255, 255, 255, ${curtainOpacity})`,
+                  zIndex: 20,
+                }}
+              />
+
+              <div
+                className="absolute top-0 h-full"
+                style={{
+                  left: `calc(100% - ${curtainWidth}px)`,
+                  width: '2px',
+                  backgroundColor: '#f22',
+                  zIndex: 25,
+                }}
+              />
+
+              {/* mouse-capture area - otherwise mouse-move couldn't be properly captured */}
+              <div
+                className="absolute top-0 left-0 w-full h-full"
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleOnMouseLeave}
+                onMouseEnter={handleOnMouseEnter}
+                style={{ zIndex: 40 }}
+              />
+            </>
+          )}
+        </>
       </div>
     </div>
   )

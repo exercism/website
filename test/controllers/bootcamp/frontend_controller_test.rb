8ae08cf9ec8771c3ca7e54fb6016973a89ec0749
dem4ron <demaaron88@gmail.com>
2025-04-25 17:08:01 +0200

    Remove fe controller test

--- test/controllers/bootcamp/frontend_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Bootcamp::FrontendControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get bootcamp_frontend_index_url
-    assert_response :success
-  end
-end

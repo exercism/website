041b964b9b5de7f6bc107f601f67271a87e18c0f
Jeremy Walker <jez.walker@gmail.com>
2025-05-09 22:36:07 +0900

    Fix the world

--- app/javascript/components/bootcamp/CSSExercisePage/utils/getIframePixels.ts
+++ app/javascript/components/bootcamp/CSSExercisePage/utils/getIframePixels.ts
@@ -1,5 +1,5 @@
-// import { toCanvas, toSvg, toJpeg } from 'html-to-image/src'
-import { toCanvas, toSvg, toJpeg } from 'html-to-image-local/'
+import { toCanvas, toSvg } from '../../../../../../../html-to-image/src'
+// import { toCanvas, toSvg  } from 'html-to-image'
 
 export async function getIframePixels(
   iframeRef: React.RefObject<HTMLIFrameElement>
@@ -17,7 +17,45 @@ export async function getIframePixels(
   }
 
   try {
-    const canvas = await toCanvas(iframeDoc.body, {
+    const encodedSvg = await toSvg(iframeDoc.body, {
+      width: iframe.clientWidth,
+      height: iframe.clientHeight,
+    })
+    // Random ID
+    const svgId = `jiki-svg-${Math.random().toString(36).substring(2, 15)}`
+
+    // Wrap everything
+    const style = iframeDoc.createElement('style')
+    style.innerHTML = `#${svgId} {`
+    // Iterate through stylesheets
+    for (const stylesheet of iframeDoc.styleSheets)
+      for (const rule of stylesheet.cssRules) {
+        // Add the rule to the style
+        style.innerText += rule.cssText.replaceAll('\n', '')
+      }
+    style.innerHTML += `}`
+
+    // Parse decoded into an SVG element
+    let decoded = decodeURIComponent(encodedSvg.split(',')[1])
+    const decodedElem = document.createElement('div')
+    decodedElem.innerHTML = decoded
+    const svg = decodedElem.firstChild! as SVGElement
+    svg.id = svgId
+    document.body.appendChild(svg)
+
+    const foreignObject = svg.firstChild! as SVGForeignObjectElement
+
+    const bodyWrapper = document.createElement('div')
+    bodyWrapper.classList.add('--jiki-faux-body')
+
+    // Move all children inside the foreignObject to the wrappedDecoded
+    for (const child of foreignObject.children) {
+      bodyWrapper.appendChild(child)
+    }
+    bodyWrapper.appendChild(style)
+    foreignObject.appendChild(bodyWrapper)
+
+    const canvas = await toCanvas(svg, {
       width: iframe.clientWidth,
       height: iframe.clientHeight,
     })

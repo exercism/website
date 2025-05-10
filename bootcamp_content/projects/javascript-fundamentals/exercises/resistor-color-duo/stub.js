d3a5ac92086fd9fa084a11397b52a2fc5954daa6
Jeremy Walker <jez.walker@gmail.com>
2025-05-11 00:32:01 +0900

    Seriously, Prettier?!

--- bootcamp_content/projects/javascript-fundamentals/exercises/resistor-color-duo/stub.js
+++ bootcamp_content/projects/javascript-fundamentals/exercises/resistor-color-duo/stub.js
@@ -3,8 +3,4 @@ export function decodedValue(values) {}
 /**
  * This is your code from the first resistor color exercise.
  */
-{
-  {
-    EXERCISE: javascript - fundamentals / resistor - color
-  }
-}
+{{EXERCISE:javascript-fundamentals/resistor-color}}

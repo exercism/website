15fa169a8fd1c83e6c1d6fdbf8390592353ffb51
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2025-05-14 11:52:21 +0200

    Editor style tweaks (#7805)
    * Adjust linenumber paddings
    
    * Adjust linenumber padding
    
    * Adjust control buttons width, and page-rhs bg color
    
    * Add gap between button groups

--- app/javascript/components/bootcamp/CSSExercisePage/LHS/ControlButtons.tsx
+++ app/javascript/components/bootcamp/CSSExercisePage/LHS/ControlButtons.tsx
@@ -88,8 +88,15 @@ export function ControlButtons({
     submitCode,
   ])
 
+  const {
+    panelSizes: { LHSWidth },
+  } = useCSSExercisePageStore()
+
   return (
-    <div className="flex py-8 justify-between">
+    <div
+      style={{ width: LHSWidth }}
+      className="flex py-8 gap-8 justify-between"
+    >
       <button onClick={handleSubmitCode} className="btn-primary btn-s">
         Check Result
       </button>

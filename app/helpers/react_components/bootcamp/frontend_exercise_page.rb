cc573470e1fea66eba35d2072577b2e6cdd2eecc
Jeremy Walker <jez.walker@gmail.com>
2025-05-16 19:07:00 +0900

    Get stuff working

--- app/helpers/react_components/bootcamp/frontend_exercise_page.rb
+++ app/helpers/react_components/bootcamp/frontend_exercise_page.rb
@@ -33,9 +33,9 @@ module ReactComponents
             css: exercise&.default("css")
           },
           stub: {
-            css: exercise&.stub("css"),
-            html: exercise&.stub("html"),
-            js: exercise&.stub("js")
+            css: exercise.stub("css"),
+            html: exercise.stub("html"),
+            js: exercise.stub("js")
           },
           aspect_ratio: editor_config[:aspect_ratio] || 1,
           code: solution&.code,

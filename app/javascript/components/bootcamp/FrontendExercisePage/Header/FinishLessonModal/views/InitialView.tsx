8b4a3aa10faaefbfbce9845c4444492a2a6bc5f8
Jeremy Walker <jez.walker@gmail.com>
2025-05-20 18:24:53 +0900

    Tweak text

--- app/javascript/components/bootcamp/FrontendExercisePage/Header/FinishLessonModal/views/InitialView.tsx
+++ app/javascript/components/bootcamp/FrontendExercisePage/Header/FinishLessonModal/views/InitialView.tsx
@@ -11,12 +11,17 @@ export function InitialView() {
 
   return (
     <>
-      <h2 className="text-[25px] mb-12 font-semibold">👋 Hey!</h2>
+      <h2 className="text-[26px] mb-20 font-semibold text-textColor1">
+        Ready to Complete? 🎉
+      </h2>
 
-      <p className="text-18 leading-140 mb-20">
-        In this exercise, it's left up to you to decide when you've got your
-        result to a standard you're happy with. Do you feel this exercise is
-        ready for you to complete?
+      <p className="text-18 leading-150 mb-20">
+        In this exercise, you decide when you're happy to finish the exercise.
+        Your aim is to have it look and function the same as the example
+        solution.
+      </p>
+      <p className="text-18 leading-140 mb-20 font-medium">
+        Is your solution ready to mark as completed?
       </p>
 
       <div className="flex items-center gap-8 self-stretch">

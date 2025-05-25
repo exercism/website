a0829a411e5928605f07fdf5d891182655ac771c
Jeremy Walker <jez.walker@gmail.com>
2025-05-26 00:57:20 +0900

    Use content box for CSS exercises unless specified

--- app/helpers/react_components/bootcamp/css_exercise.rb
+++ app/helpers/react_components/bootcamp/css_exercise.rb
@@ -75,12 +75,10 @@ module ReactComponents
 
     # rubocop:disable Layout/LineLength
     def normalize_css = "
+      * {box-sizing:content-box}
       button,hr,inputp{overflow:visible}progress,sub,sup{vertical-align:baseline}[type=checkbox],[type=radio],legend{box-sizing:border-box;padding:0}html{line-height:1.15;-webkit-text-size-adjust:100%}body, .--jiki-faux-body{margin:0}details,main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:ButtonText dotted 1px}fieldset{padding:.35em .75em .625em}legend{color:inherit;display:table;max-width:100%;white-space:normal}textarea{overflow:auto}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}[hidden],template{display:none}
       h1,h2,h3,h4,h5,h6,ul,ol,li,p { margin: 0; padding: 0; }
-      body, .--jiki-faux-body {
-       font-size: 14px; line-height: 1.3;
-    -webkit-font-smoothing: antialiased;
-       }
+      body, .--jiki-faux-body { font-size: 14px; line-height: 1.3; -webkit-font-smoothing: antialiased; }
       "
     # rubocop:enable Layout/LineLength
 

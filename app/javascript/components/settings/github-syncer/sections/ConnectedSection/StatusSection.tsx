787a0bf3e1308cafeca8a9e210b24704a9c295bb
dem4ron <demaaron88@gmail.com>
2025-05-27 16:41:46 +0200

    Style StatusSection a bit

--- app/javascript/components/settings/github-syncer/sections/ConnectedSection/StatusSection.tsx
+++ app/javascript/components/settings/github-syncer/sections/ConnectedSection/StatusSection.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { GitHubSyncerContext } from '../../GitHubSyncerForm'
+import { assembleClassNames } from '@/utils/assemble-classnames'
 
 export function StatusSection() {
-  const { repoFullName } = React.useContext(GitHubSyncerContext)
+  const { repoFullName, isUserActive } = React.useContext(GitHubSyncerContext)
+
+  const color = isUserActive ? 'successColor' : 'orange'
+  const status = isUserActive ? 'enabled' : 'paused'
 
   return (
-    <section>
-      <h2>Status</h2>
+    <section className={assembleClassNames('border-2', `border-${color}`)}>
+      <h2>
+        Status:{' '}
+        <span className={assembleClassNames(`text-${color}`)}>{status}</span>
+      </h2>
       <p className="text-16 leading-140">
-        Your GitHub syncer is enabled. It is linked to {repoFullName}.
+        Your GitHub syncer is{' '}
+        <strong className={`text-${color}`}>{status}</strong>. It is linked to{' '}
+        <code>{repoFullName || 'random-repo-name'}</code>.
       </p>
     </section>
   )

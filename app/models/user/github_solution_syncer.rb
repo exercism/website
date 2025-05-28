d9701b0d8767736a260cb02e2d30604977a63ac0
Jeremy Walker <jez.walker@gmail.com>
2025-05-28 19:09:49 +0900

    Update pull request language

--- app/models/user/github_solution_syncer.rb
+++ app/models/user/github_solution_syncer.rb
@@ -3,19 +3,19 @@ class User::GithubSolutionSyncer < ApplicationRecord
 
   validates :installation_id, :repo_full_name, presence: true
 
-  enum processing_method: { commit: 1, pr: 2 }
+  enum processing_method: { commit: 1, pull_request: 2 }
 
   before_create do
     self.commit_message_template = DEFAULT_COMMIT_MESSAGE_TEMPLATE unless self.commit_message_template.present?
     self.path_template = DEFAULT_PATH_TEMPLATE unless self.path_template.present?
-    self.processing_method = :pr unless self.processing_method.present?
+    self.processing_method = :pull_request unless self.processing_method.present?
     self.sync_on_iteration_creation = true unless self.sync_on_iteration_creation == false
     self.sync_exercise_files = false unless self.sync_exercise_files.present?
   end
 
   def processing_method = super.to_sym
   def commit_to_main? = processing_method == :commit
-  def create_pr? = processing_method == :pr
+  def create_pr? = processing_method == :pull_request
 
   DEFAULT_COMMIT_MESSAGE_TEMPLATE = "[Sync $sync_object] $track_slug/$exercise_slug/$iteration_idx".freeze
   DEFAULT_PATH_TEMPLATE = "solutions/$track_slug/$exercise_slug/$iteration_idx".freeze

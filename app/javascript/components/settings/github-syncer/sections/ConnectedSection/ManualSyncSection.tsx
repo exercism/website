d793c60af62cdef8cd222103941af1588de5293a
dem4ron <demaaron88@gmail.com>
2025-05-29 13:49:07 +0200

    Disable manual syncing on paused syncer

--- app/javascript/components/settings/github-syncer/sections/ConnectedSection/ManualSyncSection.tsx
+++ app/javascript/components/settings/github-syncer/sections/ConnectedSection/ManualSyncSection.tsx
@@ -3,6 +3,7 @@ import { GitHubSyncerContext } from '../../GitHubSyncerForm'
 import { TrackSelect } from '@/components/common/TrackSelect'
 import toast from 'react-hot-toast'
 import { fetchWithParams } from '../../fetchWithParams'
+import { StaticTooltip } from '@/components/bootcamp/JikiscriptExercisePage/Scrubber/ScrubberTooltipInformation'
 
 type Track = {
   title: string
@@ -11,7 +12,7 @@ type Track = {
 }
 
 export function ManualSyncSection() {
-  const { tracks, links } = useContext(GitHubSyncerContext)
+  const { tracks, links, isSyncingEnabled } = useContext(GitHubSyncerContext)
   const [track, setTrack] = useState<Track>({} as Track)
 
   const handleSyncSingleTrack = useCallback(
@@ -23,7 +24,8 @@ export function ManualSyncSection() {
         .then(async (response) => {
           if (response.ok) {
             toast.success(
-              `Your backup for the ${trackSlug} track has been queued. It should be completed within an hour.`
+              `Your backup for the ${trackSlug} track has been queued and should be completed within a few minutes.`,
+              { duration: 5000 }
             )
           } else {
             const data = await response.json()
@@ -50,7 +52,8 @@ export function ManualSyncSection() {
       .then(async (response) => {
         if (response.ok) {
           toast.success(
-            `Your backup for all tracks has been queued. It should be completed within an hour.`
+            `Your backup for all tracks has been queued and should be completed within a few minutes.`,
+            { duration: 5000 }
           )
         } else {
           const data = await response.json()
@@ -69,7 +72,12 @@ export function ManualSyncSection() {
   }, [links.syncEverything])
 
   return (
-    <section id="manual-sync-section">
+    <section
+      style={{
+        border: !isSyncingEnabled ? '2px solid #F69605' : '',
+      }}
+      id="manual-sync-section"
+    >
       <h2 className="!mb-6">Backup a track</h2>
       <p className="text-16 leading-150 mb-12">
         If you want to backup a track to GitHub, you can use this function.
@@ -94,14 +102,22 @@ export function ManualSyncSection() {
         />
       </div>
 
-      <button
-        onClick={() => handleSyncSingleTrack(track.slug)}
-        disabled={!track.slug}
-        className="btn btn-primary"
-      >
-        Backup Track
-      </button>
-
+      <div className="group relative">
+        <button
+          onClick={() => handleSyncSingleTrack(track.slug)}
+          disabled={!track.slug || !isSyncingEnabled}
+          className="btn btn-primary"
+        >
+          Backup Track
+        </button>
+        {!isSyncingEnabled && (
+          <StaticTooltip
+            style={{ transform: 'translate(-25%, -100%)', left: 0 }}
+            className="bg-textColor1 text-backgroundColorA opacity-90 rounded-8"
+            text="Syncing is paused. Enable it to back up this track."
+          />
+        )}
+      </div>
       <div className="border-t-1 border-borderColor6 my-32" />
       <h2 className="!mb-6">Backup everything</h2>
 
@@ -115,9 +131,22 @@ export function ManualSyncSection() {
         sparingly, for example when you want to bootstrap a new repo. This is
         not designed to be part of your normal workflow.
       </p>
-      <button onClick={handleSyncEverything} className="btn btn-primary">
-        Backup Everything
-      </button>
+      <div className="group relative">
+        <button
+          disabled={!isSyncingEnabled}
+          onClick={handleSyncEverything}
+          className="btn btn-primary relative group"
+        >
+          Backup Everything
+        </button>
+        {!isSyncingEnabled && (
+          <StaticTooltip
+            style={{ transform: 'translate(-25%, -100%)', left: 0 }}
+            className="bg-textColor1 text-backgroundColorA opacity-90 rounded-8"
+            text="Syncing is paused. Enable it to back up everything."
+          />
+        )}
+      </div>
     </section>
   )
 }

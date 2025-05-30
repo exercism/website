8b58302f761c97f66d7ba1f586084f52058457cd
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2025-05-30 13:37:23 +0200

    Adjust `formatColorInputDefaultValue` to rgb value without commas (#7879)

--- app/javascript/components/bootcamp/CSSExercisePage/SimpleCodeMirror/extensions/interaction/handlers/handleColorNode/formatColorInputDefaultValue.ts
+++ app/javascript/components/bootcamp/CSSExercisePage/SimpleCodeMirror/extensions/interaction/handlers/handleColorNode/formatColorInputDefaultValue.ts
@@ -3,7 +3,7 @@ import { rgb2hex } from '../../utils'
 export function formatColorInputDefaultValue(input: string): string {
   if (input.startsWith('#')) return input
 
-  const match = input.match(/rgb\s*\((\d+),\s*(\d+),\s*(\d+)\)/)
+  const match = input.match(/rgb\s*\(\s*(\d+)[,\s]+(\d+)[,\s]+(\d+)\s*\)/)
   if (match) {
     const [r, g, b] = match.slice(1).map(Number)
     return rgb2hex(r, g, b)

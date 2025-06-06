ffbc85b70a19db7b4379ee4bedd0662c77b54c51
Jeremy Walker <jez.walker@gmail.com>
2025-06-06 12:59:56 +0100

    Set git user details (#7906)

--- app/commands/user/github_solution_syncer/create_pull_request.rb
+++ app/commands/user/github_solution_syncer/create_pull_request.rb
@@ -38,9 +38,14 @@ class User::GithubSolutionSyncer
       # If it doesn't, then this is a naked repo, so create it.
       Dir.mktmpdir do |dir|
         @path = dir
-
         # No existing branch so create it
         git "init", "-b", base_branch
+
+        # Make sure we have the right user set
+        git "config", "user.name", "Exercism's Solution Syncer Bot"
+        git "config", "user.email", "211797793+exercism-solutions-syncer[bot]@users.noreply.github.com"
+
+        # Make an empty commit and push it
         git "commit", "--allow-empty", "-m", "Initial empty commit"
         git "remote", "add", "origin", repo_url
         git "push", "origin", base_branch

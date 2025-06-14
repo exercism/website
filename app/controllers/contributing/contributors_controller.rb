097bb4a5a3533e89d51c08c8f9623a78d822b250
Jeremy Walker <jez.walker@gmail.com>
2025-06-14 22:51:51 +0900

    More!

--- app/controllers/contributing/contributors_controller.rb
+++ app/controllers/contributing/contributors_controller.rb
@@ -1,9 +1,12 @@
 module Contributing
   class ContributorsController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index]
+    before_action :cache_public_action!, only: %i[index]
 
     def index
       response.set_header('Link', '<https://exercism.org/profiles>; rel="canonical"')
+
+      nil unless stale?(etag: Time.now.to_i / 300 * 300)
 
       # TODO: (Required) Set these correctly
       # @featured_contributor = User.first

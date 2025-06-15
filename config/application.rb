eb7da3f7adb8133c93a61ce8dab4cdf7d7354e24
Jeremy Walker <jez.walker@gmail.com>
2025-06-15 21:55:30 +0900

    Add content length header

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module Website
     end
 
     config.middleware.use Rack::CrawlerDetect
+    config.middleware.insert_after ActionDispatch::Static, Rack::ContentLength
     config.middleware.insert_before 0, RestoreIfNoneMatchHeader
 
     # Allow SVGs to render from active storage

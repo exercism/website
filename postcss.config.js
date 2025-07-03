a2d6d987e3acc6f95af2679cd8d2c8034b4e8cd7
dem4ron <demaaron88@gmail.com>
2025-07-03 17:44:15 +0200

    Try referencing instead of importing...

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
     require('@tailwindcss/postcss'),
-    require('postcss-hexrgba'),
-    require('postcss-flexbugs-fixes'),
-    require('@sector-labs/postcss-inline-class')(),
+    // require('postcss-hexrgba'),
+    // require('postcss-flexbugs-fixes'),
+    // require('@sector-labs/postcss-inline-class')(),
     // process.env.NODE_ENV === 'production'
     //   ? require('cssnano')({
     //       preset: 'default',

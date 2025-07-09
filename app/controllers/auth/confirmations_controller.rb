95faa79d1c0517f1a82304ab66d7cd265a74ec88
Jeremy Walker <jez.walker@gmail.com>
2025-07-09 06:26:21 +0100

    Try installing specific webdriver (#8035)
    * Try installing specific webdriver
    
    * Fix email

--- app/controllers/auth/confirmations_controller.rb
+++ app/controllers/auth/confirmations_controller.rb
@@ -1,5 +1,9 @@
 module Auth
   class ConfirmationsController < Devise::ConfirmationsController
     def required; end
+
+    def after_resending_confirmation_instructions_path_for(_resource_or_scope)
+      user_signed_in? ? settings_path : new_user_session_path
+    end
   end
 end

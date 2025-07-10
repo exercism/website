ed43a4b0ab3db5f76eac8837dce3f1caa16af2f4
dem4ron <demaaron88@gmail.com>
2025-07-10 11:11:47 +0200

    PoC

---
+++ app/javascript/i18n/hu/settings/github-syncer.ts
@@ -0,0 +1,27 @@
+export default {
+  connect_section: {
+    heading: 'Megoldások mentése GitHub-ra',
+    description:
+      'Mentsd automatikusan a megoldásaidat GitHub-ra a mentőeszközünkkel.',
+    benefits: {
+      safe: 'Biztonságos mentés',
+      green: 'Zöld négyzetek',
+      free: 'Ingyenes!',
+    },
+    steps: {
+      1: 'Hozz létre egy új GitHub tárolót...',
+      2: 'Kattints az alábbi gombra...',
+      3: 'Használd a "Backup Everything" opciót...',
+      4: 'A jövőbeli megoldások mentve lesznek...',
+    },
+    button: 'Mentés beállítása',
+  },
+  connect_modal: {
+    heading: 'Tároló csatlakoztatása',
+    intro1: 'Mielőtt továbblépsz...',
+    intro2:
+      'A következő képernyőn engedélyt kell adnod. <strong>Csak egy tárolót válassz</strong>.',
+    connect_button: 'GitHub tároló csatlakoztatása',
+    cancel_button: 'Mégse',
+  },
+}

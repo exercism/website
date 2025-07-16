118d82d9d638aa50bbac4c102dca2f9c51be2a4f
dem4ron <demaaron88@gmail.com>
2025-07-16 23:43:01 +0200

    Add script to extract things

--- app/javascript/i18n.ts
+++ app/javascript/i18n.ts
@@ -5,6 +5,8 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import enGithubSyncer from './i18n/en/settings/github-syncer'
 import huGithubSyncer from './i18n/hu/settings/github-syncer'
 
+// import enExerciseWidget from './i18n/exercise-widget'
+
 let isInitialized = false
 
 export function initI18n() {
@@ -24,6 +26,7 @@ export function initI18n() {
       resources: {
         en: {
           'settings/github-syncer': enGithubSyncer,
+          // 'common/exercise-widget': enExerciseWidget
         },
         hu: {
           'settings/github-syncer': huGithubSyncer,

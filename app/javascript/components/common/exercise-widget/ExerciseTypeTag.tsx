ff3a251b7df534fa4e5ea224d9305f765ab77c64
dem4ron <demaaron88@gmail.com>
2025-07-17 17:08:11 +0200

    Finally a working version

--- app/javascript/components/common/exercise-widget/ExerciseTypeTag.tsx
+++ app/javascript/components/common/exercise-widget/ExerciseTypeTag.tsx
@@ -1,6 +1,10 @@
+// i18n-key-prefix: exerciseTypeTag
+// i18n-namespace: components/common/exercise-widget
 import React from 'react'
 import { GraphicalIcon } from '../../common'
 import { Size, ExerciseType } from '../../types'
+import { useTranslation } from 'react-i18next'
+const { t } = useTranslation('components/common/exercise-widget')
 
 export const ExerciseTypeTag = ({
   type,
@@ -15,13 +19,14 @@ export const ExerciseTypeTag = ({
     case 'concept':
       return (
         <div className={`c-exercise-type-tag --concept ${sizeClassName}`}>
-          <GraphicalIcon icon="concept-exercise" /> Learning Exercise
+          <GraphicalIcon icon="concept-exercise" />{' '}
+          {t('exerciseTypeTag.learningExercise')}
         </div>
       )
     case 'tutorial':
       return (
         <div className={`c-exercise-type-tag --tutorial ${sizeClassName}`}>
-          Tutorial Exercise
+          {t('exerciseTypeTag.tutorialExercise')}
         </div>
       )
     default:

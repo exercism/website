b8fa320f2908cb53348199b8b0cd77311b3615a6
dem4ron <demaaron88@gmail.com>
2025-07-18 17:15:32 +0200

    Add community translations

---
+++ app/javascript/i18n/en/components-community.ts
@@ -0,0 +1,18 @@
+// namespace: components/community
+export default {
+  'storiesGrid.index.moreStories': 'More Stories from our community',
+  'storiesGrid.index.listenLearn':
+    'Listen, learn and be inspired by our community members.',
+  'videoGrid.index.searchCommunity': 'Search community content',
+  'videoGrid.index.learnWithCommunity': 'Learn with our community',
+  'videoGrid.index.walkthroughsTutorials':
+    'Walkthroughs, tutorials and livestreams from our community.',
+  'videoGrid.index.noVideosFound': 'No videos found.',
+  'videoGrid.index.tryChangingFilters':
+    'Try changing your filters to find the video you are looking for.',
+  'videoGrid.index.thereAreNoVideos': 'There are currently no videos.',
+  'videoGrid.index.checkBackLater': 'Check back here later for more!',
+  'videoGrid.trackFilterList.openTrackFilter': 'Open the track filter',
+  'videoGrid.trackFilterList.clickToChange': 'Click to change',
+  'videoGrid.useVideoGrid.video': 'video',
+}

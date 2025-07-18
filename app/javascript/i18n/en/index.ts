4ca3fae618194bb371e8c8ef15acc94099873786
dem4ron <demaaron88@gmail.com>
2025-07-18 17:25:16 +0200

    Extract concept-map

--- app/javascript/i18n/en/index.ts
+++ app/javascript/i18n/en/index.ts
@@ -1,5 +1,6 @@
 import componentscommunity from './components-community'
 import componentscommonexercisewidget from './components-common-exercise-widget'
+import componentsconceptmap from './components-concept-map'
 import componentscommunitysolutions from './components-community-solutions'
 import componentscommonsharepanel from './components-common-share-panel'
 import componentscommonmarkdowneditorform from './components-common-markdown-editor-form'
@@ -7,6 +8,7 @@ import componentscommonmarkdowneditorform from './components-common-markdown-edi
 export default {
   'components/community': componentscommunity,
   'components/common/exercise-widget': componentscommonexercisewidget,
+  'components/concept-map': componentsconceptmap,
   'components/community-solutions': componentscommunitysolutions,
   'components/common/share-panel': componentscommonsharepanel,
   'components/common/markdown-editor-form': componentscommonmarkdowneditorform,

ef884b1707be943cfc370ba07939af515e2c753f
dem4ron <demaaron88@gmail.com>
2025-07-18 16:32:34 +0200

    Slightly refactor code

---
+++ app/javascript/i18n/extract-copy/toCamelCase.ts
@@ -0,0 +1,5 @@
+function toCamelCase(str: string): string {
+  return str
+    .replace(/[-_\s]+(.)?/g, (_, char) => (char ? char.toUpperCase() : ''))
+    .replace(/^[A-Z]/, (char) => char.toLowerCase())
+}

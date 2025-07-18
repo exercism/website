0b9e689d195b3d1a17088f33554c1016617b315e
dem4ron <demaaron88@gmail.com>
2025-07-19 00:49:55 +0200

    Translate FeedbackPanel

--- app/javascript/i18n/en/index.ts
+++ app/javascript/i18n/en/index.ts
@@ -1,3 +1,4 @@
+import componentseditorFeedbackPanel from './components-editor-FeedbackPanel'
 import componentsdonations from './components-donations'
 import componentscommunity from './components-community'
 import componentscommonexercisewidget from './components-common-exercise-widget'
@@ -14,6 +15,7 @@ import componentscommonsharepanel from './components-common-share-panel'
 import componentscommonmarkdowneditorform from './components-common-markdown-editor-form'
 
 export default {
+  'components/editor/FeedbackPanel': componentseditorFeedbackPanel,
   'components/donations': componentsdonations,
   'components/community': componentscommunity,
   'components/common/exercise-widget': componentscommonexercisewidget,

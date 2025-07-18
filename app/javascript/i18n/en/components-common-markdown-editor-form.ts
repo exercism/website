31bf4e2d8d87b1073758bc455e7f6e6d08c86ace
dem4ron <demaaron88@gmail.com>
2025-07-18 13:29:35 +0200

    Add more translation files

---
+++ app/javascript/i18n/en/components-common-markdown-editor-form.ts
@@ -0,0 +1,8 @@
+export default {
+  'mobileIdleFormFooter.send': 'Send',
+  'newFormFooter.cancel': 'Cancel',
+  'newFormFooter.send': 'Send',
+  'editFormFooter.cancel': 'Cancel',
+  'editFormFooter.delete': 'Delete',
+  'editFormFooter.update': 'Update',
+}

5d33591941d35795e5ed70da2193a1be3c6b84f5
dem4ron <demaaron88@gmail.com>
2025-07-18 16:57:44 +0200

    Add export index generator script

---
+++ app/javascript/i18n/en/index.ts
@@ -0,0 +1,9 @@
+import componentscommonexercisewidget from './components-common-exercise-widget'
+import componentscommonsharepanel from './components-common-share-panel'
+import componentscommonmarkdowneditorform from './components-common-markdown-editor-form'
+
+export default {
+  'components/common/exercise-widget': componentscommonexercisewidget,
+  'components/common/share-panel': componentscommonsharepanel,
+  'components/common/markdown-editor-form': componentscommonmarkdowneditorform,
+}

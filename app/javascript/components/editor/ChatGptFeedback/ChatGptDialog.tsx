ef4d84d79c06fd39be38cdef99f3f71efcb3ff66
dem4ron <demaaron88@gmail.com>
2025-07-19 00:42:35 +0200

    Translate ChatGPTFeedback

--- app/javascript/components/editor/ChatGptFeedback/ChatGptDialog.tsx
+++ app/javascript/components/editor/ChatGptFeedback/ChatGptDialog.tsx
@@ -1,8 +1,12 @@
+// i18n-key-prefix: chatGptDialog
+// i18n-namespace: components/editor/ChatGptFeedback
 import React from 'react'
 import { Modal } from '@/components/modals'
 import { Submission } from '../types'
 import { SingleSelect } from '@/components/common/SingleSelect'
 import { ErrorBoundary, useErrorHandler } from '@/components/ErrorBoundary'
+import { useAppTranslation } from '@/i18n/useAppTranslation'
+import { Trans } from 'react-i18next'
 
 export type GptModelInfo = {
   version: GPTModel
@@ -93,6 +97,8 @@ export const ChatGptDialog = ({
   error,
   exceededLimit,
 }: ChatGptDialogModalProps): JSX.Element => {
+  const { t } = useAppTranslation('components/editor/ChatGptFeedback')
+
   return (
     <Modal
       open={open}
@@ -102,15 +108,16 @@ export const ChatGptDialog = ({
       shouldCloseOnOverlayClick
       ReactModalClassName={`max-w-[40%]`}
     >
-      <h3 className="text-h5 mb-8">Are you sure you want to ask ChatGPT?</h3>
+      <h3 className="text-h5 mb-8">
+        {t('chatGptDialog.areYouSureYouWantToAskChatGPT')}
+      </h3>
       <p className="text-p-base mb-16">
-        ChatGPT is a powerful tool, but it can also make it too easy to get
-        unstuck and remove a lot of the learning opportunity that comes from
-        wrestling with a problem. We recommend only using it when you&apos;re
-        too stuck to continue without help.
+        {t('chatGptDialog.chatGptIsAPowerfulTool')}
       </p>
 
-      <div className="text-p-base text-textColor6 mb-8">Select a model:</div>
+      <div className="text-p-base text-textColor6 mb-8">
+        {t('chatGptDialog.selectAModel')}
+      </div>
       <SingleSelect<GptModelInfo>
         options={gptUsageToArray(chatgptUsage)}
         OptionComponent={OptionComponent}
@@ -119,17 +126,18 @@ export const ChatGptDialog = ({
         setValue={setValue}
       />
       <div className="text-textColor6 text-p-small mt-12">
-        Note: ChatGPT 4 is{' '}
-        <strong className="font-medium">significantly</strong> better. However,
-        ChatGPT 4 costs $0.20 per request (about 15x more than ChatGPT 3.5) so
-        we have strict limits and give both options. Quotas reset on the first
-        of each month. We suggest trying ChatGPT 3 first, then use one of your
-        v4 tokens if needed.
+        {t('chatGptDialog.noteChatGPT4IsSignificantlyBetter')}
+        <Trans
+          i18nKey="chatGptDialog.noteChatGPT4IsSignificantlyBetter"
+          components={{
+            bold: <strong className="font-semibold" />,
+          }}
+        />
       </div>
 
       {exceededLimit ? (
         <div className="c-alert--danger text-16 font-body mt-16 normal-case">
-          You have exceeded the limit
+          {t('chatGptDialog.youHaveExceededTheLimit')}
         </div>
       ) : (
         <ErrorBoundary FallbackComponent={ErrorFallback}>
@@ -138,10 +146,10 @@ export const ChatGptDialog = ({
       )}
       <div className="flex gap-8 mt-32 ">
         <button className="btn-s btn-primary" onClick={onGo}>
-          Go
+          {t('chatGptDialog.go')}
         </button>
         <button className="btn-s btn-default" onClick={onClose}>
-          Cancel
+          {t('chatGptDialog.cancel')}
         </button>
       </div>
     </Modal>
---
+++ app/javascript/i18n/en/components-editor-ChatGptFeedback.ts
@@ -0,0 +1,41 @@
+// namespace: components/editor/ChatGptFeedback
+export default {
+  'chatGptTab.chatGpt': 'ChatGPT',
+  'chatGptDialog.areYouSureYouWantToAskChatGPT':
+    'Are you sure you want to ask ChatGPT?',
+  'chatGptDialog.chatGptIsAPowerfulTool':
+    'ChatGPT is a powerful tool, but it can also make it too easy to get unstuck and remove a lot of the learning opportunity that comes from wrestling with a problem. We recommend only using it when you&apos;re too stuck to continue without help.',
+  'chatGptDialog.selectAModel': 'Select a model:',
+  'chatGptDialog.noteChatGPT4IsSignificantlyBetter':
+    'Note: ChatGPT 4 is <bold>significantly</bold> better. However, ChatGPT 4 costs $0.20 per request (about 15x more than ChatGPT 3.5) so we have strict limits and give both options. Quotas reset on the first of each month. We suggest trying ChatGPT 3 first, then use one of your v4 tokens if needed.',
+  'chatGptDialog.youHaveExceededTheLimit': 'You have exceeded the limit',
+  'chatGptDialog.go': 'Go',
+  'chatGptDialog.cancel': 'Cancel',
+  'useChatGptFeedback.unableToAskChatGPT': 'Unable to ask ChatGPT',
+  'chatGptUpsellContent.exercismInsiders': 'Exercism Insiders',
+  'chatGptUpsellContent.donateToExercismAndGetBehindTheScenesAccessAndBonusFeatures':
+    'Donate to Exercism and get behind the scenes access and bonus features.',
+  'chatGptUpsellContent.needHelpGettingUnstuckUnlockOurChatGPTIntegration':
+    'Need help getting unstuck? Unlock our <strong>ChatGPT Integration</strong> and extra mentoring slots, as well as Dark Mode, exclusive badges and more, when you donate to Exercism.',
+  'chatGptUpsellContent.learnMore': 'Learn More',
+  'askChatGpt.chatGptsSuggestions': 'ChatGPT&apos;s Suggestions',
+  'askChatGpt.chatGptHasTheFollowingSuggestions':
+    'ChatGPT has the following suggestions:',
+  'askChatGpt.reminderUseThisAdviceWisely': 'Reminder: Use this advice wisely',
+  'askChatGpt.howeverCleverChatGptAppearsItDoesNotUnderstandCode':
+    'However clever ChatGPT appears, it does not &quot;understand&quot; code. Its suggestions may therefore be incorrect, muddled or misleading. However, it often provides useful insights to help unblock you. <bold>Use these suggestions as inspiration, not instruction.</bold>',
+  'askChatGpt.askingChatGPT': 'Asking ChatGPT…',
+  'askChatGpt.estimatedRunningTime15s': 'Estimated running time 15s',
+  'askChatGpt.getUnstuckWithChatGPT': 'Get unstuck with ChatGPT',
+  'askChatGpt.ifYoureFeelingStuckAndCantSeemToMakeProgress':
+    'If you&apos;re feeling stuck and can&apos;t seem to make progress, don&apos;t worry - just ask ChatGPT for help and get back on track.',
+  'askChatGptButton.pleaseRunTheTestsFirst': 'Please run the tests first.',
+  'askChatGptButton.pleaseRerunTheTestsToContinue':
+    'Please rerun the tests to continue.',
+  'askChatGptButton.testsAreCurrentlyRunning': 'Tests are currently running.',
+  'askChatGptButton.awaitingResponseFromChatGPTPleaseStandBy':
+    'Awaiting response from ChatGPT. Please stand by.',
+  'askChatGptButton.congratsTheTestsArePassing':
+    'Congrats! The tests are passing! 🎉',
+  'askChatGptButton.stuckAskChatGPT': 'Stuck? Ask ChatGPT',
+}

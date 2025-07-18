d57412a557297635075dfe21927195625dfba8e3
dem4ron <demaaron88@gmail.com>
2025-07-19 00:49:55 +0200

    Translate FeedbackPanel

--- app/javascript/components/editor/FeedbackPanel/FeedbackPanelMentoringDiscussion/PendingMentoringRequest.tsx
+++ app/javascript/components/editor/FeedbackPanel/FeedbackPanelMentoringDiscussion/PendingMentoringRequest.tsx
@@ -1,20 +1,30 @@
+// i18n-key-prefix: feedbackPanelMentoringDiscussion.pendingMentoringRequest
+// i18n-namespace: components/editor/FeedbackPanel
 import React from 'react'
 import { FeedbackPanelProps } from '../FeedbackPanel'
+import { useAppTranslation } from '@/i18n/useAppTranslation'
 
 export function PendingMentoringRequest({
   mentoringRequestLink,
 }: Pick<FeedbackPanelProps, 'mentoringRequestLink'>): JSX.Element {
+  const { t } = useAppTranslation('components/editor/FeedbackPanel')
+
   return (
     <div className="flex flex-col">
       <p className="text-p-base mb-8">
         <strong className="font-semibold text-textColor2">
-          You&apos;ve submitted your solution for Code Review.
+          {t(
+            'feedbackPanelMentoringDiscussion.pendingMentoringRequest.youveSubmittedSolution'
+          )}
         </strong>{' '}
-        A mentor will (hopefully) provide you with feedback soon. You&apos;ll
-        recieve a notification and email when this happens.
+        {t(
+          'feedbackPanelMentoringDiscussion.pendingMentoringRequest.mentorWillProvideFeedback'
+        )}
       </p>
       <a className="btn-enhanced btn-s mr-auto" href={mentoringRequestLink}>
-        View your request
+        {t(
+          'feedbackPanelMentoringDiscussion.pendingMentoringRequest.viewYourRequest'
+        )}
       </a>
     </div>
   )

5331cfc5cb0124be88b2f33f5e7b8674cbe51d41
dem4ron <demaaron88@gmail.com>
2025-07-21 17:05:43 +0200

    Translate a few single files

---
+++ app/javascript/i18n/en/components-editor-LegacyFileBanner.tsx.ts
@@ -0,0 +1,7 @@
+// namespace: components/editor/LegacyFileBanner.tsx
+export default {
+  'legacyFileBanner.fileUnexpected':
+    'This file is unexpected. Did you upload it via the CLI by accident?',
+  'legacyFileBanner.deleteFile': 'Delete file…',
+  'legacyFileBanner.learnMore': 'Learn More',
+}
---
+++ app/javascript/i18n/en/components-editor-RunTestsButton.tsx.ts
@@ -0,0 +1,6 @@
+// namespace: components/editor/RunTestsButton.tsx
+export default {
+  'runTestsButton.youHaveNotMadeChanges':
+    'You have not made any changes since you last ran the tests',
+  'runTestsButton.runTests': 'Run Tests',
+}

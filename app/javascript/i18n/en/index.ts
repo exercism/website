ea9ab48ceefc47c1b2a809b429a1066d8772ecfb
dem4ron <demaaron88@gmail.com>
2025-07-21 17:15:39 +0200

    Impact extractions

--- app/javascript/i18n/en/index.ts
+++ app/javascript/i18n/en/index.ts
@@ -10,6 +10,7 @@ import componentsgithubsyncerwidget from './components-github-syncer-widget'
 import componentseditorlegacyfilebanner from './components-editor-legacy-file-banner'
 import componentseditorpanels from './components-editor-panels'
 import componentsdonationssubscriptionform from './components-donations-subscription-form'
+import componentsimpactTopLearningCountriestsx from './components-impact-TopLearningCountries.tsx'
 import componentsconceptmap from './components-concept-map'
 import componentscontributing from './components-contributing'
 import componentseditorEditorStatusSummarytsx from './components-editor-EditorStatusSummary.tsx'
@@ -19,6 +20,7 @@ import componentsdropdownstrackmenu from './components-dropdowns-track-menu'
 import componentscommunitysolutions from './components-community-solutions'
 import componentseditorheader from './components-editor-header'
 import componentsdropdownsreputation from './components-dropdowns-reputation'
+import componentsimpactImpactTestimonialtsx from './components-impact-ImpactTestimonial.tsx'
 import componentseditorChatGptFeedback from './components-editor-ChatGptFeedback'
 import componentseditorSubmitButtontsx from './components-editor-SubmitButton.tsx'
 import componentseditorLegacyFileBannertsx from './components-editor-LegacyFileBanner.tsx'
@@ -38,6 +40,8 @@ export default {
   'components/editor/legacy-file-banner': componentseditorlegacyfilebanner,
   'components/editor/panels': componentseditorpanels,
   'components/donations/subscription-form': componentsdonationssubscriptionform,
+  'components/impact/TopLearningCountries.tsx':
+    componentsimpactTopLearningCountriestsx,
   'components/concept-map': componentsconceptmap,
   'components/contributing': componentscontributing,
   'components/editor/EditorStatusSummary.tsx':
@@ -49,6 +53,8 @@ export default {
   'components/community-solutions': componentscommunitysolutions,
   'components/editor/header': componentseditorheader,
   'components/dropdowns/reputation': componentsdropdownsreputation,
+  'components/impact/ImpactTestimonial.tsx':
+    componentsimpactImpactTestimonialtsx,
   'components/editor/ChatGptFeedback': componentseditorChatGptFeedback,
   'components/editor/SubmitButton.tsx': componentseditorSubmitButtontsx,
   'components/editor/LegacyFileBanner.tsx': componentseditorLegacyFileBannertsx,

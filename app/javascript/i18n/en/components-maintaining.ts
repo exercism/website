772f8a37f1b4c4a6eda6f73a6626a09585501fe9
dem4ron <demaaron88@gmail.com>
2025-07-21 20:58:48 +0200

    Extract maintaining

---
+++ app/javascript/i18n/en/components-maintaining.ts
@@ -0,0 +1,9 @@
+// namespace: components/maintaining
+export default {
+  'submissionsSummaryTable.id': 'ID',
+  'submissionsSummaryTable.track': 'Track',
+  'submissionsSummaryTable.exercise': 'Exercise',
+  'submissionsSummaryTable.testStatus': 'Test status',
+  'submissionsSummaryTable.representationStatus': 'Representation status',
+  'submissionsSummaryTable.analysesStatus': 'Analyses status',
+}

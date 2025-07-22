6c1354b19cdacba072c8b8963b494b76bdcbfae4
dem4ron <demaaron88@gmail.com>
2025-07-22 17:33:15 +0200

    components/profile/testimonials-list

---
+++ app/javascript/i18n/en/components-profile-testimonials-list.ts
@@ -0,0 +1,5 @@
+// namespace: components/profile/testimonials-list
+export default {
+  attribution: `<student>{{student}}</student> said this about <mentor>{{mentor}}</mentor>`,
+  exerciseTrack: `on <strong>{{title}}</strong> in`,
+}

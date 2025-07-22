7d2bbd5e8dbd36c7387d88a6190e107ee6714489
dem4ron <demaaron88@gmail.com>
2025-07-22 09:27:20 +0200

    Sort namespaces in index file, extract more copy

---
+++ app/javascript/i18n/en/components-mentoring-session-iteration-view.ts
@@ -0,0 +1,12 @@
+// namespace: components/mentoring/session/iteration-view
+export default {
+  'iterationHeader.downloadButton.downloadThisSolutionViaCli':
+    'Download this solution via the CLI',
+  'iterationHeader.downloadButton.solutionDownloadedIntoSubdirectory':
+    'This solution will be downloaded into a subdirectory specifically for this student.',
+  'iterationHeader.copyButton.copied': 'Copied',
+  'iterationHeader.outOfDateNotice.exerciseUpdated':
+    'This exercise has been updated since the student submitted this\n        iteration. It might not pass the most recent set of tests. Exercises\n        can be updated by students by clicking on the yellow bar in the main\n        exercise page.',
+  'iterationHeader.downloadButton.downloadSolution': 'Download solution',
+  'iterationHeader.copyButton.copySolution': 'Copy solution',
+}

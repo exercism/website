4593586829c58ce2dcdf8d269e5bc1566b6e2697
dem4ron <demaaron88@gmail.com>
2025-07-22 10:42:41 +0200

    mentoring/Inbox

---
+++ app/javascript/i18n/en/components-mentoring-Inbox.tsx.ts
@@ -0,0 +1,7 @@
+// namespace: components/mentoring/Inbox.tsx
+export default {
+  'inbox.inbox': 'Inbox',
+  'inbox.awaitingStudent': 'Awaiting student',
+  'inbox.finished': 'Finished',
+  'inbox.filterByStudentOrExerciseName': 'Filter by student or exercise name',
+}

b3f91c56ba2c3d76c354037a773a0fa65e4d2f8d
dem4ron <demaaron88@gmail.com>
2025-07-22 13:15:52 +0200

    upload-video/elements

---
+++ app/javascript/i18n/en/components-modals-upload-video-elements.ts
@@ -0,0 +1,23 @@
+// namespace: components/modals/upload-video/elements
+export default {
+  'uploadVideoForm.useDifferentVideo': 'Use different video',
+  'uploadVideoForm.submitVideo': 'Submit video',
+  'uploadVideoTextInput.pasteYourVideoUrlYoutube':
+    'PASTE YOUR VIDEO URL (YOUTUBE)',
+  'uploadVideoTextInput.videoTitle': 'Video title',
+  'uploadVideoTextInput.enterTheVideoTitle': 'Enter the video title',
+  'uploadVideoForm.isTheVideoYoursOrSomeoneElses':
+    "IS THE VIDEO YOURS OR SOMEONE ELSE'S?",
+  'uploadVideoForm.mine': 'Mine',
+  'uploadVideoForm.someoneElse': 'Someone else',
+  'retrieveVideoForm.pasteYourVideoUrlYoutube':
+    'PASTE YOUR VIDEO URL (YOUTUBE)',
+  'retrieveVideoForm.thisLinkIsInvalidPleaseCheckItAgain':
+    'This link is invalid, please check it again!',
+  'retrieveVideoForm.pasteYourVideoHere': 'Paste your video here',
+  'retrieveVideoForm.retrieveVideo': 'Retrieve video',
+  'uploadVideoModalHeader.submitACommunityWorkthrough':
+    'Submit a community workthrough',
+  'uploadVideoModalHeader.producedAVideoOfWorkingThroughThisExerciseYourselfWantToShareItWithTheExercismCommunity':
+    'Produced a video of working through this exercise yourself? Want to share it with the Exercism community? <strong className="font-medium text">Submit the form below and Jeremy will review and approve it.</strong>',
+}

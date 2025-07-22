e296f0c99dcb4d41f61936da177316a2bc03cd90
dem4ron <demaaron88@gmail.com>
2025-07-22 13:30:03 +0200

    BugReportModal

---
+++ app/javascript/i18n/en/components-modals-BugReportModal.tsx.ts
@@ -0,0 +1,12 @@
+// namespace: components/modals/BugReportModal.tsx
+export default {
+  reportABug: 'Report a bug',
+  bugReportSubmittedThankYou: 'Bug report submitted. Thank you!',
+  close: 'Close',
+  thanksForReportingPleaseTellUsWhatIsWrong:
+    'Thanks for reporting. Please tell us what is wrong.',
+  pleaseProvideAsMuchDetailAsPossible:
+    'Please provide as much detail as possible',
+  submitBugReport: 'Submit bug report',
+  cancel: 'Cancel',
+}

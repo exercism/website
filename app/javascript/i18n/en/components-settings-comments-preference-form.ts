afd0bf9629646e8b13f4a739c9ec93960760c7f2
dem4ron <demaaron88@gmail.com>
2025-07-22 17:46:00 +0200

    comments-pref-form

---
+++ app/javascript/i18n/en/components-settings-comments-preference-form.ts
@@ -0,0 +1,22 @@
+// namespace: components/settings/comments-preference-form
+export default {
+  'commentsPreferenceForm.commentsOnYourSolutions':
+    'Comments on your solutions',
+  'commentsPreferenceForm.settingToControlComments':
+    'Use this setting to control whether or not people can post comments on <span className="font-medium">future solutions that you publish</span>. This can be overriden on a per-solution basis and you can update all existing solutions below.',
+  'commentsPreferenceForm.updatePreference': 'Update preference',
+  'commentsPreferenceForm.yourPreferencesHaveBeenUpdated':
+    'Your preferences have been updated',
+  'manageExistingSolutions.manageExistingSolutions':
+    'Manage existing solutions',
+  'manageExistingSolutions.canCommentOnPublishedSolutions':
+    'Currently, people can comment on {commentStatusPhrase} of your published solutions. Use the buttons below to <span className="font-medium">enable or disable comments on all your existing solutions.</span>.',
+  'manageExistingSolutions.allowCommentsOnExistingSolutions':
+    'Allow comments on all existing solutions',
+  'manageExistingSolutions.disableCommentsOnExistingSolutions':
+    'Disable comments on all existing solutions',
+  'useCommentsPreferenceForm.none': 'none',
+  'useCommentsPreferenceForm.all': 'all',
+  'useCommentsPreferenceForm.xOutOfY':
+    '{{numCommentsEnabled}} / {{numPublished}}',
+}

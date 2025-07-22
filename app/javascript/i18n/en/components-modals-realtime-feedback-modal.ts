7812b530160b0f24010e4d844abd26ad6e2b5af5
dem4ron <demaaron88@gmail.com>
2025-07-22 12:15:53 +0200

    realtime-feedback

---
+++ app/javascript/i18n/en/components-modals-realtime-feedback-modal.ts
@@ -0,0 +1,15 @@
+// namespace: components/modals/realtime-feedback-modal
+export default {
+  'realTimeFeedbackModal.deepDiveVideo': 'Deep Dive Video',
+  'realTimeFeedbackModal.feedback': 'Feedback',
+  'checkingForAutomatedFeedback.checking': 'Checking for automated feedback…',
+  'checkingForAutomatedFeedback.inspectingCode':
+    'Our systems are inspecting your code to find both automated feedback and feedback given by mentors on similar solutions.',
+  'checkingForAutomatedFeedback.processTime':
+    'This process normally takes ~10 seconds.',
+  'checkingForAutomatedFeedback.continueWaiting': 'Continue without waiting',
+  'checkingForAutomatedFeedback.takingLonger':
+    'Sorry, this is taking a little longer than expected. You may wish to continue without waiting. You can view any feedback on the iterations tab of the solution later.',
+  'feedbackContent.noFeedback': 'No Automated Feedback',
+  'feedbackContent.foundFeedback': 'Found Automated Feedback',
+}

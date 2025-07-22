1fc5162380922e1fc579794891e649228498a05a
dem4ron <demaaron88@gmail.com>
2025-07-22 12:08:52 +0200

    modals/profile

---
+++ app/javascript/i18n/en/components-modals-profile.ts
@@ -0,0 +1,8 @@
+// namespace: components/modals/profile
+export default {
+  'firstTimeModal.havePublicProfile': 'You now have a public profile!',
+  'firstTimeModal.goodJob': 'Good job!',
+  'firstTimeModal.seeSolutions':
+    "You'll now see your published solutions, mentoring testimonials, contributions and badges on your public profile.",
+  'firstTimeModal.shareProfile': 'You can share your profile with others too!',
+}

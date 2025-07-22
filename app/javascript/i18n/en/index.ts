5f4bde7f870fd961eb96862275e333e5fdfe0808
dem4ron <demaaron88@gmail.com>
2025-07-22 10:20:11 +0200

    SessionInfo

--- app/javascript/i18n/en/index.ts
+++ app/javascript/i18n/en/index.ts
@@ -61,11 +61,12 @@ import bx from './components-mentoring-session-iteration-view'
 import by from './components-mentoring-session-mobile-code-panel-MobileIterationView.tsx'
 import bz from './components-mentoring-session-mobile-code-panel-SessionInfoModal.tsx'
 import b0 from './components-mentoring-session-Scratchpad.tsx'
-import b1 from './components-mentoring-session-student-info'
-import b2 from './discussion-batch'
-import b3 from './session-batch-1'
-import b4 from './session-batch-2'
-import b5 from './session-batch'
+import b1 from './components-mentoring-session-SessionInfo.tsx'
+import b2 from './components-mentoring-session-student-info'
+import b3 from './discussion-batch'
+import b4 from './session-batch-1'
+import b5 from './session-batch-2'
+import b6 from './session-batch'
 
 export default {
   'automation-batch': aa,
@@ -131,9 +132,10 @@ export default {
   'components/mentoring/session/mobile-code-panel/MobileIterationView.tsx': by,
   'components/mentoring/session/mobile-code-panel/SessionInfoModal.tsx': bz,
   'components/mentoring/session/Scratchpad.tsx': b0,
-  'components/mentoring/session/student-info': b1,
-  'discussion-batch': b2,
-  'session-batch-1': b3,
-  'session-batch-2': b4,
-  'session-batch-3': b5,
+  'components/mentoring/session/SessionInfo.tsx': b1,
+  'components/mentoring/session/student-info': b2,
+  'discussion-batch': b3,
+  'session-batch-1': b4,
+  'session-batch-2': b5,
+  'session-batch-3': b6,
 }

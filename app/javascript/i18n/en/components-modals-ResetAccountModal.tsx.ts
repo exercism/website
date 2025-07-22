7202dbea4a0048630ac8d5f1891572777b0e9d8d
dem4ron <demaaron88@gmail.com>
2025-07-22 14:30:35 +0200

    ReseAccount

---
+++ app/javascript/i18n/en/components-modals-ResetAccountModal.tsx.ts
@@ -0,0 +1,21 @@
+// namespace: components/modals/ResetAccountModal.tsx
+export default {
+  youAreAboutToReset: "You're about to reset your Exercism account",
+  pleaseReadCarefully:
+    'Please read this carefully before commiting to reset your account.',
+  thisIsIrreversible:
+    'This is <em>irreversible</em> and will mean you’ll lose everything you’ve done on your account.',
+  byResettingAccount:
+    '<strong>By resetting your account, you will lose:</strong>',
+  allSolutionsSubmitted: 'All solutions you have submitted',
+  allMentoringReceived: 'All mentoring you have received',
+  allMentoringGiven:
+    'All mentoring you have given and any testimonials received.',
+  anyReputationEarned:
+    'Any reputation you have earned through mentoring or publishing solutions.',
+  toConfirmWriteHandle:
+    'To confirm, write your handle <pre>{{handle}}</pre> in the box below:',
+  cancel: 'Cancel',
+  resetAccount: 'Reset account',
+  unableToResetAccount: 'Unable to reset account',
+}

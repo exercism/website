44b4241f30e12dc9ca0d00d8b4984fc3295ef6c1
dem4ron <demaaron88@gmail.com>
2025-07-22 14:27:39 +0200

    More Modals

---
+++ app/javascript/i18n/en/components-modals-MentorChangeTracksModal.tsx.ts
@@ -0,0 +1,6 @@
+// namespace: components/modals/MentorChangeTracksModal.tsx
+export default {
+  changeTheTracksYouMentor: 'Change the Tracks you mentor',
+  youWillStillBeAbleToContinue:
+    'You will still be able to continue any existing discussions on other tracks.',
+}
---
+++ app/javascript/i18n/en/components-modals-MentorRegistrationModal.tsx.ts
@@ -0,0 +1,6 @@
+// namespace: components/modals/MentorRegistrationModal.tsx
+export default {
+  'chooseTrackStep.selectTracks': 'Select the tracks you want to mentor',
+  'commitStep.commitToBeGoodMentor': 'Commit to being a good mentor',
+  'congratulationsStep.congratulations': 'Congratulations',
+}

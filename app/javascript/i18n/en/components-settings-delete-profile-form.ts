945c295602e79e4e76c173e87c8321313d150ad7
dem4ron <demaaron88@gmail.com>
2025-07-22 17:46:45 +0200

    DeleteProfileModal

---
+++ app/javascript/i18n/en/components-settings-delete-profile-form.ts
@@ -0,0 +1,8 @@
+// namespace: components/settings/delete-profile-form
+export default {
+  'deleteProfileModal.deleteProfile': 'Delete profile?',
+  'deleteProfileModal.areYouSure':
+    'Are you sure you want to delete your profile?',
+  'deleteProfileModal.continue': 'Continue',
+  'deleteProfileModal.cancel': 'Cancel',
+}

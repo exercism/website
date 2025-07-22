554c319e2da2e808bb44b1a60408ca7cabf72ed0
dem4ron <demaaron88@gmail.com>
2025-07-22 12:14:30 +0200

    no-automated-feedback

--- app/javascript/components/modals/realtime-feedback-modal/feedback-content/no-automated-feedback/InProgressMentoring.tsx
+++ app/javascript/components/modals/realtime-feedback-modal/feedback-content/no-automated-feedback/InProgressMentoring.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ContinueButton } from '../../components/FeedbackContentButtons'
+import { useAppTranslation } from '@/i18n/useAppTranslation'
 
 export function InProgressMentoring({
   onContinue,
@@ -8,22 +9,20 @@ export function InProgressMentoring({
   mentorDiscussionLink: string
   onContinue: () => void
 }): JSX.Element {
+  const { t } = useAppTranslation(
+    'components/modals/realtime-feedback-modal/feedback-content/no-automated-feedback'
+  )
   return (
     <div className="flex flex-col items-start">
-      <h3 className="text-h4 mb-8">
-        You have a mentoring session active for this exercise.
-      </h3>
-      <p className="text-p-base mb-12">
-        It is generally good practice to tell your mentor what you&apos;ve
-        changed in your code and ask them to take a look at your new version.
-      </p>
+      <h3 className="text-h4 mb-8">{t('index.youHaveMentoringSession')}</h3>
+      <p className="text-p-base mb-12">{t('index.itIsGenerallyGood')}</p>
 
       <div className="flex gap-12">
         <a className="btn-primary btn-s mr-auto" href={mentorDiscussionLink}>
-          Go to your discussion
+          {t('index.goToYourDiscussion')}
         </a>
         <ContinueButton
-          text="Continue to exercise"
+          text={t('index.continueToExercise')}
           onClick={onContinue}
           className="btn-secondary"
         />

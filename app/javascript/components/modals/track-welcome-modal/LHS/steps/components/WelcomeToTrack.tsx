3c5ae32ed490eef0da5b4318c8e13bc6fdc54787
dem4ron <demaaron88@gmail.com>
2025-07-22 12:43:17 +0200

    WelcomeToTrack

--- app/javascript/components/modals/track-welcome-modal/LHS/steps/components/WelcomeToTrack.tsx
+++ app/javascript/components/modals/track-welcome-modal/LHS/steps/components/WelcomeToTrack.tsx
@@ -1,6 +1,16 @@
+// i18n-key-prefix: welcomeToTrack
+// i18n-namespace: components/modals/track-welcome-modal/LHS/steps/components
 import React, { useContext } from 'react'
 import { TrackContext } from '../../../TrackWelcomeModal'
+import { useAppTranslation } from '@/i18n/useAppTranslation'
 export function WelcomeToTrack(): JSX.Element {
   const { track } = useContext(TrackContext)
-  return <h1 className="text-h1 mb-8">Welcome to {track.title}! 🎉</h1>
+  const { t } = useAppTranslation(
+    'components/modals/track-welcome-modal/LHS/steps/components'
+  )
+  return (
+    <h1 className="text-h1 mb-8">
+      {t('welcomeToTrack.welcomeTo', { trackTitle: track.title })}
+    </h1>
+  )
 }

cbdef6272800464ed6a108e28bcc378b7354e021
dem4ron <demaaron88@gmail.com>
2025-07-23 13:27:55 +0200

    dig-deeper-components

---
+++ app/javascript/i18n/en/components-track-dig-deeper-components.ts
@@ -0,0 +1,19 @@
+// namespace: components/track/dig-deeper-components
+export default {
+  'deepDiveVideo.deepDiveInto': 'Deep Dive into {{exerciseTitle}}!',
+  'approaches.approaches': 'Approaches',
+  'approaches.otherWaysCommunitySolved':
+    'Other ways our community solved this exercise',
+  'approaches.noApproaches': 'There are no Approaches for {{exerciseTitle}}.',
+  'approachSnippet.author': 'author',
+  'approachSnippet.contributor': 'contributor',
+  'sectionHeader.articles': 'Articles',
+  'sectionHeader.exploreMoreIdeas': 'Explore more ideas about this exercise',
+  'sectionHeader.noArticles': 'There are no Articles for {{exerciseTitle}}.',
+  'diggingDeeper.digDeeper': 'Dig deeper',
+  'diggingDeeper.author': 'author',
+  'diggingDeeper.contributor': 'contributor',
+  'diggingDeeper.lastUpdated': 'Last updated {{date}}',
+  'diggingDeeper.editViaGitHub': 'Edit via GitHub',
+  'diggingDeeper.linkOpensInNewTab': 'The link opens in a new window or tab',
+}

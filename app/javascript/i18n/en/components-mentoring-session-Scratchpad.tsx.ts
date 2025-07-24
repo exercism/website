314781006a8c242a488bee96d3a7683ba30e2869
dem4ron <demaaron88@gmail.com>
2025-07-24 16:43:26 +0200

    Fix scratchpad test

--- app/javascript/i18n/en/components-mentoring-session-Scratchpad.tsx.ts
+++ app/javascript/i18n/en/components-mentoring-session-Scratchpad.tsx.ts
@@ -4,7 +4,7 @@ export default {
   'scratchpad.markdownSupportedPlace':
     'A <0>Markdown-supported</0> place for you to write notes and add code snippets you’d like to refer to during mentoring.',
   'scratchpad.yourNotesForExerciseInTrack':
-    'Your notes for <0>{exerciseTitle}</0> in<1/><0>{trackTitle}</0>',
+    'Your notes for <0>{{exerciseTitle}}</0> in<1/><0>{{trackTitle}}</0>',
   'scratchpad.revertToSaved': 'Revert to saved',
   'scratchpad.save': 'Save',
   'scratchpad.unsaved': 'Unsaved',

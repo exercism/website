0d9ee9593c7f4d498a605f8c0dea0aa636f579cb
dem4ron <demaaron88@gmail.com>
2025-07-24 17:34:55 +0200

    Fix task-tooltip things

--- app/javascript/components/tooltips/task-tooltip/ActionInfo.tsx
+++ app/javascript/components/tooltips/task-tooltip/ActionInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ActionIcon } from '../../contributing/tasks-list/task/ActionIcon'
 import { TaskAction } from '../../types'
 import { useAppTranslation } from '@/i18n/useAppTranslation'
+import { Trans } from 'react-i18next'
 
 export const ActionInfo = ({ action }: { action: TaskAction }): JSX.Element => {
   return (
@@ -19,39 +20,72 @@ export const ActionInfo = ({ action }: { action: TaskAction }): JSX.Element => {
 const ActionDetails = ({ action }: { action: TaskAction }): JSX.Element => {
   const { t } = useAppTranslation('components/tooltips/task-tooltip')
 
+  const transComponents = { strong: <strong /> }
+  const transNs = 'components/tooltips/task-tooltip'
+
   switch (action) {
     case 'create':
       return (
         <>
-          <h3>{t('actionInfo.createNew')}</h3>
+          <h3>
+            <Trans
+              ns={transNs}
+              i18nKey="actionInfo.createNew"
+              components={transComponents}
+            />
+          </h3>
           <p>{t('actionInfo.buildingFromScratch')}</p>
         </>
       )
     case 'fix':
       return (
         <>
-          <h3>{t('actionInfo.fixBroken')}</h3>
+          <h3>
+            <Trans
+              ns={transNs}
+              i18nKey="actionInfo.fixBroken"
+              components={transComponents}
+            />
+          </h3>
           <p>{t('actionInfo.fixingIt')}</p>
         </>
       )
     case 'improve':
       return (
         <>
-          <h3>{t('actionInfo.improveSomething')}</h3>
+          <h3>
+            <Trans
+              ns={transNs}
+              i18nKey="actionInfo.improveSomething"
+              components={transComponents}
+            />
+          </h3>
           <p>{t('actionInfo.addFeatureOrImprovement')}</p>
         </>
       )
     case 'proofread':
       return (
         <>
-          <h3>{t('actionInfo.proofread')}</h3>
+          <h3>
+            <Trans
+              ns={transNs}
+              i18nKey="actionInfo.proofread"
+              components={transComponents}
+            />
+          </h3>
           <p>{t('actionInfo.checkDocsAndContent')}</p>
         </>
       )
     case 'sync':
       return (
         <>
-          <h3>{t('actionInfo.syncContent')}</h3>
+          <h3>
+            <Trans
+              ns={transNs}
+              i18nKey="actionInfo.syncContent"
+              components={transComponents}
+            />
+          </h3>
           <p>{t('actionInfo.updateTrackImplementation')}</p>
         </>
       )

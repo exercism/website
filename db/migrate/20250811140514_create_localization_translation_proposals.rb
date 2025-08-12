dda71afc85db8e2300b465ab1575a116739efe71
Jeremy Walker <jez.walker@gmail.com>
2025-08-12 12:23:16 +0200

    Update migrations

--- db/migrate/20250811140514_create_localization_translation_proposals.rb
+++ db/migrate/20250811140514_create_localization_translation_proposals.rb
@@ -1,6 +1,6 @@
 class CreateLocalizationTranslationProposals < ActiveRecord::Migration[7.1]
   def change
-    create_table :localization_translation_proposals do |t|
+    create_table :localization_translation_proposals, if_not_exists: true do |t|
       t.belongs_to :translation, null: false, foreign_key: { to_table: :localization_translations }
       t.belongs_to :proposer, null: false, foreign_key: { to_table: :users }
       t.belongs_to :reviewer, null: false, foreign_key: { to_table: :users }

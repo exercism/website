7456b2a6885f66e0a12d6fd6d35379e497a734dc
Jeremy Walker <jez.walker@gmail.com>
2025-08-12 12:23:16 +0200

    Update migrations

--- db/migrate/20250811111647_create_localization_originals.rb
+++ db/migrate/20250811111647_create_localization_originals.rb
@@ -1,7 +1,6 @@
 class CreateLocalizationOriginals < ActiveRecord::Migration[7.1]
   def change
-    drop_table :localization_originals, if_exists: true
-    create_table :localization_originals do |t|
+    create_table :localization_originals, if_not_exists: true do |t|
       t.string :uuid, null: false
       t.string :key, null: false
       t.text :value, null: false

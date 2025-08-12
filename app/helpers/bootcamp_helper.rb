ef5f95e41d8d61c9d35733b19ede156f55039388
Jeremy Walker <jez.walker@gmail.com>
2025-08-12 13:53:10 +0200

    Start work on things

--- app/helpers/bootcamp_helper.rb
+++ app/helpers/bootcamp_helper.rb
@@ -4,4 +4,13 @@ module BootcampHelper
       map { |char| (127_397 + char.ord).chr("UTF-8") }.
       join
   end
+
+  def flag_for_locale(locale)
+    country = locale.to_s.split("-").last
+    case country
+    when "en"
+      country = "us"
+    end
+    flag_for_country_code(country)
+  end
 end

89f1fce7b93201bd39337f1ea6cff0033b67790e
Jeremy Walker <jez.walker@gmail.com>
2025-08-13 18:07:26 +0200

    Add LLM verification

---
+++ app/commands/llm/exec_gemini_flash.rb
@@ -0,0 +1,9 @@
+class LLM::ExecGeminiFlash
+  include Mandate
+
+  initialize_with :prompt, :spi_endpoint, :stream_channel
+
+  def call
+    LLM::Exec.(:gemini, :flash, prompt, spi_endpoint, stream_channel)
+  end
+end
--- config/routes/spi.rb
+++ config/routes/spi.rb
@@ -6,4 +6,10 @@ namespace :spi do
   resources :chatgpt_responses, only: :create
   get "solution_image_data/:track_slug/:exercise_slug/:user_handle" => "solution_image_data#show"
   patch "unsubscribe_user" => "unsubscribe_users#unsubscribe_by_email", as: "unsubscribe_user"
+
+  %w[
+    verify_llm_proposal
+  ].each do |action|
+    post "llm_responses/#{action}" => "llm_responses##{action}", as: action
+  end
 end

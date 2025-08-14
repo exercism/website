a1752fd89dfa0899e23d67cce4e261796c403bc4
Jeremy Walker <jez.walker@gmail.com>
2025-08-14 16:58:30 +0200

    Reword lots

--- app/models/localization/original.rb
+++ app/models/localization/original.rb
@@ -1,9 +1,26 @@
 class Localization::Original < ApplicationRecord
+  disable_sti!
+
+  serialize :data, coder: JSONWithIndifferentAccess
+
+  ## Types
+  # unknown
+  # website_server_side
+  # website_client_side
+  # exercise_introduction
+  # exercise_instructions
+  # exercise_description
+  # concept_content
+  # blog_post_content
+  # docs_content
+
   has_many :translations, dependent: :destroy, foreign_key: :key, primary_key: :key, inverse_of: :original
 
   before_create do
     self.uuid = SecureRandom.uuid if uuid.blank?
+    self.data = {} unless data.present?
   end
 
   def to_param = uuid
+  def type = super.to_sym
 end
--- db/migrate/20250811111647_create_localization_originals.rb
+++ db/migrate/20250811111647_create_localization_originals.rb
@@ -2,11 +2,14 @@ class CreateLocalizationOriginals < ActiveRecord::Migration[7.1]
   def change
     create_table :localization_originals, if_not_exists: true do |t|
       t.string :uuid, null: false
+      t.string :type, null: false
+
       t.string :key, null: false
       t.text :value, null: false
-      t.text :context, null: false
+      t.text :data, null: false
       t.timestamps
 
+      t.index :uuid, unique: true
       t.index :key, unique: true
     end
   end

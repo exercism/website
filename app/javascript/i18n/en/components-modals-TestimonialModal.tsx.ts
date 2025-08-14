a0b71492b671c13fb74842e4ecc87d1faba0f449
dem4ron <demaaron88@gmail.com>
2025-08-14 12:09:00 +0200

    Fix missing space

--- app/javascript/i18n/en/components-modals-TestimonialModal.tsx.ts
+++ app/javascript/i18n/en/components-modals-TestimonialModal.tsx.ts
@@ -1,6 +1,6 @@
 // namespace: components/modals/TestimonialModal.tsx
 export default {
-  byLine: 'by <strong><flair>{{handle}}</flair></strong>',
+  byLine: 'by\u00A0<0><1>{{handle}}</1></0> ',
   exerciseLine:
     'on <strong>{{exercise}}</strong> in <trackIcon/><trackTitle>{{track}}</trackTitle>',
   sharePanel: {

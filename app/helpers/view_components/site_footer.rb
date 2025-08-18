9070f7673a49e46a3676d6087db72aa1f77f9f1a
Jeremy Walker <jez.walker@gmail.com>
2025-08-18 18:36:58 +0200

    Tidy

--- app/helpers/view_components/site_footer.rb
+++ app/helpers/view_components/site_footer.rb
@@ -3,17 +3,32 @@ module ViewComponents
     extend Mandate::Memoize
 
     def to_s
-      # Rails.cache.fetch(cache_key, expires_in: 1.day) do
-      tag.footer(id: "site-footer") do
-        parts = []
-        parts << render(template: 'components/footer/external') unless user_signed_in?
-        parts << render(template: 'components/footer/shared')
-        safe_join(parts)
+      base = Rails.cache.fetch(cache_key, expires_in: 1.day) do
+        tag.footer(id: "site-footer") do
+          parts = []
+          parts << render(template: 'components/footer/external') unless user_signed_in?
+          parts << render(template: 'components/footer/shared')
+          safe_join(parts)
+        end
       end
-      # end
+
+      base.gsub("{{LOCALES}}", locales).html_safe
     end
 
     private
+    def locales
+      fullpath = request.fullpath
+
+      links = supported_locales.map do |locale|
+        link_to url_for_locale(locale, fullpath, add_loop_breaker_query_param: true), class: 'locale' do
+          tag.span(flag_for_locale(locale), class: 'flag') +
+            tag.span(I18n.t(:language_version, locale: locale))
+        end
+      end
+
+      safe_join(links)
+    end
+
     def cache_key
       Cache::KeyForFooter.(current_user)
     end

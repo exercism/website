7abeeb1a94962a26b083b68a4e9bab3595872df6
Jeremy Walker <jez.walker@gmail.com>
2025-08-19 16:28:37 +0200

    Fix locale migration pain

--- db/migrate/20250819141634_add_locale_to_user_data.rb
+++ db/migrate/20250819141634_add_locale_to_user_data.rb
@@ -1,5 +1,5 @@
 class AddLocaleToUserData < ActiveRecord::Migration[7.1]
   def change
-    add_column :user_data, :locale, :string
+    add_column :user_data, :locale, :string, if_not_exists: true
   end
 end

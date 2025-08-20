af901e36986583ec304077b3e1ba9706adcfbea9
Jeremy Walker <jez.walker@gmail.com>
2025-08-20 13:47:51 +0200

    Add production guards

--- db/migrate/20250819141635_add_translator_locales_to_user_data.rb
+++ db/migrate/20250819141635_add_translator_locales_to_user_data.rb
@@ -1,5 +1,7 @@
 class AddTranslatorLocalesToUserData < ActiveRecord::Migration[7.1]
   def change
+    return if Rails.env.production?
+
     add_column :user_data, :translator_locales, :text, null: true
   end
 end

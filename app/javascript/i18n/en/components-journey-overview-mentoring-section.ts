4e2848ee2eef8865e809e0df39f9920b454baece
dem4ron <demaaron88@gmail.com>
2025-08-21 13:55:58 +0200

    Fix trackSummary.session pluralization

--- app/javascript/i18n/en/components-journey-overview-mentoring-section.ts
+++ app/javascript/i18n/en/components-journey-overview-mentoring-section.ts
@@ -3,10 +3,8 @@ export default {
   'headerSummary.youVeMostlyMentoredIn': "You've mostly mentored in ",
   'communityRank.topRank': 'Top {{rank}}%',
   'mentoringChart.sessionsMentoredPerTrack': 'Sessions mentored per track',
-  'trackSummary.session': {
-    one: '{{count}} session',
-    other: '{{count}} sessions',
-  },
+  'trackSummary.session_one': '{{count}} session',
+  'trackSummary.session_other': '{{count}} session',
   'sessionsMentoredSummary.totalSessionsMentored': 'Total sessions mentored',
   'sessionRatioSummary.sessionsPerStudent': 'Sessions per student',
   'studentsMentoredSummary.totalStudentsMentored': 'Total students mentored',

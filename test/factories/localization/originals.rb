0eda5e9fdcc0bb7474114e9752fa8edad58dcf0b
Jeremy Walker <jez.walker@gmail.com>
2025-08-21 16:28:17 +0200

    Add test

--- test/factories/localization/originals.rb
+++ test/factories/localization/originals.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :localization_original, class: 'Localization::Original' do
+    type { "unknown" }
     key { SecureRandom.hex(6) }
     value { "Sample value for #{key}" }
   end

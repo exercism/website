b47920b454791b5994acd11d63a27956f4a84102
Jeremy Walker <jez.walker@gmail.com>
2025-08-28 12:53:10 +0200

    Remove temporary override

--- app/models/user.rb
+++ app/models/user.rb
@@ -362,7 +362,6 @@ class User < ApplicationRecord
   def flair = super&.to_sym
 
   def locale = super&.to_sym
-  def translator_locales = %i[en hu]
 
   def automator?(track = nil)
     return true if staff?

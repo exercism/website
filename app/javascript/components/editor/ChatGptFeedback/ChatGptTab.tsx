a57766d9ab93604eb498bce4aed6071343a1ede5
dem4ron <demaaron88@gmail.com>
2025-08-28 19:31:53 +0200

    ChatGPT

--- app/javascript/components/editor/ChatGptFeedback/ChatGptTab.tsx
+++ app/javascript/components/editor/ChatGptFeedback/ChatGptTab.tsx
@@ -1,10 +1,7 @@
-// i18n-key-prefix: chatGptTab
-// i18n-namespace: components/editor/ChatGptFeedback
 import React from 'react'
 import { Tab } from '../../common/Tab'
 import { TabsContext } from '../../Editor'
 import { GraphicalIcon } from '../../common/GraphicalIcon'
-import { useAppTranslation } from '@/i18n/useAppTranslation'
 
 export const ChatGptTab = (): JSX.Element => {
   return (

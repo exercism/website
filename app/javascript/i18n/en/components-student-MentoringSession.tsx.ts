dbee3dab520c223b725573053fd101d21f40016e
dem4ron <demaaron88@gmail.com>
2025-08-29 15:55:13 +0200

    Try to make sense of IterationMarker

--- app/javascript/i18n/en/components-student-MentoringSession.tsx.ts
+++ app/javascript/i18n/en/components-student-MentoringSession.tsx.ts
@@ -2,7 +2,6 @@
 export default {
   'closeButton.close': 'Close',
   'sessionInfo.inTrack': 'In',
-  'sessionInfo.withMentor': 'with',
   'discussionActions.exercise': 'Exercise',
   'iterationView.discussion': 'Discussion',
   'mentoringRequest.trackObjectives': 'Track Objectives',

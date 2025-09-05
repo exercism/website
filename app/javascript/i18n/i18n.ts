b7a3bd871e636b948803ac5c9bb021871fb586c6
dem4ron <demaaron88@gmail.com>
2025-09-05 14:57:18 +0200

    Aggregate js translations

--- app/javascript/i18n/i18n.ts
+++ app/javascript/i18n/i18n.ts
@@ -1,23 +1,37 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-
 import Backend from 'i18next-http-backend'
 
-void i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
+import resources from '@/i18n/en.aggregated.test'
+const isTest = process.env.NODE_ENV === 'test'
+
+if (isTest) {
+  void i18n.use(initReactI18next).init({
+    lng: 'en',
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
+    initImmediate: false,
     interpolation: { escapeValue: false },
-    backend: {
-      loadPath: '/javascript-i18n/foo/{{lng}}.js',
-    },
-    react: {
-      useSuspense: true,
-    },
+    react: { useSuspense: false },
+    resources,
   })
+} else {
+  void i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      fallbackLng: 'en',
+      debug: true,
+      interpolation: { escapeValue: false },
+      backend: {
+        loadPath: '/javascript-i18n/foo/{{lng}}.js',
+      },
+      react: {
+        useSuspense: true,
+      },
+    })
+}
 
 export default i18n

186f735e5430f68d9660abf5ce510fe3ec631745
dem4ron <demaaron88@gmail.com>
2025-09-05 11:18:28 +0200

    Improve translation

--- app/javascript/components/community-solutions/comments-list/Count.tsx
+++ app/javascript/components/community-solutions/comments-list/Count.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import { useAppTranslation } from '@/i18n/useAppTranslation'
-import pluralize from 'pluralize'
 
 export const Count = ({ number }: { number: number }): JSX.Element => {
   const { t } = useAppTranslation('components/community-solutions')
   return (
     <h2 className="text-h4 mb-24">
-      {t('commentsList.count.numberOfComments', {
-        number: number,
-        pluralize: pluralize('comment', number),
-      })}
+      {t('commentsList.comment', { count: number })}
     </h2>
   )
 }

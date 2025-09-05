8b81a069a959d2843d40ea8e524e7ebb448f095e
Aron Demeter <66035744+dem4ron@users.noreply.github.com>
2025-09-05 21:14:22 +0200

    Add Glossary entries UI (#8189)
    * Add Glossary entries UI
    
    * Fix types
    
    * Tiny adjustment

---
+++ app/javascript/components/localization/glossary-entries/types.d.ts
@@ -0,0 +1,70 @@
+type GlossaryEntriesListContextType = Pick<
+  GlossaryEntriesListProps,
+  'links' | 'originals'
+> & {
+  setCriteria: (criteria: string) => void
+  setPage: (page: number) => void
+  resolvedData?: GlossaryEntriesListData
+  request: import('@/hooks/use-list').ListState
+  setQuery: (query: Record<string, any>) => void
+  isFetching: boolean
+  status: 'pending' | 'error' | 'success'
+  error?: unknown
+}
+
+type GlossaryEntry = {
+  prettyType: ReactI18NextChildren | Iterable<ReactI18NextChildren>
+  title: ReactI18NextChildren | Iterable<ReactI18NextChildren>
+  usageDetails: ReactI18NextChildren | Iterable<ReactI18NextChildren>
+  uuid: string
+  key: string
+  value: string
+  translations: Translation[]
+}
+
+type Translation = {
+  uuid: string
+  locale: string
+  status: string
+  value?: string
+  proposal?: string[]
+}
+
+type GlossaryEntriesListData = {
+  meta: {
+    currentPage: number
+    totalPages: number
+    totalCount: number
+    unscopedTotal: number
+  }
+  results: GlossaryEntry[]
+}
+
+type GlossaryEntriesListProps = {
+  originals: GlossaryEntry[]
+  request: {
+    endpoint: string
+    query?: Record<string, any>
+    options: {
+      initialData: GlossaryEntriesListData
+    }
+  }
+  links?: { localizationGlossaryEntriesPath: string; endpoint: string }
+}
+
+type GlossaryEntriesShowProps = {
+  original: GlossaryEntry
+  currentUserId: number
+  links: GlossaryEntriesShowLinks
+}
+
+type GlossaryEntriesShowLinks = {
+  approveLlmTranslation: string
+  originalsListPage: string
+  createProposal: string
+  approveProposal: string
+  rejectProposal: string
+  updateProposal: string
+}
+
+type GlossaryEntriesShowContextType = GlossaryEntriesShowProps

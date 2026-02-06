1c023fa50a12a719e356e611f7f2a0ab133abe3b
M0RP43U588 <114691960+M0RP43U588@users.noreply.github.com>
2026-02-06 21:20:51 +0100

    Fix: corrected misspelt word: platfrom -> platform

--- app/javascript/i18n/en/components-modals-track-welcome-modal-LHS.ts
+++ app/javascript/i18n/en/components-modals-track-welcome-modal-LHS.ts
@@ -4,7 +4,7 @@ export default {
   'bootcampRecommendation.tracksAudience':
     "Exercism's tracks are designed for people who <strong>already know how to code</strong> and are practicing or learning new languages.",
   'bootcampRecommendation.codingJourney':
-    "If you're just starting out on your coding journey, our new platfrom <strong>Jiki</strong> might be a better fit for you.",
+    "If you're just starting out on your coding journey, our new platform <strong>Jiki</strong> might be a better fit for you.",
   'bootcampRecommendation.selfPaced':
     "It launches in January 2026 and you can sign up for the waiting list now! And best of all, <strong>it's free!</strong>",
   'bootcampRecommendation.cta.checkOutCourse': 'Join the Waiting List',
